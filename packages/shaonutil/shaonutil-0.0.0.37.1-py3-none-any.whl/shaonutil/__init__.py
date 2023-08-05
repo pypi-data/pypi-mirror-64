1668e70615067b92e691e723c95a1e1857d7bbb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:09 +0000

    Add package shaonutil/0.0.0.37.1/shaonutil-0.0.0.37.1-py3-none-any.whl

---
+++ packages/shaonutil/shaonutil-0.0.0.37.1-py3-none-any.whl/shaonutil/__init__.py
@@ -0,0 +1,8 @@
+#from shaonutil.stats import ClassA
+import shaonutil.stats
+import shaonutil.network
+import shaonutil.image
+import shaonutil.strings
+import shaonutil.file
+import shaonutil.windows
+import shaonutil.process
