6f17f6358ea23ffb49a05b3d93ef824ee5f68ca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:48 +0000

    Add package py-lambda-base/0.0.6.dev9/py-lambda-base-0.0.6.dev9.tar.gz

---
+++ packages/py-lambda-base/py-lambda-base-0.0.6.dev9.tar.gz/py-lambda-base-0.0.6.dev9/lambda_base/jsonapi.py
@@ -0,0 +1,22 @@
+from functools import wraps
+from .route_holder import RouteHolder
+
+def jsonapi(reject_if_no_data=False):
+  def decorator_wrapper(func):
+    holder = RouteHolder()
+    if reject_if_no_data:
+      def check_no_data(req, respondent):
+        if not req.data:
+          respondent.add_error({
+            'code': 'invalid_jsonapi',
+            'title': 'Body is not valid JSONAPI'
+          })
+      holder.add_route_partial(func, jsonapi=True, append_before_action=check_no_data)
+    else:
+      holder.add_route_partial(func, jsonapi=True)
+
+    @wraps(func)
+    def func_wrapper(*args, **kwArgs):
+      return func(*args, **kwArgs)
+    return func_wrapper
+  return decorator_wrapper
