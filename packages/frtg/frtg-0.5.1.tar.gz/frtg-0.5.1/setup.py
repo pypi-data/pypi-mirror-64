7165e40ad24f7732c1c58e47eed605f91990f3cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:49 +0000

    Add package frtg/0.5.1/frtg-0.5.1.tar.gz

---
+++ packages/frtg/frtg-0.5.1.tar.gz/frtg-0.5.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name='frtg',
+    version="0.5.1",
+    author='Erik Reid',
+    author_email='nobody@nowhere.org',
+    description='Flask/React Twitter grid',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/susrik/frtg',
+    packages=find_packages(),
+    install_requires=[
+        'flask',
+        'pika',
+        'jsonschema',
+        'twython'
+    ],
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 4 - Beta'
+    ],
+    python_requires='>=3.6'
+)
+
