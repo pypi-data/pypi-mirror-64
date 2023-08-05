caa1feadcb0c36d485fdb4c0ccf3dac8a418cba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:52 +0000

    Add package svdawg/0.2/svdawg-0.2.tar.gz

---
+++ packages/svdawg/svdawg-0.2.tar.gz/svdawg-0.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='svdawg',
+     version='0.2',
+     author="Simone Longo",
+     author_email="s.longo@utah.edu",
+     description="SVD accessories, widgets, and graphics",
+     long_description=long_description,
+     long_description_content_type='text/markdown',
+     install_requires=[
+          'pandas',
+          'numpy',
+          'seaborn',
+          'matplotlib',
+          'sklearn'
+      ],
+     url="https://github.com/SpacemanSpiff7/svdawg",
+     packages=setuptools.find_packages(),
+     license='MIT',
+     python_requires='>=3.6',
+     zip_safe=False
+ )
