a2442de02accb69777c9519b7f91fe68213c20e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:14 +0000

    Add package xj_palindrome/1.0/xj_palindrome-1.0.tar.gz

---
+++ packages/xj_palindrome/xj_palindrome-1.0.tar.gz/xj_palindrome-1.0/cinderella/demo.py
@@ -0,0 +1,24 @@
+#Author xiaojun zhang
+
+
+"""
+验证传入的字符串或者一个数字是否是回文串
+函数返回值：
+    True : 传入的值是回文串(数)
+    False : 传入的值不是回文串(数)
+"""
+def checkPalindrome(v):
+    #判断参数的类型
+    if not isinstance(v,int) and not isinstance(v,str):
+        raise TypeError("参数值只能是整数或者字符串")
+
+    v1 = str(v)
+    return v1 == v1[::-1]
+
+
+#测试函数的功能
+if __name__ == "__main__":
+    print(checkPalindrome(123))
+    print(checkPalindrome("121"))
+    print(checkPalindrome(123.321))
+    print(checkPalindrome("123.321"))
---
+++ packages/xj_palindrome/xj_palindrome-1.0.tar.gz/xj_palindrome-1.0/setup.py
@@ -0,0 +1,11 @@
+#Author xiaojun zhang
+
+from distutils.core import setup
+setup(
+    name='xj_palindrome',   # 对外我们模块的名字
+    version='1.0',  # 版本号
+    description='回文串检测方法，测试哦',   #描述
+    author='张晓君',   # 作者
+    author_email='1356062990@qq.com',
+    py_modules=['cinderella.demo'] # 要发布的模块
+    )
