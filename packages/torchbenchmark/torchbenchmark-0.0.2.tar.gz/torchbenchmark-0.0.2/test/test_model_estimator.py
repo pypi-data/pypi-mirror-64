442eed40ee03ee5f1ddaa77fed4f80f9c9e16598
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:45 +0000

    Add package torchbenchmark/0.0.2/torchbenchmark-0.0.2.tar.gz

---
+++ packages/torchbenchmark/torchbenchmark-0.0.2.tar.gz/torchbenchmark-0.0.2/test/test_model_estimator.py
@@ -0,0 +1,20 @@
+import torch
+from torchvision import models
+from torchbench.network.core import AlexNet
+
+network_name = "resnet152"
+
+num_classes = 1000
+model_alexnet = AlexNet(num_classes=num_classes)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+vgg = models.alexnet().to(device)
+
+
+
+import torchbench.estimator.model_estimator as est
+
+input_size = (3, 227, 227)
+mest = est.ModelEstimator(name='alexnet', model=vgg, input_size=input_size, batch_size=2 ,save=True, save_path='stats/alexnet_info_dummy.info')
+
+mest.generate_summary()
