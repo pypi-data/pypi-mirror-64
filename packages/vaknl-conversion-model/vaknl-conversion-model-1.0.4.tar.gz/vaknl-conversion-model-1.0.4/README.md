0e175d893f8e05cfd2a467beacdfac0bbc0da854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:56 +0000

    Add package vaknl-conversion-model/1.0.4/vaknl-conversion-model-1.0.4.tar.gz

---
+++ packages/vaknl-conversion-model/vaknl-conversion-model-1.0.4.tar.gz/vaknl-conversion-model-1.0.4/README.md
@@ -0,0 +1,7 @@
+# vaknl-conversion-model
+
+Support package for the conversion model on the vakanties.nl website.
+
+## training_serving
+
+Support library for training and serving predictions.
---
+++ packages/vaknl-conversion-model/vaknl-conversion-model-1.0.4.tar.gz/vaknl-conversion-model-1.0.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='vaknl-conversion-model',
+    description='Conversion model package. Only contains training serving library for now. ',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version='1.0.4',
+    url='https://github.com/vakantiesnl/vaknl-PyPi.git',
+    author='Merijn van Es',
+    author_email='merijn.vanes@vakanties.nl',
+    keywords=['vaknl', 'pip'],
+    packages=find_packages(),
+    python_requires='>=2.7',
+    install_requires=['tensorflow>=2.1.0', 'google']
+)
