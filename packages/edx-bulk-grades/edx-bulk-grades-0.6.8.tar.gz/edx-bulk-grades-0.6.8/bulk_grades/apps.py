9545fab1a1169c5eada4dc5f7d1961166d5a42d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:37 +0000

    Add package edx-bulk-grades/0.6.8/edx-bulk-grades-0.6.8.tar.gz

---
+++ packages/edx-bulk-grades/edx-bulk-grades-0.6.8.tar.gz/edx-bulk-grades-0.6.8/bulk_grades/apps.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+bulk_grades Django application initialization.
+"""
+
+from __future__ import absolute_import, unicode_literals
+
+from django.apps import AppConfig
+
+
+class BulkGradesConfig(AppConfig):
+    """
+    Configuration for the bulk_grades Django application.
+    """
+
+    name = 'bulk_grades'
+    plugin_app = {
+        'url_config': {
+            'lms.djangoapp': {
+                'namespace': u'bulk_grades',
+                'regex': u'^api/',
+                'relative_path': u'urls',
+            },
+        },
+        u'settings_config': {
+            u'lms.djangoapp': {
+                u'common': {'relative_path': u'settings.common'},
+                u'production': {'relative_path': u'settings.production'},
+            },
+        },
+    }
