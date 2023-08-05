6d75a630f611680e84b7e11e816e827d60ebf1f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:10 +0000

    Add package cluttermanager/0.4.4/cluttermanager-0.4.4.tar.gz

---
+++ packages/cluttermanager/cluttermanager-0.4.4.tar.gz/cluttermanager-0.4.4/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='cluttermanager',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      version='0.4.4',
+      description='Clutter Manager for your folders',
+      author='Puneet Saini',
+      author_email='puneet29saini@gmail.com',
+      url='https://github.com/puneet29/ClutterManager',
+      packages=find_packages(exclude=['*.pyc']),
+      scripts=['./bin/cluttermanager'],
+      )
