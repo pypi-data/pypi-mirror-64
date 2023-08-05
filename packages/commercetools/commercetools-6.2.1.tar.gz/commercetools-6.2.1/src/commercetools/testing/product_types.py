ad0cbd5a71be78502e53382d50cf25cadf9cee32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:25 +0000

    Add package commercetools/6.2.1/commercetools-6.2.1.tar.gz

---
+++ packages/commercetools/commercetools-6.2.1.tar.gz/commercetools-6.2.1/src/commercetools/testing/product_types.py
@@ -0,0 +1,116 @@
+import copy
+import datetime
+import typing
+import uuid
+
+from commercetools import schemas, types
+from commercetools.testing.abstract import BaseModel, ServiceBackend
+from commercetools.testing.utils import InternalUpdateError, update_attribute
+
+
+class ProductTypesModel(BaseModel):
+    _primary_type_name = "product-type"
+    _resource_schema = schemas.ProductTypeSchema
+
+    def _create_from_draft(
+        self, draft: types.ProductTypeDraft, id: typing.Optional[str] = None
+    ) -> types.ProductType:
+        object_id = str(uuid.UUID(id) if id is not None else uuid.uuid4())
+        return types.ProductType(
+            id=str(object_id),
+            version=1,
+            name=draft.name,
+            description=draft.description,
+            key=draft.key,
+            created_at=datetime.datetime.now(datetime.timezone.utc),
+            last_modified_at=datetime.datetime.now(datetime.timezone.utc),
+            attributes=self._create_attributes_from_draft(draft.attributes),
+        )
+
+    def _create_attributes_from_draft(
+        self,
+        draft_attributes: typing.Optional[typing.List[types.AttributeDefinitionDraft]],
+    ) -> typing.List[types.AttributeDefinition]:
+
+        result: typing.List[types.AttributeDefinition] = []
+        if not draft_attributes:
+            return result
+
+        for draft in draft_attributes:
+            ad = types.AttributeDefinition(
+                type=draft.type,
+                name=draft.name,
+                label=draft.label,
+                is_required=draft.is_required,
+                attribute_constraint=draft.attribute_constraint,
+                input_tip=draft.input_tip,
+                input_hint=draft.input_hint,
+                is_searchable=draft.is_searchable,
+            )
+            result.append(ad)
+        return result
+
+
+def change_label_action():
+    def updater(self, obj: dict, action: types.ProductTypeChangeLabelAction):
+        new = copy.deepcopy(obj)
+
+        for attribute in new["attributes"]:
+            if attribute["name"] == action.attribute_name:
+                attribute["label"] = action.label
+                return new
+
+        raise InternalUpdateError(
+            "No attribute found with name %r" % action.attribute_name
+        )
+
+    return updater
+
+
+def change_localized_enum_value_label():
+    def updater(
+        self, obj: dict, action: types.ProductTypeChangeLocalizedEnumValueLabelAction
+    ):
+        new = copy.deepcopy(obj)
+
+        for attribute in new["attributes"]:
+            if attribute["name"] == action.attribute_name:
+                for lenum_value in attribute["type"]["values"]:
+                    if lenum_value["key"] == action.new_value.key:
+                        lenum_value["label"] = action.new_value.label
+                        return new
+
+        raise InternalUpdateError(
+            "No attribute found with name %r"
+            " and local enum value key %r"
+            % (action.attribute_name, action.new_value.key)
+        )
+
+    return updater
+
+
+class ProductTypesBackend(ServiceBackend):
+    service_path = "product-types"
+    model_class = ProductTypesModel
+
+    _schema_draft = schemas.ProductTypeDraftSchema
+    _schema_update = schemas.ProductTypeUpdateSchema
+    _schema_query_response = schemas.ProductTypePagedQueryResponseSchema
+
+    def urls(self):
+        return [
+            ("^$", "GET", self.query),
+            ("^$", "POST", self.create),
+            ("^key=(?P<key>[^/]+)$", "GET", self.get_by_key),
+            ("^(?P<id>[^/]+)$", "GET", self.get_by_id),
+            ("^key=(?P<key>[^/]+)$", "POST", self.update_by_key),
+            ("^(?P<id>[^/]+)$", "POST", self.update_by_id),
+            ("^(?P<id>[^/]+)$", "DELETE", self.delete_by_id),
+            ("^key=(?P<key>[^/]+)$", "DELETE", self.delete_by_key),
+        ]
+
+    _actions = {
+        "changeDescription": update_attribute("description", "description"),
+        "changeLabel": change_label_action(),
+        "changeLocalizedEnumValueLabel": change_localized_enum_value_label(),
+    }
---
+++ packages/commercetools/commercetools-6.2.1.tar.gz/commercetools-6.2.1/src/commercetools/testing/products.py
@@ -0,0 +1,190 @@
+import copy
+import datetime
+import typing
+import uuid
+
+from commercetools import schemas, types
+from commercetools.testing.abstract import BaseModel, ServiceBackend
+from commercetools.testing.utils import custom_fields_from_draft
+
+
+class ProductsModel(BaseModel):
+    _primary_type_name = "product"
+    _resource_schema = schemas.ProductSchema
+
+    def _create_from_draft(
+        self, draft: types.ProductDraft, id: typing.Optional[str] = None
+    ) -> types.Product:
+        object_id = str(uuid.UUID(id) if id is not None else uuid.uuid4())
+
+        product = types.Product(
+            id=str(object_id),
+            key=draft.key,
+            product_type=draft.product_type,
+            version=1,
+            created_at=datetime.datetime.now(datetime.timezone.utc),
+            last_modified_at=datetime.datetime.now(datetime.timezone.utc),
+        )
+
+        master_variant = None
+        if draft.master_variant:
+            master_variant = self._create_variant_from_draft(draft.master_variant)
+
+        product_data = types.ProductData(
+            name=draft.name,
+            categories=draft.categories,
+            category_order_hints=draft.category_order_hints,
+            description=draft.description,
+            master_variant=master_variant,
+            slug=draft.slug or types.LocalizedString(),
+        )
+
+        if draft.publish:
+            product.master_data = types.ProductCatalogData(
+                staged=None, current=product_data, published=True
+            )
+        else:
+            product.master_data = types.ProductCatalogData(
+                staged=product_data, current=None, published=False
+            )
+
+        return product
+
+    def _create_variant_from_draft(
+        self, draft: types.ProductVariantDraft
+    ) -> types.ProductVariant:
+
+        assets: typing.Optional[typing.List[types.Asset]] = None
+        if draft.assets:
+            assets = self._create_assets_from_draft(draft.assets)
+
+        prices: typing.Optional[typing.List[types.Price]] = None
+        if draft.prices:
+            prices = self._create_prices_from_draft(draft.prices)
+
+        return types.ProductVariant(
+            id=1,
+            sku=draft.sku,
+            key=draft.key,
+            prices=prices,
+            attributes=draft.attributes,
+            price=None,
+            images=draft.images,
+            assets=assets,
+            availability=None,
+            is_matching_variant=None,
+            scoped_price=None,
+            scoped_price_discounted=None,
+        )
+
+    def _create_assets_from_draft(
+        self, drafts: typing.List[types.AssetDraft]
+    ) -> typing.List[types.Asset]:
+        assets: typing.List[types.Asset] = []
+        for draft in drafts:
+            custom: typing.Optional[types.CustomFields] = None
+            if draft.custom:
+                custom = custom_fields_from_draft(self._storage, draft.custom)
+
+            asset = types.Asset(
+                sources=draft.sources,
+                name=draft.name,
+                description=draft.description,
+                tags=draft.tags,
+                custom=custom,
+                key=draft.key,
+            )
+            assets.append(asset)
+        return assets
+
+    def _create_prices_from_draft(
+        self, drafts: typing.List[types.PriceDraft]
+    ) -> typing.List[types.Price]:
+        prices: typing.List[types.Price] = []
+        for draft in drafts:
+            custom = None
+            if draft.custom:
+                custom = custom_fields_from_draft(self._storage, draft.custom)
+
+            price = types.Price(
+                value=self._create_price_from_draft(draft.value),
+                country=draft.country,
+                customer_group=draft.customer_group,
+                channel=draft.channel,
+                valid_from=draft.valid_from,
+                valid_until=draft.valid_until,
+                discounted=None,
+                custom=custom,
+                tiers=draft.tiers,
+            )
+            prices.append(price)
+        return prices
+
+    def _create_price_from_draft(
+        self, draft: typing.Optional[types.TypedMoneyDraft]
+    ) -> typing.Optional[types.TypedMoney]:
+        if draft is None:
+            return None
+
+        if isinstance(draft, types.CentPrecisionMoneyDraft):
+            return types.CentPrecisionMoney(
+                cent_amount=draft.cent_amount, currency_code=draft.currency_code
+            )
+        elif isinstance(draft, types.HighPrecisionMoneyDraft):
+            return types.HighPrecisionMoney(
+                cent_amount=draft.cent_amount,
+                currency_code=draft.currency_code,
+                precise_amount=draft.precise_amount,
+            )
+        elif isinstance(draft, types.Money):
+            return types.CentPrecisionMoney(
+                cent_amount=draft.cent_amount, currency_code=draft.currency_code
+            )
+        else:
+            return types.TypedMoney(
+                cent_amount=draft.cent_amount,
+                currency_code=draft.currency_code,
+                type=draft.type,
+            )
+
+
+class ProductsBackend(ServiceBackend):
+    service_path = "products"
+    model_class = ProductsModel
+    _schema_draft = schemas.ProductDraftSchema
+    _schema_update = schemas.ProductUpdateSchema
+    _schema_query_response = schemas.ProductPagedQueryResponseSchema
+
+    def urls(self):
+        return [
+            ("^$", "GET", self.query),
+            ("^$", "POST", self.create),
+            ("^key=(?P<key>[^/]+)$", "GET", self.get_by_key),
+            ("^(?P<id>[^/]+)$", "GET", self.get_by_id),
+            ("^key=(?P<key>[^/]+)$", "POST", self.update_by_key),
+            ("^(?P<id>[^/]+)$", "POST", self.update_by_id),
+            ("^(?P<id>[^/]+)$", "DELETE", self.delete_by_id),
+            ("^key=(?P<key>[^/]+)$", "DELETE", self.delete_by_key),
+        ]
+
+    def _update_productdata_attr(dst: str, src: str):
+        def updater(self, obj: dict, action: types.ProductUpdateAction):
+            value = getattr(action, src)
+            staged = getattr(action, "staged", False) or True
+
+            # Action.staged default is True
+            if staged:
+                target_obj = obj["masterData"]["staged"]
+            else:
+                target_obj = obj["masterData"]["current"]
+
+            if target_obj[dst] != value:
+                new = copy.deepcopy(obj)
+                new[dst] = value
+                return new
+            return obj
+
+        return updater
+
+    # Fixme: use decorator for this
+    _actions = {"changeSlug": _update_productdata_attr("slug", "slug")}
