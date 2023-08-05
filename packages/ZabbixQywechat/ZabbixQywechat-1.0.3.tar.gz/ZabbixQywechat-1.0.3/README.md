c6a1f92785506e5f08b746ad88179d71f67e4001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:42 +0000

    Add package ZabbixQywechat/1.0.3/ZabbixQywechat-1.0.3.tar.gz

---
+++ packages/ZabbixQywechat/ZabbixQywechat-1.0.3.tar.gz/ZabbixQywechat-1.0.3/README.md
@@ -0,0 +1,31 @@
+This is a package for sending messages to enterprise WeChat applications in zabbix. You can use [Github](https://github.com/mikecui426/ZabbixQywechat)
+
+### View at:
+
+<code>
+https://pypi.org/project/ZabbixQywechat/1.0.3/
+</code>
+
+
+### pip install ZabbixQywechat
+
+
+### Demo test
+
+<pre><code>
+from ZabbixQywechat import Qywechat
+
+import sys
+
+Agentid="1000002"
+Corpid="1111"
+Corpsecret="2222"
+PATH="/tmp/test.json"
+
+if __name__ == '__main__':
+    Touser=sys.argv[1]
+    Subject=str(sys.argv[2])
+    Content=str(sys.argv[3])
+    Result=Qywechat(Agentid,Corpid,Corpsecret,PATH)
+    Result.SendMSGtoWechat(Touser,Subject,Content)
+</code></pre>
---
+++ packages/ZabbixQywechat/ZabbixQywechat-1.0.3.tar.gz/ZabbixQywechat-1.0.3/ZabbixQywechat/__init__.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import json
+import sys
+import urllib,urllib2
+import time
+
+class Qywechat(object):
+
+        Touser=sys.argv[1]
+        Subject=str(sys.argv[2])
+        Content=str(sys.argv[3])
+	
+	def __init__(self,agentid,corpid,corpsecret,path):
+	    self.AGENTID=agentid
+	    self.CORPID=corpid
+	    self.CORPSECRET=corpsecret
+            self.PATH=path
+	
+	def GetTokenFromWechat(self):
+	    gettoken_url = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=' + self.CORPID + '&corpsecret=' + self.CORPSECRET
+            token_file=urllib2.urlopen(gettoken_url)
+            #self.PATH=path
+        #print token_file
+            token_data=token_file.read().decode('utf-8')
+        #print token_data    
+            token_json=json.loads(token_data)
+            Token_file=json.dumps(token_json)
+        #print type(Token_file),type(token_json)
+        #print token_json['errcode']
+            if token_json['errcode'] != 0:
+               return False
+            else:
+           	Token = token_json['access_token']
+           	file = open(self.PATH, 'w')
+           	file.write(Token_file)
+           	file.close()
+           	return Token
+		   
+	def  SendMSGtoWechat(self,Touser,Subject,Content):
+	     post_content = {
+        	"touser":Touser,
+        	"agentid":self.AGENTID,
+        	"msgtype":"text",
+        	"text":{
+                	"content":Subject + '\n\n' + Content
+         	  },
+        	"safe": "0"
+      		}
+
+       	     json_content = json.dumps(post_content)
+	   
+	     try:
+          	file=open(self.PATH,'r')
+                Token=json.load(file)['access_token']
+                #print "Access_token cache is %s" % Token
+                file.close()
+       	     except:
+		Token = self.GetTokenFromWechat()
+       #Token = GetTokenFromWechat(Corpid,Corpsecret)
+       #print "API Access_token is %s" % Token
+       	     i=0
+       	     wcurl="https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token="+ Token
+       #print json_content
+	     response = urllib2.urlopen(wcurl,json_content)
+	     result=response.read().decode('utf-8')
+	     res1=json.loads(result)
+	     res2=json.dumps(res1)
+	     print res2
+	     while res1['errcode'] != 0 and i < 1:
+			time.sleep(2)
+			i+=1
+			Token2 = GetTokenFromWechat(self)
+			if Token:
+				Url = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=%s" % Token2
+				response = urllib2.urlopen(Url,json_content)
+				result=response.read().decode('utf-8')
+				return result
+	     
