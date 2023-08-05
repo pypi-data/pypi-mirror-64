2b5e21dced68cafa2be3b2c5a7a3a86f11e292b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:00 +0000

    Add package luriegold/0.3.0/luriegold-0.3.0.tar.gz

---
+++ packages/luriegold/luriegold-0.3.0.tar.gz/luriegold-0.3.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(name='luriegold',
+      version='0.3.0',
+      description='deprecated',
+      url='http://github.com/ulf1/scipy-psdm',
+      author='Ulf Hamster',
+      author_email='554c46@gmail.com',
+      license='MIT',
+      packages=['luriegold'],
+      install_requires=['setuptools>=40.0.0'],
+      python_requires='>=3.6',
+      zip_safe=False)
