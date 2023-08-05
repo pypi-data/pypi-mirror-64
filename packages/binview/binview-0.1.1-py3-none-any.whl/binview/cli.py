ae356a1dc466f88ffa74e688ff273d2a2a83f3fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:19 +0000

    Add package binview/0.1.1/binview-0.1.1-py3-none-any.whl

---
+++ packages/binview/binview-0.1.1-py3-none-any.whl/binview/cli.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+from argparse import ArgumentParser, RawTextHelpFormatter
+
+from .dump import dumper
+
+
+def main():
+    ''' Dumps the file given as arg.
+    '''
+    desc = 'Binary Dumper'
+    parser = ArgumentParser(
+        description = desc,
+        formatter_class = RawTextHelpFormatter,
+    )
+    parser.add_argument(
+        'file',
+        help='File to be dumped.'
+    )
+    parser.add_argument(
+        '--format',
+        help='''
+Output format.
+May be :
+    hex, oct, dec, bin
+default is hex.
+        '''
+    )
+    args = parser.parse_args()
+    path = Path(args.file)
+    fmt = args.format
+
+    for line in dumper(path, fmt):
+        print(line)
