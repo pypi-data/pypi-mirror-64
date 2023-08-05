e546f71b7e8623364a63164f0014b6067bd47609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:02 +0000

    Add package weibo-2-album/0.0.14/weibo_2_album-0.0.14.tar.gz

---
+++ packages/weibo-2-album/weibo_2_album-0.0.14.tar.gz/weibo_2_album-0.0.14/weibo_2_album/__init__.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+name = 'weibo_2_album'
+
+import cached_url
+import yaml
+from bs4 import BeautifulSoup
+from telegram_util import AlbumResult as Result
+
+prefix = 'https://m.weibo.cn/statuses/show?id='
+
+def getWid(path):
+	for special_chat in ['?', '#']:
+		index = path.find(special_chat)
+		if index > -1:
+			path = path[:index]
+	return path.split('/')[-1]
+
+def getCap(json):
+	text = json['text']
+	b = BeautifulSoup(text, features="lxml")
+	for elm in b.find_all('a'):
+		if not elm.get('href'):
+			continue
+		md = '[%s](%s)' % (elm.text, elm['href'])
+		elm.replaceWith(BeautifulSoup(md, features='lxml').find('p'))
+	return BeautifulSoup(str(b).replace('<br/>', '\n'), features='lxml').text.strip()
+
+def enlarge(url):
+	return url.replace('orj360', 'large')
+	
+def getImages(json):
+	return [enlarge(x['url']) for x in json.get('pics', [])]
+
+def get(path):
+	wid = getWid(path)
+	r = Result()
+	try:
+		json = yaml.load(cached_url.get(prefix + wid), Loader=yaml.FullLoader)
+	except:
+		return r
+	json = json['data']
+	r.imgs = getImages(json)
+	r.cap_html = json['text']
+	r.title = json['status_title']
+	r.cap = getCap(json)
+	return r
+
+	
+
