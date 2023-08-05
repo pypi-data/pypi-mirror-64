79e31a690250071fd2d623948617452f40c32bcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:50 +0000

    Add package pytblocklib/0.0.2/pytblocklib-0.0.2-py3-none-any.whl

---
+++ packages/pytblocklib/pytblocklib-0.0.2-py3-none-any.whl/pytblocklib/__init__.py
@@ -0,0 +1,13 @@
+"""
+a python library for youtube blocking functions without using yt api, Selenium, or BeautifulSoup.
+"""
+__copyright__    = 'Copyright (C) 2020 taizan-hokuto'
+__version__      = '0.0.2'
+__license__      = 'GNU GENERAL PUBLIC LICENSE Version 3'
+__author__       = 'taizan-hokuto'
+__author_email__ = '55448286+taizan-hokuto@users.noreply.github.com'
+__url__          = 'https://github.com/taizan-hokuto/pytblocklib'
+
+RELEASE = True
+
+from .watcher import Watcher
