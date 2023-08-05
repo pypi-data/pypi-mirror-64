1507cce30ff977c4f8be0e0ad44d77fa22956933
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:41 +0000

    Add package jhTAlib/20200322.0/jhTAlib-20200322.0.tar.gz

---
+++ packages/jhTAlib/jhTAlib-20200322.0.tar.gz/jhTAlib-20200322.0/setup.py
@@ -0,0 +1,31 @@
+
+
+from setuptools import setup, find_packages
+
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+
+setup(
+    name='jhTAlib',
+    version='20200322.0',
+    author='Joost Hoeks',
+    author_email='joosthoeks@gmail.com',
+    description='Technical Analysis Library Time-Series',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/joosthoeks/jhTAlib',
+    packages=find_packages(),
+    classifiers=[
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    'Operating System :: OS Independent',
+    ],
+)
+
+"""
+Upload PyPI:
+$ python3 setup.py sdist bdist_wheel
+$ twine upload dist/*
+"""
