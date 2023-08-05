0faa742ce2f382677fae768106a2a831160cd87b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:57 +0000

    Add package wordsum/0.1.1/wordsum-0.1.1.tar.gz

---
+++ packages/wordsum/wordsum-0.1.1.tar.gz/wordsum-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['wordsum', 'wordsum._file_types', 'wordsum._io', 'wordsum._util']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['black>=19.10b0,<20.0', 'mypy>=0.770,<0.771', 'nbformat>=5.0.4,<6.0.0']
+
+setup_kwargs = {
+    'name': 'wordsum',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'JackMcKew',
+    'author_email': 'jackmckew2@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
