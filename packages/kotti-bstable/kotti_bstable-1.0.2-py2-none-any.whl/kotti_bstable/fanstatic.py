1a0bc31e23793fb5cccad87e7702738bd6dd6a37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:49 +0000

    Add package kotti-bstable/1.0.2/kotti_bstable-1.0.2-py2-none-any.whl

---
+++ packages/kotti-bstable/kotti_bstable-1.0.2-py2-none-any.whl/kotti_bstable/fanstatic.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+"""
+Created on 2020-03-21
+:author: Oshane Bailey (b4.oshany@gmail.com)
+"""
+
+from __future__ import absolute_import, division, print_function
+
+from fanstatic import Group
+from fanstatic import Library
+from fanstatic import Resource
+
+
+library = Library("kotti_bstable", "static")
+
+js = Resource(
+    library,
+    "scripts.js")
+
+bootstrap_table = Group([
+    Resource(library, "ext/bootstrap-table/dist/bootstrap-table.min.css"),
+    Resource(
+        library,
+        "ext/bootstrap-table/dist/bootstrap-table.min.js",
+        depends=[js])
+])
+
+bootstrap_table_date_picker = Group([
+    Resource(library, "ext/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css"),
+    Resource(
+        library,
+        "ext/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js",
+        depends=[bootstrap_table]
+    )
+])
+
+bootstrap_table_filter_control = Group([
+    Resource(library, "ext/bootstrap-table/dist/extensions/filter-control/bootstrap-table-filter-control.css"),
+    Resource(
+        library,
+        "ext/bootstrap-table/dist/extensions/filter-control/bootstrap-table-filter-control.min.js",
+        depends=[bootstrap_table, bootstrap_table_date_picker]
+    )
+])
+
+select2_component = Group([
+    Resource(library, "ext/select2/dist/css/select2.min.css"),
+    Resource(library, "ext/select2/dist/js/select2.min.js")
+])
+
+font_awesome = Resource(library, "ext/font-awesome/css/font-awesome.min.css")
+
+css_and_js = Group([bootstrap_table, bootstrap_table_filter_control])
