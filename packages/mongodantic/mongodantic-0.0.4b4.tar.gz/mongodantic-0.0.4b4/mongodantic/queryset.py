853677059eddb34d115859c11d6ba71a338fbcd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:10 +0000

    Add package mongodantic/0.0.4b4/mongodantic-0.0.4b4.tar.gz

---
+++ packages/mongodantic/mongodantic-0.0.4b4.tar.gz/mongodantic-0.0.4b4/mongodantic/queryset.py
@@ -0,0 +1,41 @@
+from typing import Generator, List
+from pymongo.errors import ServerSelectionTimeoutError, AutoReconnect, NetworkTimeout, ConnectionFailure, \
+    WriteConcernError
+
+from .exceptions import MongoConnectionError
+
+
+class QuerySet(object):
+    def __init__(self, data: Generator):
+        self._data = data
+
+    def __iter__(self):
+        try:
+            return (obj for obj in self._data)
+        except (ServerSelectionTimeoutError, AutoReconnect, NetworkTimeout, ConnectionFailure, WriteConcernError) as e:
+            raise MongoConnectionError(e)
+
+    def __data_iter(self) -> Generator:
+        try:
+            return (obj.data for obj in self._data)
+        except (ServerSelectionTimeoutError, AutoReconnect, NetworkTimeout, ConnectionFailure, WriteConcernError) as e:
+            raise MongoConnectionError(e)
+
+    @property
+    def data(self) -> List:
+        return list(self.__data_iter())
+
+    @property
+    def generator(self) -> Generator:
+        return self.__iter__()
+
+    @property
+    def data_generator(self) -> Generator:
+        return self.__data_iter()
+
+    @property
+    def list(self) -> List:
+        return list(self.__iter__())
+
+    def first(self) -> any:
+        return next(self.__iter__())
