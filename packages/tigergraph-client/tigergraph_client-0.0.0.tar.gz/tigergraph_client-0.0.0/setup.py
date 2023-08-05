21cc18bbe7ffb44ef20c1c4911d3bb49d37ecddd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:27 +0000

    Add package tigergraph-client/0.0.0/tigergraph_client-0.0.0.tar.gz

---
+++ packages/tigergraph-client/tigergraph_client-0.0.0.tar.gz/tigergraph_client-0.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='tigergraph_client',
+      version='0.0.0',
+      description='Dummy package for tigergraph_client package name usage',
+      url='',
+      author='mtaifi',
+      license='GPL-3',
+      packages=['tigergraph_client'],
+      zip_safe=False)
