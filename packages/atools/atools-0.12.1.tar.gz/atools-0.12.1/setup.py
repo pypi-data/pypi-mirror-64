9332bc349e87fece5f08933fce2d6ea9a6644da4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:42 +0000

    Add package atools/0.12.1/atools-0.12.1.tar.gz

---
+++ packages/atools/atools-0.12.1.tar.gz/atools-0.12.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='atools',
+    version='0.12.1',
+    packages=find_packages(),
+    python_requires='>=3.6',
+    url='https://github.com/cevans87/atools',
+    license='mit',
+    author='cevans',
+    author_email='c.d.evans87@gmail.com',
+    description='Python 3.6+ async/sync memoize and rate decorators',
+    tests_require=[
+        'pytest',
+        'pytest-asyncio',
+        'pytest-cov',
+    ],
+)
