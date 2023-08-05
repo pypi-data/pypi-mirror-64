589fc1f622e5aac40c6a6d26ff2d9ed9032d76e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:07 +0000

    Add package img-renamer/2.2.5/img_renamer-2.2.5.tar.gz

---
+++ packages/img-renamer/img_renamer-2.2.5.tar.gz/img_renamer-2.2.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="img_renamer",
+    version="2.2.5",
+    author="Miika Launiainen",
+    author_email="miika.launiainen@gmail.com",
+    description="Script to rename images in numberic order",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/miicat/img_renamer",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires='>=3.6',
+)
