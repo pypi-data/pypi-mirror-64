8b750e5249854b9ecedb7d4c48e96277ea1fc5d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:42 +0000

    Add package label-studio-evalme/0.0.4rc2/label_studio_evalme-0.0.4rc2-py3-none-any.whl

---
+++ packages/label-studio-evalme/label_studio_evalme-0.0.4rc2-py3-none-any.whl/evalme/eval_item.py
@@ -0,0 +1,26 @@
+from operator import itemgetter
+
+
+class EvalItem(object):
+    """
+    Generic class that contains all info about evaluation item
+    """
+    def __init__(self, raw_data):
+        self._raw_data = raw_data
+
+    @property
+    def raw_data(self):
+        return self._raw_data
+
+    def get_values_iter(self):
+        return map(itemgetter('value'), self._raw_data)
+
+    def get_values(self):
+        return list(self.get_values_iter())
+
+    @property
+    def empty(self):
+        return len(self._raw_data) == 0
+
+    def __len__(self):
+        return len(self._raw_data)
