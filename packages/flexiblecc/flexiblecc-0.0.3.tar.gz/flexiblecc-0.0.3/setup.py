44578f1e50eeee3bf42a496f1f50fa885646b9eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:07 +0000

    Add package flexiblecc/0.0.3/flexiblecc-0.0.3.tar.gz

---
+++ packages/flexiblecc/flexiblecc-0.0.3.tar.gz/flexiblecc-0.0.3/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="flexiblecc",
+    version="0.0.3",
+    author="Simon L. B. Sørensen and contributors",
+    author_email="simonlyckbjaert@hotmail.com",
+    description="Flexible Camera Calibration",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SimonLBSoerensen/Flexible-Camera-Calibration",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords="camera calibration",
+    python_requires='>=3.6',
+    license='MIT',
+    install_requires=["numpy", "opencv-python>=3.1", "tqdm"],
+)
+
+#python setup.py sdist bdist_wheel
+#python -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
