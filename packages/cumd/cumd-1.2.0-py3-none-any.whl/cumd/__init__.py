7b1356162ca0f7cf6cda30966b733fc56999f74f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:09 +0000

    Add package cumd/1.2.0/cumd-1.2.0-py3-none-any.whl

---
+++ packages/cumd/cumd-1.2.0-py3-none-any.whl/cumd/__init__.py
@@ -0,0 +1,6 @@
+__version__      = '1.2.0'
+__author__       = 'Mike Kroutikov'
+__author_email__ = 'pgmmpk@gmail.com'
+__description__  = 'Church Slavonic extensions to Markdown'
+__keywords__     = 'church slavonic, markdown'
+__url__          = 'https://github.com/slavonic/cumd/'
