f744f273754dfa5119348d81a15a54d7c6509d69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:30 +0000

    Add package mi/2020.3.21.19.54.26/mi-2020.3.21.19.54.26.tar.gz

---
+++ packages/mi/mi-2020.3.21.19.54.26.tar.gz/mi-2020.3.21.19.54.26/mi/config/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : mi.
+# @File         : __init__.py
+# @Time         : 2020-03-04 14:46
+# @Author       : yuanjie
+# @Email        : yuanjie@xiaomi.com
+# @Software     : PyCharm
+# @Description  : 
+
+
+class Config(object):
+    zk_cfg = None
