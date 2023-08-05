73c9bcf1b6ad7de46e64cb8b5aaa2246a3f7338c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:03 +0000

    Add package covid-19-dryampy/0.3.9/covid-19-dryampy-0.3.9.tar.gz

---
+++ packages/covid-19-dryampy/covid-19-dryampy-0.3.9.tar.gz/covid-19-dryampy-0.3.9/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="covid-19-dryampy", # Replace with your own username
+    version="0.3.9",
+    author="Steven Yampolsky",
+    author_email="syampols@gmail.com",
+    description="packages used to throw around covid-19 data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DrYampy/covid_19.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
