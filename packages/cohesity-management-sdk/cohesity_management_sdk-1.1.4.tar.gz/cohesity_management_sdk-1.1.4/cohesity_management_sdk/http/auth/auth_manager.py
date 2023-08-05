0b24ec52e93c8fcbd76a41858de3ca422c3c4d82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:07 +0000

    Add package cohesity-management-sdk/1.1.4/cohesity_management_sdk-1.1.4.tar.gz

---
+++ packages/cohesity-management-sdk/cohesity_management_sdk-1.1.4.tar.gz/cohesity_management_sdk-1.1.4/cohesity_management_sdk/http/auth/auth_manager.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Cohesity Inc.
+
+from cohesity_management_sdk.controllers.access_tokens_controller import AccessTokensController
+from cohesity_management_sdk.models.access_token_credential import AccessTokenCredential
+
+class AuthManager:
+
+    @classmethod
+    def apply(cls, http_request, Configuration):
+        """ Add authentication to the request.
+
+        Args:
+            http_request (HttpRequest): The HttpRequest object to which
+                authentication header will be added.
+
+        """
+        cls.check_auth(Configuration)
+        token = Configuration.auth_token.access_token
+        token_type = Configuration.auth_token.token_type
+        http_request.headers['Authorization'] = token_type+" "+token
+
+    @classmethod
+    def check_auth(cls, Configuration):
+        """ Checks if access token is valid."""
+        if not Configuration.auth_token:
+            cls.authorize(Configuration)
+
+    @classmethod
+    def authorize(cls, Configuration):
+        """ Authorizes the client.
+
+        Returns:
+            AccessToken: The access token.
+
+        """
+
+        body = AccessTokenCredential()
+        body.username = Configuration.username
+        body.password = Configuration.password
+        if Configuration.domain is not None:
+            body.domain = Configuration.domain
+        auth_controller = AccessTokensController(Configuration)
+        token = auth_controller.create_generate_access_token(body)
+        Configuration.auth_token = token
+        return token
---
+++ packages/cohesity-management-sdk/cohesity_management_sdk-1.1.4.tar.gz/cohesity_management_sdk-1.1.4/cohesity_management_sdk/models/isilon_cluster.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Cohesity Inc.
+
+
+class IsilonCluster(object):
+
+    """Implementation of the 'IsilonCluster' model.
+
+    Specifies information about an Isilon Cluster.
+
+    Attributes:
+        api_version (int): Specifies the API version of an Isilon Cluster.
+        description (string): Specifies the description of an Isilon Cluster.
+        guid (string): Specifies a globally unique id of an Isilon Cluster.
+        version (string): Specifies the version of an Isilon Cluster.
+
+    """
+
+    # Create a mapping from Model property names to API property names
+    _names = {
+        "api_version":'apiVersion',
+        "description":'description',
+        "guid":'guid',
+        "version":'version'
+    }
+
+    def __init__(self,
+                 api_version=None,
+                 description=None,
+                 guid=None,
+                 version=None):
+        """Constructor for the IsilonCluster class"""
+
+        # Initialize members of the class
+        self.api_version = api_version
+        self.description = description
+        self.guid = guid
+        self.version = version
+
+
+    @classmethod
+    def from_dictionary(cls,
+                        dictionary):
+        """Creates an instance of this model from a dictionary
+
+        Args:
+            dictionary (dictionary): A dictionary representation of the object as
+            obtained from the deserialization of the server's response. The keys
+            MUST match property names in the API description.
+
+        Returns:
+            object: An instance of this structure class.
+
+        """
+        if dictionary is None:
+            return None
+
+        # Extract variables from the dictionary
+        api_version = dictionary.get('apiVersion')
+        description = dictionary.get('description')
+        guid = dictionary.get('guid')
+        version = dictionary.get('version')
+
+        # Return an object of this model
+        return cls(api_version,
+                   description,
+                   guid,
+                   version)
+
+
