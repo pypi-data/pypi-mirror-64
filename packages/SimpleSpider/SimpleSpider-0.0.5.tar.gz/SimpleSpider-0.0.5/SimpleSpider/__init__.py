ad9be247ff79b964743c46486ce9c74c37378369
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:02 +0000

    Add package SimpleSpider/0.0.5/SimpleSpider-0.0.5.tar.gz

---
+++ packages/SimpleSpider/SimpleSpider-0.0.5.tar.gz/SimpleSpider-0.0.5/SimpleSpider/__init__.py
@@ -0,0 +1,47 @@
+import SimpleSpider
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--url', type=str, default=None)
+    parser.add_argument("--single",type=str, default="True")
+    parser.add_argument('--re', type=str, default=None)
+    parser.add_argument('--xpath', type=str, default=None)
+    parser.add_argument('--index', type=str, default=None)
+    parser.add_argument('--print', type=str, default="True")
+    parser.add_argument('--output',type=str, default=None )
+
+    args = parser.parse_args()
+
+    if(args.single=="True"):
+            print(args.single)
+            if(args.re!=None):
+                result=SinglePageGetByRegEx(Url=args.url,RegEx=args.re)
+                if(args.print=="True"):
+                    print(result)
+                if (args.output != None):
+                    ExportFileToExcel(result, args.output)
+
+            else:
+                result=SinglePageGetByXpath(Url=args.url,Xpath=args.xpath)
+                if (args.print == "True"):
+                    print(result)
+
+                if(args.output!=None):
+                    ExportFileToExcel(result,args.output)
+    else:
+        indexlist = str(args.index).split(",")
+        if (args.re != None):
+
+            result = MulityPageGetByRegEx(Url=args.url, IndexList=indexlist, RegEx=args.re)
+            if (args.print == "True"):
+                print(result)
+            if (args.output != None):
+                ExportFileToExcel(result, args.output)
+        else:
+            result = MulityPageGetByXpath(Url=args.url, IndexList=indexlist, RegEx=args.re)
+            if (args.print == "True"):
+                print(result)
+            if (args.output != None):
+                ExportFileToExcel(result, args.output)
