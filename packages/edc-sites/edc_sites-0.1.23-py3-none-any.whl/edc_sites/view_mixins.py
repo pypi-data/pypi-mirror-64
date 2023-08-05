dec6fb60b11641aa89aab5331888184b0da3c0a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:28 +0000

    Add package edc-sites/0.1.23/edc_sites-0.1.23-py3-none-any.whl

---
+++ packages/edc-sites/edc_sites-0.1.23-py3-none-any.whl/edc_sites/view_mixins.py
@@ -0,0 +1,16 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.views.generic.base import ContextMixin
+
+from .models import SiteProfile
+
+
+class SiteViewMixin(ContextMixin):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        try:
+            site_profile = SiteProfile.objects.get(site__id=self.request.site.id)
+        except ObjectDoesNotExist:
+            pass
+        else:
+            context.update(site_title=site_profile.title)
+        return context
