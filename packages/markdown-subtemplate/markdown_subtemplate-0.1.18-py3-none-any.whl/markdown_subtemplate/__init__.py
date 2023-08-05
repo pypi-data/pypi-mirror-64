d75572e13aa44123141b840ec2a5d4b177d9e374
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:07 +0000

    Add package markdown-subtemplate/0.1.18/markdown_subtemplate-0.1.18-py3-none-any.whl

---
+++ packages/markdown-subtemplate/markdown_subtemplate-0.1.18-py3-none-any.whl/markdown_subtemplate/__init__.py
@@ -0,0 +1,14 @@
+"""
+markdown_subtemplate - A template engine to render
+Markdown with external template imports and variable replacements.
+"""
+
+__version__ = '0.1.18'
+__author__ = 'Michael Kennedy <michael@talkpython.fm>'
+__all__ = []
+
+from . import engine
+from . import exceptions
+from . import logging
+from . import caching
+from . import storage
