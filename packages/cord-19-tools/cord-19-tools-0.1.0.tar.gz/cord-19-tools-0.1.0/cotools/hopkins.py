236279ff52dc51692d237676471d8952c12d295c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:12 +0000

    Add package cord-19-tools/0.1.0/cord-19-tools-0.1.0.tar.gz

---
+++ packages/cord-19-tools/cord-19-tools-0.1.0.tar.gz/cord-19-tools-0.1.0/cotools/hopkins.py
@@ -0,0 +1,31 @@
+import csv
+from urllib.request import urlopen
+import io
+
+def _read_data(url: str) -> dict:
+    response = urlopen(url)
+    byts = response.read()
+    data = io.StringIO(byts.decode())
+    reader = csv.DictReader(data)
+    result = {}
+    for row in reader:
+        for column, value in row.items():
+            result.setdefault(column, []).append(value)
+    return result
+
+def _convert_data(data: dict) -> dict:
+    out = {}
+    for k in data.keys():
+        if k in ['Province/State', 'Country/Region']:
+            out[k] = data[k]
+        elif k in ['Lat', 'Long']:
+            out[k] = list(map(float, data[k]))
+        else:
+            out[k] = list(map(int, data[k]))
+    return out
+
+def get_hopkins() -> (dict, dict):
+    datafiles = ["https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv",
+                 "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
+]
+    return (_convert_data(_read_data(dat)) for dat in datafiles)
