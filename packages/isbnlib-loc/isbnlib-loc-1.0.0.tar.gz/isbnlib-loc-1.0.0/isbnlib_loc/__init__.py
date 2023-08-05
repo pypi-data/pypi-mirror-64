06f5b7da598ee7017d6765226e8376329a3d0b06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:56 +0000

    Add package isbnlib-loc/1.0.0/isbnlib-loc-1.0.0.tar.gz

---
+++ packages/isbnlib-loc/isbnlib-loc-1.0.0.tar.gz/isbnlib-loc-1.0.0/isbnlib_loc/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa
+# pylint: skip-file
+from ._loc import query
+
+__version__ = '1.0.0'
+
+__plugin_main_language__ = 'en'
+__plugin_service_name__ = 'loc'
+__plugin_type__ = 'metadata'
