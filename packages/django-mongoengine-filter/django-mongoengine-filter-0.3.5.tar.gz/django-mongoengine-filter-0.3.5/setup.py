b066ec88523723afc0ca91fa4c5b38e760c4a6cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:27:43 +0000

    Add package django-mongoengine-filter/0.3.5/django-mongoengine-filter-0.3.5.tar.gz

---
+++ packages/django-mongoengine-filter/django-mongoengine-filter-0.3.5.tar.gz/django-mongoengine-filter-0.3.5/setup.py
@@ -0,0 +1,67 @@
+from setuptools import setup, find_packages
+
+f = open("README.rst")
+readme = f.read()
+f.close()
+
+install_requires = [
+    'six>=1.9',
+]
+
+extras_require = []
+
+tests_require = [
+    'factory_boy',
+    'fake-factory',
+    'pytest',
+    'pytest-django',
+    'pytest-cov',
+    'tox',
+]
+
+setup(
+    name="django-mongoengine-filter",
+    version="0.3.5",
+    description=(
+        "django-mongoengine-filter is a reusable Django application inspired "
+        "from django-filter for allowing mongoengine users to filter querysets "
+        "dynamically."
+    ),
+    long_description=readme,
+    author="Artur Barseghyhan",
+    author_email="artur.barseghyan@gmail.com",
+    url="https://github.com/barseghyanartur/django-mongoengine-filter",
+    project_urls={
+        "Bug Tracker": "https://github.com/barseghyanartur/"
+                       "django-mongoengine-filter/issues",
+        "Documentation": "https://django-mongoengine-filter.readthedocs.io/",
+        "Source Code": "https://github.com/barseghyanartur/"
+                       "django-mongoengine-filter",
+        "Changelog": "https://django-mongoengine-filter.readthedocs.io/"
+                     "en/latest/changelog.html",
+    },
+    packages=find_packages(exclude=["tests"]),
+    package_data={"django_mongoengine_filter": ["locale/*/LC_MESSAGES/*"]},
+    license="GPL-2.0-only OR LGPL-2.1-or-later",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "License :: OSI Approved :: GNU Lesser General Public License v2 or "
+        "later (LGPLv2+)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Framework :: Django",
+    ],
+    python_requires=">=2.7",
+    install_requires=(install_requires + extras_require),
+    tests_require=tests_require,
+    include_package_data=True,
+    zip_safe=False,
+)
---
+++ packages/django-mongoengine-filter/django-mongoengine-filter-0.3.5.tar.gz/django-mongoengine-filter-0.3.5/tests/settings/base.py
@@ -0,0 +1,47 @@
+DATABASES = {
+    "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}
+}
+
+SECRET_KEY = "top-secret"
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    # Third party
+    "dfm_app",
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = "urls"
+
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ]
+        },
+    }
+]
+
+ALLOWED_HOSTS = ["*", "127.0.0.1", "localhost"]
