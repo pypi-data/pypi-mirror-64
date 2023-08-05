c85462f04aac57dc057732cdb77456971ae2e7fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:39 +0000

    Add package spaceone-secret/0.1.1.post100/spaceone_secret-0.1.1.post100-py3-none-any.whl

---
+++ packages/spaceone-secret/spaceone_secret-0.1.1.post100-py3-none-any.whl/spaceone/secret/manager/identity_manager.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from spaceone.core.manager import BaseManager
+from spaceone.secret.connector.identity_connector import IdentityConnector
+
+
+class IdentityManager(BaseManager):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.identity_conn: IdentityConnector = self.locator.get_connector('IdentityConnector')
+
+    def get_project(self, project_id, domain_id):
+        return self.identity_conn.get_project(project_id, domain_id)
+
+    def list_projects(self, query, domain_id):
+        return self.identity_conn.list_projects(query, domain_id)
