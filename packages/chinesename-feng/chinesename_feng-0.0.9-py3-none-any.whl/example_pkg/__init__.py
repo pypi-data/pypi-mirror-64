b86c3aa4c161409f628dc83cb0bc337d0e29210d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:40 +0000

    Add package chinesename-feng/0.0.9/chinesename_feng-0.0.9-py3-none-any.whl

---
+++ packages/chinesename-feng/chinesename_feng-0.0.9-py3-none-any.whl/example_pkg/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020 NetEase.com, Inc. All Rights Reserved.
+# Copyright 2020, The Fuxi AI Lab.
+"""
+desc
+
+Authors: fengxiaochuan(fengxiaochuan@corp.netease.com)
+Phone: 15664601006
+Date: 2020/3/24
+
+"""
+name = "example_pkg"
