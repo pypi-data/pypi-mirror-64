04e4bebabce760d14906696df3717da2d4f895e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:30 +0000

    Add package s3recon/1.3.1/s3recon-1.3.1-py3-none-any.whl

---
+++ packages/s3recon/s3recon-1.3.1-py3-none-any.whl/s3recon/constants.py
@@ -0,0 +1,13 @@
+format_list = [
+    "https://{region}.amazonaws.com/{word}{sep}{env}",
+    "https://{region}.amazonaws.com/{env}{sep}{word}",
+    # "https://{word}{sep}{env}.{region}.amazonaws.com",
+    # "https://{env}{sep}{word}.{region}.amazonaws.com",
+]
+
+useragent_list = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/603.2.4 (KHTML, like Gecko) Version/10.1.1 Safari/603.2.4",
+]
