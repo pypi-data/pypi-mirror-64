d21946cf97d968a6aa1be0db1aac9484a0d4f555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:12 +0000

    Add package chompjs/1.0.3/chompjs-1.0.3.tar.gz

---
+++ packages/chompjs/chompjs-1.0.3.tar.gz/chompjs-1.0.3/module.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <Python.h>
+#include "parser.h"
+
+static PyObject* parse_python_object(PyObject *self, PyObject *args) {
+    const char* string;
+    size_t initial_stack_size = 10;
+    if (!PyArg_ParseTuple(args, "s|n", &string, &initial_stack_size)) {
+        return NULL;
+    }
+
+    struct Lexer lexer = {
+        // input js object
+        string,
+        // output JSON
+        //
+        // alloc twice the size of input because characters are added when
+        // identifiers are quoted, e.g. from '{a:1}' to  '{"a":1}'
+        // so output might be larger than input, especially for malicious input
+        // such as '{a:1,b:1,c:1,d:1,e:1,f:1,g:1,h:1,i:1,j:1}' is translated to
+        // '{"a":1,"b":1,"c":1,"d":1,"e":1,"f":1,"g":1,"h":1,"i":1}'
+        malloc(2*strlen(string)),
+        // input string position
+        0,
+        // output string position
+        0,
+        // initial state
+        {begin},
+        // initial lexer status
+        CAN_ADVANCE,
+        // initial stack index
+        0,
+        // initial stack size
+        initial_stack_size,
+        // initial stack
+        malloc(initial_stack_size*sizeof(Type))
+    };
+
+    while(lexer.lexer_status == CAN_ADVANCE) {
+        advance(&lexer);
+    }
+
+    PyObject* ret = Py_BuildValue("s", lexer.output);
+    free((char*)lexer.output);
+    free((Type*)lexer.stack);
+    if(lexer.lexer_status == ERROR) {
+        char error_message[50];
+        strcpy(error_message, "Parser error: ...");
+        strncpy(error_message+17, lexer.input+lexer.input_position, 30);
+
+        PyErr_SetString(PyExc_ValueError, error_message);
+        return NULL;
+    }
+    return ret;
+}
+
+static PyMethodDef parser_methods[] = { 
+    {   
+        "parse", parse_python_object, METH_VARARGS,
+        "Parse JavaScript object string"
+    },  
+    {NULL, NULL, 0, NULL}
+};
+
+static struct PyModuleDef parser_definition = { 
+    PyModuleDef_HEAD_INIT,
+    "_chompjs",
+    "C extension for fast JavaScript object parsing",
+    -1, 
+    parser_methods
+};
+
+PyMODINIT_FUNC PyInit__chompjs(void) {
+    Py_Initialize();
+    return PyModule_Create(&parser_definition);
+}
---
+++ packages/chompjs/chompjs-1.0.3.tar.gz/chompjs-1.0.3/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+from os import path
+from setuptools import setup, Extension
+
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+chompjs_extension = Extension('_chompjs', sources=['module.c', 'parser.c'])
+
+setup(
+    name='chompjs',
+    version='1.0.3',
+    description='Parsing JavaScript objects into Python dictionaries',
+    author='Mariusz Obajtek',
+    author_email='nykakin@gmail.com',
+    keywords='parsing parser JavaScript json',
+    ext_modules=[chompjs_extension],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: JavaScript",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing :: General",
+        "Topic :: Text Processing :: Linguistic",
+    ],
+    python_requires='>=3.6',
+    url='https://github.com/Nykakin/chompjs',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    packages=['chompjs'],
+)
