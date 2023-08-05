823602d39d17ce89b3b163359a7b81e8cc4e7b7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:12 +0000

    Add package pyUnit-math/2020.3.23/pyUnit_math-2020.3.23.tar.gz

---
+++ packages/pyUnit-math/pyUnit_math-2020.3.23.tar.gz/pyUnit_math-2020.3.23/pyunit_math/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+# @Time  : 2019/3/21 9:52
+# @Author: Jtyoui@qq.com
+from .distance import Distance  # 两点之间的距离
+from .function import *
+from .pi import cpi, ppi
+
+__version__ = '2020.3.23'
+__author__ = 'Jtyoui'
+__description__ = '数据分析和数学常用的方法'
+__email__ = 'jtyoui@qq.com'
+__names__ = 'pyUnit_math'
+__url__ = 'https://github.com/PyUnit/pyunit-math'
