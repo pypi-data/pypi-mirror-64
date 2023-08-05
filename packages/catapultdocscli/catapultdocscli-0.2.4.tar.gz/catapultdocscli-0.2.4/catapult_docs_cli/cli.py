777769afe39c0b176674e4e1f19c315a6510f7e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:03 +0000

    Add package catapultdocscli/0.2.4/catapultdocscli-0.2.4.tar.gz

---
+++ packages/catapultdocscli/catapultdocscli-0.2.4.tar.gz/catapultdocscli-0.2.4/catapult_docs_cli/cli.py
@@ -0,0 +1,36 @@
+import json
+
+import click
+
+from catapult_docs_cli.commands import PropertiesCommand, StatusErrorsCommand, CLIUsageCommand
+
+
+def load_config(config):
+    """Reads the configuration from a file.
+
+    Args:
+        config (str): Relative path to the config (.catdocs) file.
+
+    Returns:
+        obj: The config parsed as a JSON object.
+    """
+    try:
+        with open(config, encoding='utf-8') as file:
+            return json.load(file)
+    except IOError:
+        click.ClickException('Operation failed: .catdocs file was not found.')
+
+
+@click.command()
+@click.option('--config', '-c', default='.catdocs', help='.catdocs file path')
+@click.argument('command', required=True)
+def main(command, config):
+    config = load_config(config)
+    if command == 'properties':
+        PropertiesCommand(config).execute()
+    elif command == 'status-errors':
+        StatusErrorsCommand(config).execute()
+    elif command == 'cli-usage':
+        CLIUsageCommand(config).execute()
+    else:
+        raise click.ClickException('The specified option is not implemented.')
