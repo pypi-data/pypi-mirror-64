1a674381016364ebc4f1184508c1e226810ec4c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:23 +0000

    Add package st-spin/0.0.14/st_spin-0.0.14.tar.gz

---
+++ packages/st-spin/st_spin-0.0.14.tar.gz/st_spin-0.0.14/stspin/__init__.py
@@ -0,0 +1,7 @@
+from .spin_device import SpinDevice
+from .spin_chain import SpinChain
+
+from .constants import Command, SpinCommand
+from .constants import Register
+from .constants import Constant, SpinValue
+from .constants import Status
