4c9a5dccc57df51dd9428dbb11442542b446d160
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:09 +0000

    Add package herming/1.1/herming-1.1.tar.gz

---
+++ packages/herming/herming-1.1.tar.gz/herming-1.1/setup.py
@@ -0,0 +1,10 @@
+﻿from setuptools import setup
+setup(name='herming',
+      version='1.1',
+      description='for you',
+      url='http://github.com/tongling/ab_resbase',
+      author='herming',
+      author_email='an520999@gmail.com',
+      license='JUST',
+      packages=['herming'],
+      zip_safe=False)
