59bd1e762fe1b57fde7377879c16610502619b0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:59 +0000

    Add package ptcime/1.0.3/ptcime-1.0.3.tar.gz

---
+++ packages/ptcime/ptcime-1.0.3.tar.gz/ptcime-1.0.3/README.md
@@ -0,0 +1,34 @@
+# ptcime
+Small time tracker built on python.
+
+## Installation
+
+```bash
+pip install ptcime
+```
+
+## Usage
+
+
+Start tracking time:
+```bash
+ptcime start
+```
+Stop tracking time and log currently worked on:
+```bash    
+ptcime stop
+```
+
+Log currently worked time:
+```bash
+ptcime current
+```
+
+Log day work time:
+```bash
+ptcime day
+```
+Log week work time:
+```bash
+ptcime week
+```
---
+++ packages/ptcime/ptcime-1.0.3.tar.gz/ptcime-1.0.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ptcime", # Replace with your own username
+    version="1.0.3",
+    author="Pedro Tavares de Carvalho",
+    author_email="ptcar2009@gmail.com",
+    description="A simple time tracking script.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ptcar2009/ptcime",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    scripts=["./scripts/ptcime"],
+    python_requires='>=3.6',
+)
