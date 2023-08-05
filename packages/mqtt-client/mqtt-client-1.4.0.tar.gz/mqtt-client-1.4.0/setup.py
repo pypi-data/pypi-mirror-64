8a64bc9f9ef425f063a94c22fbe0a1ac89b088a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:14 +0000

    Add package mqtt-client/1.4.0/mqtt-client-1.4.0.tar.gz

---
+++ packages/mqtt-client/mqtt-client-1.4.0.tar.gz/mqtt-client-1.4.0/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='mqtt-client',
+    version='1.4.0',
+    description='Simple MQTT Client.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords='mqtt',
+    author='Samuel de Ancos',
+    author_email='sdeancos@gmail.com',
+    url='https://github.com/sdeancos/mqtt-client',
+    packages=find_packages(),
+    include_package_data=False,
+    scripts=['bin/mqtt-client'],
+    python_requires='>=3.6',
+    install_requires=[
+        'docopt',
+        'paho-mqtt',
+        'terminaltables'
+    ]
+)
