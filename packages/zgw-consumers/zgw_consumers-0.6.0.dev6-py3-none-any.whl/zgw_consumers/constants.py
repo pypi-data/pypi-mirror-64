530c74a83531bcda370bc3e36ea23e31f4a26371
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:43 +0000

    Add package zgw-consumers/0.6.0.dev6/zgw_consumers-0.6.0.dev6-py3-none-any.whl

---
+++ packages/zgw-consumers/zgw_consumers-0.6.0.dev6-py3-none-any.whl/zgw_consumers/constants.py
@@ -0,0 +1,16 @@
+from django.utils.translation import ugettext_lazy as _
+
+from djchoices import ChoiceItem, DjangoChoices
+
+
+class APITypes(DjangoChoices):
+    ac = ChoiceItem("ac", _("AC (Authorizations)"))
+    nrc = ChoiceItem("nrc", _("NRC (Notifications)"))
+
+    zrc = ChoiceItem("zrc", _("ZRC (Zaken)"))
+    ztc = ChoiceItem("ztc", _("ZTC (Zaaktypen)"))
+    drc = ChoiceItem("drc", _("DRC (Informatieobjecten)"))
+    brc = ChoiceItem("brc", _("BRC (Besluiten)"))
+    kic = ChoiceItem("kic", _("KIC (Klantinteracties)"))
+
+    orc = ChoiceItem("orc", _("ORC (Overige)"))
