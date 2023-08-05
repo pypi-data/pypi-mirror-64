c26c30f1ed09607ab25d061aedddeb0f9a50e737
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:33 +0000

    Add package lurl/0.0.2/lurl-0.0.2.tar.gz

---
+++ packages/lurl/lurl-0.0.2.tar.gz/lurl-0.0.2/lurl/args_parser.py
@@ -0,0 +1,39 @@
+from optparse import OptionParser
+import sys
+
+def parse_options():
+    """
+    Handle command-line options with optparse.OptionParser.
+
+    Return list of arguments, largely for use in `parse_arguments`.
+    """
+
+    # Initialize
+    parser = OptionParser(usage="lurl [options]")
+
+    parser.add_option(
+        '-p','--prod',
+        dest='curl_prod',
+        default=None,
+        help="put your cURL for production"
+    )
+
+    parser.add_option(
+       '-s','--stag',
+        dest='curl_stag',
+        default=None,
+        help="put your cURL for staging"
+    )
+
+    parser.add_option(
+       '--skip-response',
+        dest='skip_response',
+        action='store_true',
+        default=False,
+        help="skip set response"
+    )
+
+    # Finalize
+    # Return three-tuple of parser + the output from parse_args (opt obj, args)
+    opts, args = parser.parse_args()
+    return parser, opts, args
