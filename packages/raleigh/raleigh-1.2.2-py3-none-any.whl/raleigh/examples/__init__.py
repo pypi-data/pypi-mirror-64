35ef4638e59f59948b144d5d3aca1befe7aafbe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:22 +0000

    Add package raleigh/1.2.2/raleigh-1.2.2-py3-none-any.whl

---
+++ packages/raleigh/raleigh-1.2.2-py3-none-any.whl/raleigh/examples/__init__.py
@@ -0,0 +1,11 @@
+'''Example scripts.
+
+core_solver.py:
+    Basic core solver usage demonstration.
+
+sparse_evp.py:
+    Computes several eigenvalues and eigenvectors of a real symmetric matrix.
+
+truncated_svd.py:
+    Computes truncated SVD.
+'''
