248f55643c15ec3ba367180646a2d67e2bccd4bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:05 +0000

    Add package pytest-salt-factories/0.7.20/pytest-salt-factories-0.7.20.tar.gz

---
+++ packages/pytest-salt-factories/pytest-salt-factories-0.7.20.tar.gz/pytest-salt-factories-0.7.20/tests/functional/factories/conftest.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+tests.functional.factories.conftest
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
