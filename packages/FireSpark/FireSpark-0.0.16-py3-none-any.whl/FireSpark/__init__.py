4d982c1bb99e374d2377fde15bc024445be61c79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:11 +0000

    Add package FireSpark/0.0.16/FireSpark-0.0.16-py3-none-any.whl

---
+++ packages/FireSpark/FireSpark-0.0.16-py3-none-any.whl/FireSpark/__init__.py
@@ -0,0 +1,5 @@
+from .proto_utils import ProtoTemplate, ProtoDataset
+from .data_utils import SparkDataset
+from .protos.mas import annotation_pb2 as annotation_proto
+from .protos.mas import image_data_pb2 as image_proto
+from .aws_utils import FireSparkAWS as S3
