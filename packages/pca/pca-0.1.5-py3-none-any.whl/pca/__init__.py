43921ce82e65fc7ae433e0b1676c7f44fd3afe26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:59 +0000

    Add package pca/0.1.5/pca-0.1.5-py3-none-any.whl

---
+++ packages/pca/pca-0.1.5-py3-none-any.whl/pca/__init__.py
@@ -0,0 +1,30 @@
+from pca.pca import (
+    fit,
+ 	biplot,
+ 	biplot3d,
+ 	plot,
+    norm,
+)
+
+__author__ = 'Erdogan Tasksen'
+__email__ = 'erdogant@gmail.com'
+__version__ = '0.1.5'
+
+# module level doc-string
+__doc__ = """
+pca
+=====================================================================
+
+Description
+-----------
+pca is a python package that performs the principal component analysis and to make insightful plots.
+
+Example
+-------
+>>> model = pca.fit(X)
+>>> ax = pca.biplot(model) 
+>>> ax = pca.biplot3d(model)
+>>> ax = pca.plot(model)
+>>> X_norm = pca.norm(X)
+
+"""
