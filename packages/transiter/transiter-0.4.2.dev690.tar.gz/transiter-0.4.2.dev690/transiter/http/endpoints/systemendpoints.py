f0c267239c3e58e954a556f0c26acd30416e26a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:09 +0000

    Add package transiter/0.4.2.dev690/transiter-0.4.2.dev690.tar.gz

---
+++ packages/transiter/transiter-0.4.2.dev690.tar.gz/transiter-0.4.2.dev690/transiter/http/endpoints/systemendpoints.py
@@ -0,0 +1,113 @@
+import flask
+import requests
+
+from transiter import exceptions
+from transiter.http import httpmanager
+from transiter.http.httpmanager import (
+    http_endpoint,
+    link_target,
+    HttpMethod,
+    HttpStatus,
+)
+from transiter.http.permissions import requires_permissions, PermissionsLevel
+from transiter.services import systemservice, links
+
+system_endpoints = flask.Blueprint(__name__, __name__)
+
+
+@http_endpoint(system_endpoints, "")
+@link_target(links.SystemsIndexLink)
+def list_all():
+    """List all systems"""
+    return systemservice.list_all()
+
+
+@http_endpoint(system_endpoints, "/<system_id>")
+@link_target(links.SystemEntityLink)
+def get_by_id(system_id):
+    """Get data on a specific system."""
+    return systemservice.get_by_id(system_id)
+
+
+@http_endpoint(
+    system_endpoints, "/<system_id>", method=HttpMethod.PUT,
+)
+@requires_permissions(PermissionsLevel.ALL)
+def install(system_id):
+    """Install a system."""
+    form_key_to_value = flask.request.form.to_dict()
+    form_key_to_file_storage = flask.request.files.to_dict()
+
+    if "config_file" in form_key_to_value:
+        config_file_location = form_key_to_value["config_file"]
+        try:
+            response = requests.get(config_file_location)
+            response.raise_for_status()
+        except requests.exceptions.RequestException:
+            raise exceptions.InvalidInput(
+                "Could not download YAML config file from '{}'".format(
+                    config_file_location
+                )
+            )
+        config_str = response.text
+        del form_key_to_value["config_file"]
+    elif "config_file" in form_key_to_file_storage:
+        config_str = flask.request.files["config_file"].read().decode("utf-8")
+    else:
+        raise exceptions.InvalidInput("YAML config file not provided!")
+
+    sync = httpmanager.is_sync_request()
+    if sync:
+        install_method = systemservice.install
+    else:
+        install_method = systemservice.install_async
+    response = install_method(
+        system_id=system_id, config_str=config_str, extra_settings=form_key_to_value,
+    )
+
+    # This means the system already exists and nothing was done.
+    if not response:
+        status = HttpStatus.OK
+    else:
+        if sync:
+            status = HttpStatus.CREATED
+        else:
+            status = HttpStatus.ACCEPTED
+    return systemservice.get_by_id(system_id), status
+
+
+@http_endpoint(
+    system_endpoints,
+    "/<system_id>",
+    method=HttpMethod.DELETE,
+    returns_json_response=False,
+)
+@requires_permissions(PermissionsLevel.ALL)
+def delete_by_id(system_id):
+    """Uninstall a system."""
+    systemservice.delete_by_id(
+        system_id, error_if_not_exists=True, sync=httpmanager.is_sync_request()
+    )
+    return flask.Response(response="", status=HttpStatus.NO_CONTENT, content_type="")
+
+
+@http_endpoint(
+    system_endpoints, "/<system_id>/auto-update", method=HttpMethod.PUT,
+)
+@requires_permissions(PermissionsLevel.ALL)
+def set_auto_update_enabled(system_id):
+    form_key_to_value = flask.request.form.to_dict()
+    enabled = form_key_to_value.get("enabled")
+    if enabled is None:
+        raise exceptions.InvalidInput("The form variable 'enabled' is required")
+    enabled = enabled.lower()
+    if enabled not in {"false", "true"}:
+        raise exceptions.InvalidInput(
+            "The form variable 'enabled' has to be 'true' or 'false', not '{}'".format(
+                enabled
+            )
+        )
+    systemservice.set_auto_update_enabled(
+        system_id, form_key_to_value["enabled"].lower() == "true"
+    )
+    return "", HttpStatus.NO_CONTENT
---
+++ packages/transiter/transiter-0.4.2.dev690.tar.gz/transiter-0.4.2.dev690/transiter/models/system.py
@@ -0,0 +1,41 @@
+import enum
+
+from sqlalchemy import Column, Integer, String, Enum, Boolean
+from sqlalchemy.orm import relationship
+
+from .base import Base, ToDictMixin
+
+
+class System(ToDictMixin, Base):
+    __tablename__ = "system"
+
+    pk = Column(Integer, primary_key=True)
+    id = Column(String, unique=True, index=True)
+
+    class SystemStatus(enum.Enum):
+        SCHEDULED = 1
+        INSTALLING = 2
+        ACTIVE = 3
+        INSTALL_FAILED = 4
+        DELETING = 5
+
+    name = Column(String, nullable=True)
+    status = Column(Enum(SystemStatus), nullable=False)
+    error_message = Column(String, nullable=True)
+    timezone = Column(String, nullable=True)
+    raw_config = Column(String)
+    auto_update_enabled = Column(Boolean, nullable=False, server_default="True")
+
+    routes = relationship(
+        "Route", back_populates="system", cascade="all, delete-orphan"
+    )
+    stops = relationship("Stop", back_populates="system", cascade="all, delete-orphan")
+    feeds = relationship("Feed", back_populates="system", cascade="all, delete-orphan")
+    scheduled_services = relationship(
+        "ScheduledService", back_populates="system", cascade="all, delete-orphan"
+    )
+    service_map_groups = relationship(
+        "ServiceMapGroup", back_populates="system", cascade="all, delete-orphan"
+    )
+
+    __dict_columns__ = [id, status, name]
