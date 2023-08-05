05ebaacf60f59be887fb914392320b3569dea0b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:27 +0000

    Add package oapi/0.0.61/oapi-0.0.61.tar.gz

---
+++ packages/oapi/oapi-0.0.61.tar.gz/oapi-0.0.61/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='oapi',
+    version="0.0.61",
+    description=(
+        'An SDK for parsing OpenAPI (Swagger) 2.0 - 3.0 specifications'
+    ),
+    url='https://bitbucket.com/davebelais/oapi.git',
+    author='David Belais',
+    author_email='david@belais.me',
+    license='MIT',
+    python_requires='>=3.6, <4',
+    keywords='openapi swagger json rest',
+    packages=find_packages(),
+    install_requires=[
+        "pyyaml>=5.3",
+        "iso8601>=0.1.12",
+        "sob>=0.2.6",
+        "jsonpointer>=2.0"
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=5.4.1",
+            "tox>=3.14.5",
+            "flake8>=3.7.9"
+        ],
+        "test": [
+            "pytest>=5.4.1",
+            "tox>=3.14.5",
+            "flake8>=3.7.9"
+        ]
+    }
+)
