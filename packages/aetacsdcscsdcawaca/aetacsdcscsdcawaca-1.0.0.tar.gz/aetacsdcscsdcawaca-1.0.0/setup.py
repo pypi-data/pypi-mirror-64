9512705bc78661eb9f66133d17768d7ec9796f8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:39 +0000

    Add package aetacsdcscsdcawaca/1.0.0/aetacsdcscsdcawaca-1.0.0.tar.gz

---
+++ packages/aetacsdcscsdcawaca/aetacsdcscsdcawaca-1.0.0.tar.gz/aetacsdcscsdcawaca-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='aetacsdcscsdcawaca',
+      version='1.0.0',
+      description='A print test for PyPI',
+      author='lingshiyinzi',
+      author_email='294594605@qq.com',
+      url='https://www.python.org/',
+      license='MIT',
+      keywords='ga nn',
+      packages=['aetacsdcscsdcawaca'],
+      install_requires=['numpy>=1.14', 'tensorflow>=1.7'],
+      python_requires='>=3'
+     )
