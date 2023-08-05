b7e987357b24432983a0bc02f598232743ecc463
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:45 +0000

    Add package st-spin/0.0.18/st_spin-0.0.18.tar.gz

---
+++ packages/st-spin/st_spin-0.0.18.tar.gz/st_spin-0.0.18/cc/__init__.py
@@ -0,0 +1,14 @@
+from .command import Command, SpinCommand
+
+from .constant import (
+    Constant,
+    SpinValue,
+)
+
+from .register import Register
+
+from .status import (
+    Status,
+    MotorStatus,
+    SwitchStatus,
+)
