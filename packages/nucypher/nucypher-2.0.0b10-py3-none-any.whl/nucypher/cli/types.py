0f5ed5a4d8b0af640768ddb98259cda3957870d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:45 +0000

    Add package nucypher/2.0.0b10/nucypher-2.0.0b10-py3-none-any.whl

---
+++ packages/nucypher/nucypher-2.0.0b10-py3-none-any.whl/nucypher/cli/types.py
@@ -0,0 +1,66 @@
+"""
+This file is part of nucypher.
+
+nucypher is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+nucypher is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with nucypher.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from ipaddress import ip_address
+
+import click
+from eth_utils import to_checksum_address
+
+from nucypher.blockchain.eth.interfaces import BlockchainInterface
+from nucypher.blockchain.eth.networks import NetworksInventory
+
+
+class ChecksumAddress(click.ParamType):
+    name = 'checksum_address'
+
+    def convert(self, value, param, ctx):
+        try:
+            value = to_checksum_address(value=value)
+        except ValueError as e:
+            self.fail("Invalid ethereum address")
+        else:
+            return value
+
+
+class IPv4Address(click.ParamType):
+    name = 'ipv4_address'
+
+    def convert(self, value, param, ctx):
+        try:
+            _address = ip_address(value)
+        except ValueError as e:
+            self.fail("Invalid IP Address")
+        else:
+            return value
+
+
+# NuCypher
+NETWORK_DOMAIN = click.Choice(choices=NetworksInventory.NETWORKS)
+
+# Ethereum
+EIP55_CHECKSUM_ADDRESS = ChecksumAddress()
+WEI = click.IntRange(min=1, clamp=False)  # TODO: Better validation for ether and wei values?
+
+# Filesystem
+EXISTING_WRITABLE_DIRECTORY = click.Path(exists=True, dir_okay=True, file_okay=False, writable=True)
+EXISTING_READABLE_FILE = click.Path(exists=True, dir_okay=False, file_okay=True, readable=True)
+
+# Network
+NETWORK_PORT = click.IntRange(min=0, max=65535, clamp=False)
+IPV4_ADDRESS = IPv4Address()
+
+GAS_STRATEGY_CHOICES = click.Choice(list(BlockchainInterface.GAS_STRATEGIES.keys()))
