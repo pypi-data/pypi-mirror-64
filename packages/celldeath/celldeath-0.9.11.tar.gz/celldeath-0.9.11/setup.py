6953fc36e3120124d36dd68dccc56100f5dc7772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:35 +0000

    Add package celldeath/0.9.11/celldeath-0.9.11.tar.gz

---
+++ packages/celldeath/celldeath-0.9.11.tar.gz/celldeath-0.9.11/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+
+
+with open('README.md', 'r') as fh:
+     long_description = fh.read()
+
+
+
+
+setuptools.setup(
+     name='celldeath',
+     version='0.9.11',
+     author='Santiago Miriuka, Alejandro La Greca, Nelba Pérez',
+     author_email='smiriuka@fleni.org.ar, ale.lagreca@gmail.com, nelbap@hotmail.com',
+     description='A tool to identify cell death based on deep learning',
+     long_description=long_description, 
+     long_description_content_type='text/markdown', 
+     url='https://github.com/sgmiriuka/celldeath', 
+     packages=setuptools.find_packages(),
+     install_requires=['fastai>=1.0.60',
+                         'image-slicer==0.3.0',
+                         'matplotlib>=3.1.1'], 
+     classifiers=[
+          'Development Status :: 4 - Beta',
+          'Programming Language :: Python :: 3',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Topic :: Scientific/Engineering :: Medical Science Apps.'
+     ],
+     python_requires='>=3.6'
+)
+
