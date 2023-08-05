9749ee33f0c743b23ede290e3ed10ab6ca224673
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:23 +0000

    Add package ask-schools/0.3.6/ask_schools-0.3.6.tar.gz

---
+++ packages/ask-schools/ask_schools-0.3.6.tar.gz/ask_schools-0.3.6/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ask_schools']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'ask-schools',
+    'version': '0.3.6',
+    'description': 'Ask Scholars Portal School Name Conversion',
+    'long_description': "# Ask Schools\n\n[\n![PyPI](https://img.shields.io/pypi/v/ask_schools.svg)\n![PyPI](https://img.shields.io/pypi/pyversions/ask_schools.svg)\n![PyPI](https://img.shields.io/github/license/guinslym/ask_schools.svg)\n](https://pypi.org/project/ask_schools/)\n[![TravisCI](https://travis-ci.org/guinslym/ask_schools.svg?branch=master)](https://travis-ci.org/guinslym/ask_schools)\n\n\nThis package helps convert Ask School suffixes to the school full name.\n\n\n## Installation\n\n**Ask Schools** can be installed from PyPI using `pip` or your package manager of choice:\n\n```\npip install ask_schools\n```\n\n## Usage\n\n\nExample:\n\n```python\n\nfrom ask_schools import find_school_by_operator_suffix\n\ndef test_check_school_name_equal_toronto():\n  result = find_school_by_operator_suffix('_tor')\n  assert result == 'Tordonto'\n```\n\n## Code of Conduct\n\nEveryone interacting in the project's codebases, issue trackers, chat rooms, and mailing lists is expected to follow the [PyPA Code of Conduct](https://www.pypa.io/en/latest/code-of-conduct/).\n",
+    'author': 'Guinsly Mondesir',
+    'author_email': 'guinslym@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/guinslym/ask_schools',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.4,<4.0',
+}
+
+
+setup(**setup_kwargs)
