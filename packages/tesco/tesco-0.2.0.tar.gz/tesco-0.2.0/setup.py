1fb6226fcc35b17a0b201b954097ed71f136382f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:28 +0000

    Add package tesco/0.2.0/tesco-0.2.0.tar.gz

---
+++ packages/tesco/tesco-0.2.0.tar.gz/tesco-0.2.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['tesco']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.23.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'tesco',
+    'version': '0.2.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
