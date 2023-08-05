19d82f8712dd9c0ca11c3acab8c6ac9e0d5ce01f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:41 +0000

    Add package mysql-pydump/0.1/mysql-pydump-0.1.tar.gz

---
+++ packages/mysql-pydump/mysql-pydump-0.1.tar.gz/mysql-pydump-0.1/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+
+setup(
+    name='mysql-pydump',
+    packages=['mysql-pydump'],
+    version='0.1',
+    license='MIT',
+    description='mysql-pydump allows you to easily backup your mysql database',
+    author='Ali Marefati',
+    author_email='marefati110@gmail.com',
+    url='https://github.com/marefati110/pydump',
+    download_url='https://github.com/marefati110/pydump/archive/0.1.tar.gz',
+    keywords=['mysql', 'mariadb', 'python', 'backup', 'dump', 'rstore'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+
+    ],
+)
