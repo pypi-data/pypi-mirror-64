932210759f50be64f7e47d444bc46054f8400891
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:42 +0000

    Add package pyfiles/0.2.0/pyfiles-0.2.0.tar.gz

---
+++ packages/pyfiles/pyfiles-0.2.0.tar.gz/pyfiles-0.2.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='pyfiles',
+    packages = ['pyfiles'],
+    version='0.2.0',
+    license='MIT',
+    description='management for files with python',
+    author='TBA',
+    keywords=['files', 'management'],
+    url='https://github.com/tba91/pyfiles',
+    download_url='https://github.com/tba91/pyfiles/archive/0.2.1.tar.gz',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+  ],
+)
