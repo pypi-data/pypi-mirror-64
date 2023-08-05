3da1eb440f1e6cfd340c0f37f37724f266b00fe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:15 +0000

    Add package spaceone-secret/0.1.1.post102/spaceone_secret-0.1.1.post102-py3-none-any.whl

---
+++ packages/spaceone-secret/spaceone_secret-0.1.1.post102-py3-none-any.whl/spaceone/secret/model/secret_model.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+import logging
+from mongoengine import *
+
+from spaceone.core.locator import Locator
+from spaceone.core.model.mongo_model import MongoModel
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class Secret(MongoModel):
+    secret_id = StringField(max_length=40, generate_id='secret', unique=True)
+    name = StringField(max_length=255, unique_with='domain_id')
+    tags = DictField()
+    schema_id = StringField(max_length=40, null=True, default=None)
+    project_id = StringField(max_length=40, null=True, default=None)
+    domain_id = StringField(max_length=255)
+    created_at = DateTimeField(auto_now_add=True)
+
+    meta = {
+        'updatable_fields': [
+            'name',
+            'tags',
+            'project_id'
+        ],
+        'exact_fields': [
+            'secret_id',
+            'schema_id',
+            'project_id',
+            'domain_id'
+        ],
+        'minimal_fields': [
+            'secret_id',
+            'name'
+        ],
+        'ordering': [
+            'name'
+        ],
+        'indexes': [
+            'secret_id',
+            'schema_id',
+            'project_id',
+            'domain_id'
+        ]
+    }
+
+    @classmethod
+    def query(cls, *args, **kwargs):
+        change_filter = []
+
+        for condition in kwargs.get('filter', []):
+            key = condition.get('k') or condition.get('key')
+            if key == 'secret_group_id':
+                change_filter.append(cls._change_secret_group_id_filter(condition))
+
+            else:
+                change_filter.append(condition)
+
+        kwargs['filter'] = change_filter
+
+        return super().query(*args, **kwargs)
+
+    @staticmethod
+    def _change_secret_group_id_filter(condition):
+        value = condition.get('v') or condition.get('value')
+        operator = condition.get('o') or condition.get('operator')
+
+        map_query = {
+            'filter': [{
+                'k': 'secret_group_id',
+                'v': value,
+                'o': operator
+            }]
+        }
+        locator = Locator()
+        secret_group_model = locator.get_model('SecretGroupMap')
+        map_vos, total_count = secret_group_model.query(map_query)
+
+        return {
+            'k': 'secret_id',
+            'v': list(map(lambda map_vo: map_vo.secret.secret_id, map_vos)),
+            'o': 'in'
+        }
