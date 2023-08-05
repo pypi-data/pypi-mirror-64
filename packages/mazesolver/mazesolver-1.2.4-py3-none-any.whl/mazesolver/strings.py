f1a637fd401e18736d7ff419f4dffcc1113fe62a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:20 +0000

    Add package mazesolver/1.2.4/mazesolver-1.2.4-py3-none-any.whl

---
+++ packages/mazesolver/mazesolver-1.2.4-py3-none-any.whl/mazesolver/strings.py
@@ -0,0 +1,24 @@
+# This file contains all long strings used by the program
+help_message = """\
+Valid Commands
+---------------
+-h or --help    -  Prints this help page and exits
+-v or --version -  Prints the version of the program and exits
+
+-i or --input   -  Input maze file
+-o or --output  -  Output directory
+
+Example usage: mazesolver -i path/to/maze.jpg -o path/to/dir/
+
+
+Contact info
+---------------
+Email: tom@digitalnook.net
+Github: https://github.com/exciteabletom
+
+*Licensed under GPLv3* [https://www.gnu.org/licenses/gpl-3.0.en.html]
+"""
+
+version="1.2.4"
+version_long=f"mazesolver{version}"
+
