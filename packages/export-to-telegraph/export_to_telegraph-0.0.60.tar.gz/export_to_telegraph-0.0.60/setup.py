9faabe05608268cce582a4846543ab1855da2b0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:30:26 +0000

    Add package export-to-telegraph/0.0.60/export_to_telegraph-0.0.60.tar.gz

---
+++ packages/export-to-telegraph/export_to_telegraph-0.0.60.tar.gz/export_to_telegraph-0.0.60/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="export_to_telegraph",
+    version="0.0.60",
+    author="Yunzhi Gao",
+    author_email="gaoyunzhi@gmail.com",
+    description="Library for export webpage to Telegraph.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gaoyunzhi/export_to_telegraph",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'html_telegraph_poster',
+        'bs4',
+        'readability-lxml',
+        'telegram_util',
+        'readee',
+        'hanziconv',
+        'cached_url'
+    ],
+    python_requires='>=3.0',
+)
