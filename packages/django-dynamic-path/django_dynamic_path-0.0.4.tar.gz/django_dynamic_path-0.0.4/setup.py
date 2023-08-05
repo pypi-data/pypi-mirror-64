7398718529d2720c9444027af0aa62744df9e6a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:49 +0000

    Add package django-dynamic-path/0.0.4/django_dynamic_path-0.0.4.tar.gz

---
+++ packages/django-dynamic-path/django_dynamic_path-0.0.4.tar.gz/django_dynamic_path-0.0.4/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="django_dynamic_path",
+    version="0.0.4",
+    author="Alex Fischer",
+    author_email="alex@quadrant.net",
+    description="A django path() replacement enabling truly dynamic urls",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/quadrant-newmedia/django_dynamic_path",
+    packages=['django_dynamic_path', 'django_dynamic_path.tests'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=["Django>=2.2,<3.1"],
+)
