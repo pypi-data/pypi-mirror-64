2589615569b3a13a72cfddf1a05603d89cde40db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:05 +0000

    Add package alchemyml/0.1.25/alchemyml-0.1.25.tar.gz

---
+++ packages/alchemyml/alchemyml-0.1.25.tar.gz/alchemyml-0.1.25/alchemyml/__init__.py
@@ -0,0 +1,15 @@
+from ._CRUD_classes import authentication, dataset, experiment, project
+from ._manual_ops import actions
+from ._version import __version__
+
+__doc__ = '''
+alchemyml - Automation of data lifecycle
+========================================
+
+**alchemyml** is a multi-environment tool for automating the data lifecycle 
+and accelerating advanced data analysis processes. From data ingest and 
+preparation, through pre-processing and up to the construction of predictive 
+models, **alchemyml** provides the data scientist with aldata science applied 
+artificial intelligence tools to optimize their work, time and resources. 
+'''
+
