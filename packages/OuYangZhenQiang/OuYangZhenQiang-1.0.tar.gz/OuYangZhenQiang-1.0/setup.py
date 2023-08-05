1ef33af5dd3df02e5492ac45485ac80c81305340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:30 +0000

    Add package OuYangZhenQiang/1.0/OuYangZhenQiang-1.0.tar.gz

---
+++ packages/OuYangZhenQiang/OuYangZhenQiang-1.0.tar.gz/OuYangZhenQiang-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name='OuYangZhenQiang',#对外我们模块的名字
+    version='1.0',#版本号
+    description='用于判断回文串第一个对外发布的模块',#描述
+    author='欧阳贞强',#作者
+    author_email='3223684916@qq.com',#邮箱
+    py_modules=['OYangZQiang.demo']#要发布的模块
+)
