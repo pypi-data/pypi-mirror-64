d7c97e9ed36cdaee6aa2e37daa49b87eb58dade8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:44 +0000

    Add package Caly/1.0/Caly-1.0-py3.6.egg

---
+++ packages/Caly/Caly-1.0-py3.6.egg/Caly/__init__.py
@@ -0,0 +1,8 @@
+
+import os
+
+DIR = os.path.dirname(__file__)
+CALYDIR = os.path.dirname(DIR)
+CONFDIR = os.path.join(CALYDIR,"conf")
+TXTDIR  = os.path.join(CONFDIR,"txt")
+SNDDIR  = os.path.join(CONFDIR,"sounds")
