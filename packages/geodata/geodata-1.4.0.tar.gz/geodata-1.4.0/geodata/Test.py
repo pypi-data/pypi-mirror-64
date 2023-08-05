a886c162d85fe05052665f8b0c747d492541a25a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:24 +0000

    Add package geodata/1.4.0/geodata-1.4.0.tar.gz

---
+++ packages/geodata/geodata-1.4.0.tar.gz/geodata-1.4.0/geodata/Test.py
@@ -0,0 +1,18 @@
+
+result_list = []
+
+georow_list = ['rome', 'ny', 'seattle']
+result_list.extend(georow_list)
+
+georow_list.clear()
+#georow_list = ['boston', 'LA', 'denver']
+georow_list = []
+result_list.extend(georow_list)
+
+georow_list.clear()
+georow_list.extend(result_list)
+
+print(georow_list)
+
+
+
