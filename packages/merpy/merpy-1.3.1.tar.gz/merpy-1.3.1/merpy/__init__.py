b0fa81a3400a15f4989f6a944635793eeb1d0265
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:53 +0000

    Add package merpy/1.3.1/merpy-1.3.1.tar.gz

---
+++ packages/merpy/merpy-1.3.1.tar.gz/merpy-1.3.1/merpy/__init__.py
@@ -0,0 +1,36 @@
+from .merpy import (
+    get_entities,
+    generate_lexicon,
+    process_lexicon,
+    show_lexicons,
+    get_lexicons,
+    download_lexicon,
+    create_lexicon,
+    create_mappings,
+    download_mer,
+    download_lexicons,
+    mer_path,
+    get_entities_mp,
+    create_lexicon_from_file,
+    delete_lexicon,
+    merge_processed_lexicons,
+)
+
+name = "merpy"
+__all__ = [
+    "get_entities",
+    "generate_lexicon",
+    "process_lexicon",
+    "show_lexicons",
+    "get_lexicons",
+    "download_lexicon",
+    "create_mappings",
+    "create_lexicon",
+    "download_mer",
+    "download_lexicons",
+    "mer_path",
+    "get_entities_mp",
+    "create_lexicon_from_file",
+    "delete_lexicon",
+    "merge_processed_lexicons",
+]
