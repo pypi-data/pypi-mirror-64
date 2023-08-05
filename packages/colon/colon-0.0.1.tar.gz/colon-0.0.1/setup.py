fc303206b66e8a176c0641b23ae32b5eb472aeff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:03 +0000

    Add package colon/0.0.1/colon-0.0.1.tar.gz

---
+++ packages/colon/colon-0.0.1.tar.gz/colon-0.0.1/setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(
+    name='colon',
+    version='0.0.1',
+    description='',
+    long_description='',
+    author='me',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
