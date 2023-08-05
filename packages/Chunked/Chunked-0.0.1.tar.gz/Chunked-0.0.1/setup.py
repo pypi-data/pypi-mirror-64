f63f1565bd7c038133a022d934c3fa24fb3ebd58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:53 +0000

    Add package Chunked/0.0.1/Chunked-0.0.1.tar.gz

---
+++ packages/Chunked/Chunked-0.0.1.tar.gz/Chunked-0.0.1/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='Chunked',
+      version='0.0.1',
+      description='This is a simple program that encodes an input stream '
+                  'using HTTP "chunked" encoding',
+      author='Rustam Kashapov',
+      author_email='hardtechnik91@gmail.com',
+      url='https://github.com/rkashapov/chunked',
+      scripts=['chunked'])
