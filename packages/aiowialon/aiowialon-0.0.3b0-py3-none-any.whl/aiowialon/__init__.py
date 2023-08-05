022359f2f51e2fb47ac7cf3c5742d3d8b592a384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:35 +0000

    Add package aiowialon/0.0.3b0/aiowialon-0.0.3b0-py3-none-any.whl

---
+++ packages/aiowialon/aiowialon-0.0.3b0-py3-none-any.whl/aiowialon/__init__.py
@@ -0,0 +1,14 @@
+import logging
+
+from aiowialon.client import Session, connect, APIError
+
+logging.getLogger(__name__).setLevel(logging.DEBUG)
+
+
+def add_log_handler(handler: logging.Handler) -> None:
+    """Add log handler to the module logger
+
+    Arguments:
+        handler {logging.Handler} -- log handler
+    """
+    logging.getLogger(__name__).addHandler(handler)
