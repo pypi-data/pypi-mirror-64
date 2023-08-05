1b58b840246878524ef730ee762827c9424e1ae0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:08 +0000

    Add package scml/0.2.10/scml-0.2.10-py2.py3-none-any.whl

---
+++ packages/scml/scml-0.2.10-py2.py3-none-any.whl/scml/scml2020/__init__.py
@@ -0,0 +1,12 @@
+"""Implements the SCML 2020 world design"""
+
+from .common import *
+from .world import *
+from .components import *
+from .agents import *
+from . import utils
+from .utils import *
+
+__all__ = (
+    common.__all__ + agents.__all__ + world.__all__ + components.__all__ + ["utils"]
+)
