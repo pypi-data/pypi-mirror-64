8d9bac53ece05dfd70e9edb6d585784b2c67973e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:50 +0000

    Add package qq_palindrome/1.0/qq_palindrome-1.0.tar.gz

---
+++ packages/qq_palindrome/qq_palindrome-1.0.tar.gz/qq_palindrome-1.0/setup.py
@@ -0,0 +1,10 @@
+
+from distutils.core import setup
+setup(
+    name='qq_palindrome',   # 对外我们模块的名字
+    version='1.0',  # 版本号
+    description='回文串检测方法，测试哦',   #描述
+    author='钱琦',   # 作者
+    author_email='2195472784@qq.com',
+    py_modules=['meatBun_QianQi.demo'] # 要发布的模块
+    )
