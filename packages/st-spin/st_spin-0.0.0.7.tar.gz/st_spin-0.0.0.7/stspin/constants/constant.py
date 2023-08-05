d5a9b4cacaa1d64430bb54cca351a69e02396867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:34 +0000

    Add package st-spin/0.0.0.7/st_spin-0.0.0.7.tar.gz

---
+++ packages/st-spin/st_spin-0.0.0.7.tar.gz/st_spin-0.0.0.7/stspin/constants/constant.py
@@ -0,0 +1,17 @@
+from typing_extensions import (
+    Final,
+)
+
+class Constant:
+    DirReverse: Final           = 0
+    DirForward: Final           = 1
+    DirMax: Final               = 2
+
+    ActResetPos: Final          = (0 << 3)
+    ActSetMark: Final           = (1 << 3)
+
+    MaxStepsPerSecond: Final[float]     = 15625.0
+    MaxSteps: Final                     = int(2 ** 22) - 1
+
+    TickSeconds: Final[float]           = 250 * (10 ** -9)
+    SpsToSpeed: Final[float]            = TickSeconds / (2 ** -28)
