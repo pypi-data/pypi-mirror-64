d312fe3be4a2f68201f8a0813272226fa5fa7b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:06 +0000

    Add package disco/1.34.1/disco-1.34.1.tar.gz

---
+++ packages/disco/disco-1.34.1.tar.gz/disco-1.34.1/disco/models/__init__.py
@@ -0,0 +1,5 @@
+from .job_details import JobDetails
+from .task import Task
+from .cluster_details import ClusterDetails
+from .repository_details import RepositoryDetails
+from .docker_image_details import  DockerImageDetails
