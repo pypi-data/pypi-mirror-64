3155f26f75ab7683bab126318b2da58f41a8a522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:32 +0000

    Add package DEODR/0.1.10/deodr-0.1.10-cp36-cp36m-manylinux2010_x86_64.whl

---
+++ packages/DEODR/deodr-0.1.10-cp36-cp36m-manylinux2010_x86_64.whl/deodr/tensorflow/tools.py
@@ -0,0 +1,8 @@
+import numpy as np
+import tensorflow as tf
+
+
+def scipy_sparse_matrix_to_tensorflow(x):
+    coo = x.tocoo()
+    indices = np.mat([coo.row, coo.col]).transpose()
+    return tf.SparseTensor(indices, coo.data, coo.shape)
