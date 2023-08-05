3cbcc086e4e5926d1466c63a037797d206fd8352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:27 +0000

    Add package onionr/1.0/onionr-1.0.tar.gz

---
+++ packages/onionr/onionr-1.0.tar.gz/onionr-1.0/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name='onionr',
+    version='1.0',
+    description='parking onionr',
+    license="GNU GPLv3",
+    long_description_content_type="text/markdown",
+    author='Kevin Froman',
+    author_email='beardog@mailbox.org',
+    url="https://onionr.net/",
+    packages=[],
+    install_requires=[],
+    python_requires='>=3.6',
+)
