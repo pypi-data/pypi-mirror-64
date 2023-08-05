b29aa79a907338bab917178e23df39d673a777ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:52 +0000

    Add package pulp-file-client/0.3.0.dev1584979590/pulp_file_client-0.3.0.dev1584979590-py3-none-any.whl

---
+++ packages/pulp-file-client/pulp_file_client-0.3.0.dev1584979590-py3-none-any.whl/pulpcore/client/pulp_file/api/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from pulpcore.client.pulp_file.api.content_files_api import ContentFilesApi
+from pulpcore.client.pulp_file.api.distributions_file_api import DistributionsFileApi
+from pulpcore.client.pulp_file.api.file_exporters_file_api import FileExportersFileApi
+from pulpcore.client.pulp_file.api.publications_file_api import PublicationsFileApi
+from pulpcore.client.pulp_file.api.remotes_file_api import RemotesFileApi
+from pulpcore.client.pulp_file.api.repositories_file_api import RepositoriesFileApi
+from pulpcore.client.pulp_file.api.repositories_file_versions_api import RepositoriesFileVersionsApi
