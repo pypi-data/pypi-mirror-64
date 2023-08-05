091b913df27458c69216f3b1d4d07f823e5d90e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:54 +0000

    Add package proposal/6.1.0/proposal-6.1.0.tar.gz

---
+++ packages/proposal/proposal-6.1.0.tar.gz/proposal-6.1.0/private/PROPOSAL/medium/MediumFactory.cxx
@@ -0,0 +1,42 @@
+
+#include <algorithm>
+#include <functional>
+#include <cctype>
+#include <string>
+#include <memory>
+
+#include "PROPOSAL/Logging.h"
+#include "PROPOSAL/medium/Medium.h"
+#include "PROPOSAL/medium/MediumFactory.h"
+#include "PROPOSAL/medium/density_distr/density_homogeneous.h"
+
+
+namespace PROPOSAL {
+std::shared_ptr<Medium> CreateMedium(Medium_Type type, double density_correction)
+{
+    std::unique_ptr<Density_distr> density_distr(new Density_homogeneous(density_correction));
+    auto searched_medium = Medium_Map.find(type);
+    if (searched_medium != Medium_Map.end()) {
+        searched_medium->second->SetDensityDistribution(*density_distr);
+    } else {
+        throw std::invalid_argument("Medium not found.");
+    }
+
+    return searched_medium->second;
+}
+} // namespace PROPOSAL
+
+namespace PROPOSAL {
+std::shared_ptr<Medium> CreateMedium(std::string name, double density_correction)
+{
+    std::transform(name.begin(), name.end(), name.begin(),
+        [](unsigned char c){ return std::tolower(c); });
+
+    for (size_t id = 0; id < Medium_Name.size(); ++id) {
+        if (name ==Medium_Name[id]){
+            return CreateMedium(static_cast<Medium_Type>(id), density_correction);
+        }
+    }
+    throw std::invalid_argument("Medium not found.");
+}
+} // namespace PROPOSAL
