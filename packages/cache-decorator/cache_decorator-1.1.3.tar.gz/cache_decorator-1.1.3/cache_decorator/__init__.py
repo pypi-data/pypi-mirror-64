2aafeaddd63aec516a05bf935236166413ed6ac4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:46 +0000

    Add package cache-decorator/1.1.3/cache_decorator-1.1.3.tar.gz

---
+++ packages/cache-decorator/cache_decorator-1.1.3.tar.gz/cache_decorator-1.1.3/cache_decorator/__init__.py
@@ -0,0 +1,4 @@
+"""Package that automatically caches and dispatch serialization and deserialization to the correct functions depending on the extension."""
+from .cache import Cache
+
+__all__ = ["Cache"]
