60094992c31ffe96144b9a32c727ea63a8254267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:39 +0000

    Add package django-log-labeler/1.0.4/django-log-labeler-1.0.4.tar.gz

---
+++ packages/django-log-labeler/django-log-labeler-1.0.4.tar.gz/django-log-labeler-1.0.4/log_labeler/filters.py
@@ -0,0 +1,18 @@
+import logging
+from django.conf import settings
+from log_labeler import local, DEFAULT_HEADER_VALUE, LOG_LABEL_REQUEST_SETTING
+
+
+class HeaderToLabelFilter(logging.Filter):
+    def filter(self, record):
+        if hasattr(settings, LOG_LABEL_REQUEST_SETTING) and isinstance(getattr(settings, LOG_LABEL_REQUEST_SETTING),
+                                                                       dict):
+            for label in getattr(settings, LOG_LABEL_REQUEST_SETTING):
+                header_value = getattr(local, label, DEFAULT_HEADER_VALUE)
+                setattr(record, label, header_value)
+                if isinstance(record.msg, dict):
+                    if "_log_identifier" not in record.msg:
+                        record.msg["_log_identifier"] = dict()
+                    record.msg["_log_identifier"][label] = header_value
+        return True
+
