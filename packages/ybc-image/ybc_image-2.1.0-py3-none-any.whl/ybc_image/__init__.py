e57f93213bf7cb922960b340de41e001d6c7ea93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:12 +0000

    Add package ybc-image/2.1.0/ybc_image-2.1.0-py3-none-any.whl

---
+++ packages/ybc-image/ybc_image-2.1.0-py3-none-any.whl/ybc_image/__init__.py
@@ -0,0 +1,18 @@
+from ybc_image.image import img
+from ybc_image.image import open
+from ybc_image.image import setbg
+from ybc_image.image import Image
+from ybc_image.image import EditableImage
+from ybc_image.image import QrcodeImage
+
+
+__all__ = [
+    'img',
+    'open',
+    'setbg',
+    'Image',
+    'EditableImage',
+    'QrcodeImage'
+]
+
+__version__ = '2.1.0'
