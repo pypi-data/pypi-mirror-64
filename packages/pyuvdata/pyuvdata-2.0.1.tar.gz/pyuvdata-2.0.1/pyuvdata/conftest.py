0c3a06b26696a508bd4b45a55489f9b4071843c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:35 +0000

    Add package pyuvdata/2.0.1/pyuvdata-2.0.1.tar.gz

---
+++ packages/pyuvdata/pyuvdata-2.0.1.tar.gz/pyuvdata-2.0.1/pyuvdata/conftest.py
@@ -0,0 +1,43 @@
+# -*- mode: python; coding: utf-8 -*-
+# Copyright (c) 2019 Radio Astronomy Software Group
+# Licensed under the 2-clause BSD License
+
+"""Testing environment setup and teardown for pytest."""
+from __future__ import absolute_import, division, print_function
+
+import os
+import shutil
+
+import pytest
+from astropy.utils import iers
+from astropy.time import Time
+
+from pyuvdata.data import DATA_PATH
+
+
+@pytest.fixture(autouse=True, scope="session")
+def setup_and_teardown_package():
+    """Make data/test directory to put test output files in."""
+    testdir = os.path.join(DATA_PATH, "test/")
+    if not os.path.exists(testdir):
+        print("making test directory")
+        os.mkdir(testdir)
+
+    # Do a calculation that requires a current IERS table. This will trigger
+    # automatic downloading of the IERS table if needed, including trying the
+    # mirror site in python 3 (but won't redownload if a current one exists).
+    # If there's not a current IERS table and it can't be downloaded, turn off
+    # auto downloading for the tests and turn it back on once all tests are
+    # completed (done by extending auto_max_age).
+    # Also, the checkWarnings function will ignore IERS-related warnings.
+    try:
+        t1 = Time.now()
+        t1.ut1
+    except (Exception):
+        iers.conf.auto_max_age = None
+
+    yield
+
+    iers.conf.auto_max_age = 30
+
+    shutil.rmtree(testdir)
