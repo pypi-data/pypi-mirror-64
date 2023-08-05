652713ad230cc8c96bd477fe35af4ae38e790060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:19 +0000

    Add package py2c2p/1.0.0/py2c2p-1.0.0.tar.gz

---
+++ packages/py2c2p/py2c2p-1.0.0.tar.gz/py2c2p-1.0.0/README.md
@@ -0,0 +1,7 @@
+# py2c2p
+
+A Python library for [2c2p](https://developer.2c2p.com/docs) API
+
+| Name                                              | Description            |
+| --------------------------------------------------|------------------------|
+| [payment_gateway_sdk.md](payment_gateway_sdk.md)  | Payment Gateway SDK    |
---
+++ packages/py2c2p/py2c2p-1.0.0.tar.gz/py2c2p-1.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(
+    name='py2c2p',
+    version='1.0.0',
+    description='A Python library for 2c2p API',
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/sang-sakarin/py2c2p',
+    author='sang_sakarin',
+    author_email='sang_sakarin@outlook.com',
+    license='sang_sakarin',
+    scripts=[],
+    keywords='2c2p 2c2p-python 2c2p-python-sdk',
+    packages=['py2c2p'],
+    install_requires=['requests'],
+)
