d0562b18a5cdfea000d7013e52ec01865a23e10e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:55 +0000

    Add package PyShapes/1.1.2/PyShapes-1.1.2.tar.gz

---
+++ packages/PyShapes/PyShapes-1.1.2.tar.gz/PyShapes-1.1.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PyShapes", # Replace with your own username
+    version="1.1.2",
+    author="Richeek Das",
+    author_email="richeekdas2001@gmail.com",
+    description="A shape detection module for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sudoRicheek/PyShapes",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
