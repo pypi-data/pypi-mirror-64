e0b37ec3f46ecaeb73e352b3ae2ebde26ea5bd05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:37 +0000

    Add package av2bv/0.1.2/av2bv-0.1.2.tar.gz

---
+++ packages/av2bv/av2bv-0.1.2.tar.gz/av2bv-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+        name= 'av2bv',
+        version= '0.1.2',
+        description=('Bilibili av⇔BV'),
+        author= 'liziyi0914',
+        author_email= 'liziyi0914@gmail.com',
+        license= 'MIT License',
+        packages= find_packages(),
+        platforms= ["all"],
+        url= 'https://github.com/liziyi0914/av2bv-py')
