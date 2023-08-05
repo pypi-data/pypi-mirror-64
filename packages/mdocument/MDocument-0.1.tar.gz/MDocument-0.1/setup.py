8d050112996f2091d098d982bd76b9fbc87a44a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:59 +0000

    Add package mdocument/0.1/MDocument-0.1.tar.gz

---
+++ packages/mdocument/MDocument-0.1.tar.gz/MDocument-0.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+with open("README.rst") as long_description_file:
+    setup(name="MDocument",
+          version="0.1",
+          description="Simple DRM for motor client",
+          author="Yurzs",
+          author_email="yurzs+MDocument@icloud.com",
+          packages=["MDocument"],
+          install_requires=["motor"],
+          license="MIT",
+          long_description=long_description_file.read(),
+          keywords=["mongo", "motor", "DRM"],
+          url="https://git.yurzs.dev/MDocument",
+          classifiers=[
+              "Development Status :: 3 - Alpha",
+              "License :: OSI Approved :: MIT License",
+              "Programming Language :: Python :: 3.6",
+              "Programming Language :: Python :: 3.7",
+              "Programming Language :: Python :: 3.8",
+          ])
