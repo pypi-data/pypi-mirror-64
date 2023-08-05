6047e31a6e1b89e2c4a9a3ba2f36df8097448267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:20 +0000

    Add package gopup/0.0.1/gopup-0.0.1.tar.gz

---
+++ packages/gopup/gopup-0.0.1.tar.gz/gopup-0.0.1/gopup/my_fun.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/3/20 0020
+# @Author  : justin.郑 3907721@qq.com
+# @File    : my_fun.py
+# @Desc    : 
+ 
+
+def gopup_test():
+    return "测试成功"
+
