3995fe97bb2670a26e54b9999eed1bbc42985176
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:03 +0000

    Add package leprikon/2.20.0/leprikon-2.20.0.tar.gz

---
+++ packages/leprikon/leprikon-2.20.0.tar.gz/leprikon-2.20.0/leprikon/search_indexes/base.py
@@ -0,0 +1,11 @@
+from haystack import indexes
+
+
+class BaseIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True, template_name='leprikon/search.txt')
+    title = indexes.CharField(stored=True, indexed=False, model_attr='name')
+    url = indexes.CharField(stored=True, indexed=False)
+    school_year = indexes.IntegerField(stored=True, indexed=True, model_attr='school_year_id')
+
+    def prepare_url(self, obj):
+        return obj.get_absolute_url()
