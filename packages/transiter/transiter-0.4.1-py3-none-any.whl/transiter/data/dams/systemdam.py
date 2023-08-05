ccd5ccb060974b434dfb94987fdf07c84fdf70f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:43 +0000

    Add package transiter/0.4.1/transiter-0.4.1-py3-none-any.whl

---
+++ packages/transiter/transiter-0.4.1-py3-none-any.whl/transiter/data/dams/systemdam.py
@@ -0,0 +1,119 @@
+from typing import Optional
+
+from sqlalchemy import func
+
+from transiter import models
+from transiter.data import dbconnection
+from transiter.data.dams import genericqueries
+
+
+def create(entity=None):
+    """
+    Create a new System.
+
+    :param entity: An optional System to persist
+    :return: the persisted System
+    """
+    return genericqueries.create(models.System, entity)
+
+
+def delete_by_id(id_):
+    """
+    Delete a System from the DB whose ID is given.
+
+    :param id_:
+    :return: True if an entity was found and deleted, false if no such
+     entity exists
+    """
+    session = dbconnection.get_session()
+    entity = get_by_id(id_)
+    if entity is None:
+        return False
+    session.delete(entity)
+    return True
+
+
+def list_all():
+    """
+    List all Systems in the database.
+
+    :return: list of Systems
+    """
+    return genericqueries.list_all(models.System, models.System.id)
+
+
+def get_by_id(id_, only_return_active=False) -> Optional[models.System]:
+    """
+    Get a system by its ID.
+
+    :param id_: the ID
+    :param only_return_active:
+    :return: the System
+    """
+    system = genericqueries.get_by_id(models.System, id_)
+    if system is None:
+        return None
+    if only_return_active and system.status != system.SystemStatus.ACTIVE:
+        return None
+    return system
+
+
+def _count_child_entity_in_system(system_id, Model):
+    """
+    Return the number of entities of a certain type that are children of a System.
+
+    :param system_id: the system's ID
+    :param Model: the model type: Trip, Route or Stop
+    :return: the integer count
+    """
+    session = dbconnection.get_session()
+    query = (
+        session.query(func.count(Model.pk))
+        .filter(Model.system_pk == models.System.pk)
+        .filter(models.System.id == system_id)
+    )
+    return query.one()[0]
+
+
+def count_stops_in_system(system_id):
+    """
+    Return the number of Stops in a system.
+
+    :param system_id: the system's ID
+    :return: the integer count
+    """
+    return _count_child_entity_in_system(system_id, models.Stop)
+
+
+def count_routes_in_system(system_id):
+    """
+    Return the number of Routes in a system.
+
+    :param system_id: the system's ID
+    :return: the integer count
+    """
+    return _count_child_entity_in_system(system_id, models.Route)
+
+
+def count_feeds_in_system(system_id):
+    """
+    Return the number of Feeds in a system.
+
+    :param system_id: the system's ID
+    :return: the integer count
+    """
+    return _count_child_entity_in_system(system_id, models.Feed)
+
+
+def list_all_alerts_associated_to_system(system_pk):
+    """
+    List all of the alerts associated directly to a system.
+
+    This does *not* return alerts that are associated other entities in the system
+
+    :param system_id: the system's ID
+    :return: list of Alerts
+    """
+    session = dbconnection.get_session()
+    query = session.query(models.Alert).filter(models.Alert.system_pk == system_pk)
+    return query.all()
