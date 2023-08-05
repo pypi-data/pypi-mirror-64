bf5cca2583c71e799b0ba4e82232fd2ec6ebb6c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:55 +0000

    Add package validium/0.2.1/validium-0.2.1.tar.gz

---
+++ packages/validium/validium-0.2.1.tar.gz/validium-0.2.1/validium/core.py
@@ -0,0 +1,25 @@
+import logging
+log = logging.getLogger("[validium]")
+log.addHandler(logging.NullHandler()) # ignore log messages by defualt
+
+class Validator:
+
+  def __init__(self, predicate, msg=None):
+
+    assert callable(predicate), "the argument 'predicate' must be callable"
+    assert msg is None or isinstance(msg, str), "the argument 'msg' must be None or an instance of str"
+
+    self.__dict__ = dict(
+      predicate=predicate,
+      msg=msg,
+    )
+
+  def validate(self, target, tag=None):
+    assert self.confirm(target, tag), self.msg
+
+  def confirm(self, target, tag=None):
+    result = self.predicate(target)
+    t = tag if tag is not None else target
+    log.info(f"💎({repr(t)}) - {'✅ pass' if result else '❌ fail'}: {self.msg}")
+    
+    return result
