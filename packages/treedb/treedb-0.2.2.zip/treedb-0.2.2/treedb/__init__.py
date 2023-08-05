925439586b8cae0128ecca533f07ad874c9b2b8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:24 +0000

    Add package treedb/0.2.2/treedb-0.2.2.zip

---
+++ packages/treedb/treedb-0.2.2.zip/treedb-0.2.2/treedb/__init__.py
@@ -0,0 +1,45 @@
+# treedb - load ../../glottolog/languoids/tree/**/md.ini into sqlite3
+
+"""Load glottolog lanuoid tree ini files into SQLite3 database."""
+
+from . import proxies as _proxies
+
+ROOT, ENGINE = _proxies.PathProxy(), _proxies.SQLiteEngineProxy()
+
+from .files import get_default_root, set_root, iterfiles
+
+from .languoids import iterlanguoids, write_json_csv, compare_with_raw
+
+from .backend import (create_engine, load, Dataset, Session,
+                      print_schema, dump_sql, export, backup)
+
+from .models import LEVEL, Languoid
+
+from .checks import check
+
+from .queries import print_rows, write_csv, hash_csv, get_query, iterdescendants
+
+from .sa_helpers import text, select, count
+
+__all__ = ['ROOT', 'ENGINE',
+           'set_root', 'iterfiles',
+           'iterlanguoids', 'write_json_csv', 'compare_with_raw',
+           'create_engine', 'load', 'Dataset', 'Session',
+           'print_schema', 'dump_sql', 'export', 'backup',
+           'LEVEL', 'Languoid',
+           'check',
+           'print_rows', 'write_csv', 'hash_csv', 'get_query', 'iterdescendants',
+           'text', 'select', 'count',
+           'root', 'engine']
+
+__title__ = 'treedb'
+__version__ = '0.2.2'
+__author__ = 'Sebastian Bank <sebastian.bank@uni-leipzig.de>'
+__license__ = 'MIT, see LICENSE.txt'
+__copyright__ = 'Copyright (c) 2017-2020 Sebastian Bank'
+
+# default root: TREEDB_REPO or repo_root in sister git checkout or cwd
+root = set_root(get_default_root('TREEDB_REPO', '../glottolog', '.'))
+
+# default engine: in-memory database
+engine = create_engine(None, title=__title__)
---
+++ packages/treedb/treedb-0.2.2.zip/treedb-0.2.2/try-treedb.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# try-treedb.py - usage examples
+
+import logging
+
+import treedb
+
+from treedb import Languoid, select
+
+logging.basicConfig(format='[%(levelname)s@%(name)s] %(message)s')
+logging.captureWarnings(True)
+logging.getLogger('treedb').setLevel(logging.INFO)
+
+treedb.set_root('../glottolog')
+
+print(next(treedb.iterlanguoids()))
+
+#treedb.create_engine('treedb.sqlite3')
+
+engine = treedb.load()
+
+treedb.print_rows(select([Languoid]).order_by('id').limit(5))
+
+tree = Languoid.tree(include_self=True, with_steps=True, with_terminal=True)
+treedb.print_rows(tree.select().where(tree.c.child_id == 'book1242'))
+treedb.print_rows(tree.select().where(tree.c.child_id == 'ramo1244'))
+
+print(next(treedb.iterdescendants(parent_level='top', child_level='language')))
+
+query = treedb.get_query()  # big example query containing 'everything'
+
+try:
+    import pandas as pd
+except ImportError:
+    pass
+else:
+    df = pd.read_sql_query(query, con=engine, index_col='id')
+    df.info()
+
+# run sanity checks
+treedb.check()
+
+#treedb.export()
+#treedb.write_csv()
+
+#treedb.files.roundtrip()
+
+#from treedb import raw
+
+#print(next(raw.iterrecords()))
+#print(next(treedb.iterlanguoids(engine)))
+#raw.print_stats()
+#raw.checksum()
+#raw.checksum(weak=True)
+#raw.write_raw_csv()
+
+#treedb.languoids.compare_with_raw()
+#treedb.languoids.to_json_csv(engine)
+
+#raw.drop_duplicate_sources()
+#raw.drop_duplicated_triggers()
+#raw.drop_duplicated_crefs()
+#raw.write_files()
+
+#import treedb.raw.fixes
+#treedb.raw.fixes.update_countries()
+#treedb.raw.fixes.update_wikidata_links()
+#treedb.raw.write_files()
