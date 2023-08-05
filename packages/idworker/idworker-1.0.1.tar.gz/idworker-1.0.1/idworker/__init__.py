8fd5dcb72accd865c8654f56d77ca6f4ed6f9ab4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:13 +0000

    Add package idworker/1.0.1/idworker-1.0.1.tar.gz

---
+++ packages/idworker/idworker-1.0.1.tar.gz/idworker-1.0.1/idworker/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# __author__ = 'Jim Yong'
+# __date__ = '2020/03/23'
+# __contact__ = 'jimyong88@gmail.com'
+# __copyright__ = 'Copyright (C) 2020, JimYong.com'
+
+from idworker.idworker import IdWorker
