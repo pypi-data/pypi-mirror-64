34e065e5a1b28d20311dec9161f5a81f690bb77e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:34 +0000

    Add package getrails/2.5/getrails-2.5-py3-none-any.whl

---
+++ packages/getrails/getrails-2.5-py3-none-any.whl/getrails/__init__.py
@@ -0,0 +1,12 @@
+from getrails.duckduckgo.search import go_duck
+from getrails.google.search import go_gle
+from getrails.torch.search import go_onion
+
+def search (query):
+    try:
+        result = go_gle(query)
+    except urllib.error.HTTPError:
+        result = go_duck(query)
+
+    result = result.append(go_onion(query))
+    return result
