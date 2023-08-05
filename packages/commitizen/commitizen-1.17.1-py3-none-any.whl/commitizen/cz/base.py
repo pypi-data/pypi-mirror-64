877eb149860660b5d398fa58a70aa2d253728f3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:46 +0000

    Add package commitizen/1.17.1/commitizen-1.17.1-py3-none-any.whl

---
+++ packages/commitizen/commitizen-1.17.1-py3-none-any.whl/commitizen/cz/base.py
@@ -0,0 +1,59 @@
+from abc import ABCMeta, abstractmethod
+from typing import List, Optional, Tuple
+
+from prompt_toolkit.styles import Style, merge_styles
+
+
+class BaseCommitizen(metaclass=ABCMeta):
+    bump_pattern: Optional[str] = None
+    bump_map: Optional[dict] = None
+    default_style_config: List[Tuple[str, str]] = [
+        ("qmark", "fg:#ff9d00 bold"),
+        ("question", "bold"),
+        ("answer", "fg:#ff9d00 bold"),
+        ("pointer", "fg:#ff9d00 bold"),
+        ("highlighted", "fg:#ff9d00 bold"),
+        ("selected", "fg:#cc5454"),
+        ("separator", "fg:#cc5454"),
+        ("instruction", ""),
+        ("text", ""),
+        ("disabled", "fg:#858585 italic"),
+    ]
+
+    def __init__(self, config: dict):
+        self.config = config
+        if not self.config.settings.get("style"):
+            self.config.settings.update({"style": BaseCommitizen.default_style_config})
+
+    @abstractmethod
+    def questions(self) -> list:
+        """Questions regarding the commit message."""
+
+    @abstractmethod
+    def message(self, answers: dict) -> str:
+        """Format your git message."""
+
+    @property
+    def style(self):
+        return merge_styles(
+            [
+                Style(BaseCommitizen.default_style_config),
+                Style(self.config.settings["style"]),
+            ]
+        )
+
+    def example(self) -> str:
+        """Example of the commit message."""
+        raise NotImplementedError("Not Implemented yet")
+
+    def schema(self) -> str:
+        """Schema definition of the commit message."""
+        raise NotImplementedError("Not Implemented yet")
+
+    def schema_pattern(self) -> str:
+        """Regex matching the schema used for message validation"""
+        raise NotImplementedError("Not Implemented yet")
+
+    def info(self) -> str:
+        """Information about the standardized commit message."""
+        raise NotImplementedError("Not Implemented yet")
