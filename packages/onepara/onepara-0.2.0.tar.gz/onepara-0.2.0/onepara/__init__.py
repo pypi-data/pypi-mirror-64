6192339d098eca7198597706836fa4bc03ddd04b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:59 +0000

    Add package onepara/0.2.0/onepara-0.2.0.tar.gz

---
+++ packages/onepara/onepara-0.2.0.tar.gz/onepara-0.2.0/onepara/__init__.py
@@ -0,0 +1,5 @@
+import warnings
+warnings.warn(
+    "the 'onepara' module is deprecated",
+    DeprecationWarning,
+    stacklevel=2)
---
+++ packages/onepara/onepara-0.2.0.tar.gz/onepara-0.2.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(name='onepara',
+      version='0.2.0',
+      description='deprecated',
+      url='http://github.com/ulf1/scipy-psdm',
+      author='Ulf Hamster',
+      author_email='554c46@gmail.com',
+      license='MIT',
+      packages=['onepara'],
+      install_requires=['setuptools>=40.0.0'],
+      python_requires='>=3.6',
+      zip_safe=False)
