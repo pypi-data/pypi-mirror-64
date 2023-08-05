c15a83ae34dad6d0f853f61c6dc364ede9ab1fed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:49 +0000

    Add package django-dynamic-path/0.0.4/django_dynamic_path-0.0.4-py3-none-any.whl

---
+++ packages/django-dynamic-path/django_dynamic_path-0.0.4-py3-none-any.whl/django_dynamic_path/__init__.py
@@ -0,0 +1,29 @@
+from django.urls.resolvers import ResolverMatch, RegexPattern, URLPattern
+
+class DynamicPath(URLPattern):
+    def __init__(self, resolver_func, view_func):
+        self.resolver_func = resolver_func
+        self.view_func = view_func
+
+        super().__init__(
+            # Note - this regex matches nothing
+            # We implement our own resolve() method, and we ignore this.
+            # It still needs to be a valid regex, though, or urls.reverse() will break.
+            RegexPattern(r'^(?!x)x'),
+            view_func,
+        )
+
+    def resolve(self, path):
+        r = self.resolver_func(path)
+        if r :
+            args, kwargs = r
+            # Django expects a tuple, not a list, of args
+            # This only occurs in certain situations.
+            # Lets cast to tuple, in case resolver_func returns list/iterable
+            args = tuple(args)
+            return ResolverMatch(
+                self.view_func,
+                tuple(args),
+                kwargs,
+                route='_DYNAMIC_PATH_ROUTE_',
+            )
