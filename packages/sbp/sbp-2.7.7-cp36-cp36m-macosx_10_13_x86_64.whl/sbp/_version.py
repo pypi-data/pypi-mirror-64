2624fa0a5eddc991734f14eedff0941c3a7112f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:47 +0000

    Add package sbp/2.7.7/sbp-2.7.7-cp36-cp36m-macosx_10_13_x86_64.whl

---
+++ packages/sbp/sbp-2.7.7-cp36-cp36m-macosx_10_13_x86_64.whl/sbp/_version.py
@@ -0,0 +1,7 @@
+# THIS FILE IS GENERATED FROM TRAITS SETUP.PY
+version = '2.7.7'
+full_version = '2.7.7'
+git_revision = 'b44d22d8'
+is_released = True
+if not is_released:
+    version = full_version
