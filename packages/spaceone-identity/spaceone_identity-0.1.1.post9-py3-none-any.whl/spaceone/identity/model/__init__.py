998d307c5a4361b1e171355e8b42ae02046b1942
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:02 +0000

    Add package spaceone-identity/0.1.1.post9/spaceone_identity-0.1.1.post9-py3-none-any.whl

---
+++ packages/spaceone-identity/spaceone_identity-0.1.1.post9-py3-none-any.whl/spaceone/identity/model/__init__.py
@@ -0,0 +1,10 @@
+from spaceone.identity.model.api_key_model import APIKey
+from spaceone.identity.model.domain_model import Domain
+from spaceone.identity.model.domain_owner_model import DomainOwner
+from spaceone.identity.model.domain_secret_model import DomainSecret
+from spaceone.identity.model.policy_model import Policy
+from spaceone.identity.model.project_group_model import ProjectGroup, ProjectGroupMemberMap
+from spaceone.identity.model.project_model import Project, ProjectMemberMap
+from spaceone.identity.model.role_model import Role
+from spaceone.identity.model.user_model import User
+
