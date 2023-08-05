b9d847acb737654fe27a7493100ab2aa8a28e3eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:31 +0000

    Add package csvquery/0.2.1/csvquery-0.2.1.tar.gz

---
+++ packages/csvquery/csvquery-0.2.1.tar.gz/csvquery-0.2.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="csvquery",
+    version="0.2.1",
+    author="Houston Youth Computer Science Group",
+    author_email="houstoncsgroup@gmail.com",
+    description="A python package that allows you to execute NoSQL-style queries on CSV files.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Houston-Youth-Computer-Science-Group/csv-query",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
