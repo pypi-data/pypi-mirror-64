dd916a8929948d46109a30e511baa0317547a54a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:31 +0000

    Add package familiar/0.1.3/familiar-0.1.3.tar.gz

---
+++ packages/familiar/familiar-0.1.3.tar.gz/familiar-0.1.3/familiar_tools/familiar.py
@@ -0,0 +1,22 @@
+from familiar_tools.settings import familiar_settings
+
+
+def dice_roll(number_of_dice, dice_sides, mod):
+    import random
+    total = 0
+    for i in range(1, number_of_dice + 1):
+        total += random.randint(1, dice_sides)
+    return total + mod
+
+
+def get_modifier(stat):
+    mod = stat - 10
+    mod = mod / 2
+    return round(mod)
+
+
+def get_game_version():
+    return familiar_settings.VERSION
+
+
+print(get_game_version())
