7a4278b8dbf83634826f20894c717d09c3e4640d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:33 +0000

    Add package trac-IncludeSource/0.5.1/trac-IncludeSource-0.5.1.tar.gz

---
+++ packages/trac-IncludeSource/trac-IncludeSource-0.5.1.tar.gz/trac-IncludeSource-0.5.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='trac-IncludeSource',
+    version='0.5.1',
+    packages=['includesource'],
+    author='Chris Heller',
+    maintainer='Thomas Tressieres',
+    maintainer_email='ttressieres@gmail.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    description='Include (parts of) a source file into the current wiki page',
+    url='http://trac-hacks.org/wiki/IncludeSourcePartialPlugin',
+    license='BSD',
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Operating System :: OS Independent",
+        "Framework :: Trac"
+    ],
+    install_requires = ['Trac'],
+    entry_points = {'trac.plugins': ['includesource.IncludeSource = includesource.IncludeSource']})
