5c158e68ecd02dd2c9595f30f852a8bfd0b3cd5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:30 +0000

    Add package icemac.ab.calendar/3.5/icemac.ab.calendar-3.5-py2.py3-none-any.whl

---
+++ packages/icemac.ab.calendar/icemac.ab.calendar-3.5-py2.py3-none-any.whl/icemac/ab/calendar/masterdata/tests/test_calendar.py
@@ -0,0 +1,48 @@
+from icemac.ab.calendar.interfaces import IEvent
+from icemac.addressbook.interfaces import IEntity
+from zope.testbrowser.browser import LinkNotFoundError
+import pytest
+import zope.component.hooks
+
+
+def test_calendar__CalendarView__1(address_book, FieldFactory, browser):
+    """It allows to select fields for display in the calendar."""
+    field = FieldFactory(
+        address_book, IEvent, u'Int', u'reservations').__name__
+    browser.login('cal-editor')
+    browser.open(browser.CALENDAR_MASTERDATA_URL)
+    browser.getLink('Calendar view').click()
+    assert browser.CALENDAR_MASTERDATA_EDIT_DISPLAY_URL == browser.url
+    browser.getControl('Additional event fields').displayValue = [
+        'persons',
+        'reservations',
+    ]
+    browser.getControl('Save').click()
+    assert 'Data successfully updated.' == browser.message
+    browser.open(browser.CALENDAR_MASTERDATA_EDIT_DISPLAY_URL)
+    assert browser.getControl('Additional event fields').displayValue == [
+        'persons',
+        'reservations',
+    ]
+    # It does not break on selected but deleted user defined field:
+    with zope.component.hooks.site(address_book):
+        event_entity = IEntity(IEvent)
+        event_entity.removeField(event_entity.getRawField(field))
+    browser.open(browser.CALENDAR_MASTERDATA_EDIT_DISPLAY_URL)
+    assert browser.getControl('Additional event fields').displayValue == [
+        'persons',
+    ]
+
+
+def test_calendar__CalendarView__2(address_book, browser):
+    """It is not shown on calendar master data for a calendar visitor."""
+    browser.login('cal-visitor')
+    browser.open(browser.CALENDAR_MASTERDATA_URL)
+    with pytest.raises(LinkNotFoundError):
+        browser.getLink('Calendar view')
+
+
+def test_calendar__CalendarView__3(address_book, browser):
+    """It is not accessible for a calendar visitor."""
+    browser.login('cal-visitor')
+    browser.assert_forbidden(browser.CALENDAR_MASTERDATA_EDIT_DISPLAY_URL)
