00d6aabf7ad903363ce142cb5d0cef9bdbe99d5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:15 +0000

    Add package Xiao/1.0/Xiao-1.0.tar.gz

---
+++ packages/Xiao/Xiao-1.0.tar.gz/Xiao-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name='Xiao',   # 对外我们模块的名字
+    version='1.0',  # 版本号
+    description='回文串检测方法，测试哦',   #描述
+    author='肖成正',   # 作者
+    author_email='1556674665@qq.com',
+    py_modules=['Xiao.demo'] # 要发布的模块
+)
