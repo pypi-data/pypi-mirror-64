88431403ed30f1dcb854a917fa5c5d147a6c462b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:40 +0000

    Add package bcpalindrome/1.0/bcpalindrome-1.0.tar.gz

---
+++ packages/bcpalindrome/bcpalindrome-1.0.tar.gz/bcpalindrome-1.0/setup.py
@@ -0,0 +1,11 @@
+#Author xiaojun zhang
+
+from distutils.core import setup
+setup(
+name='bcpalindrome',# 对外我们模块的名字
+version='1.0', #版本号
+description='俩数求和', #描述
+author='张百川',# 作者
+author_email='2020054830@qq.com',
+py_modules=['bao1.demo']#要发布的模块
+)
