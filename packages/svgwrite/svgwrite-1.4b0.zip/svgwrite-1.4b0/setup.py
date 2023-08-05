7bcacabc5c93cf6d9b2fa495162df4047a644437
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:20 +0000

    Add package svgwrite/1.4b0/svgwrite-1.4b0.zip

---
+++ packages/svgwrite/svgwrite-1.4b0.zip/svgwrite-1.4b0/setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+#coding:utf-8
+# Author:  mozman
+# Purpose: setup
+# Created: 08.09.2010
+# License: MIT License
+# Copyright (C) 2010-2018  Manfred Moitzi
+import os
+from setuptools import setup
+
+AUTHOR_NAME = 'Manfred Moitzi'
+AUTHOR_EMAIL = 'me@mozman.at'
+
+
+def get_version():
+    v = {}
+    # do not import svgwrite, because required packages may not installed yet
+    for line in open('./svgwrite/version.py').readlines():
+        if line.strip().startswith('__version__'):
+            exec(line, v)
+            return v['__version__']
+    raise IOError('__version__ string not found')
+
+
+def read(fname):
+    try:
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    except IOError:
+        return "File '%s' not found.\n" % fname
+
+
+setup(name='svgwrite',
+      version=get_version(),
+      description='A Python library to create SVG drawings.',
+      author=AUTHOR_NAME,
+      url='http://github.com/mozman/svgwrite.git',
+      download_url='http://github.com/mozman/svgwrite/releases',
+      author_email=AUTHOR_EMAIL,
+      python_requires='>=3.6',
+      packages=['svgwrite', 'svgwrite/data', 'svgwrite/extensions'],
+      provides=['svgwrite'],
+      long_description=read('README.rst') + read('NEWS.rst'),
+      platforms="OS Independent",
+      license="MIT License",
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",
+          "Programming Language :: Python :: 3.8",
+          "Programming Language :: Python :: Implementation :: CPython",
+          "Programming Language :: Python :: Implementation :: PyPy",
+          "Intended Audience :: Developers",
+          "Topic :: Multimedia :: Graphics",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+      ]
+)
