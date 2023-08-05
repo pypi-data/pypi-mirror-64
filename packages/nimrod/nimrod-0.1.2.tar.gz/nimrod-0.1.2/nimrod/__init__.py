8006d13cbfc1e0d11c57189f2aca6b4b300c92d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:16 +0000

    Add package nimrod/0.1.2/nimrod-0.1.2.tar.gz

---
+++ packages/nimrod/nimrod-0.1.2.tar.gz/nimrod-0.1.2/nimrod/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""Lightweight python package for context-free random text generation.
+"""
+__author__ = 'Ryan Foltz'
+__email__ = 'scrudgey@gmail.com'
+__version__ = '0.1.0'
+
+from .nimrod import Grammar
