98aed9629bbbc2a42a7e2292eebf366c4e4b770e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:22 +0000

    Add package pingmeen/0.0.2/pingmeen-0.0.2.tar.gz

---
+++ packages/pingmeen/pingmeen-0.0.2.tar.gz/pingmeen-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name                          = "pingmeen", 
+    version                       = "0.0.2",
+    author                        = "Nikita M. Minaev",
+    author_email                  = "nikita.minaev1995@gmail.com",
+    description                   = "Pingmeen package",
+    long_description              = long_description,
+    long_description_content_type = "text/markdown",
+    url                           = "",
+    packages                      = setuptools.find_packages(),
+    classifiers                   = [
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires               = '>=3.6',
+)
