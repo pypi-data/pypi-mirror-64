a243a6a38863f21bce8ffdbb2b9a35a21ab7e027
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:15 +0000

    Add package test-timeswipe/0.0.21/test_timeswipe-0.0.21.tar.gz

---
+++ packages/test-timeswipe/test_timeswipe-0.0.21.tar.gz/test_timeswipe-0.0.21/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+from cmake_setuptools import *
+setuptools.setup(
+     name='test_timeswipe',
+     version='0.0.21',
+     ext_modules=[CMakeExtension('timeswipe_py')],
+     cmdclass={'build_ext': CMakeBuildExt},
+     author="Ilya Gavrilov",
+     author_email="gilyav@gmail.com",
+     description="TimeSwipe python3 module",
+     long_description="pydoc timeswipe",
+     long_description_content_type="text/markdown",
+     url="https://github.com/panda-official/TimeSwipe",
+     install_requires = ['cmake_setuptools'],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
