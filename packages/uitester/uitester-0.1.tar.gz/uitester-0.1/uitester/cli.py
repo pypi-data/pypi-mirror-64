743cdd6b3839b1eae746ab1a728194bd7e4902b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:45 +0000

    Add package uitester/0.1/uitester-0.1.tar.gz

---
+++ packages/uitester/uitester-0.1.tar.gz/uitester-0.1/uitester/cli.py
@@ -0,0 +1,22 @@
+import os
+import fire
+import time
+
+
+class UiTester(object):
+
+    def start(self,filename):
+        os.system("pytest " + filename)
+
+    def server(self,order):
+        if order == "start":
+            os.system("selenium-standalone start")
+        if order == "install":
+            install_standalone = "sudo npm install selenium-standalone@latest -g --registry=https://registry.npm.taobao.org"
+            install_dirver = "sudo selenium-standalone install --drivers.firefox.baseURL=http://npm.taobao.org/mirrors/geckodriver --baseURL=http://npm.taobao.org/mirrors/selenium --drivers.chrome.baseURL=http://npm.taobao.org/mirrors/chromedriver --drivers.ie.baseURL=http://npm.taobao.org/mirrors/selenium"
+            os.system(install_standalone)
+            os.system(install_dirver)
+
+
+def cli():
+    fire.Fire(UiTester)
