453b9e0e23afbf6a72ccfbf3d6f4cdbbb414374e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:40 +0000

    Add package aetacsdcscsdcawacab/1.0.0/aetacsdcscsdcawacab-1.0.0.tar.gz

---
+++ packages/aetacsdcscsdcawacab/aetacsdcscsdcawacab-1.0.0.tar.gz/aetacsdcscsdcawacab-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='aetacsdcscsdcawacab',
+      version='1.0.0',
+      description='nothing',
+      author='lingshiyinzi',
+      author_email='294594605@qq.com',
+      url='https://www.python.org/',
+      license='MIT',
+      keywords='www',
+      packages=['aetacsdcscsdcawaca'],
+      install_requires=['numpy>=1.14', 'tensorflow>=1.7'],
+      python_requires='>=3'
+     )
