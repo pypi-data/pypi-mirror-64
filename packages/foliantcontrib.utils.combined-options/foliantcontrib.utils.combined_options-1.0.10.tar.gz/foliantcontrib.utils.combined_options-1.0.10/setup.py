c4c265dca152179af8135393877283f71ef33ec9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:01 +0000

    Add package foliantcontrib.utils.combined-options/1.0.10/foliantcontrib.utils.combined_options-1.0.10.tar.gz

---
+++ packages/foliantcontrib.utils.combined-options/foliantcontrib.utils.combined_options-1.0.10.tar.gz/foliantcontrib.utils.combined_options-1.0.10/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+
+SHORT_DESCRIPTION = ''
+
+try:
+    with open('README.md', encoding='utf8') as readme:
+        LONG_DESCRIPTION = readme.read()
+
+except FileNotFoundError:
+    LONG_DESCRIPTION = SHORT_DESCRIPTION
+
+
+setup(
+    name='foliantcontrib.utils.combined_options',
+    description=SHORT_DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    version='1.0.10',
+    author='Daniil Minukhin',
+    author_email='ddddsa@gmail.com',
+    url='https://github.com/foliant-docs/foliantcontrib.utils.combined_options',
+    packages=['foliant.preprocessors.utils'],
+    license='MIT',
+    platforms='any',
+    install_requires=[
+        'foliant>=1.0.8',
+        'PyYAML'
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Documentation",
+        "Topic :: Utilities",
+    ]
+)
