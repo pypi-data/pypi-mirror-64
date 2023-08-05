b9411ddd8081bbfd468cbe4cb3d06d8827d06490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:19 +0000

    Add package dtool-azure/0.7.0/dtool-azure-0.7.0.tar.gz

---
+++ packages/dtool-azure/dtool-azure-0.7.0.tar.gz/dtool-azure-0.7.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+url = "https://github.com/jic-dtool/dtool-azure"
+version = "0.7.0"
+readme = open('README.rst').read()
+
+setup(
+    name="dtool-azure",
+    packages=["dtool_azure"],
+    version=version,
+    description="Add Azure dataset support to dtool",
+    long_description=readme,
+    include_package_data=True,
+    author="Matthew Hartley",
+    author_email="Matthew.Hartley@jic.ac.uk",
+    url=url,
+    install_requires=[
+        "dtoolcore>=3.17",
+        "azure-storage"
+    ],
+    entry_points={
+        "dtool.storage_brokers": [
+            "AzureStorageBroker=dtool_azure.storagebroker:AzureStorageBroker",
+        ],
+    },
+    download_url="{}/tarball/{}".format(url, version),
+    license="MIT"
+)
