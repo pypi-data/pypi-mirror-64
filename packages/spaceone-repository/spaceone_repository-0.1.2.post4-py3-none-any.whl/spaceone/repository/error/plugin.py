26a7236100730c0ef5a5804ae31bb85ddf494ce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:36 +0000

    Add package spaceone-repository/0.1.2.post4/spaceone_repository-0.1.2.post4-py3-none-any.whl

---
+++ packages/spaceone-repository/spaceone_repository-0.1.2.post4-py3-none-any.whl/spaceone/repository/error/plugin.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from spaceone.core.error import *
+
+
+class ERROR_NO_PLUGIN(ERROR_BASE):
+    _message = 'Plugin: {plugin_id} does not exist.'
+
+
---
+++ packages/spaceone-repository/spaceone_repository-0.1.2.post4-py3-none-any.whl/spaceone/repository/error/repository.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from spaceone.core.error import *
+
+
+class ERROR_NO_REPOSITORY(ERROR_BASE):
+    _message = 'There is no Repository. count = 0'
+
+
