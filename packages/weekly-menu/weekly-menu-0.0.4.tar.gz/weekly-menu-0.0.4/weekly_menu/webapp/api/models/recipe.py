9411f2cc6089f531dfb1bb77ca303655838803f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:49 +0000

    Add package weekly-menu/0.0.4/weekly-menu-0.0.4.tar.gz

---
+++ packages/weekly-menu/weekly-menu-0.0.4.tar.gz/weekly-menu-0.0.4/weekly_menu/webapp/api/models/recipe.py
@@ -0,0 +1,35 @@
+from .. import mongo
+
+
+class RecipeIngredient(mongo.EmbeddedDocument):
+    quantity = mongo.FloatField()
+    unitOfMeasure = mongo.StringField(max_length=10)
+    required = mongo.BooleanField()
+    ingredient = mongo.ReferenceField('Ingredient', required=True)
+
+
+class Recipe(mongo.Document):
+    name = mongo.StringField(required=True)
+    description = mongo.StringField()
+    note = mongo.StringField()
+    availabilityMonths = mongo.ListField(mongo.IntField(
+        min_value=1, max_value=12), max_length=12, default=None)
+    ingredients = mongo.EmbeddedDocumentListField(
+        'RecipeIngredient', default=None)
+    servs = mongo.IntField(min_value=1)
+    estimatedCookingTime = mongo.IntField(min_value=1)
+    estimatedPreparationTime = mongo.IntField(min_value=1)
+    rating = mongo.IntField(min_value=1, max_value=5)
+    recipeUrl = mongo.StringField()
+    tags = mongo.ListField(
+        mongo.StringField(), default=None
+    )
+
+    owner = mongo.ReferenceField('User', required=True)
+
+    meta = {
+        'collection': 'recipes'
+    }
+
+    def __repr__(self):
+        return "<Recipe '{}'>".format(self.name)
---
+++ packages/weekly-menu/weekly-menu-0.0.4.tar.gz/weekly-menu-0.0.4/weekly_menu/webapp/api/v1/recipes/__init__.py
@@ -0,0 +1,23 @@
+from .. import BASE_PATH
+
+def create_module(app, api):
+    
+    from .resources import RecipeList, RecipeInstance, RecipeIngredientsList, RecipeIngredientInstance
+    
+    api.add_resource(
+        RecipeList,
+        BASE_PATH + '/recipes'
+    )
+    api.add_resource(
+        RecipeInstance,
+        BASE_PATH + '/recipes/<string:recipe_id>'
+    )
+
+    api.add_resource(
+        RecipeIngredientsList,
+        BASE_PATH + '/recipes/<string:recipe_id>/ingredients'
+    )
+    api.add_resource(
+        RecipeIngredientInstance,
+        BASE_PATH + '/recipes/<string:recipe_id>/ingredients/<string:ingredient_id>'
+    )
