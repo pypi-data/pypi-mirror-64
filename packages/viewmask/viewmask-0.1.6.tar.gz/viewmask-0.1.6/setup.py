a1425abd14ecb4e41fc2f4c44a859df380949548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:43 +0000

    Add package viewmask/0.1.6/viewmask-0.1.6.tar.gz

---
+++ packages/viewmask/viewmask-0.1.6.tar.gz/viewmask-0.1.6/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['viewmask']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Pillow>=7.0.0,<8.0.0',
+ 'click>=7.1.1,<8.0.0',
+ 'napari>=0.2.12,<0.3.0',
+ 'numpy>=1.18.1,<2.0.0',
+ 'opencv-python-headless>=4.2.0,<5.0.0']
+
+entry_points = \
+{'console_scripts': ['viewmask = viewmask.cli:cli']}
+
+setup_kwargs = {
+    'name': 'viewmask',
+    'version': '0.1.6',
+    'description': '',
+    'long_description': None,
+    'author': 'sumanthratna',
+    'author_email': 'sumanthratna@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
