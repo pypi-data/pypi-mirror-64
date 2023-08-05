6afcc4aba5a63124ec401367cf04cf97c7a7a541
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:27 +0000

    Add package supengsupermath/1.0/supengsupermath-1.0.tar.gz

---
+++ packages/supengsupermath/supengsupermath-1.0.tar.gz/supengsupermath-1.0/setup.py
@@ -0,0 +1,11 @@
+#coding=utf-8
+from distutils.core import setup
+
+setup(
+    name='supengsupermath',  # 对外我们模块的名字
+    version='1.0', # 版本号
+    description='这是第一个对外发布的模块，里面只有数学方法，用于测试哦',  #描述
+    author='gaoqi', # 作者
+    author_email='gaoqi110@163.com',
+    py_modules=['supengsupermath.demo1','supengsupermath.demo2'] # 要发布的模块
+)
