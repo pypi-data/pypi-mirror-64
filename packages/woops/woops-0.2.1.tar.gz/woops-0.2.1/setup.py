5363da52ad8880f3eb17ca3f4db332ce3bcb7b30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:44 +0000

    Add package woops/0.2.1/woops-0.2.1.tar.gz

---
+++ packages/woops/woops-0.2.1.tar.gz/woops-0.2.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['woops']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'woops',
+    'version': '0.2.1',
+    'description': 'Handle and manage Python errors with ease',
+    'long_description': '# Woops!\n\nHandle and manage Python errors with ease.\n',
+    'author': 'Sébastien Eustace',
+    'author_email': 'sebastien@eustace.io',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/sdispater/woops',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
