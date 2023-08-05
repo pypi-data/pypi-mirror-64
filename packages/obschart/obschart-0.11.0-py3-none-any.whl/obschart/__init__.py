fdd2b2c3123a709faaac0573cd48ce2848d74306
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:48 +0000

    Add package obschart/0.11.0/obschart-0.11.0-py3-none-any.whl

---
+++ packages/obschart/obschart-0.11.0-py3-none-any.whl/obschart/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+ObsChart API Wrapper
+"""
+
+__title__ = "obschart"
+__author__ = "Deepscope"
+
+from .obschart_client import ObschartClient
+from .api.nodes import *
