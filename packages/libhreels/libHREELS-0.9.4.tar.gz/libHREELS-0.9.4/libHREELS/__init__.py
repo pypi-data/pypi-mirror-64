2182deaa1e2c89b1cf96fcddea5bd3fce5e4fb49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:34 +0000

    Add package libhreels/0.9.4/libHREELS-0.9.4.tar.gz

---
+++ packages/libhreels/libHREELS-0.9.4.tar.gz/libHREELS-0.9.4/libHREELS/__init__.py
@@ -0,0 +1,19 @@
+# necessary for this directory to be a module
+# __init__.py
+
+from libHREELS.HREELS import HREELS
+from libHREELS.HREELS import offset, createDataDictionary, HREELS_elog_Dictionary
+from libHREELS.LEED import LEED, LEED_elog_Dictionary
+from libHREELS.Auger.Auger import Auger, Auger_elog_Dictionary
+from libHREELS.ViewHREELS import HREELS_Window
+import libHREELS.ViewHREELS as ViewHREELS
+import libHREELS.ViewAuger as ViewAuger
+
+from libHREELS.calcHREELS import importMaterials
+from libHREELS.calcHREELS import lambin
+import libHREELS.myEels2 as myEels2    # wrapper for myEels2.f90
+import libHREELS.myBoson as myBoson  # wrapper for myBoson.f90
+
+# __all__ = ["HREELS","LEED", "Auger/Auger", "ViewHREELS"]
+
+__version__ = '0.9.1'
