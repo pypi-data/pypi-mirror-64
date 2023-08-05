a74cf0ca39ec6371c499397ec12dbd2bbedec56b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:45 +0000

    Add package vectorslow/1.3/vectorslow-1.3.tar.gz

---
+++ packages/vectorslow/vectorslow-1.3.tar.gz/vectorslow-1.3/vectorslow/joke.py
@@ -0,0 +1,14 @@
+
+
+class Overload_Joke():
+    
+    def __init__(self):
+        self.phrase = "phrase"
+        
+    def pls_why_joke(self):
+        return ("Why this not work?")
+
+class Autism():
+
+    def brah(self):
+        return("wtf") 
