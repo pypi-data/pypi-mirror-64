235e41b3dafca23e5c8f5d1643fd02dc2a728bb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:41 +0000

    Add package xxh-xxh/0.6.7/xxh-xxh-0.6.7.tar.gz

---
+++ packages/xxh-xxh/xxh-xxh-0.6.7.tar.gz/xxh-xxh-0.6.7/xxh_xxh/settings.py
@@ -0,0 +1,20 @@
+import sys, os
+
+global_settings = {
+    'XXH_VERSION': '0.6.7'
+}
+
+if __name__ == "__main__":
+
+    for e in ['XXH_HOME', 'PIP_TARGET', 'PYTHONPATH']:
+        if e in os.environ:
+            global_settings[e] = os.environ[e]
+
+    if len(sys.argv) > 1:
+        setting_name = sys.argv[1]
+        if setting_name in global_settings:
+            print(global_settings[setting_name])
+        else:
+            sys.exit(1)
+    else:
+        print(global_settings)
