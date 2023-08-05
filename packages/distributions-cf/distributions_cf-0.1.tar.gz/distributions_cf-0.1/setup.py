f57ca25dfc6af7e985ff5f07fd8896702c16f8f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:01 +0000

    Add package distributions-cf/0.1/distributions_cf-0.1.tar.gz

---
+++ packages/distributions-cf/distributions_cf-0.1.tar.gz/distributions_cf-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_cf',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_cf'],
+      author='CF Khor',
+      author_email='khorcf@gmail.com',
+      zip_safe=False)
