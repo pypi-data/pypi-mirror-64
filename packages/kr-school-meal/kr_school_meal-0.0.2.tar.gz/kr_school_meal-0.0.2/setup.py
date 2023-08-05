95eb0de565c59fb05439fba8ab83370016a3e9ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:29 +0000

    Add package kr-school-meal/0.0.2/kr_school_meal-0.0.2.tar.gz

---
+++ packages/kr-school-meal/kr_school_meal-0.0.2.tar.gz/kr_school_meal-0.0.2/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding='UTF8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kr_school_meal",
+    version="0.0.2",
+    author="moseoridev",
+    author_email="sjssjs1344@gmail.com",
+    description="Korea School Meal",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/moseoridev/korea-school-meal",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Operating System :: OS Independent",
+        "Natural Language :: Korean"
+    ],
+    python_requires='>=3.6',
+)
