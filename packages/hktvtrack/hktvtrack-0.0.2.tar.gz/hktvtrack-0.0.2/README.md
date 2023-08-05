878694f44eb0fca776767bc35b81dcb997b9e3a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:59 +0000

    Add package hktvtrack/0.0.2/hktvtrack-0.0.2.tar.gz

---
+++ packages/hktvtrack/hktvtrack-0.0.2.tar.gz/hktvtrack-0.0.2/README.md
@@ -0,0 +1,21 @@
+                    Fung PUBLIC LICENSE
+                       Version 3, 19 November 2019
+
+Example:
+
+
+from hktvtrack import track
+
+print(track.getProductStatus('H0888001_S_P10021200'))
+
+*********************************************************
+Result [JSON]:
+
+{"name": "可口可樂 - [原箱] 罐裝可口可樂汽水", "isBuy": true}
+
+
+ Copyright (C) 2019 Fung Free Software Foundation, Inc. 
+ Everyone is permitted to copy and distribute verbatim copies
+ of this license document, but changing it is not allowed.
+
+
---
+++ packages/hktvtrack/hktvtrack-0.0.2.tar.gz/hktvtrack-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hktvtrack", # Replace with your own username
+    version="0.0.2",
+    author="fung",
+    author_email="fung20052006@gmail.com",
+    description="HktvTrack",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    install_requires=['beautifulsoup4'],
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7',
+)
