4ba9608b47130989c00e786210860b7b29b8e041
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:27 +0000

    Add package jijcloud/0.2003.2/jijcloud-0.2003.2.tar.gz

---
+++ packages/jijcloud/jijcloud-0.2003.2.tar.gz/jijcloud-0.2003.2/jijcloud/setting.py
@@ -0,0 +1,23 @@
+import toml
+
+
+def load_config(file_name: str, config='defalut') -> dict:
+    """load config file (TOML file)
+
+    Args:
+        file_name (str): path to config file.
+        config (str, optional): loading enviroment name. Defaults to 'defalut'.
+
+    Raises:
+        TypeError: if 'config' enviroment is not defined in config file.
+
+    Returns:
+        dict: {'token': 'xxxx', 'url': 'xxxx'} 
+    """
+    with open(file_name) as f:
+        toml_setting_file = toml.load(f)
+    if config not in toml_setting_file:
+        raise TypeError(
+            "'{}' is not define in config file ({}).".format(config, file_name))
+    return toml_setting_file
+
