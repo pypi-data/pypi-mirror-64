bf6ca7e64d30c86f8bf101f5d25ed22c387b31bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:18 +0000

    Add package serverD/0.1.6/serverD-0.1.6.tar.gz

---
+++ packages/serverD/serverD-0.1.6.tar.gz/serverD-0.1.6/serverD/conf.py
@@ -0,0 +1,14 @@
+pwd = ""
+root_pwd = ""
+tbj_pem = "序列号"
+
+
+server_dict = {
+    "vpc实例名称": {"host": "外网地址", "user": "用户名", "port": "端口号", "pwd": "密码"},
+    "tbj": {"host": "", "user": "", "port": "22", "pwd": "", "pem": tbj_pem},
+    "233": {"host": "", "user": "", "port": "22", "pwd": ""},
+}
+
+
+if __name__ == '__main__':
+    print(str(server_dict))
---
+++ packages/serverD/serverD-0.1.6.tar.gz/serverD-0.1.6/serverD/conf_demo.py
@@ -0,0 +1,10 @@
+pwd = ""
+root_pwd = ""
+tbj_pem = "pem文件路径"
+
+
+server_dict = {
+    "vpc实例名称": {"host": "外网地址", "user": "用户名", "port": "端口号", "pwd": "密码"},
+    "tbj": {"host": "", "user": "", "port": "22", "pwd": "", "pem": tbj_pem},
+    "233": {"host": "", "user": "", "port": "22", "pwd": ""},
+}
