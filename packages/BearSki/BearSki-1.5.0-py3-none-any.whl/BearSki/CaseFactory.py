7733104f906b7631155c248cad13d4fdae223367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:27:44 +0000

    Add package BearSki/1.5.0/BearSki-1.5.0-py3-none-any.whl

---
+++ packages/BearSki/BearSki-1.5.0-py3-none-any.whl/BearSki/CaseFactory.py
@@ -0,0 +1,14 @@
+from BearSki.case.TestRunnerSet import *
+import logging
+logger=logging.getLogger("BearSki.RunUnittest")
+
+
+
+class SkiTestFactory():
+
+    def run(self,args,commond):
+        ts = eval(args.testsuit_runner)(args,commond)
+        logger.info("Creating TestSuit..{0}".format(type(ts).__name__))
+        logger.info("Commond List: {}".format(ts.getCommond()))
+        logger.info("RunArguments List: {}".format(ts.getRunArg()))
+        ts.runTest()
