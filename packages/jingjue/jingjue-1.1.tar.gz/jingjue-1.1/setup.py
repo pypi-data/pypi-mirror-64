9d050d99d8113edde06ef8fa2e1b553ab5e95778
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:32 +0000

    Add package jingjue/1.1/jingjue-1.1.tar.gz

---
+++ packages/jingjue/jingjue-1.1.tar.gz/jingjue-1.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools 
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+   
+setuptools.setup(
+    name="jingjue",
+    version="1.1",
+    author="Ken Tang",
+    author_email="kinyeah@gmail.com",
+    install_requires=[            
+      ],
+	description= "Jingjue is an ancient Chinese divination method similar as ichingshifa found from a bunch of bamboo slips relics collected by Peking University",
+	long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kentang2017/jingjue",
+	packages=setuptools.find_packages(),
+	package_data = {},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
