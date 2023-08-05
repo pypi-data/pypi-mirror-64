e8bb29db924e88458869081bd4b41cc029f81ade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:41 +0000

    Add package cohesivenet/0.1.28/cohesivenet-0.1.28-py3-none-any.whl

---
+++ packages/cohesivenet/cohesivenet-0.1.28-py3-none-any.whl/cohesivenet/version.py
@@ -0,0 +1,6 @@
+VERSION = "0.1.28"
+LATEST_VNS3_VERSION = "4.8.4"
+LATEST_VNS3_API_SPEC = (
+    "https://cohesive-networks.s3.amazonaws.com/apis/vns3/vns3-v%s.oasv3.json"
+    % LATEST_VNS3_VERSION.replace(".", "-")
+)
