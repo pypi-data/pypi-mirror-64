368b65294b0f315a0dfd6e98bb8da8c7cf779570
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:17 +0000

    Add package despair/0.0.1/despair-0.0.1.tar.gz

---
+++ packages/despair/despair-0.0.1.tar.gz/despair-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name = 'despair',
+    version = '0.0.1',
+    
+    author = 'selfquery',
+    author_email = 'pypi@selfquery.dev',
+    
+    license='MIT',
+    
+    url = 'https://github.com/selfquery/despair',
+    install_requires=[],
+    
+    py_modules = ['despair'],
+    scripts = ['despair.py']
+)
