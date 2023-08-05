5aa7cf8f4a98ecbec1f09ba130621c9888a4601e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:46 +0000

    Add package ZabbixQywechat/1.0.1/ZabbixQywechat-1.0.1.tar.gz

---
+++ packages/ZabbixQywechat/ZabbixQywechat-1.0.1.tar.gz/ZabbixQywechat-1.0.1/README.md
@@ -0,0 +1,1 @@
+This is a package for sending messages to enterprise WeChat applications in zabbix. You can use [Github](https://github.com/mikecui426/ZabbixQywechat)
---
+++ packages/ZabbixQywechat/ZabbixQywechat-1.0.1.tar.gz/ZabbixQywechat-1.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ZabbixQywechat", # Replace with your own username
+    version="1.0.1",
+    author="mike",
+    author_email="441616837@qq.com",
+    description="This is a package for sending messages to enterprise WeChat applications in zabbix",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mikecui426/ZabbixQywechat",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.6',
+)
