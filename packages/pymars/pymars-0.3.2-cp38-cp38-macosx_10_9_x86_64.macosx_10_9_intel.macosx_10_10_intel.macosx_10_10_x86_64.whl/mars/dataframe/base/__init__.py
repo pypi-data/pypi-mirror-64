c2dd07065e156986bdd2caae31e567cf5f0b3a67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:23 +0000

    Add package pymars/0.3.2/pymars-0.3.2-cp38-cp38-macosx_10_9_x86_64.macosx_10_9_intel.macosx_10_10_intel.macosx_10_10_x86_64.whl

---
+++ packages/pymars/pymars-0.3.2-cp38-cp38-macosx_10_9_x86_64.macosx_10_9_intel.macosx_10_10_intel.macosx_10_10_x86_64.whl/mars/dataframe/base/__init__.py
@@ -0,0 +1,53 @@
+# Copyright 1999-2020 Alibaba Group Holding Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from .to_gpu import to_gpu
+from .to_cpu import to_cpu
+from .rechunk import rechunk
+from .reset_index import df_reset_index, series_reset_index
+from .describe import describe
+from .fillna import fillna, ffill, bfill
+from .string_ import SeriesStringMethod
+from .datetimes import SeriesDatetimeMethod
+
+
+def _install():
+    from ..core import DATAFRAME_TYPE, SERIES_TYPE, INDEX_TYPE
+
+    for t in DATAFRAME_TYPE:
+        setattr(t, 'to_gpu', to_gpu)
+        setattr(t, 'to_cpu', to_cpu)
+        setattr(t, 'rechunk', rechunk)
+        setattr(t, 'reset_index', df_reset_index)
+        setattr(t, 'describe', describe)
+        setattr(t, 'fillna', fillna)
+        setattr(t, 'ffill', ffill)
+        setattr(t, 'bfill', bfill)
+    for t in SERIES_TYPE:
+        setattr(t, 'to_gpu', to_gpu)
+        setattr(t, 'to_cpu', to_cpu)
+        setattr(t, 'rechunk', rechunk)
+        setattr(t, 'reset_index', series_reset_index)
+        setattr(t, 'describe', describe)
+        setattr(t, 'fillna', fillna)
+        setattr(t, 'ffill', ffill)
+        setattr(t, 'bfill', bfill)
+    for t in INDEX_TYPE:
+        setattr(t, 'rechunk', rechunk)
+
+
+_install()
+del _install
+del SeriesStringMethod
+del SeriesDatetimeMethod
