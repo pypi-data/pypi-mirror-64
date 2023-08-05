0eb80e82a39cef73f3958959e272587870b5e297
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:21 +0000

    Add package kt17py/0.0.1a1/kt17py-0.0.1a1.tar.gz

---
+++ packages/kt17py/kt17py-0.0.1a1.tar.gz/kt17py-0.0.1a1/README.md
@@ -0,0 +1,6 @@
+# kt17py
+
+A Python wrapper to the FORTRAN code for the KT17 dynamic magnetic field model for Mercury's magnetosphere.
+
+See Korth, H., Johnson, C. L., Philpott, L.,Tsyganenko, N. A., & Anderson, B. J.(2017). A dynamic model of Mercury’s
+magnetospheric magnetic field. Geophysical Research Letters, 44. https://doi.org/10.1002/2017GL074699
---
+++ packages/kt17py/kt17py-0.0.1a1.tar.gz/kt17py-0.0.1a1/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+try:
+    from numpy.distutils.core import Extension
+    from numpy.distutils.core import setup
+except ImportError:
+    raise NoNumpy('Numpy Needs to be installed '
+                  'for extensions to be compiled.')
+
+class NoNumpy(Exception):
+    pass
+
+if __name__ == "__main__":
+    with open("README.md", "r") as fh:
+        long_description = fh.read()
+
+    setup(name="kt17py", 
+        version="0.0.1a1",
+        author="Inaki Ortiz de Landaluce",
+        author_email="inaki.ortizdelandaluce@gmail.com",
+        description="A Python wrapper to the FORTRAN code for the KT17 dynamic magnetic field model for Mercury's magnetosphere",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/inaki-ortizdelandaluce/kt17py",
+        packages=setuptools.find_packages(),
+        ext_modules=[Extension(name = 'kt17',
+                sources = ['kt17py/kt17.pyf', 'kt17py/kt17/kt17.f90'])],
+        license="MIT",
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+        ],
+        install_requires=['numpy>=1.16'],
+        python_requires='>=2.7',
+        )
