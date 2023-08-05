d4a8bbb46ae37b63deae9a313143ed24536c3095
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:17 +0000

    Add package dash-resumable-upload-bb/0.0.5/dash_resumable_upload_bb-0.0.5.tar.gz

---
+++ packages/dash-resumable-upload-bb/dash_resumable_upload_bb-0.0.5.tar.gz/dash_resumable_upload_bb-0.0.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+exec (open('dash_resumable_upload_bb/version.py').read())
+
+
+setup(
+    name='dash_resumable_upload_bb',
+    version=__version__,
+    author='marrenr',
+    packages=['dash_resumable_upload_bb'],
+    include_package_data=True,
+    license='MIT',
+    description='Dash Resumable Upload component for large files.',
+    install_requires=[],
+    download_url='https://github.com/den-mi/dash-resumable-upload-bb/archive/0.0.5.tar.gz'
+)
