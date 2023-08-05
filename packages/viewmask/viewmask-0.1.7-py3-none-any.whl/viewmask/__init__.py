bd7c8f7371341cd0c7cc0fe6ea87afcacdf0059d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:51 +0000

    Add package viewmask/0.1.7/viewmask-0.1.7-py3-none-any.whl

---
+++ packages/viewmask/viewmask-0.1.7-py3-none-any.whl/viewmask/__init__.py
@@ -0,0 +1,15 @@
+__version__ = '0.1.7'
+
+
+from viewmask.utils import (xml_to_contours,
+                            centers_of_contours,
+                            xml_to_image,
+                            get_stroke_color,
+                            mask_to_contours,
+                            centers_to_image)
+__all__ = ['xml_to_contours',
+           'centers_of_contours',
+           'xml_to_image',
+           'get_stroke_color',
+           'mask_to_contours',
+           'centers_to_image']
