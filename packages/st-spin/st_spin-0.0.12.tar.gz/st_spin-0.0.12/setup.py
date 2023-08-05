782b2f267da682323847d094813b22b636fa7e0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:15 +0000

    Add package st-spin/0.0.12/st_spin-0.0.12.tar.gz

---
+++ packages/st-spin/st_spin-0.0.12.tar.gz/st_spin-0.0.12/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+      name='st_spin',
+      version='0.0.12',
+      url='https://github.com/m-laniakea/st_spin',
+      license='GPLv1',
+      author='eir',
+      author_email='m-laniakea@users.noreply.github.com',
+      description='Interface for ST SpinFamily motor drivers',
+      packages=find_packages(exclude=['tests']),
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
+      zip_safe=False)
