4b72d775d7c3dab1a19396939dce4ac9e7a434f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:48 +0000

    Add package co2Estimator/0.1.1/co2Estimator-0.1.1.tar.gz

---
+++ packages/co2Estimator/co2Estimator-0.1.1.tar.gz/co2Estimator-0.1.1/co2Estimator/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+__author__ = """Mayank Bhandari"""
+__email__ = 'bhandarimayank01@gmail.com'
+__version__ = '0.2.0'
---
+++ packages/co2Estimator/co2Estimator-0.1.1.tar.gz/co2Estimator-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+    
+setup(
+    name='co2Estimator',
+    version='0.1.1',
+    packages=['co2Estimator',],
+    license='MIT license',
+    url = 'https://github.com/mayank-siingh/CO2Estimator/',
+    author = 'Mayank Bhandari',
+	author_email = 'bhandarimayank01@gmail.com'
+)
+
+
+
+
+	
