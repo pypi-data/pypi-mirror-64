b6b84f95641c56c0a5d54b89e23126df909c9fc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:15 +0000

    Add package packagerit/0.1/packagerit-0.1.tar.gz

---
+++ packages/packagerit/packagerit-0.1.tar.gz/packagerit-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup 
+setup(name = "packagerit",
+version = "0.1",
+description= "this package packages Rit",
+author= "Rit Chowdhury",
+packages=["packagerit"],
+install_requires=[])
