62b17c9501c29b20ffe44deafbe9a42c4f7d674d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:09 +0000

    Add package arize/0.0.2/arize-0.0.2.tar.gz

---
+++ packages/arize/arize-0.0.2.tar.gz/arize-0.0.2/arize/examples/client.py
@@ -0,0 +1,26 @@
+import random
+import time
+import functools 
+
+from arize.api import Client
+
+ITERATIONS = 10
+CALLS = 10
+times = []
+
+arize = Client(account_id=0, api_key='0000', uri='https://dev.arize.com/v1/log')
+
+for j in range(ITERATIONS) :
+    start = time.time() * 1000
+    for i in range(CALLS):
+        arize.log(
+            model_id=str(j),
+            prediction_id=str(i),
+            prediction_value=True,
+            labels={'label_key':'label_value', 'label_key1': 'label_value1'}
+        )
+    end = time.time() * 1000
+    times.append((end-start))
+    print('{}th iteration took {}ms per request'.format(j, (end-start)/CALLS))
+
+print('Total itertions took on average {}ms'.format(functools.reduce(lambda a,b: a+b, times)/ITERATIONS))
