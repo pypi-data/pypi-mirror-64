8dd99919142d8475a41b33104bc6e41e5aa7ad24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:00 +0000

    Add package ponomarenko/0.0.4.post1/ponomarenko-0.0.4.post1.tar.gz

---
+++ packages/ponomarenko/ponomarenko-0.0.4.post1.tar.gz/ponomarenko-0.0.4.post1/__about__.py
@@ -0,0 +1,12 @@
+VERSION = (
+    0,
+    0,
+    4,
+)
+
+__title__ = 'ponomarenko'
+__description__ = "Python bindings for M. Colom's IPOL Ponomarenko implementation"
+__url__ = 'https://github.com/cmla/ponomarenko.git'
+__version__ = '.'.join(map(str, VERSION)) + '-post1'
+__author__ = "Charles Hessel"
+__author_email__ = 'charles.hessel@ens-paris-saclay.fr'
