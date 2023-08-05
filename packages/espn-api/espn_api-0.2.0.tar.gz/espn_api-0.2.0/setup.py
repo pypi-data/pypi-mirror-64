a166d0604e7f7d0e427201005e1c3e41248d684c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:30:27 +0000

    Add package espn-api/0.2.0/espn_api-0.2.0.tar.gz

---
+++ packages/espn-api/espn_api-0.2.0.tar.gz/espn_api-0.2.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='espn_api',
+    packages=['espn_api'],
+    version='0.2.0',
+    author='Christian Wendt',
+    description='ESPN API',
+    install_requires=['requests>=2.0.0,<3.0.0'],
+    setup_requires=['nose>=1.0'],
+    test_suite='nose.collector',
+    tests_require=['nose', 'requests_mock', 'coverage'],
+    url='https://github.com/cwendt94/ff-espn-api',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+
+)
