b6a9b3f275f7aeec64036f1da980e0acea432ed5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:07 +0000

    Add package contractpy/0.1.1/contractpy-0.1.1.tar.gz

---
+++ packages/contractpy/contractpy-0.1.1.tar.gz/contractpy-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='contractpy',
+    packages=find_packages(),
+    version='0.1.1',
+    license='MIT',
+    description='A tiny library for validating our data if its adhere the contract.',
+    author='Manoj Kumar S',
+    author_email='kumarmanoj1158@gmail.com',
+    url='https://github.com/KumarManoj-S/contractpy',
+    download_url='https://github.com/KumarManoj-S/contractpy/archive/0.1.1.tar.gz',
+    keywords=['contract', 'contract testing', 'template', 'validate'],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
