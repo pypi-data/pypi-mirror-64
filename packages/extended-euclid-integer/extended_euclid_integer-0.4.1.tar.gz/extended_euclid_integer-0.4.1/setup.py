85dba262f6d01eb43ed9a51136e48eebce46ac90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:03 +0000

    Add package extended-euclid-integer/0.4.1/extended_euclid_integer-0.4.1.tar.gz

---
+++ packages/extended-euclid-integer/extended_euclid_integer-0.4.1.tar.gz/extended_euclid_integer-0.4.1/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+  name = 'extended_euclid_integer',                                                     #название библиотеки
+  packages = ['extended_euclid_integer'],                                               #имя пакета, расположение файлов
+  version = '0.4.1',                                                                      #версия
+  license='MIT',                                                                        #кодовое название лицензии
+  description = 'modular inverse',                                                      #описание
+  author = 'Vova Nenashkin',                                                            #автор
+  author_email = 'nenashkinvov@gmail.com',                                              #почта
+  url = 'https://github.com/SuLG-ik/Euclid-Algorithm',                                  #ссылка на github проект
+  download_url = 'https://github.com/SuLG-ik/Euclid-Algorithm/archive/master.zip',      #ссылка для скачивания с github
+  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],                                       #ключевые слова  
+  classifiers=[
+    'Development Status :: 3 - Alpha',                                                  #статус библиотеки
+    'Intended Audience :: Developers',                                                  #целевая аудитория
+    'Topic :: Software Development :: Build Tools',                                     #тема
+    'License :: OSI Approved :: MIT License',                                           #лицензия
+    'Programming Language :: Python :: 3',                                              #язык программирования
+  ],
+)
