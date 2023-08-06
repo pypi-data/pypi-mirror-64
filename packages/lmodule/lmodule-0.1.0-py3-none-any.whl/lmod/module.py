c600a68302c74cc4baee774a759d7f84e1c7252f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:54 +0000

    Add package lmodule/0.1.0/lmodule-0.1.0-py3-none-any.whl

---
+++ packages/lmodule/lmodule-0.1.0-py3-none-any.whl/lmod/module.py
@@ -0,0 +1,325 @@
+import os
+import subprocess
+
+
+def get_user_collections():
+    """Get all user collections that is retrieved by running ``module -t savelist``. The output
+       is of type ``list`` and each entry in list is the name of the user collection.
+
+       :return: Return all user collections
+       :rtype: list
+     """
+
+    collections = "module -t savelist"
+    ret = subprocess.run(
+        collections,
+        shell=True,
+        encoding="utf-8",
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+    )
+    output = ret.stdout.split()
+
+    return output
+
+
+class Module:
+    """This is the class declaration of Module class which emulates the ``module`` command
+       provided by Lmod
+
+       Public Methods:
+
+       avail: implements ``module avail`` option
+       version: gets Lmod version by reading LMOD_VERSION
+       is_avail: implements ``module is-avail`` option
+       get_command: gets the ``module load`` command based on modules passed to the class
+       test_modules: test the ``module load`` command based on modules passed to the class
+       save: implements ``module save`` option
+       describe: implements ``module describe`` option
+       get_collection: gets the ``module restore`` command with the specified collection name
+       test_collection: test the ``module restore`` command with the specified collection name
+    """
+
+    def __init__(self, modules=None, purge=True, force=False, debug=False):
+        """Initialize method for Module class. This class can accept module names that
+           can be used to load or test modules. You can tweak the behavior of how module
+           command is generated such as purging of force purge modules. The debug option
+           can be useful for troubleshooting.
+
+           Parameters:
+
+           :param modules: list of modules
+           :type modules: list, optional
+           :param purge: boolean to control whether to purge modules before loading
+           :type purge: bool, optional
+           :param force: boolean to control whether to force purge modules before loading
+           :type force: bool, optional
+           :param debug: debug mode for troubleshooting
+           :type debug: bool, optional
+        """
+
+        self.debug = debug
+        self.modules = modules
+
+        # when no modules are passed into initializer, just return immediately
+        if self.modules is None:
+            return
+
+        # catch all exceptions to argument modules. Must be of type list or string.
+        if (not isinstance(modules, list)) and (not isinstance(modules, str)):
+            raise TypeError(
+                f"Expecting of type 'list' or 'string' for argument modules. Got of type {type(modules)}"
+            )
+
+        # if argument is a string, than use space as delimeter to get list of all modules.
+        if isinstance(modules, str):
+            self.modules = modules.split(" ")
+
+        # building actual command. Note that we are doing command chaining when loading modules
+        self.module_load_cmd = [f"module load {x} && " for x in self.modules]
+
+        # remove last '&&' from module load command
+        self.module_load_cmd[-1] = self.module_load_cmd[-1].replace("&&", "")
+
+        if purge:
+            # force purge modules before loading modules. Used when dealing with sticky modules
+            if force:
+                self.module_load_cmd = [
+                    "module --force purge && "
+                ] + self.module_load_cmd
+            # purge modules before loading modules.
+            else:
+                self.module_load_cmd = ["module purge &&"] + self.module_load_cmd
+
+    def avail(self, name=None):
+        """This method implements the ``module avail`` command. The output of module avail will return available
+           modules in the system. The output is returned as a list using the ``module -t avail`` which presents the
+           output in a single line per module.
+
+           Parameters:
+
+           :param name: argument passed to ``module avail``. This is used for showing what modules are available
+           :type name: str, optional
+
+           :return: Return output of ``module avail`` as a list
+           :rtype: list
+        """
+
+        cmd = "module -t avail"
+        # if argument specified
+        if name:
+            cmd = f"module -t avail {name}"
+        print(cmd)
+        ret = subprocess.run(
+            cmd,
+            shell=True,
+            encoding="utf-8",
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+        )
+
+        return ret.stdout.split()
+
+    def version(self):
+        """Get Lmod version by reading environment variable ``LMOD_VERSION`` and return as a string
+
+           :return: Return the Lmod version as a string
+           :rtype: str
+        """
+
+        return os.getenv("LMOD_VERSION") or None
+
+    def is_avail(self, name):
+        """This method implements the ``module is-avail`` command which is used for checking if a module is available
+           before loading it. The return value is a 0 or 1.
+
+           Parameters:
+
+           :param name: argument passed to ``module is-avail``. This is used for checking if module is available
+           :type name: str, required
+
+           :return: Return output of ``module is-avail``. This checks if module is available and return code is a 0 or 1
+           :rtype: int
+        """
+
+        cmd = f"module is-avail {name}"
+
+        ret = subprocess.run(
+            cmd,
+            shell=True,
+            encoding="utf-8",
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+        )
+
+        return ret.returncode
+
+    def get_command(self):
+        """Get the actual module load command that can be used to load the given modules.
+
+           :return: return the actual module load command
+           :rtype: str
+        """
+
+        return " ".join(self.module_load_cmd)
+
+    def test_modules(self, login=False):
+        """Test all modules passed to Module class by loading them using ``module load``. The default behavior
+           is to run the command in a sub-shell but this can be changed to run in a new login shell if ``login=True`` is
+           specified. The return value is a return code (type ``int``) of the ``module load`` command.
+
+           Parameters:
+
+           :param login: When ``login=True`` is set, it will run the test in a login shell, the default is to run in a sub-shell
+           :type login: bool, optional
+
+           :return: return code of ``module load`` command
+           :rtype: int
+        """
+
+        cmd_executed = self.get_command()
+
+        # run test in login shell
+        if login:
+            cmd_executed = 'bash -l -c "' + cmd_executed + '"'
+
+        ret = subprocess.run(
+            cmd_executed,
+            shell=True,
+            encoding="utf-8",
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+        )
+
+        # print executed command for debugging
+        if self.debug:
+            print(f"[DEBUG] Executing module command: {cmd_executed}")
+            print(f"[DEBUG] Return Code: {ret.returncode}")
+
+        return ret.returncode
+
+    def save(self, collection="default"):
+        """Save modules specified in Module class into a user collection. This implements the ``module save`` command
+           for active modules. In this case, we are saving modules that were passed to the Module class. If no argument
+           is specified, we will save to ``default`` collection, but user can specify a collection name, in that case
+           we are running ``module save <collection>``. The collection name must be of type ``str`` in order for
+           this to work, otherwise an exception of ``TypeError`` will be raised.
+
+           Parameters:
+
+           :param collection: collection name to save modules. If none specified, ``default`` is the collection.
+           :type collection: str, optional
+        """
+
+        # raise TypeError exception if collection is not a string type since that is required
+        # when working with module collection
+        if not isinstance(collection, str):
+            raise TypeError(f"Type Error: {collection} is not of type string")
+
+        module_save_cmd = f"{self.get_command()} && module save {collection}"
+
+        ret = subprocess.run(
+            module_save_cmd,
+            shell=True,
+            encoding="utf-8",
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+        )
+
+        # print executed command for debugging
+        if self.debug:
+            print(f"[DEBUG] Executing module command: {module_save_cmd}")
+            print(f"[DEBUG] Return Code: {ret.returncode}")
+
+        print(f"Saving modules {self.modules} to module collection name: {collection}")
+        print(ret.stdout)
+
+    def describe(self, collection="default"):
+        """Show content of a user collection and implements the command ``module describe``. By default, if no argument
+           is specified it will resort to showing contents of ``default`` collection. One can pass a collection name
+           which must be of type ``str`` that is the user collection name. Internally it will run ``module describe <collection>``.
+           If collection name is not of type ``str``, then an exception of ``TypeError`` will be raised.
+
+           Parameters:
+
+           :param collection: name of user collection to show.
+           :type collection: str, optional
+        """
+
+        # raise TypeError exception if collection is not a string type since that is required
+        # when working with module collection
+        if not isinstance(collection, str):
+            raise TypeError(f"Type Error: {collection} is not of type string")
+
+        module_describe_cmd = f"module describe {collection}"
+        ret = subprocess.run(
+            module_describe_cmd,
+            shell=True,
+            encoding="utf-8",
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+        )
+
+        # print executed command for debugging
+        if self.debug:
+            print(f"[DEBUG] Executing module command: {module_describe_cmd}")
+            print(f"[DEBUG] Return Code: {ret.returncode}")
+
+        print(ret.stdout)
+
+    def get_collection(self, collection="default"):
+        """Return the module command to restore a collection. If no argument is specified, it will resort to the ``default``
+           collection, otherwise one can specify a collection name of type ``str``. The output will be of type ``str``
+           such as ``module restore default``. If argument to class is not of type ``str`` then an exception of type
+           ``TypeError`` will be raised.
+
+           Parameters:
+
+           :param collection: collection name to restore
+           :type collection: str, optional
+
+           :return: return the ``module restore`` command with the collection name
+           :rtype: str
+        """
+
+        # raise error if collection is not a string
+        if not isinstance(collection, str):
+            raise TypeError(f"Type Error: {collection} is not of type string")
+
+        return f"module restore {collection}"
+
+    def test_collection(self, collection="default"):
+        """Test the user collection by running ``module restore`` against a collection name. This is useful, to test a
+           user collection is working before using it in your scripts. If no argument is specified, it will test the
+           ``default`` collection. One can specify a user collection name which must of be of type ``str`` and it must
+           exist. The output will be a return code of the ``module restore`` command which would be of type ``int``.
+           If argument to method is not of type ``str`` an exception of ``TypeError`` will be raised.
+
+           Parameters:
+
+           :param collection: collection name to test
+           :type collection: str, optional
+
+           :return: return code of ``module restore`` against the collection name
+           :rtype: int
+        """
+
+        # raise error if collection is not a string
+        if not isinstance(collection, str):
+            raise TypeError(f"Type Error: {collection} is not of type string")
+
+        module_restore_cmd = f"module restore {collection}"
+        ret = subprocess.run(
+            module_restore_cmd,
+            shell=True,
+            encoding="utf-8",
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+        )
+
+        # print executed command for debugging
+        if self.debug:
+            print(f"[DEBUG] Executing command: {module_restore_cmd}")
+            print(f"[DEBUG] Return Code: {ret.returncode}")
+
+        return ret.returncode
---
+++ packages/lmodule/lmodule-0.1.0-py3-none-any.whl/lmod/moduleloadtest.py
@@ -0,0 +1,117 @@
+import os
+from lmod.module import Module
+from lmod.spider import Spider
+
+
+class ModuleLoadTest:
+    """This is the class declaration of ModuleLoadTest. This class will automate module load
+       test for all modules in one or more module tree (Software Stack) retrieved by Spider class. The output
+       of ``module load`` is a 0 or 1 that can be used to determine PASS/FAIL on a module. Each module test will attempt
+       to test the ``module load`` command using the ``Module().test_modules()``. In order to run tests properly,
+       MODULEPATH must be set in your environment.
+    """
+
+    def __init__(
+        self,
+        tree=None,
+        purge=True,
+        force=False,
+        debug=False,
+        count=999999999,
+        name=[],
+        include=[],
+        exclude=[],
+        login=False,
+    ):
+        """This is the initializer method for ModuleLoadTest class.
+
+           Parameters:
+           -----------
+           :param tree: specify one or more module trees to test. The module tree must be root directory where modulefiles
+            are found. Use a colon ``:`` to define more than one module tree.
+           :type tree: str
+
+           :param purge: control whether to run ``module purge`` before loading each module
+           :type purge: bool
+
+           :param force: control whether to run ``module --force purge`` before loading each module
+           :type purge: bool
+
+           :param login: controls whether to run test in login shell when ``login=True``. By default tests are run in sub-shell.
+           :type purge: bool
+
+           :param count: control how many tests to run before exiting
+           :type purge: int
+
+           :param name: filter modules by software name to test
+           :type name: list
+
+           :param include: specify a list of modules to **include** by full canonical name for testing
+           :type purge: list
+
+           :param exclude: specify a list of modules to **exclude** by full canonical name for testing
+           :type purge: list
+
+           :return: Result of module load test
+           :rtype: None
+        """
+
+        # setting module tree to argument passed in or default to MODULEPATH
+        self.tree = tree or os.getenv("MODULEPATH")
+        self.debug = debug
+        self.purge = purge
+        self.force = force
+        self.login = login
+        self.count = count
+        self.name = name
+        self.include = include
+        self.exclude = exclude
+        filter_modules = None
+
+        spider_cmd = Spider(self.tree)
+        module_dict, modules = (
+            spider_cmd.get_modules(self.name),
+            list(spider_cmd.get_modules(self.name).values()),
+        )
+
+        if self.include:
+            filter_modules = set(self.include).intersection(modules)
+
+        # only do exclusion if include list is not specified
+        if self.exclude and not self.include:
+            filter_modules = set(modules).difference(self.exclude)
+
+        modules = filter_modules or modules
+
+        modulecount = 0
+
+        print(f"Testing the Following Module Trees: {self.tree}")
+        print("{:_<80}".format(""))
+
+        for module_name in modules:
+            module_cmd = Module(
+                module_name, purge=self.purge, force=self.force, debug=self.debug,
+            )
+            ret = module_cmd.test_modules(self.login)
+
+            # extract modulefile based on module name. This is basically getting the key from dictionary (module_dict)
+            # This is only used for printing purposes since it helps to know which module is tested. Simply putting
+            # the full module canonical name is not enough.
+            modulefile = list(module_dict.keys())[
+                list(module_dict.values()).index(module_name)
+            ]
+
+            if ret == 0:
+                print(
+                    f"PASSED -  Module Name: {module_name} ( modulefile={modulefile} )"
+                )
+            else:
+                print(
+                    f"FAILED -  Module Name: {module_name} ( modulefile={modulefile} )"
+                )
+
+            modulecount += 1
+
+            # terminate module load test once we have tested up to specified count
+            if self.count <= modulecount:
+                return
