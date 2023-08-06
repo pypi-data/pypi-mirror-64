72c91082334c1566e634fb0860643606731f0aa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:55 +0000

    Add package tully/0.0.3/tully-0.0.3.tar.gz

---
+++ packages/tully/tully-0.0.3.tar.gz/tully-0.0.3/tully/network.py
@@ -0,0 +1,39 @@
+from typing import List
+
+import socket
+import sys
+import subprocess
+
+
+def ip():
+    """Get IP address from default network route."""
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = s.getsockname()[0]
+    s.close()
+    return ip
+
+
+def nics() -> List[str]:
+    """Information on installed network interface cards."""
+    output = []
+
+    if sys.platform == 'linux':
+        try:
+            # first we try ip addr command
+            out = subprocess.Popen(["ip", "addr"],
+                                   stdout=subprocess.PIPE)
+            stdout, stderr = out.communicate()
+            output = stdout.decode('utf-8').split("\n")
+        except FileNotFoundError:
+            # ip addr command failed so lets try ifconfig
+            out = subprocess.Popen("ifconfig",
+                                   stdout=subprocess.PIPE)
+            stdout, stderr = out.communicate()
+            output = stdout.decode('utf-8').split("\n")
+    elif sys.platform == 'darwin':
+        return subprocess.call('ifconfig')
+    elif sys.platform == 'win32':
+        return subprocess.call('ipconfig')
+
+    return output
