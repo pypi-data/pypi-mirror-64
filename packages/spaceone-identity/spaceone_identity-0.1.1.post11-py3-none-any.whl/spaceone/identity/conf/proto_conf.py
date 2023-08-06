e15ed74bb5981f044a85e2c5a43348132b54bbb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:58 +0000

    Add package spaceone-identity/0.1.1.post11/spaceone_identity-0.1.1.post11-py3-none-any.whl

---
+++ packages/spaceone-identity/spaceone_identity-0.1.1.post11-py3-none-any.whl/spaceone/identity/conf/proto_conf.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+PROTO = {
+    'v1': {
+        'domain': ['Domain'],
+        'domain_owner': ['DomainOwner'],
+        'project_group': ['ProjectGroup'],
+        'project': ['Project'],
+        'policy': ['Policy'],
+        'role': ['Role'],
+        'user': ['User'],
+        'api_key': ['APIKey'],
+        'token': ['Token'],
+        'authorization': ['Authorization'],
+    }
+}
