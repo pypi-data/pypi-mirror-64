6b3486eda0fa4cad6997d846bb0ef17abd74f79e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:18 +0000

    Add package xvg/2020.3.29.0/xvg-2020.3.29.0-py2-none-any.whl

---
+++ packages/xvg/xvg-2020.3.29.0-py2-none-any.whl/xvg/core/context.py
@@ -0,0 +1,16 @@
+class Context():
+
+    def __init__(self):
+        pass
+
+    def setSize(self, *args):
+        pass
+
+    def setView(self, *args):
+        pass
+
+    def addText(self, *args):
+        pass
+
+    def addPath(self, *args):
+        pass
