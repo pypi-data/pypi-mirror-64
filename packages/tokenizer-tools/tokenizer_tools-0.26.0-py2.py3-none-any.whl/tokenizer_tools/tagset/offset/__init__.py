260af81ceaa6e3d187f97e9fb8a31e6fb789b4b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:05 +0000

    Add package tokenizer-tools/0.26.0/tokenizer_tools-0.26.0-py2.py3-none-any.whl

---
+++ packages/tokenizer-tools/tokenizer_tools-0.26.0-py2.py3-none-any.whl/tokenizer_tools/tagset/offset/__init__.py
@@ -0,0 +1,7 @@
+from .span import Span
+from .span_set import SpanSet
+from .document import Document
+from .corpus import Corpus
+from .analysis.entity_placeholder import EntityPlaceholder
+from .analysis.document_pattern import DocumentPattern
+from .analysis.corpus_pattern import CorpusPattern
