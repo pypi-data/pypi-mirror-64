2d7a2a4a1a5992892f97c60971bc02cf37b388ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:53 +0000

    Add package direct-redis/0.1.7/direct_redis-0.1.7-py3-none-any.whl

---
+++ packages/direct-redis/direct_redis-0.1.7-py3-none-any.whl/direct_redis/direct_redis.py
@@ -0,0 +1,41 @@
+from redis import Redis
+from direct_redis.functions import *
+
+
+class DirectRedis(Redis):
+    def keys(self, pattern: str = "*"):
+        encoded = super().keys(pattern)
+        return [convert_get_type(key, force_decode=False) for key in encoded]
+
+    def set(self, key, value):
+        super().set(key, convert_set_type(value))
+
+    def hset(self, name, key, value):
+        super().hset(name, key, convert_set_type(value))
+
+    def hmset(self, name, mapping):
+        if not isinstance(mapping, dict):
+            raise Exception("mapping must be a python dictionary")
+        else:
+            mapping = convert_set_mapping_dic(mapping)
+            super().hmset(name, mapping)
+
+    def get(self, key, force_decode=False):
+        encoded = super().get(key)
+        return convert_get_type(encoded, force_decode)
+
+    def hget(self, name, key, force_decode=False):
+        encoded = super().hget(name, key)
+        return convert_get_type(encoded, force_decode)
+
+    def hmget(self, name, *keys, force_decode=False):
+        encoded = super().hmget(name, *keys)
+        return [convert_get_type(value, force_decode) for value in encoded]
+
+    def hgetall(self, name, force_decode=False):
+        encoded = super().hgetall(name)
+        dic = dict()
+        for k, v in encoded.items():
+            new_k = k.decode('utf-8')
+            dic[new_k] = convert_get_type(v, force_decode)
+        return dic
