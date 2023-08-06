7f7c54a2191014ed5129edb0c1b32ea4e4942dcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:37 +0000

    Add package mgsolver/0.0.1/mgsolver-0.0.1.tar.gz

---
+++ packages/mgsolver/mgsolver-0.0.1.tar.gz/mgsolver-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='mgsolver',
+      version='0.0.1',
+      packages=['src/ball_sort_puzzle', 'src/nonogram', 'src/sudoku'],
+      description='Collection of mini game solvers.',
+      keywords="mgsolver mini game solver",
+      author='Roman Stražanec',
+      author_email='roman.strazanec007@gmail.com',
+      license='MIT',
+      url='https://github.com/romanstrazanec/mgsolver',
+      # download_url='',
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "Environment :: Console",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: MIT License",
+          "Programming Language :: Python :: 3.8",
+          "Topic :: Other/Nonlisted Topic"
+      ]
+      )
