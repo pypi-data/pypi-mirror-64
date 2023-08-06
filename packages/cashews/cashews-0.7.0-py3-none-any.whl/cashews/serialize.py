8713012d6538b8c1e2d8cd017f2f17da4ea77bd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:29 +0000

    Add package cashews/0.7.0/cashews-0.7.0-py3-none-any.whl

---
+++ packages/cashews/cashews-0.7.0-py3-none-any.whl/cashews/serialize.py
@@ -0,0 +1,38 @@
+import hashlib
+import hmac
+import pickle
+
+
+class UnSecureDataError(Exception):
+    pass
+
+
+class PickleSerializerMixin:
+    def __init__(self, *args, hash_key=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._hash_key = hash_key
+
+    async def get(self, key):
+        value = await super().get(key)
+        if not value:
+            return value
+        try:
+            sign, value = value.split(b"_", 1)
+        except ValueError:
+            raise UnSecureDataError()
+        expected_sign = self.get_sign(key, value)
+        if expected_sign != sign:
+            raise UnSecureDataError()
+        try:
+            return pickle.loads(value, fix_imports=False, encoding="bytes")
+        except pickle.PickleError:
+            return None
+
+    def get_sign(self, key: str, value: bytes):
+        value = key.encode() + value
+        return hmac.new(self._hash_key, value, hashlib.md5).hexdigest().encode()
+
+    async def set(self, key: str, value, *args, **kwargs):
+        value = pickle.dumps(value, protocol=pickle.HIGHEST_PROTOCOL, fix_imports=False)
+        sign = self.get_sign(key, value)
+        return await super().set(key, sign + b"_" + value, *args, **kwargs)
