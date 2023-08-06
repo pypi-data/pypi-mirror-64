4487429a29394262fd3d44326b79515491173cb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:19 +0000

    Add package language-detector/5.0.0/language-detector-5.0.0.tar.gz

---
+++ packages/language-detector/language-detector-5.0.0.tar.gz/language-detector-5.0.0/README.md
@@ -0,0 +1,80 @@
+[![Build Status](https://travis-ci.org/DanielJDufour/language-detector.svg?branch=master)](https://travis-ci.org/DanielJDufour/language-detector)
+
+# language-detector
+language-detector detects the language of text
+
+# Installation
+```
+pip install language-detector
+```
+
+# Python Version
+Works with both Python 2 and 3
+
+# Use
+```
+from language_detector import detect_language
+text = "I arrived in that city on January 4, 1937"
+language = detect_language(text)
+# prints English
+```
+
+# Features
+| Languages Supported |
+| ------------------- |
+| Arabic |
+| English |
+| Farsi |
+| French |
+| German |
+| Kurmanci (Kurdish) |
+| Mandarin |
+| Russian |
+| Sorani (Kurdish) |
+| Spanish |
+| Turkish |
+
+# Testing
+To test the package run
+```
+python -m unittest language_detector.tests.test
+```
+
+# Comparison
+Test is a comparison of how well language-detector and langid identify languages in the [data sources](language_detector/prep/sources).
+| package | language-detector | langid |
+| ------- | ----------------- | ------ |
+| test-duration (in seconds)| 0.10 | 3.83 |
+| accuracy | 96.77% | 67.74% |
+
+
+# Excluding Languages
+If you don't want language-detector to look for certain languages, you can monkey-patch the code.  For example, in order to exclude English:
+```
+import language_detector
+language_detector.char_language = [cl for cl in char_language if cl[1] != "English"]
+
+# proceed as normal
+``` 
+
+# Datasets
+The following is a list of datasets used for each language:
+| Language | Datasets |
+| ------------------- | -- |
+| Arabic | [UN Corpora](http://www.uncorpora.org/) |
+| English |  [UN Corpora](http://www.uncorpora.org/) |
+| Farsi | [BBC News Persian](https://www.bbc.com/persian) | 
+| French | [UN Corpora](http://www.uncorpora.org/) |
+| German | [Deutsche Welle](https://www.dw.com/de) |
+| Kurmanci (Kurdish) | [Rudaw](https://rudaw.net/kurmanci) |
+| Mandarin | [UN Corpora](http://www.uncorpora.org/) |
+| Russian | [UN Corpora](http://www.uncorpora.org/) |
+| Sorani (Kurdish) | [Rudaw](https://www.rudaw.net/sorani) |
+| Spanish | [UN Corpora](http://www.uncorpora.org/) |
+| Turkish | [BBC News Türkçe](https://www.bbc.com/turkce) | 
+
+# Contributing
+If you'd like to contribute a new language, please consult [CONTRIBUTING.md](CONTRIBUTING.md)
+
+# Support
+Contact the package author, Daniel J. Dufour, at daniel.j.dufour@gmail.com
---
+++ packages/language-detector/language-detector-5.0.0.tar.gz/language-detector-5.0.0/language_detector/__init__.py
@@ -0,0 +1,86 @@
+from sys import version_info
+python_version = version_info.major
+
+from os.path import dirname, realpath
+from collections import Counter
+
+directory = dirname(realpath(__file__))
+
+language_to_code = {
+"Arabic": "ar",
+"English": "en",
+"French": "fr",
+"German": "de",
+"Kurdish": "ku",
+"Kurmanci": "ku-ku",
+"Italian": "it",
+"Portuguese": "pt",
+"Sorani": "ku-so",
+"Spanish": "es",
+"Turkish": "tr",
+"Vietnamese": "vi",
+"Welsh": "cy"
+}
+
+
+char_language = []
+if python_version == 2:
+    char_language_file = open(directory + "/prep/char_language.txt")
+elif python_version == 3:
+    char_language_file = open(directory + "/prep/char_language.txt", encoding='utf-8')
+
+for line in char_language_file:
+    if line:
+        if python_version == 2:
+            char, language, score = line.decode("utf-8").strip().split(u"\t")
+        elif python_version == 3:
+            char, language, score = line.strip().split(u"\t")
+        score = float(score)
+        char_language.append((char, language, score))
+
+char_language_file.close()
+
+def detect_language_text(text):
+    if python_version == 2 and isinstance(text, str):
+        text = text.decode("utf-8")
+
+    # could make this more sophisticated with using TFIDF scores probably
+    counts = Counter()
+    for char, language, score in char_language:
+        if char in text:
+            #print "MATCHED", [char]
+            counts[language] += score
+    # basically return whichever language has most amount of special letters in there
+    results = counts.most_common(1) 
+    if results:
+        return results[0][0]
+
+# just returns the most common language of an iterable of string values
+# an example of this can be a list of names
+def detect_language_iterable(iterable):
+    # may be able to speed this up with out using counter
+    # hmmm nvm.. it's so freaking quick anyway, that probably don't worry about this now
+    most_common = Counter([detect_language(iteration) for iteration in iterable]).most_common(2)
+    if most_common[0][0]:
+        return most_common[0][0]
+    elif len(most_common) == 2:
+        # know that not returning 1st one
+        return most_common[1][0]
+    # will return None if doesn't match any above
+
+def detect_language(inpt, return_as_code=False):
+    if isinstance(inpt, str) or (isinstance(inpt, unicode) if python_version == 2 else isinstance(inpt, bytes)):
+        language = detect_language_text(inpt)
+    elif isinstance(inpt, set) or isinstance(inpt, list):
+        language = detect_language_iterable(inpt)
+    if return_as_code:
+        return language_to_code[language]
+    else:
+        return language
+
+
+def isArabic(inpt):
+    return detect_language(inpt) == "Arabic"
+
+def isEnglish(inpt):
+    return detect_language(inpt) == "English"
