407d275f7da7478abdcde6cfc622ceb75ab7d280
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:13 +0000

    Add package tc2calculator/1.1/tc2calculator-1.1.tar.gz

---
+++ packages/tc2calculator/tc2calculator-1.1.tar.gz/tc2calculator-1.1/tests/test_tc2calculator.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+"""Tests for `tc2calculator` package."""
+
+
+import unittest
+from click.testing import CliRunner
+
+from tc2calculator import tc2calculator
+from tc2calculator import cli
+
+
+class TestTc2calculator(unittest.TestCase):
+    """Tests for `tc2calculator` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
+
+    def test_command_line_interface(self):
+        """Test the CLI."""
+        runner = CliRunner()
+        result = runner.invoke(cli.main)
+        assert result.exit_code == 0
+        assert 'tc2calculator.cli.main' in result.output
+        help_result = runner.invoke(cli.main, ['--help'])
+        assert help_result.exit_code == 0
+        assert '--help  Show this message and exit.' in help_result.output
