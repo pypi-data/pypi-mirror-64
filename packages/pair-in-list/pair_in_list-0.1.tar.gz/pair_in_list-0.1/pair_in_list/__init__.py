6a975c9b005868f15366b2b768bacdc486081de9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:28 +0000

    Add package pair-in-list/0.1/pair_in_list-0.1.tar.gz

---
+++ packages/pair-in-list/pair_in_list-0.1.tar.gz/pair_in_list-0.1/pair_in_list/__init__.py
@@ -0,0 +1,1 @@
+from .pair_in_list import pair_in_list
---
+++ packages/pair-in-list/pair_in_list-0.1.tar.gz/pair_in_list-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='pair_in_list',
+      version='0.1',
+      description='Class including two functions to check if there are two elements in a list that add up to a specified sum',
+      packages=['pair_in_list'],
+      autho='Alex YU',
+      zip_safe=False)
