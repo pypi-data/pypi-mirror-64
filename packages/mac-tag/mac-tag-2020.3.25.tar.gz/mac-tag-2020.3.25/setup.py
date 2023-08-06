345a0e71286bb7b117d994f6e8a31f89e55123c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:44 +0000

    Add package mac-tag/2020.3.25/mac-tag-2020.3.25.tar.gz

---
+++ packages/mac-tag/mac-tag-2020.3.25.tar.gz/mac-tag-2020.3.25/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='mac-tag',
+    version='2020.3.25',
+    install_requires=[
+        'public',
+        'runcmd',
+        'setuptools',
+        'values',
+    ],
+    packages=[
+        'mac_tag',
+    ],
+)
