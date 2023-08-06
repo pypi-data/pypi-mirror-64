ba0d27117f2965b5eea6d64ffb39ac4a1c686b95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:17 +0000

    Add package my-statistical-distributions/1.3/my_statistical_distributions-1.3.tar.gz

---
+++ packages/my-statistical-distributions/my_statistical_distributions-1.3.tar.gz/my_statistical_distributions-1.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="my_statistical_distributions",
+    version="1.3",
+    description="A simple class for Binomial, Gaussian, and Poisson distributions",
+    long_description="A simple class for Binomial, Gaussian, and Poisson distributions",
+    packages=["my_statistical_distributions"],
+    author="Timo Hartmann",
+    author_email="timo.hartmann@posteo.de",
+    url="https://github.com/tihartmann/my_statistical_distributions",
+    zip_safe=False
+)
