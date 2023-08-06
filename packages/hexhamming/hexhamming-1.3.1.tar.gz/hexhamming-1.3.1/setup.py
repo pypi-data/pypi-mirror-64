838264d8770f065c05e4dd3ee383785954525d54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:15 +0000

    Add package hexhamming/1.3.1/hexhamming-1.3.1.tar.gz

---
+++ packages/hexhamming/hexhamming-1.3.1.tar.gz/hexhamming-1.3.1/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from setuptools import setup, Extension
+
+
+with open("./README.rst", "r") as readme_file:
+    LONG_DESCRIPTION = readme_file.read()
+
+setup(
+    name="hexhamming",
+    version="1.3.1",
+    description="Fast Hamming distance calculation for hexadecimal strings",
+    url="https://github.com/mrecachinas/hexhamming.git",
+    long_description=LONG_DESCRIPTION,
+    ext_modules=[
+        Extension(name="hexhamming", sources=["hexhamming/python_hexhamming.c"])
+    ],
+)
