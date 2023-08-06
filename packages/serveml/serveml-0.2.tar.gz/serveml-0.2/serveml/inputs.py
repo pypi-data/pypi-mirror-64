0615806ee93c39b70297bc5e9c83681bb9fef7f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:26 +0000

    Add package serveml/0.2/serveml-0.2.tar.gz

---
+++ packages/serveml/serveml-0.2.tar.gz/serveml-0.2/serveml/inputs.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel as BasicInput
+
+
+class FeedbackInput(BasicInput):
+    """
+    Basic input validator for feedbacks
+    """
+
+    request_id: str
+    status: bool
+    expected_result: str = None
---
+++ packages/serveml/serveml-0.2.tar.gz/serveml-0.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name="serveml",
+    version="0.2",
+    author="gfalcone",
+    author_email="gfalcone@github.com",
+    description="serveml is a machine learning serving tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=required,
+    url="http://github.com/gfalcone/mlserve",
+    packages=["serveml"],
+    license="Apache",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    zip_safe=False,
+)
