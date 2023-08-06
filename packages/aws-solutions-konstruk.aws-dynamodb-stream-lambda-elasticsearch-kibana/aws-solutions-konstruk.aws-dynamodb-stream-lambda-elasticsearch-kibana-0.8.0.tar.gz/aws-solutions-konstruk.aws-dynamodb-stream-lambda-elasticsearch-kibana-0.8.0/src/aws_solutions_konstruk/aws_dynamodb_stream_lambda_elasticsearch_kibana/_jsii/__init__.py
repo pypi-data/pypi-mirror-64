544ef4a0f1182a0bdfdd388779264ae174e011ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:56 +0000

    Add package aws-solutions-konstruk.aws-dynamodb-stream-lambda-elasticsearch-kibana/0.8.0/aws-solutions-konstruk.aws-dynamodb-stream-lambda-elasticsearch-kibana-0.8.0.tar.gz

---
+++ packages/aws-solutions-konstruk.aws-dynamodb-stream-lambda-elasticsearch-kibana/aws-solutions-konstruk.aws-dynamodb-stream-lambda-elasticsearch-kibana-0.8.0.tar.gz/aws-solutions-konstruk.aws-dynamodb-stream-lambda-elasticsearch-kibana-0.8.0/src/aws_solutions_konstruk/aws_dynamodb_stream_lambda_elasticsearch_kibana/_jsii/__init__.py
@@ -0,0 +1,23 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.aws_cloudwatch
+import aws_cdk.aws_cognito
+import aws_cdk.aws_dynamodb
+import aws_cdk.aws_elasticsearch
+import aws_cdk.aws_lambda
+import aws_cdk.aws_lambda_event_sources
+import aws_cdk.core
+import aws_solutions_konstruk.aws_dynamodb_stream_lambda
+import aws_solutions_konstruk.aws_lambda_elasticsearch_kibana
+import aws_solutions_konstruk.core
+__all__ = []
+
+publication.publish()
