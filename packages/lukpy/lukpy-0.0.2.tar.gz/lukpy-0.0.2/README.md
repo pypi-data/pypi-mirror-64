261ef410bf97cd7c8f459d39b93b6c2022f6a7b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:19 +0000

    Add package lukpy/0.0.2/lukpy-0.0.2.tar.gz

---
+++ packages/lukpy/lukpy-0.0.2.tar.gz/lukpy-0.0.2/README.md
@@ -0,0 +1,1 @@
+# Nothing here in my README
---
+++ packages/lukpy/lukpy-0.0.2.tar.gz/lukpy-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="lukpy", #
+    version="0.0.2",
+    author="Lukas Hecker",
+    author_email="lukas_hecker@web.com",
+    description="My personal functions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://bitbucket.org/LukeTheHecker/lukpy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['scipy', 'numpy', 'matplotlib']
+)
