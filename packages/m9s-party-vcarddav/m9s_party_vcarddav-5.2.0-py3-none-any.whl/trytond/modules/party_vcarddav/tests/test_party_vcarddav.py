87fb571eaa539e34fb112ddbe70bb5f3f03d7c08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:28 +0000

    Add package m9s-party-vcarddav/5.2.0/m9s_party_vcarddav-5.2.0-py3-none-any.whl

---
+++ packages/m9s-party-vcarddav/m9s_party_vcarddav-5.2.0-py3-none-any.whl/trytond/modules/party_vcarddav/tests/test_party_vcarddav.py
@@ -0,0 +1,19 @@
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+import unittest
+
+
+from trytond.tests.test_tryton import ModuleTestCase
+from trytond.tests.test_tryton import suite as test_suite
+
+
+class PartyVcarddavTestCase(ModuleTestCase):
+    'Test Party Vcarddav module'
+    module = 'party_vcarddav'
+
+
+def suite():
+    suite = test_suite()
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+            PartyVcarddavTestCase))
+    return suite
