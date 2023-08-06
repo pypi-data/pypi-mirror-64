337b35e7dd6b2764a5368c0c878fce2fd3dd7367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:17 +0000

    Add package gcloud-rest-datastore/5.4.1/gcloud-rest-datastore-5.4.1.tar.gz

---
+++ packages/gcloud-rest-datastore/gcloud-rest-datastore-5.4.1.tar.gz/gcloud-rest-datastore-5.4.1/gcloud/rest/datastore/datastore_operation.py
@@ -0,0 +1,37 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+from future import standard_library
+standard_library.install_aliases()
+from builtins import object
+from typing import Any
+from typing import Dict
+from typing import Optional
+
+
+class DatastoreOperation(object):
+    def __init__(self, name     , done      ,
+                 metadata                           = None,
+                 error                 = None,
+                 response                           = None)        :
+        self.name = name
+        self.done = done
+
+        self.metadata = metadata
+        self.error = error
+        self.response = response
+
+    @classmethod
+    def from_repr(cls, data                )                        :
+        return cls(data['name'], data.get('done', False), data.get('metadata'),
+                   data.get('error'), data.get('response'))
+
+    def to_repr(self)                  :
+        return {
+            'done': self.done,
+            'error': self.error,
+            'metadata': self.metadata,
+            'name': self.name,
+            'response': self.response,
+        }
