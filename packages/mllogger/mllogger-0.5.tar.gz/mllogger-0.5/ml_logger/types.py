c011bfc23b2860a618b47f273ed3b18a86fff772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:19 +0000

    Add package mllogger/0.5/mllogger-0.5.tar.gz

---
+++ packages/mllogger/mllogger-0.5.tar.gz/mllogger-0.5/ml_logger/types.py
@@ -0,0 +1,10 @@
+from typing import Any, Callable, Dict, Union
+
+NumType = Union[int, float]
+ValueType = Union[str, int, float]
+LogType = Dict[str, Any]
+ConfigType = LogType
+MetricType = Dict[str, ValueType]
+ModelType = Any
+ComparisonOpType = Callable[[ValueType, ValueType], bool]
+KeyMapType = Dict[str, str]
