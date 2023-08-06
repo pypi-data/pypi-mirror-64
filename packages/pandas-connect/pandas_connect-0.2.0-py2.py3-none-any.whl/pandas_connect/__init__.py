55740c77075f4479afbe1649f5350b1467a52b47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:05 +0000

    Add package pandas-connect/0.2.0/pandas_connect-0.2.0-py2.py3-none-any.whl

---
+++ packages/pandas-connect/pandas_connect-0.2.0-py2.py3-none-any.whl/pandas_connect/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for Pandas Connect."""
+
+from pandas_connect.pandas_connect import *
+
+__author__ = """Eric Hamers"""
+__email__ = 'erichamers@gmail.com'
+__version__ = '0.1.0'
