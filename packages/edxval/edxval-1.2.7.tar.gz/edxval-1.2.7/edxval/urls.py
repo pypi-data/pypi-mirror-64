ba469e0662951b3d2dd11d6e770f6ddf55b99fcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:31 +0000

    Add package edxval/1.2.7/edxval-1.2.7.tar.gz

---
+++ packages/edxval/edxval-1.2.7.tar.gz/edxval-1.2.7/edxval/urls.py
@@ -0,0 +1,43 @@
+"""
+Url file for django app edxval.
+"""
+
+from __future__ import absolute_import
+
+from django.conf.urls import url
+
+from edxval import views
+
+urlpatterns = [
+    url(r'^videos/$', views.VideoList.as_view(), name='video-list'),
+    url(
+        r'^videos/(?P<edx_video_id>[-\w]+)$',
+        views.VideoDetail.as_view(),
+        name='video-detail'
+    ),
+    url(
+        r'^videos/status/$',
+        views.VideoStatusView.as_view(),
+        name='video-status-update'
+    ),
+    url(
+        r'^videos/missing-hls/$',
+        views.HLSMissingVideoView.as_view(),
+        name='hls-missing-video'
+    ),
+    url(
+        r'^videos/video-transcripts/create/$',
+        views.VideoTranscriptView.as_view(),
+        name='create-video-transcript'
+    ),
+    url(
+        r'^videos/video-images/update/$',
+        views.VideoImagesView.as_view(),
+        name='update-video-images'
+    ),
+    url(
+        r'^videos/transcript-credentials/(?P<provider>[\w]*)/(?P<org>[\w]*)$',
+        views.TranscriptCredentialsView.as_view(),
+        name='transcript-credentials'
+    )
+]
