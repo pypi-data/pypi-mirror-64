d76ab6a0b00de25fdd025138eb264d1c380f2cee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:55 +0000

    Add package snakeflake/0.1.5.1/snakeflake-0.1.5.1-py3-none-any.whl

---
+++ packages/snakeflake/snakeflake-0.1.5.1-py3-none-any.whl/snakeflake/exceptions.py
@@ -0,0 +1,10 @@
+"""Defines Custom Exceptions"""
+
+class ExceededTimeException(Exception):
+    pass
+
+class ExceededBitsException(Exception):
+    pass
+
+class EpochFutureException(Exception):
+    pass
