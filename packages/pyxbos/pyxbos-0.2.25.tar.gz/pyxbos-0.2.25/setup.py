2654f9a611bffad690f25383486edfece4587259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:23 +0000

    Add package pyxbos/0.2.25/pyxbos-0.2.25.tar.gz

---
+++ packages/pyxbos/pyxbos-0.2.25.tar.gz/pyxbos-0.2.25/setup.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['pyxbos', 'pyxbos.drivers', 'pyxbos.drivers.pbc']
+
+package_data = \
+{'': ['*'],
+ 'pyxbos': ['wave/*', 'wavemq/*'],
+ 'pyxbos.drivers': ['bacnet/*',
+                    'bacnet/buildings/orinda-public-library/*',
+                    'dark_sky/*',
+                    'hue/*',
+                    'obvius/*',
+                    'parker/*',
+                    'system_monitor/*',
+                    'weather_current/*',
+                    'weather_prediction/*']}
+
+install_requires = \
+['aiogrpc>=1.6,<2.0',
+ 'beautifulsoup4>=4.7,<5.0',
+ 'googleapis-common-protos>=1.5,<2.0',
+ 'grpcio-tools>=1.18,<2.0',
+ 'grpcio>=1.18,<2.0',
+ 'jq>=0.1.6,<0.2.0',
+ 'tlslite-ng>=0.7.5,<0.8.0',
+ 'toml>=0.10.0,<0.11.0',
+ 'xxhash>=1.3,<2.0']
+
+setup_kwargs = {
+    'name': 'pyxbos',
+    'version': '0.2.25',
+    'description': 'Python bindings for XBOS-flavored WAVEMQ and related services',
+    'long_description': None,
+    'author': 'Gabe Fierro',
+    'author_email': 'gtfierro@cs.berkeley.edu',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
