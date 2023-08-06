e1b9b5644db978201ada496775448868e7073a7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:37 +0000

    Add package KlWrappers/0.0.2/KLWrappers-0.0.2.tar.gz

---
+++ packages/KlWrappers/KLWrappers-0.0.2.tar.gz/KLWrappers-0.0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='KLWrappers',
+      version='0.0.2',
+      py_modules=["UploadWrappers"],
+      package_dir={'': 'src'},
+      install_requires=["paho-mqtt", "kafka-python"])
