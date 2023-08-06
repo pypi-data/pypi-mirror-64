d91e41b6a6dcc74c25e478ccd6e4587fe0a033f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:41 +0000

    Add package lins-plugboleto/1.0.0.19/lins_plugboleto-1.0.0.19.tar.gz

---
+++ packages/lins-plugboleto/lins_plugboleto-1.0.0.19.tar.gz/lins_plugboleto-1.0.0.19/lins_plugboleto/cedente.py
@@ -0,0 +1,7 @@
+class Cedente(object):
+
+    def __init__(self, cedente):
+        self.CedenteContaCodigoBanco = str(cedente['CedenteContaCodigoBanco'])
+        self.CedenteContaNumero = str(cedente['CedenteContaNumero'])
+        self.CedenteContaNumeroDV = str(cedente['CedenteContaNumeroDV'])
+        self.CedenteConvenioNumero = str(cedente['CedenteConvenioNumero'])
---
+++ packages/lins-plugboleto/lins_plugboleto-1.0.0.19.tar.gz/lins_plugboleto-1.0.0.19/lins_plugboleto/sacado.py
@@ -0,0 +1,16 @@
+class Sacado(object):
+
+    def __init__(self, sacado):
+        self.SacadoCPFCNPJ = str(sacado['SacadoCPFCNPJ'])
+        self.SacadoNome = str(sacado['SacadoNome'])
+        self.SacadoEnderecoLogradouro = str(sacado['SacadoEnderecoLogradouro'])
+        self.SacadoEnderecoNumero = str(sacado['SacadoEnderecoNumero'])
+        self.SacadoEnderecoBairro = str(sacado['SacadoEnderecoBairro'])
+        self.SacadoEnderecoCep = str(sacado['SacadoEnderecoCep'])
+        self.SacadoEnderecoCidade = str(sacado['SacadoEnderecoCidade'])
+        self.SacadoEnderecoComplemento = str(sacado['SacadoEnderecoComplemento'])
+        self.SacadoEnderecoPais = str(sacado['SacadoEnderecoPais'])
+        self.SacadoEnderecoUf = str(sacado['SacadoEnderecoUf'])
+        self.SacadoEmail = str(sacado['SacadoEmail'])
+        self.SacadoTelefone = str(sacado['SacadoTelefone'])
+        self.SacadoCelular = str(sacado['SacadoCelular'])
