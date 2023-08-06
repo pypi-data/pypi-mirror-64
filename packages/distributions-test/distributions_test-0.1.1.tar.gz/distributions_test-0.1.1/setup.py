731db9a71469571f045199b9a0c9dff2c014ea6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:03 +0000

    Add package distributions-test/0.1.1/distributions_test-0.1.1.tar.gz

---
+++ packages/distributions-test/distributions_test-0.1.1.tar.gz/distributions_test-0.1.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_test',
+      version='0.1.1',
+      description='Gaussian and Binomial distributions ',
+      packages=['distributions'],
+      zip_safe=False)
