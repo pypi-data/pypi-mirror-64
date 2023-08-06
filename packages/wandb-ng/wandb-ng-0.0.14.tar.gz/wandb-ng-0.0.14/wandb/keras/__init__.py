14dda9e467d8c66fdcca6230752637989af2972f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:15 +0000

    Add package wandb-ng/0.0.14/wandb-ng-0.0.14.tar.gz

---
+++ packages/wandb-ng/wandb-ng-0.0.14.tar.gz/wandb-ng-0.0.14/wandb/keras/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Compatibility keras module.
+
+In the future use:
+    from wandb.framework.keras import WandbCallback
+"""
+
+from wandb.framework.keras import WandbCallback
+
+__all__ = ['WandbCallback']
