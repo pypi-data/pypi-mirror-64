9d35b996630a34d1e1eee103bd244203a4e51043
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:17 +0000

    Add package creme/0.5.1/creme-0.5.1-cp38-cp38-manylinux1_i686.whl

---
+++ packages/creme/creme-0.5.1-cp38-cp38-manylinux1_i686.whl/creme/anomaly/__init__.py
@@ -0,0 +1,13 @@
+"""Anomaly detection.
+
+The estimators in ``anomaly`` are slightly different than the rest of the estimators. Instead of a
+``predict_one`` method, each anomaly detector has a ``score_one`` method which returns an anomaly
+score for a given set of features. High scores indicate anomalies whereas low scores indicate
+normal observations will have low scores. Note that the range of the scores depends on each
+particular estimator.
+
+"""
+from .hst import HalfSpaceTrees
+
+
+__all__ = ['HalfSpaceTrees']
