7a2a7c8bf6468d2285ec3efe83ce448948ba194d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:57 +0000

    Add package clubs/0.1.0/clubs-0.1.0.tar.gz

---
+++ packages/clubs/clubs-0.1.0.tar.gz/clubs-0.1.0/setup.py
@@ -0,0 +1,42 @@
+import os
+
+from setuptools import setup
+
+import clubs
+
+file_dir = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(file_dir, 'README.md'), encoding='utf-8') as file:
+    long_description = file.read()
+
+setup(
+    name='clubs',
+    version=clubs.__version__,
+    description=clubs.__docs__,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=clubs.__author__,
+    url=clubs.__homepage__,
+    license=clubs.__license__,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Topic :: Games/Entertainment',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    keywords=['reinforcment learning', 'poker', 'AI', 'gym'],
+    packages=['clubs'],
+    python_requires='>=3.6',
+    install_requires=['numpy>=1.16'],
+    project_urls={
+        'Bug Reports': 'https://github.com/fschlatt/clubs/issues',
+        'Source': 'https://github.com/fschlatt/clubs/',
+    },
+)
