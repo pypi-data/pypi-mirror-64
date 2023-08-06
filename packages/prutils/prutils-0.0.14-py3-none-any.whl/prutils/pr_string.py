9a178404deee9be4d65c5502666a8d8bef16f2c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:22 +0000

    Add package prutils/0.0.14/prutils-0.0.14-py3-none-any.whl

---
+++ packages/prutils/prutils-0.0.14-py3-none-any.whl/prutils/pr_string.py
@@ -0,0 +1,24 @@
+import re
+
+def get_re_first(pattern, string, flags=0):
+    """
+    print(get_re_first("\\d", "abc1") == "1")
+    :param pattern:
+    :param string:
+    :param flags:
+    :return:
+    """
+    r = re.search(pattern, string, flags)
+    return r.group()
+
+
+def get_re_all(pattern, string, flags=0):
+    r = re.findall(pattern, string, flags)
+    return r
+
+
+def get_re_last(pattern, string, flags=0):
+    return get_re_all(pattern, string, flags)[-1]
+
+
+
