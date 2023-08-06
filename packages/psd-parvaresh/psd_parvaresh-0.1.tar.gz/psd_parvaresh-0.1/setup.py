66dd5a0df484588c2382ed96f8beaa372d4521c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:20 +0000

    Add package psd-parvaresh/0.1/psd_parvaresh-0.1.tar.gz

---
+++ packages/psd-parvaresh/psd_parvaresh-0.1.tar.gz/psd_parvaresh-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='psd_parvaresh',
+      version='0.1',
+      description='Power Spectral Density Calculator',
+      packages=['psd_parvaresh'],
+      author='Stephen Parvaresh',
+      zip_safe=False)
