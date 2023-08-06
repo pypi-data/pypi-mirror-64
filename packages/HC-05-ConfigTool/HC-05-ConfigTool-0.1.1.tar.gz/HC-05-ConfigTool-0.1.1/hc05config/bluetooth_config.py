a5543346db165bb278ef198316f975ac439b26a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:44 +0000

    Add package HC-05-ConfigTool/0.1.1/HC-05-ConfigTool-0.1.1.tar.gz

---
+++ packages/HC-05-ConfigTool/HC-05-ConfigTool-0.1.1.tar.gz/HC-05-ConfigTool-0.1.1/hc05config/bluetooth_config.py
@@ -0,0 +1,47 @@
+from .port_select import getPort
+from .input_lib import get_input, INVALID_MESSAGE, BLUETOOTH_CONFIG_VALIDATE
+from .basic_info import get_basic_info
+from .preset import selectPreset, adjustPreset, savePreset
+
+from copy import deepcopy
+
+def bt_config():
+	port = getPort("Please select the port of your HC-05: ")
+
+	isUsePreset = get_input(str, "Do you want to use preset to set up the pair? (Y/N) ", INVALID_MESSAGE, correct=["Y", "N", "y", "n"]).upper()
+
+	presetReady = False
+	title = None
+	if isUsePreset == "Y":
+		title, config = selectPreset("Bluetooth Config")
+		if config:
+			presetReady = True
+
+	if not presetReady:
+		config = get_basic_info(port=port, is_print=False)
+		config["UART"] = [config["Baud Rate"], config["Stop Bit"], config["Parity Bit"]]
+		old_config = deepcopy(config)
+		del config["UART"]
+		del config["Address"]
+		del config["Version"]
+
+	print("\n----------------------------------------------------\n")
+	adjustPreset(config, inputFunc=BLUETOOTH_CONFIG_VALIDATE)
+
+	config["UART"] = [config["Baud Rate"], config["Stop Bit"], config["Parity Bit"]]
+	if not presetReady:
+		new_config = {}
+		for key in config.keys():
+			if old_config[key] != config[key]:
+				new_config[key] = config[key]
+
+	print("Setting HC-05 bluetooth module")
+	success, fail = port.set(config if presetReady else new_config)
+
+	for item in fail:
+		print("Fail to set {}".format(item))
+	
+	print("Setting completed")
+
+	del config["UART"]
+	savePreset("Bluetooth Config", config, title)
---
+++ packages/HC-05-ConfigTool/HC-05-ConfigTool-0.1.1.tar.gz/HC-05-ConfigTool-0.1.1/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="HC-05-ConfigTool",
+    version="0.1.1",
+    author="Joseph Lam",
+    author_email="mhlamaf@connect.ust.hk",
+    description="A terminal tool for configuring HC-05 with AT mode.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Doma1204/HC-05_Bluetooth_Tool",
+    packages=setuptools.find_packages(),
+    license="MIT",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Utilities",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7"
+    ],
+    python_requires='>=3',
+    keywords="bluetooth hc-05",
+    install_requires=["pyserial"]
+)
