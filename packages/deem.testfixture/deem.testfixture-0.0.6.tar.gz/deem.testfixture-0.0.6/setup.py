472d9ba0ed2c33023d338b7d97d6f4b6f1f1c3c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:37 +0000

    Add package deem.testfixture/0.0.6/deem.testfixture-0.0.6.tar.gz

---
+++ packages/deem.testfixture/deem.testfixture-0.0.6.tar.gz/deem.testfixture-0.0.6/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+  name = 'deem.testfixture',
+  packages = ['deem.testfixture'],
+  version = '0.0.6',
+  license='MIT',
+  description = 'Python3 Deem Base Tests',
+  author = 'Alexander Eliseyev',
+  author_email = 'alex@deem.ai',
+  url = 'https://github.com/deem-team',
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6'
+  ]
+)
