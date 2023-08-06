872bcc36e999012b8b8c3c2e15b9d5a80e3e341f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:58 +0000

    Add package pyadtpulse/0.1.5/pyadtpulse-0.1.5-py3-none-any.whl

---
+++ packages/pyadtpulse/pyadtpulse-0.1.5-py3-none-any.whl/pyadtpulse/const.py
@@ -0,0 +1,29 @@
+DEFAULT_API_HOST = 'https://portal.adtpulse.com'
+API_HOST_CA      = 'https://portal-ca.adtpulse.com' # Canada
+
+API_PREFIX = '/myhome/'
+
+ADT_LOGIN_URI      = '/access/signin.jsp?partner=adt'
+ADT_LOGOUT_URI     = '/access/signout.jsp'
+
+ADT_SUMMARY_URI    = '/summary/summary.jsp'
+ADT_ZONES_URI      = '/ajax/homeViewDevAjax.jsp'
+ADT_ORB_URI        = '/ajax/orb.jsp'
+ADT_STATES_URI     = '/ajax/currentStates.jsp'
+ADT_SYNC_CHECK_URI = '/Ajax/SyncCheckServ'
+
+ADT_ARM_URI        = '/quickcontrol/serv/RunRRACommand'
+ADT_ARM_DISARM_URI = '/quickcontrol/armDisarm.jsp?href=rest/adt/ui/client/security/setArmState'
+
+ADT_STATUS_CHANGE_URI = '/quickcontrol/serv/ChangeVariableServ'
+
+ADT_SET_PREMISE_URI = '/site/site?action=changepremise&siteId=[NETWORKID]'
+
+ADT_SYSTEM_SETTINGS = '/system/settings.jsp'
+
+STATE_OK = 'OK'
+STATE_OPEN = 'Open'
+STATE_MOTION = 'Motion'
+STATE_TAMPER = 'Tamper'
+STATE_ALARM = 'Alarm'
+STATE_UNKNOWN = 'Unknown'
