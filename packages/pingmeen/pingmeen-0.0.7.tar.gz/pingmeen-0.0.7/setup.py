0b80c058e10d7b246b9261713ba004ec46ef2c35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:24 +0000

    Add package pingmeen/0.0.7/pingmeen-0.0.7.tar.gz

---
+++ packages/pingmeen/pingmeen-0.0.7.tar.gz/pingmeen-0.0.7/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+import pingmeen
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name                          = "pingmeen", 
+    version                       = pingmeen.__version__,
+    author                        = "Nikita M. Minaev",
+    author_email                  = "nikita.minaev1995@gmail.com",
+    description                   = "Pingmeen package",
+    long_description              = long_description,
+    long_description_content_type = "text/markdown",
+    url                           = "",
+    packages                      = setuptools.find_packages(),
+    classifiers                   = [
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires               = '>=3.6',
+)
