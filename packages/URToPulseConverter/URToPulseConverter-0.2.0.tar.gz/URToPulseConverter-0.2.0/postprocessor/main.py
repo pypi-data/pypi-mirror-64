72ffb1c7ebe45a46fa617692178f65e85b3fec3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:03 +0000

    Add package URToPulseConverter/0.2.0/URToPulseConverter-0.2.0.tar.gz

---
+++ packages/URToPulseConverter/URToPulseConverter-0.2.0.tar.gz/URToPulseConverter-0.2.0/postprocessor/main.py
@@ -0,0 +1,12 @@
+from argsParser import arguments
+from converter.programParser import program_parser, read_file
+from converter.RobodkUR2Pulse import Postprocessor
+
+def main() -> None:
+    args = arguments.parse_args()
+
+    postprocessor = Postprocessor(robothost=args.host_name)
+    program = read_file(args.initial_program)
+
+    program_parser(program, postprocessor)
+    postprocessor.progSave(args.save_path, args.name, show_result=True)
