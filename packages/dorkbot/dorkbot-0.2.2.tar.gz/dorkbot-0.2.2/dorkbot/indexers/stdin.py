25cdd067c61220fa2345729a9a11eee837395fa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:32 +0000

    Add package dorkbot/0.2.2/dorkbot-0.2.2.tar.gz

---
+++ packages/dorkbot/dorkbot-0.2.2.tar.gz/dorkbot-0.2.2/dorkbot/indexers/stdin.py
@@ -0,0 +1,12 @@
+import sys
+import io
+import logging
+from urllib.parse import urlparse
+
+def run(args):
+    with io.open(sys.stdin.fileno(), encoding="utf-8") as stdin:
+        results = [urlparse(item.strip()).geturl() for item in stdin]
+        for result in results:
+            logging.debug(result)
+        logging.info("Fetched %d results", len(results))
+        return results
