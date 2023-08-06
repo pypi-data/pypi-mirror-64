1dec0fd239e513204039ff86095a6d24b94a74d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:00 +0000

    Add package gdaps/0.4.13/gdaps-0.4.13-py3-none-any.whl

---
+++ packages/gdaps/gdaps-0.4.13-py3-none-any.whl/gdaps/__init__.py
@@ -0,0 +1,5 @@
+from importlib import metadata
+
+
+default_app_config = "gdaps.apps.GdapsConfig"
+__version__ = "0.4.13"
---
+++ packages/gdaps/gdaps-0.4.13-py3-none-any.whl/gdaps/frontend/__init__.py
@@ -0,0 +1,4 @@
+default_app_config = "gdaps.frontend.apps.FrontendConfig"
+
+
+
