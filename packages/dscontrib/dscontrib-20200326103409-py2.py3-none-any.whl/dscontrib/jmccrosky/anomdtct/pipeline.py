2ae510f41183358c1a6dd5823ebb88a8379d95e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:00 +0000

    Add package dscontrib/20200326103409/dscontrib-20200326103409-py2.py3-none-any.whl

---
+++ packages/dscontrib/dscontrib-20200326103409-py2.py3-none-any.whl/dscontrib/jmccrosky/anomdtct/pipeline.py
@@ -0,0 +1,34 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+from dscontrib.jmccrosky.forecast.utils import s2d
+from dscontrib.jmccrosky.anomdtct.data import get_raw_data, prepare_data
+from dscontrib.jmccrosky.anomdtct.forecast import forecast
+
+
+def pipeline(bq_client, bq_storage_client):
+    city_raw_data = get_raw_data(
+        bq_client,
+        bq_storage_client,
+        "light_funnel_dau_city"
+    )
+    (city_clean_data, city_clean_training_data) = prepare_data(
+        city_raw_data, s2d('2016-04-08'), s2d('2020-01-30')
+    )
+    city_forecast_data = forecast(city_clean_training_data, city_clean_data)
+
+    country_raw_data = get_raw_data(
+        bq_client,
+        bq_storage_client,
+        "light_funnel_dau_country"
+    )
+    (country_clean_data, country_clean_training_data) = prepare_data(
+        country_raw_data, s2d('2016-04-08'), s2d('2020-01-30')
+    )
+    country_forecast_data = forecast(
+        country_clean_training_data, country_clean_data
+    )
+    city_forecast_data.update(country_forecast_data)
+    return city_forecast_data
