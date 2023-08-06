37cae450c8d6cf8c695eb22876dceb4d8a290bd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:28 +0000

    Add package checkmk-commander/0.8.2/checkmk-commander-0.8.2.tar.gz

---
+++ packages/checkmk-commander/checkmk-commander-0.8.2.tar.gz/checkmk-commander-0.8.2/setup.py
@@ -0,0 +1,67 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='checkmk-commander',
+    version='0.8.2',
+    description='Curses interface to Checkmk Raw.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    # The project's main homepage.
+    url='https://gitlab.com/larsfp/checkmk-commander',
+
+    author='Lars Falk-Petersen',
+    author_email='dev@falkp.no',
+
+    license='GPLv3+',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        "Topic :: System :: Networking",
+        "Environment :: Console :: Curses",
+        "Operating System :: POSIX :: Linux",
+        "Environment :: Console",
+        "Intended Audience :: System Administrators",
+        "Topic :: System :: Networking :: Monitoring"
+    ],
+    keywords='monitoring',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=['urwid', 'requests'],
+    python_requires='>=3.6',
+    scripts=['bin/chkcom'],
+)
+
