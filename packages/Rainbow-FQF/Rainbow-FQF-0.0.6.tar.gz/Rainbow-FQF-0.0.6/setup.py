99491232dbedb75c4c8d8890d3251a567cc8256f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:30 +0000

    Add package Rainbow-FQF/0.0.6/Rainbow-FQF-0.0.6.tar.gz

---
+++ packages/Rainbow-FQF/Rainbow-FQF-0.0.6.tar.gz/Rainbow-FQF-0.0.6/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Rainbow-FQF',
+    version='0.0.6',
+    packages=['FQF'],
+    url='',
+    license='',
+    author='yeachanheo',
+    author_email='',
+    description=''
+)
