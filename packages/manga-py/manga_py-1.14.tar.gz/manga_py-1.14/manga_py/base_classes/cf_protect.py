7e74e8ca546dfc8dc25c6bd23b4aafb052b1673e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:52 +0000

    Add package manga-py/1.14/manga_py-1.14.tar.gz

---
+++ packages/manga-py/manga_py-1.14.tar.gz/manga_py-1.14/manga_py/base_classes/cf_protect.py
@@ -0,0 +1,18 @@
+from sys import stderr
+
+import cloudscraper
+
+
+class CloudFlareProtect:
+    protector = []
+
+    def run(self, url):  # pragma: no cover
+
+        if not self.protector:
+            scraper = cloudscraper.create_scraper()
+            try:
+                self.protector = scraper.get_tokens(url)
+            except Exception as e:
+                print('CF error! %s' % e, file=stderr)
+
+        return self.protector
