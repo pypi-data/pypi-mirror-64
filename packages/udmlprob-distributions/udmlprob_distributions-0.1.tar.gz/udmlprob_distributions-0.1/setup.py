ac82abf3ef68d6c661f2b7a5c25010cbde673422
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:04 +0000

    Add package udmlprob-distributions/0.1/udmlprob_distributions-0.1.tar.gz

---
+++ packages/udmlprob-distributions/udmlprob_distributions-0.1.tar.gz/udmlprob_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='udmlprob_distributions',
+      version='0.1',
+      description='Gaussian and Binomial Distributions',
+      packages=['udmlprob_distributions'],
+      author = 'Ikenna Nwosu',
+      author_email = 'ikenna.nwosu@kositechconsultingltd.com',
+      zip_safe=False)
