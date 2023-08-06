6c613ffb79dd782412766bc45f0e523bee573b61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:09 +0000

    Add package IHEWAdataanalysis/0.0.1/IHEWAdataanalysis-0.0.1-py2.py3-none-any.whl

---
+++ packages/IHEWAdataanalysis/IHEWAdataanalysis-0.0.1-py2.py3-none-any.whl/IHEWAdataanalysis/templates/util.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+**utils**
+
+Utilities for IHEWAdataanalysis template modules.
+
+**Examples:**
+::
+
+    from IHEWAdataanalysis.templates.util import Plot
+"""
+
+
+class Plot(object):
+    def __init__(self):
+        pass
