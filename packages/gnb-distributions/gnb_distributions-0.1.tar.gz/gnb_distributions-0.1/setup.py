c1740e5faa516c0d4295682b1ad37513112bfa01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:55 +0000

    Add package gnb-distributions/0.1/gnb_distributions-0.1.tar.gz

---
+++ packages/gnb-distributions/gnb_distributions-0.1.tar.gz/gnb_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gnb_distributions',
+      version='0.1',
+      description='Gaussian and Bionomial distributions',
+      author="Daniel Chris Daniil",
+      author_email="dannyd1997@g.ucla.edu",
+      packages=['gnb_distributions'],
+      zip_safe=False)
