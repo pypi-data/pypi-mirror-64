ea94fa9f3cdbce301fb8b2d3c5368235d49a9ad1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:42 +0000

    Add package distributions-edu/0.2/distributions_edu-0.2.tar.gz

---
+++ packages/distributions-edu/distributions_edu-0.2.tar.gz/distributions_edu-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_edu',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['distributions_edu'],
+      zip_safe=False)
