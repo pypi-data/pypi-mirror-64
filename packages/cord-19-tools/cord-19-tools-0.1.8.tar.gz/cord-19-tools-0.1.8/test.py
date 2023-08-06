370d6ec0f8f7c765fe5fe5c47bafb2c974c2dc27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:50 +0000

    Add package cord-19-tools/0.1.8/cord-19-tools-0.1.8.tar.gz

---
+++ packages/cord-19-tools/cord-19-tools-0.1.8.tar.gz/cord-19-tools-0.1.8/test.py
@@ -0,0 +1,44 @@
+import cotools
+from pprint import pprint
+
+
+#cotools.download(dir="data")
+
+#noncomm = cotools.Paperset("data/noncomm_use_subset")
+
+data = cotools.Paperset("data/comm_use_subset")
+#pprint(data[0])
+#print(type(data[0]))
+
+# get the text for one feature
+cotools.text(data[0])
+
+cotools.texts(data[:15])
+
+
+#data.apply(len)
+
+# dict
+
+#pprint(data[:2])
+print(type(data[2:5]))
+# list
+
+print(len(data))
+
+# takes about 5gb in memory
+#alldata = data[:]
+
+#len(data)
+
+#len(alldata)
+
+txt = [
+    ["covid", "novel coronavirus"],
+    ["ventilator", "cpap","bipap"]
+]
+
+x = cotools.search(data, txt)
+print(len(x))
+import pdb; pdb.set_trace()  # XXX BREAKPOINT
+
