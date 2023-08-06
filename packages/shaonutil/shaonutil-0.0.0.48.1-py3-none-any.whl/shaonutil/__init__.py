ccadb2a7220c94eb22b3e0c5dd287c1460e6cedb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:41 +0000

    Add package shaonutil/0.0.0.48.1/shaonutil-0.0.0.48.1-py3-none-any.whl

---
+++ packages/shaonutil/shaonutil-0.0.0.48.1-py3-none-any.whl/shaonutil/__init__.py
@@ -0,0 +1,10 @@
+#from shaonutil.stats import ClassA
+from os.path import dirname, basename, isfile, join
+import glob
+import shaonutil
+
+modules = glob.glob(join(dirname(__file__), "*.py"))
+__all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
+for module_name in __all__:
+	__import__('shaonutil'+'.'+module_name)
+	getattr(shaonutil, module_name)
