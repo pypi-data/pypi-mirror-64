7c4466ffcd09d3a0dc312bb72f8b7d6fcd12b493
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:12 +0000

    Add package tentaclio/0.0.1a9/tentaclio-0.0.1a9.tar.gz

---
+++ packages/tentaclio/tentaclio-0.0.1a9.tar.gz/tentaclio-0.0.1a9/src/tentaclio/clients/exceptions.py
@@ -0,0 +1,17 @@
+"""Client exceptions."""
+
+
+class ClientError(Exception):
+    """Basic exception raised for a given product."""
+
+
+class FTPError(ClientError):
+    """Exception encountered over a FTP client connection."""
+
+
+class S3Error(ClientError):
+    """Exception encountered over a S3 client connection."""
+
+
+class HTTPError(ClientError):
+    """Exception encountered over a HTTP client connection."""
