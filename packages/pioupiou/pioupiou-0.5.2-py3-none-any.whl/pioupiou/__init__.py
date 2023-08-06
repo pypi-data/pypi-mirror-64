5fad9e99fc4dae5567eb7e5b23d0c5821e1c12d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:32 +0000

    Add package pioupiou/0.5.2/pioupiou-0.5.2-py3-none-any.whl

---
+++ packages/pioupiou/pioupiou-0.5.2-py3-none-any.whl/pioupiou/__init__.py
@@ -0,0 +1,3 @@
+from pioupiou.avatar_generator import AvatarTheme  # noqa: F401
+from pioupiou.avatar_generator import FolderAvatarTheme  # noqa: F401
+from pioupiou.avatar_generator import Layer  # noqa: F401
