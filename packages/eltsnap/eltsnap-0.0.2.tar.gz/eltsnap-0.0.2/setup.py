7f438d3df818590a002b797304a3848156a858f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:34 +0000

    Add package eltsnap/0.0.2/eltsnap-0.0.2.tar.gz

---
+++ packages/eltsnap/eltsnap-0.0.2.tar.gz/eltsnap-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name='eltsnap',
+    author='Jim Miller',
+    version='0.0.2',
+    license='MIT license',
+    description='Third version test',
+    packages=['PyADSNewVersionAlpha'],
+    package_dir={'PyADSNewVersionAlpha': 'PyADSNewVersionAlpha'},
+    package_data={'PyADSNewVersionAlpha': ['html/*', 'html/bootstrap-4.3.1-dist/css/*',
+                                           'html/bootstrap-4.3.1-dist/js/*',
+                                           'html/images/*'], },
+    include_package_data=True,
+    install_requires=[
+        'pyodbc',
+        'jinja2',
+        'tabulate'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3'
+    ]
+)
