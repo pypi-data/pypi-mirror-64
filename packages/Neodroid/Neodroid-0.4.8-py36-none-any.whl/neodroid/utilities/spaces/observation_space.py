bd7ad7565ac58f70cbdff82d127d0266aa5231ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:11 +0000

    Add package Neodroid/0.4.8/Neodroid-0.4.8-py36-none-any.whl

---
+++ packages/Neodroid/Neodroid-0.4.8-py36-none-any.whl/neodroid/utilities/spaces/observation_space.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from typing import Sequence
+
+from neodroid.utilities.spaces.range import Range
+from neodroid.utilities.spaces.space import Space
+
+__author__ = "Christian Heider Nielsen"
+
+__all__ = ["ObservationSpace"]
+
+
+class ObservationSpace(Space):
+    @property
+    def space(self) -> Sequence:
+        return self.continuous_shape
+
+
+if __name__ == "__main__":
+    acs = ObservationSpace([Range()], ())
+    print(acs)
