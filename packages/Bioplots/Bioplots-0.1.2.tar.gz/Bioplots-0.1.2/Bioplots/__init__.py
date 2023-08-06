f4d3cefbd6bb72e8042a6954840803ada9b02fce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:32 +0000

    Add package Bioplots/0.1.2/Bioplots-0.1.2.tar.gz

---
+++ packages/Bioplots/Bioplots-0.1.2.tar.gz/Bioplots-0.1.2/Bioplots/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# License           : MIT
+# Author            : Jingxin Fu <jingxinfu.tj@gmail.com>
+# Date              : 24/01/2020
+# Last Modified Date: 27/01/2020
+# Last Modified By  : Jingxin Fu <jingxinfu.tj@gmail.com>
+# -*- coding:utf-8 -*-
+
+__version__ = '0.1.2'
+
+from .utils import *
+from .distribution import *
+
