50961cc25f6576da98a4ac13d0d6ca658e50a992
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:55 +0000

    Add package testhdnester/1.0.0/testhdnester-1.0.0.tar.gz

---
+++ packages/testhdnester/testhdnester-1.0.0.tar.gz/testhdnester-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name		= 'testhdnester',
+	version		= '1.0.0',
+	pymodules	= ['testhdnester'],
+	author		= 'pyrookie',
+	author_email	= '1297017056@qq.com',
+	url         		= 'http://www.headfirstlabs.com',
+	description	= 'A simple printer of nested lists'
+         )
