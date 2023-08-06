13f74936057d72410ae1cb560f516a438acda54b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:07 +0000

    Add package pynytimes/0.2.1/pynytimes-0.2.1.tar.gz

---
+++ packages/pynytimes/pynytimes-0.2.1.tar.gz/pynytimes-0.2.1/pynytimes/__init__.py
@@ -0,0 +1,5 @@
+"""A Python 3 wrapper library for the New York Times API"""
+from .api import NYTAPI
+
+__all__ = ["NYTAPI"]
+__version__ = "0.2"
---
+++ packages/pynytimes/pynytimes-0.2.1.tar.gz/pynytimes-0.2.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pynytimes",
+    version="0.2.1",
+    description="A Python wrapper for (most) New York Times APIs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Micha den Heijer",
+    author_email="micha@michadenheijer.com",
+    python_requires="~=3.5",
+    packages=setuptools.find_packages(),
+    include_package_data = True,
+    url="https://github.com/michadenheijer/pynytimes",
+    license="MIT",
+    install_requires = ["requests==2.22.0"],
+    classifiers = [
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3 :: Only"
+    ]
+)
