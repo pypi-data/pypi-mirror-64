e8a6a3041d14f3a8a37c2d405cb949e3d2190773
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:31 +0000

    Add package fakeme/0.0.0.dev1/fakeme-0.0.0.dev1.tar.gz

---
+++ packages/fakeme/fakeme-0.0.0.dev1.tar.gz/fakeme-0.0.0.dev1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['fakeme', 'fakeme.cli', 'fakeme.examples.space_ship_parts']
+
+package_data = \
+{'': ['*'], 'fakeme': ['examples/cli_usage/*', 'examples/cli_usage/schemas/*']}
+
+install_requires = \
+['mimesis>=4.0,<5.0', 'pandas>=1.0,<2.0', 'ply>=3.11,<4.0']
+
+entry_points = \
+{'console_scripts': ['fakeme = fakeme.cli:cli']}
+
+setup_kwargs = {
+    'name': 'fakeme',
+    'version': '0.0.0.dev1',
+    'description': 'Relative Dataframes Generator: generate tables with data, that depend each other',
+    'long_description': None,
+    'author': 'xnuinside',
+    'author_email': 'xnuinside@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/xnuinside/fakeme',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
