7c3b9d05f5a32cc300a1071417c36ab2945e7097
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:41 +0000

    Add package distributionsGB/0.1/distributionsGB-0.1.tar.gz

---
+++ packages/distributionsGB/distributionsGB-0.1.tar.gz/distributionsGB-0.1/README.md
@@ -0,0 +1,10 @@
+# distributionsGB
+
+This package contains the Gaussian and Binomial class, and their parent class: Distribution
+
+# Installation
+
+Install this library directly into an activated virtual environment:
+
+    $ pip install distributionsGB
+    
---
+++ packages/distributionsGB/distributionsGB-0.1.tar.gz/distributionsGB-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributionsGB',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['distributionsGB'],
+      author = 'Carolina Negrelli',
+      author_email = 'caro.negrelli@gmail.com',
+      zip_safe=False)
