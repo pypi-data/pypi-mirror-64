8ee915df2c65ba2755c35cb4fefbfb390d10f895
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:47 +0000

    Add package cds-api-client/0.0.1/cds_api_client-0.0.1-py3-none-any.whl

---
+++ packages/cds-api-client/cds_api_client-0.0.1-py3-none-any.whl/cds/api/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from cds.api.accounts_api import AccountsApi
+from cds.api.banking_api import BankingApi
+from cds.api.common_api import CommonApi
+from cds.api.customer_api import CustomerApi
+from cds.api.direct_debits_api import DirectDebitsApi
+from cds.api.discovery_api import DiscoveryApi
+from cds.api.payees_api import PayeesApi
+from cds.api.products_api import ProductsApi
+from cds.api.scheduled_payments_api import ScheduledPaymentsApi
