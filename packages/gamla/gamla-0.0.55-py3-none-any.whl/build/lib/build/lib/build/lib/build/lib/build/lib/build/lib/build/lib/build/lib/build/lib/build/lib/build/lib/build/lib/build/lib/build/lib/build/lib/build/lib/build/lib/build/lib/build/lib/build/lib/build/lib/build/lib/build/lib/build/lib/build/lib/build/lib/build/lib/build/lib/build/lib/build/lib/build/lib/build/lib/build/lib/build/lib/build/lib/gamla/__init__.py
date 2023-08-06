b4bc6f1ba1f315285118a12c2347624487d50445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:09 +0000

    Add package gamla/0.0.55/gamla-0.0.55-py3-none-any.whl

---
+++ packages/gamla/gamla-0.0.55-py3-none-any.whl/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/gamla/__init__.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+
+from .functional import *
+from .functional_async import *
+from .graph import *
+from .graph_async import *
+from .io_utils import *
---
+++ packages/gamla/gamla-0.0.55-py3-none-any.whl/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/gamla/graph_async.py
@@ -0,0 +1,21 @@
+import itertools
+
+import toolz
+
+from gamla import functional, functional_async, graph
+
+
+@toolz.curry
+async def agroupby_many(f, it):
+    return await functional_async.apipe(
+        it,
+        functional_async.amap(
+            functional_async.acompose_left(
+                functional_async.apair_with(f),
+                functional.star(lambda x, y: (x, [y])),
+                functional.star(itertools.product),
+            )
+        ),
+        toolz.concat,
+        graph.edges_to_graph,
+    )
