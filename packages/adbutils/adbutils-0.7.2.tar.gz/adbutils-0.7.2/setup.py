8791d245257e122db923996e7a77fdc420696901
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:11 +0000

    Add package adbutils/0.7.2/adbutils-0.7.2.tar.gz

---
+++ packages/adbutils/adbutils-0.7.2.tar.gz/adbutils-0.7.2/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+#
+# Licensed under MIT
+#
+
+from __future__ import print_function
+
+import setuptools
+
+setuptools.setup(setup_requires=['pbr'], python_requires='>=3.5', pbr=True)
