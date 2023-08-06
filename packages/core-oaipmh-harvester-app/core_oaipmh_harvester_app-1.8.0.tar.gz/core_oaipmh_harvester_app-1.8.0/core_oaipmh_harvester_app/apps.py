0ae417b175dff944d8e87527d61fe72c1c3825eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:55 +0000

    Add package core-oaipmh-harvester-app/1.8.0/core_oaipmh_harvester_app-1.8.0.tar.gz

---
+++ packages/core-oaipmh-harvester-app/core_oaipmh_harvester_app-1.8.0.tar.gz/core_oaipmh_harvester_app-1.8.0/core_oaipmh_harvester_app/apps.py
@@ -0,0 +1,22 @@
+""" Apps file for setting oai-pmh when app is ready
+"""
+from django.apps import AppConfig
+
+from core_main_app.utils.databases.mongoengine_database import init_text_index
+from core_oaipmh_harvester_app.components.oai_record.models import OaiRecord
+from core_oaipmh_harvester_app.tasks import init_harvest
+
+
+class HarvesterAppConfig(AppConfig):
+    """ Core application settings
+    """
+    name = 'core_oaipmh_harvester_app'
+
+    def ready(self):
+        """ Run when the app is ready
+
+        Returns:
+
+        """
+        init_text_index(OaiRecord)
+        init_harvest()
