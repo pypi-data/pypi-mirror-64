067b551c06687f45bbcfa17486edb3a72dbd6a11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:22 +0000

    Add package suanpan/0.12.4/suanpan-0.12.4-py3-none-any.whl

---
+++ packages/suanpan/suanpan-0.12.4-py3-none-any.whl/suanpan/error/component.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+from __future__ import absolute_import, print_function
+
+from suanpan.error import base
+
+
+class ComponentError(base.Error):
+    MESSAGE = "Suanpan Component Error: {}"
+
+
+class ComponentHandlerNotSet(ComponentError):
+    MESSAGE = "Suanpan Component Handler Not Set: {}"
