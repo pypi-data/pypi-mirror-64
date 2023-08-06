a0d25aee70fdba35c0b961dfd244ed98a19723f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:22 +0000

    Add package geodesign/0.02/geodesign-0.02.tar.gz

---
+++ packages/geodesign/geodesign-0.02.tar.gz/geodesign-0.02/geodesign/__init__.py
@@ -0,0 +1,6 @@
+from experimentaldesign import latin_design
+from experimentaldesign import select_geoms
+from experimentaldesign import check_topology
+from experimentaldesign import rotate_plot
+from experimentaldesign import check_crs
+from experimentaldesign import composite_union
