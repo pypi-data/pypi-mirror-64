34da2afc668dde29ef706a5ed42a2f97321b43d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:00 +0000

    Add package medcat/0.3.0.9/medcat-0.3.0.9.tar.gz

---
+++ packages/medcat/medcat-0.3.0.9.tar.gz/medcat-0.3.0.9/setup.py
@@ -0,0 +1,46 @@
+import setuptools
+from setuptools.command.install import install
+
+with open("./README.md", "r") as fh:
+    long_description = fh.read()
+
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        import subprocess
+        import sys
+
+        print("Installing the missing models for scispacy\n")
+        pkg = 'https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.2.4/en_core_sci_md-0.2.4.tar.gz'
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', pkg])
+
+        install.run(self)
+
+
+setuptools.setup(
+    name="medcat",
+    version="0.3.0.9",
+    author="w-is-h",
+    author_email="w.kraljevic@gmail.com",
+    description="Concept annotation tool for Electronic Health Records",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CogStack/MedCAT",
+    packages=['medcat', 'medcat.utils', 'medcat.preprocessing'],
+    install_requires=[
+        'numpy~=1.15',
+        'pandas~=0.23',
+        'gensim~=3.7',
+        'spacy==2.2.4',
+        'scipy~=1.1',],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    cmdclass={
+        'install': PostInstallCommand,
+        },
+
+)
