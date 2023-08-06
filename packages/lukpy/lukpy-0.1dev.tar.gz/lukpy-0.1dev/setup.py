dba58a8530fe93c696fe5bf8bc867a737270da93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:11 +0000

    Add package lukpy/0.1dev/lukpy-0.1dev.tar.gz

---
+++ packages/lukpy/lukpy-0.1dev.tar.gz/lukpy-0.1dev/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='lukpy',
+    version='0.1dev',
+    packages=['stats', 'signal'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
