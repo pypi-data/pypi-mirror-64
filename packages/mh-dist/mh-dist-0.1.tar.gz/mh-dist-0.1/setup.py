707c672b4ba1eabcc3c54b6703bfeda90f589c7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:57 +0000

    Add package mh-dist/0.1/mh-dist-0.1.tar.gz

---
+++ packages/mh-dist/mh-dist-0.1.tar.gz/mh-dist-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='mh-dist',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['mh-dist'],
+      author = 'Marcelo Almeida Hirota',
+      author_email = 'marcelohirota@gmail.com',
+      zip_safe=False)
