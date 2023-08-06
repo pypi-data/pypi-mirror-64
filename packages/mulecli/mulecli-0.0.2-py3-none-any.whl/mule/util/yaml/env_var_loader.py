e8e2c5bac5111cb1b17557d879d641597cb70f97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:17 +0000

    Add package mulecli/0.0.2/mulecli-0.0.2-py3-none-any.whl

---
+++ packages/mulecli/mulecli-0.0.2-py3-none-any.whl/mule/util/yaml/env_var_loader.py
@@ -0,0 +1,26 @@
+import os
+import re
+import yaml
+from mule.error import messages
+from termcolor import cprint
+
+env_var_path_matcher = re.compile(r'.*\$\{([^}^{]+)\}.*')
+
+def path_constructor(loader, node):
+    value = os.path.expandvars(node.value)
+    if env_var_path_matcher.match(value):
+        cprint(
+            messages.CANNOT_EVALUATE_ENV_VAR.format(node.value),
+            'yellow',
+            attrs=['bold']
+        )
+    return os.path.expandvars(node.value)
+
+def getYamlLoaderWithEnvVars():
+    yaml.add_implicit_resolver('!path', env_var_path_matcher)
+    yaml.add_constructor('!path', path_constructor)
+    return yaml
+
+def readYamlWithEnvVars(stream):
+    loader = getYamlLoaderWithEnvVars()
+    return loader.load(stream, Loader=yaml.FullLoader)
