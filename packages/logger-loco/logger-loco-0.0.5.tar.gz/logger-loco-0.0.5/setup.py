4c1e3a1b0deb17165e017063716d40ebdfd1d28f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:44 +0000

    Add package logger-loco/0.0.5/logger-loco-0.0.5.tar.gz

---
+++ packages/logger-loco/logger-loco-0.0.5.tar.gz/logger-loco-0.0.5/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="logger-loco",
+    version="0.0.5",
+    author="Sergey Lushkovsky",
+    author_email="s.lushkovsky@gmail.com",
+    description="Comment-driven python loggin package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=['logging', 'logger', 'log', 'comments'],
+    url="https://github.com/lushkovsky-s/logger-loco",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
