ebc1ae7a4a0db5b9540cb709363e8fc0f6bdf002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:27 +0000

    Add package shiftleft-scan-reports/1.0.2/shiftleft_scan_reports-1.0.2-py3-none-any.whl

---
+++ packages/shiftleft-scan-reports/shiftleft_scan_reports-1.0.2-py3-none-any.whl/reporter/utils.py
@@ -0,0 +1,30 @@
+def auto_text_highlight(text):
+    """
+    Automatically highlight interesting parts in the text
+    :param text:
+    :return:
+    """
+    text = text.replace("In class", "<span class='bg-light chip'>Class</span>")
+    text = text.replace("In method", "<span class='bg-light chip'>Method</span>")
+    text = text.replace("At ", "<span class='bg-light chip'>File</span>")
+    text = text.replace("line ", "<span class='bg-light chip'>line</span>")
+    text = text.replace("\n", "<br/>")
+    return text
+
+
+def auto_colourize(text):
+    """
+    Automatically convert text to suitable colour
+    :param text:
+    :return:
+    """
+    text = text.lower()
+    if text == "critical":
+        return "error"
+    if text == "high":
+        return "warning"
+    if text == "medium":
+        return "dark"
+    if text == "low":
+        return "success"
+    return text
