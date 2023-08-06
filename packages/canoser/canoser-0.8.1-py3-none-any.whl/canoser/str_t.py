0485a5d5c490f638278830db3faf4f835e729604
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:01 +0000

    Add package canoser/0.8.1/canoser-0.8.1-py3-none-any.whl

---
+++ packages/canoser/canoser-0.8.1-py3-none-any.whl/canoser/str_t.py
@@ -0,0 +1,27 @@
+from canoser.int_type import Uint32
+from canoser.base import Base
+
+class StrT(Base):
+    @classmethod
+    def encode(self, value):
+        output = b''
+        utf8 = value.encode('utf-8')
+        output += Uint32.encode(len(utf8))
+        output += utf8
+        return output
+
+    @classmethod
+    def decode(self, cursor):
+        strlen = Uint32.decode(cursor)
+        return str(cursor.read_bytes(strlen), encoding='utf-8')
+
+    @classmethod
+    def check_value(cls, value):
+        if not isinstance(value, str):
+            raise TypeError('value {} is not string'.format(value))
+
+    @classmethod
+    def to_json_serializable(cls, obj):
+        return obj
+
+
