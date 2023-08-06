6cb20c0bb620a02447d9f80faef0977ed34d5d36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:27 +0000

    Add package luftdaten/0.6.4/luftdaten-0.6.4.tar.gz

---
+++ packages/luftdaten/luftdaten-0.6.4.tar.gz/luftdaten-0.6.4/README.rst
@@ -0,0 +1,42 @@
+python-luftdaten
+================
+
+Python client for interacting with `luftdaten.info <http://luftdaten.info/>`_.
+
+This module is not official, developed, supported or endorsed by luftdaten.info.
+
+Installation
+------------
+
+The module is available from the `Python Package Index <https://pypi.python.org/pypi>`_.
+
+.. code:: bash
+
+    $ pip3 install luftdaten
+
+On a Fedora-based system.
+
+.. code:: bash
+
+    $ sudo dnf -y install python3-luftdaten
+
+Usage
+-----
+
+The file ``example.py`` contains an example about how to use this module.
+
+Development
+-----------
+
+For development is recommended to use a ``venv``.
+
+.. code:: bash
+
+    $ python3.6 -m venv .
+    $ source bin/activate
+    $ python3 setup.py develop
+
+License
+-------
+
+``python-luftdaten`` is licensed under MIT, for more details check LICENSE.
---
+++ packages/luftdaten/luftdaten-0.6.4.tar.gz/luftdaten-0.6.4/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""Luftdaten Python Wrapper setup script."""
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.rst'), encoding='utf-8') as readme_file:
+    long_description = readme_file.read()
+
+if sys.argv[-1] == 'publish':
+    os.system('python3 setup.py sdist upload')
+    sys.exit()
+
+setup(
+    name='luftdaten',
+    version='0.6.4',
+    description='Python API for interacting with luftdaten.info.',
+    long_description=long_description,
+    url='https://github.com/fabaff/python-luftdaten',
+    download_url='https://github.com/fabaff/python-luftdaten/releases',
+    author='Fabian Affolter',
+    author_email='fabian@affolter-engineering.ch',
+    license='MIT',
+    install_requires=['aiohttp', 'async_timeout'],
+    packages=['luftdaten'],
+    zip_safe=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Utilities',
+    ],
+)
