61f90c87bd56980038a5b6015065ada3df84ecfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:29 +0000

    Add package y2z-distributions/0.5/y2z_distributions-0.5.tar.gz

---
+++ packages/y2z-distributions/y2z_distributions-0.5.tar.gz/y2z_distributions-0.5/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='y2z_distributions',
+      version='0.5',
+      description='Gaussian and Binomail distributions',
+      author ='Wisdom Chibeze',
+      packages=['y2z_distributions'],
+      author_email= 'nwachukwuchibeze29@outlook.com',
+      zip_safe=False)
