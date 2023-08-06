d237133f698c86ffa88c55f9fce0f2f69e26bd61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:25 +0000

    Add package covid19-open-api/0.0.2/covid19-open-api-0.0.2.tar.gz

---
+++ packages/covid19-open-api/covid19-open-api-0.0.2.tar.gz/covid19-open-api-0.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md', 'r') as readme, open('production_requirements.txt', 'r') as dependencies:
+    long_description = readme.read()
+    dependencies = dependencies.read()
+
+    kwargs = {
+        'name': 'covid19-open-api',
+        'version': '0.0.2',
+        'author': 'Natworpong Loyswai',
+        'author_email': 'Natworpong.Loyswai@mail.kmutt.ac.th',
+        'description': 'Thai Covid-19 status',
+        'long_description': long_description,
+        'long_description_content_type': 'text/markdown',
+        'url': 'https://github.com/rsxss/covid19-open-api',
+        'packages': find_packages(),
+        'classifiers': [
+            'Programming Language :: Python :: 3'
+        ],
+        'keywords': ['covid19'],
+        'install_requires': dependencies
+    }
+
+    setup(**kwargs)
