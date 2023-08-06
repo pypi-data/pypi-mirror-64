d4872715e40feffb929fbaeb730a078d676f6ba6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:38 +0000

    Add package nester_hzf_1/1.1.0/nester_hzf_1-1.1.0.tar.gz

---
+++ packages/nester_hzf_1/nester_hzf_1-1.1.0.tar.gz/nester_hzf_1-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'nester_hzf_1',
+    version = '1.1.0',
+    py_modules = ['nester_1'],
+    autor = 'hzf_python',
+    autor_email = '15184455137@163.com',
+    url = 'http://www.hzfdechuangzi.club',
+    description = 'A simple printer of nested lists',
+    )
