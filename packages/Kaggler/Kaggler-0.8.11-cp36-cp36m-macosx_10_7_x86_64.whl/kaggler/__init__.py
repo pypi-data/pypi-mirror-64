d536e9ac4250d60b01004ef0d2505e124e7a93b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:11 +0000

    Add package Kaggler/0.8.11/Kaggler-0.8.11-cp36-cp36m-macosx_10_7_x86_64.whl

---
+++ packages/Kaggler/Kaggler-0.8.11-cp36-cp36m-macosx_10_7_x86_64.whl/kaggler/__init__.py
@@ -0,0 +1,10 @@
+__version__ = '0.8.11'
+__all__ = ['const',
+           'data_io',
+           'ensemble',
+           'feature_selection',
+           'metrics',
+           'model',
+           'online_model',
+           'preprocessing',
+           'util']
