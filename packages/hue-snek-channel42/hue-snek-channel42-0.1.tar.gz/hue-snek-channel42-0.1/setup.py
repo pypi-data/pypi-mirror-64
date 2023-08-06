8e97a81cfadaa72c62d1f8e2d3d905ffacf6bddd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:17 +0000

    Add package hue-snek-channel42/0.1/hue-snek-channel42-0.1.tar.gz

---
+++ packages/hue-snek-channel42/hue-snek-channel42-0.1.tar.gz/hue-snek-channel42-0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='hue-snek-channel42',
+    version='0.1',
+    author="Channel 42",
+    author_email="",
+    description="A library for the Philips Hue API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/channel-42/hue-snek",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
