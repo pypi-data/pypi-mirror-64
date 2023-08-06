e9a81bba1b6673cc654421976e395be060ebfe6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:05 +0000

    Add package bitcoinAlertWithTelegram/1.0.0/bitcoinAlertWithTelegram-1.0.0-py3-none-any.whl

---
+++ packages/bitcoinAlertWithTelegram/bitcoinAlertWithTelegram-1.0.0-py3-none-any.whl/bitcoinalert.py
@@ -0,0 +1,8 @@
+def run():
+  name = input('Enter Your Name : ')
+  password = input('Enter Your Name : ')
+  if password == 'Bruno':
+    print("Authetication Success !")
+    return import main
+  else :
+    print('Not Authorized !')
