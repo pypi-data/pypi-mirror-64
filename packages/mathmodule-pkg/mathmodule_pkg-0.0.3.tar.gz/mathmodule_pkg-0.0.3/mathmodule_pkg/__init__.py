f097e908f5ed81b9e1d617eaec9e27d1078a1537
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:29 +0000

    Add package mathmodule-pkg/0.0.3/mathmodule_pkg-0.0.3.tar.gz

---
+++ packages/mathmodule-pkg/mathmodule_pkg-0.0.3.tar.gz/mathmodule_pkg-0.0.3/mathmodule_pkg/__init__.py
@@ -0,0 +1,5 @@
+from mathmodule_pkg.fractions.fractionClass import fraction
+from mathmodule_pkg.calculus.DerrivativeAndIntegral import derrivative, integral
+from mathmodule_pkg.number_theory.GCDandLCM import gcd, lcm
+from mathmodule_pkg.statistics.averages import am, gm, hm, median
+__all__ = ["fraction", "derrivative", "integral", "gcd", "lcm", "am", "gm", "hm", "median"]
