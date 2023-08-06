a5ee7f15c551685046bddac9c980aaa138861a6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:38 +0000

    Add package probability-and-distributions/0.2/probability_and_distributions-0.2.tar.gz

---
+++ packages/probability-and-distributions/probability_and_distributions-0.2.tar.gz/probability_and_distributions-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+      name='probability_and_distributions',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['probability_and_distributions'],
+      author='Wesley Jeng',
+      zip_safe=False
+)
