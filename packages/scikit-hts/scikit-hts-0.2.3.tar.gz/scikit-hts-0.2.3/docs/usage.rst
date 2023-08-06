d0f95a443984e471651231ea6f057649874596c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:10 +0000

    Add package scikit-hts/0.2.3/scikit-hts-0.2.3.tar.gz

---
+++ packages/scikit-hts/scikit-hts-0.2.3.tar.gz/scikit-hts-0.2.3/docs/usage.rst
@@ -0,0 +1,12 @@
+=====
+Usage
+=====
+
+To use scikit-hts in a project::
+
+
+    from hts import HTSRegressor
+    reg = HTSRegressor()
+
+
+
---
+++ packages/scikit-hts/scikit-hts-0.2.3.tar.gz/scikit-hts-0.2.3/tests/integ/test_fit_regressor.py
@@ -0,0 +1,44 @@
+import pandas
+
+from hts import HTSRegressor
+from hts.core.result import HTSResult
+
+
+def test_instantiate_regressor():
+    ht = HTSRegressor()
+    assert ht.model == 'prophet'
+    assert isinstance(ht.hts_result, HTSResult)
+    assert isinstance(ht.hts_result.residuals, dict)
+
+
+def test_fit_regressor_uv(load_df_and_hier_uv):
+    hierarchical_sine_data, sine_hier = load_df_and_hier_uv
+    ht = HTSRegressor(model='holt_winters', revision_method='OLS')
+    ht.fit(df=hierarchical_sine_data, nodes=sine_hier)
+    assert isinstance(ht.hts_result.errors, dict)
+    assert isinstance(ht.hts_result.models, dict)
+    assert 'total' in ht.hts_result.models
+
+
+def test_predict_regressor_uv(load_df_and_hier_uv):
+    hierarchical_sine_data, sine_hier = load_df_and_hier_uv
+    hsd = hierarchical_sine_data.head(200)
+    for model in ['holt_winters', 'auto_arima']:
+        ht = HTSRegressor(model=model, revision_method='OLS')
+
+        ht.fit(df=hsd, nodes=sine_hier)
+        preds = ht.predict(steps_ahead=10)
+
+        assert isinstance(preds, pandas.DataFrame)
+
+        # base + steps ahead
+        assert len(preds) == len(hsd) + 10
+        assert len(ht.hts_result.forecasts['total']) == len(hsd) + 10
+        assert 'total' in ht.hts_result.errors
+        assert 'total' in ht.hts_result.forecasts
+        assert 'total' in ht.hts_result.residuals
+
+        assert 'a' in ht.hts_result.errors
+        assert 'a' in ht.hts_result.forecasts
+        assert 'a' in ht.hts_result.residuals
+
