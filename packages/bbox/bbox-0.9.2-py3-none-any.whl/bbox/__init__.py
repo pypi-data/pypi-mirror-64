a997fcc7e6a1e99e54a7bdf32bf9ef1e421fe851
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:45 +0000

    Add package bbox/0.9.2/bbox-0.9.2-py3-none-any.whl

---
+++ packages/bbox/bbox-0.9.2-py3-none-any.whl/bbox/__init__.py
@@ -0,0 +1,16 @@
+"""Main package initialization"""
+
+from bbox.bbox2d import BBox2D
+from bbox.bbox2d_list import BBox2DList
+from bbox.box_modes import XYWH, XYXY
+from bbox.bbox3d import BBox3D
+from bbox import utils, metrics
+
+
+__author__ = "Varun Agrawal"
+__copyright__ = "GTRC & Argo AI LLC"
+__version__ = "0.9.2"
+__maintainer__ = "Varun Agrawal"
+__email__ = "varunagrawal@gatech.com"
+__status__ = "Beta"
+__license__ = "MIT"
