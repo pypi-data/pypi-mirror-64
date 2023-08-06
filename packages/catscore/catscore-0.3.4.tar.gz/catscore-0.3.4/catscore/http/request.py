830b98c3d2f04a17a48368fa49878c1dbaac5f7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:09 +0000

    Add package catscore/0.3.4/catscore-0.3.4.tar.gz

---
+++ packages/catscore/catscore-0.3.4.tar.gz/catscore-0.3.4/catscore/http/request.py
@@ -0,0 +1,136 @@
+from catscore.lib.logger import CatsLogging as logging
+import requests
+from requests import Session
+import json
+from bs4 import BeautifulSoup
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from abc import ABCMeta, abstractmethod
+from catscore.http.error import CatsRequestSessionError
+from catscore.http.response import Response, ResponseHtml, ResponseJson
+import time
+from asgiref.sync import sync_to_async
+import asyncio
+import aiohttp
+
+class CatsRequest:
+    def __init__(self, proxy=None, verify=True):
+        """[summary]
+        """
+        self.session = requests.Session()
+        self.proxy = proxy
+        self.verify = verify
+
+    @classmethod
+    def create_instance_from_json(cls, json_path:str):
+        with open(json_path, "r") as f:
+            j = json.load(f)
+            proxy = None
+            try:
+                http_proxy = j["web_driver"]["http_proxy"]
+                https_proxy = j["web_driver"]["https_proxy"]
+                proxy = {"http": http_proxy, "https": https_proxy}
+            except Exception:
+                print("proxy is None")
+            d = CatsRequest(proxy=proxy)
+        return d
+    
+    def __enter__(self):
+        """[summary]
+        
+        Returns:
+            [type] -- [description]
+        """
+        return self
+
+    def __exit__(self, ex_type, ex_value, trace):
+        """[summary]
+        
+        Arguments:
+            ex_type {[type]} -- [description]
+            ex_value {[type]} -- [description]
+            trace {[type]} -- [description]
+        """
+        self.close()
+        
+    def close(self):
+        """[summary]
+        """
+        self.session.close()
+
+    def get_cookie(self, key):
+        return self.session.cookies.get(key)
+
+    def get_cookies(self):
+        return self.session.cookies.get_dict()
+
+    def _mk_result(self, ret, response_content_type: str):
+        if response_content_type == "html":
+            soup = BeautifulSoup(ret.content, features="html.parser")
+            return ResponseHtml(ret.headers, soup, ret)
+        elif response_content_type == "json":
+            soup = BeautifulSoup(ret.content, features="html.parser")
+            return ResponseJson(ret.headers, json.loads(str(soup)), ret)
+        else:
+            return Response(ret.headers, ret.content, ret)
+
+    def _check_status_code(self, url, status_code):
+        if status_code != 200:
+            raise CatsRequestSessionError(f"{url} response code is {status_code}")
+        return True
+
+    def retry_get(self, url, response_content_type=None, retry_num=4, wait=1):
+        reponse = None
+        for i in range(retry_num):
+            try:
+                reponse = self.get(url, response_content_type)
+                return reponse
+            except Exception:
+                print(f"retry_get: {url} retry {i}")
+                time.sleep(wait)
+        raise CatsRequestSessionError(f"{url} retry count is {retry_num}")
+
+    def get(self, url, response_content_type=None):
+        """[summary]
+        
+        Arguments:
+            url {[type]} -- [description]
+        
+        Keyword Arguments:
+            response_content_type {[type]} -- [html or json] (default: {None})
+        
+        Raises:
+            RuntimeError: [description]
+        
+        Returns:
+            [type] -- [description]
+        """
+        if self.proxy:
+            ret = self.session.get(url, proxies= self.proxy, verify=self.verify)
+        else:
+            ret = self.session.get(url)
+        self._check_status_code(url, ret.status_code)
+        return self._mk_result(ret, response_content_type)
+
+    def post(self, url, post_data, response_content_type):
+        if self.proxy:
+            ret = self.session.post(url, post_data, proxies=self.proxy, verify=self.verify)
+        else:
+            ret = self.session.post(url, post_data)
+        self._check_status_code(url, ret.status_code)
+        return self._mk_result(ret, response_content_type)
+
+    def download(self, url:str, fullpath:str, request_type:str = "get", post_data=None):
+        if request_type == "post":
+            bi = self.post(url=url, post_data=post_data)
+        else:
+            bi = self.get(url=url, proxy=proxy).content
+        with open(fullpath, "wb") as f:
+            f.write(bi)
+
+    def get_global_info(self):
+        url = "https://ipinfo.io"
+        return self.get(url=url, response_content_type="json").content
---
+++ packages/catscore/catscore-0.3.4.tar.gz/catscore-0.3.4/catscore/lib/logger.py
@@ -0,0 +1,184 @@
+import logging
+import logging.handlers
+from datetime import datetime
+import os
+import errno
+import logging
+import smtplib
+from email.mime.text import MIMEText
+from email.utils import formatdate
+import socket
+import json
+from dataclasses import dataclass
+import slackweb
+import sys
+
+class Gmail:
+    smtp_url = 'smtp.gmail.com'
+    smtp_port = 587
+
+    def __init__(self, username, password, to_address):
+        """[summary]
+
+        Arguments:
+            username {[type]} -- [description]
+            password {[type]} -- [description]
+            to_address {[type]} -- [description]
+        """
+        self.username = username
+        self.password = password
+        self.to_address = to_address
+
+    def login(self):
+        """[summary]
+        """
+        self.smtp = smtplib.SMTP(self.smtp_url, self.smtp_port)
+        self.smtp.ehlo()
+        self.smtp.starttls()
+        self.smtp.ehlo()
+        self.smtp.login(self.username, self.password)
+
+    def send(self, body, subject):
+        msg = MIMEText(body)
+        msg['Subject'] = subject
+        msg['From'] = self.username
+        msg['To'] = self.conf.to_address
+        msg['Date'] = formatdate()
+        self.smtp.sendmail(self.username, self.to_address, msg.as_string())
+
+    def close(self):
+        self.smtp.close()
+
+@dataclass
+class Slack:
+    notice: str
+    error: str
+
+class CatsLogging:
+    """[summary]
+    """
+    app_name = ""
+    datefmt = '%d-%m-%Y:%H:%M:%S'
+    format = '%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s'
+    slack_notice = None
+    slack_error = None
+
+    @classmethod
+    def init(cls, app_name, ouput_dir, log_level, gmail:Gmail=None, slack:Slack=None):
+        """[Init logger]
+        
+        Arguments:
+            app_name {[type]} -- [description]
+            ouput_dir {[type]} -- [description]
+            log_level {[type]} -- [debug/info/error/critical]
+        
+        Keyword Arguments:
+            gmail {Gmail} -- [Send logmessage over error level] (default: {None})
+        """
+        cls.app_name = app_name
+        
+        if log_level == "debug":
+            level = logging.DEBUG
+        elif log_level == "info":
+            level = logging.INFO
+        elif log_level == "warn":
+            level = logging.WARN
+        elif log_level== "error":
+            level = logging.ERROR
+        elif log_level == "critical":
+            level = logging.CRITICAL
+        else:
+            level = logging.INFO
+
+        logging.basicConfig(level=level,
+                            filename=f"{ouput_dir}/{app_name}.log",
+                            format=cls.format,
+                            datefmt=cls.datefmt)
+        logging.getLogger('sqlalchemy.engine').setLevel(level)
+        if gmail:
+                try:
+                    gmail.login()
+                    handler = logging.handlers.SMTPHandler(mailhost=(gmail.smtp_url, gmail.smtp_port),
+                                                        fromaddr=gmail.username,
+                                                        toaddrs=gmail.to_address,
+                                                        subject=app_name,
+                                                        credentials=(gmail.username, gmail.password),
+                                                        secure=())
+                    handler.setLevel(logging.ERROR)
+                    logger = logging.getLogger('root')
+                    logger.addHandler(handler)
+                except smtplib.SMTPAuthenticationError:
+                    print("Cannot login to google")
+        if slack:
+            cls.slack_notice = slackweb.Slack(url=slack.notice)
+            cls.slack_error = slackweb.Slack(url=slack.error)
+            
+    @classmethod
+    def init_from_json(cls, json_path):
+        with open(json_path, "r") as f:
+            j = json.load(f)
+            print(j)
+            gmail = None
+            try:
+                gmail = Gmail(
+                    username=j["gmail"]["username"],
+                    password=j["gmail"]["password"],
+                    to_address=j["gmail"]["to_address"])
+            except Exception:
+                print("gmail dosen't set")
+            slack = None
+            try:
+                slack = Slack(
+                    notice=j["slack"]["notice"],
+                    error=j["slack"]["error"])
+            except Exception:
+                print("slack dosen't set")
+            cls.init(app_name=j["log"]["app_name"], ouput_dir=j["log"]["output_dir"], log_level=j["log"]["level"], gmail=gmail, slack=slack)
+        
+    @classmethod
+    def __add_message_option(cls, mesasge, func_name:str=None):
+        host_name = socket.gethostname()
+        ip_addr  =socket.gethostbyname(socket.gethostname())
+        return f"{host_name},{ip_addr},{func_name},{mesasge}"
+
+    @classmethod
+    def notify(cls, message, func_name:str=None):
+        try:
+            cls.slack_notice.notify(text=cls.app_name + ",notify," + cls.__add_message_option(message,func_name))
+        except Exception:
+            print(f"cls.slack_notice has Exception: {sys.exc_info()}")
+              
+    @classmethod
+    def debug(cls, message, func_name:str=None):
+        #logger = logging.getLogger('root')
+        #print(message)
+        logging.debug(cls.__add_message_option(message,func_name))
+
+    @classmethod
+    def info(cls, message, func_name:str=None):
+        #logger = logging.getLogger('root')
+        logging.info(cls.__add_message_option(message,func_name))
+
+    @classmethod
+    def warn(cls, message, func_name:str=None):
+        #logger = logging.getLogger('root')
+        logging.warn(cls.__add_message_option(message,func_name))
+        
+    @classmethod
+    def error(cls, message, func_name:str=None):
+        logger = logging.getLogger('root')
+        logger.error(cls.__add_message_option(message,func_name))
+        try:
+            cls.slack_error.notify(text=cls.app_name + ",error," + cls.__add_message_option(message,func_name))
+        except Exception:
+            print(f"error: cls.slack_error has Exception: {sys.exc_info()}")
+
+    @classmethod
+    def critical(cls, message, func_name:str=None):
+        logger = logging.getLogger('root')
+        logger.critical(cls.__add_message_option(message,func_name))
+        if cls.slack_error:
+            try:
+                cls.slack_error.notify(text=cls.app_name + ",critical," + cls.__add_message_option(message,func_name))
+            except Exception:
+                print(f"critical: cls.slack_error has Exception: {sys.exc_info()}")
