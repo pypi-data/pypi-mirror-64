5645f5e3a89526911687d86ec7f74a2282665ed8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:43 +0000

    Add package flask-feature-flag/0.0.1/flask_feature_flag-0.0.1-py3-none-any.whl

---
+++ packages/flask-feature-flag/flask_feature_flag-0.0.1-py3-none-any.whl/flask_feature_flag/__init__.py
@@ -0,0 +1,11 @@
+from .decorators import is_enabled # noqa
+from .decorators import command_enabled # noqa
+from .decorators import route_enabled # noqa
+from .decorators import use_case_enabled # noqa
+
+__all__ = (
+    'is_enabled',
+    'command_enabled',
+    'route_enabled',
+    'use_case_enabled'
+)
