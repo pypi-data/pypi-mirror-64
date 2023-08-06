5f2e2b5a4a7f2636b4545e79826c98c3e6dccc10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:22 +0000

    Add package st3r-distributions/0.1/st3r_distributions-0.1.tar.gz

---
+++ packages/st3r-distributions/st3r_distributions-0.1.tar.gz/st3r_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='st3r_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['st3r_distributions'],
+      authoer=['st3r'],
+      zip_safe=False)
