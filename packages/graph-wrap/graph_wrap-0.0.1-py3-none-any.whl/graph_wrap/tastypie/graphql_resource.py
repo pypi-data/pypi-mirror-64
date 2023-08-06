ca1ca5329fd37af6a8da8ae3f1735811bb4fcf16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:45 +0000

    Add package graph-wrap/0.0.1/graph_wrap-0.0.1-py3-none-any.whl

---
+++ packages/graph-wrap/graph_wrap-0.0.1-py3-none-any.whl/graph_wrap/tastypie/graphql_resource.py
@@ -0,0 +1,15 @@
+from graphene_django.views import GraphQLView
+from tastypie.resources import Resource
+
+
+class GraphQLResource(Resource):
+    class Meta:
+        resource_name = 'graphql'
+        allowed_methods = ['post']
+
+    def dispatch(self, request_type, request, **kwargs):
+        from graph_wrap import schema
+        schema = schema()
+        view = GraphQLView.as_view(schema=schema)
+        return view(request)
+
