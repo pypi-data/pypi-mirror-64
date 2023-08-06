70a29b934acd7ba5975b91e8c2712f73fbb20d86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:27 +0000

    Add package lins-plugboleto/1.0.0.3/lins_plugboleto-1.0.0.3.tar.gz

---
+++ packages/lins-plugboleto/lins_plugboleto-1.0.0.3.tar.gz/lins_plugboleto-1.0.0.3/lins_plugboleto/outros.py
@@ -0,0 +1,15 @@
+class Outros(object):
+
+    def __init__(self, emi_boleto,categoria,postagem,cod_emissao,cod_distribuicao,outros_acrescimos,adicionais,instrucoes,parcela,variacao,codigo_ref,tipo_cobranca):
+        self.TituloEmissaoBoleto = None
+        self.TituloCategoria = None
+        self.TituloPostagemBoleto = None
+        self.TituloCodEmissaoBloqueto = None
+        self.TituloCodDistribuicaoBloqueto = None
+        self.TituloOutrosAcrescimos = None
+        self.TituloInformacoesAdicionais = None
+        self.TituloInstrucoes = None
+        self.TituloParcela = None
+        self.TituloVariacaoCarteira = None
+        self.TituloCodigoReferencia = None
+        self.TituloTipoCobranca = None
