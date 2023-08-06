eba70f05a527bb669f2f13164a50a0348b5ffecc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:26 +0000

    Add package yamlns/0.8/yamlns-0.8.tar.gz

---
+++ packages/yamlns/yamlns-0.8.tar.gz/yamlns-0.8/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+readme = open("README.rst").read()
+
+setup(
+	name = "yamlns",
+	version = "0.8",
+	description = "YAML serializable dictionary with dual item and attribute accessors",
+	author = "David Garcia Garzon",
+	author_email = "voki@canvoki.net",
+	url = 'https://github.com/GuifiBaix/python-yamlns',
+	long_description = readme,
+	license = 'GNU General Public License v3 or later (GPLv3+)',
+	packages=find_packages(exclude=['*_[tT]est*']),
+	scripts=[
+		'yamlns/nstemplate.py',
+		],
+	install_requires=[
+		'PyYAML>=5.3.1', # security
+		'nose',
+		'rednose',
+	],
+	include_package_data = True,
+	test_suite = 'yamlns',
+#	test_runner = 'colour_runner.runner.ColourTextTestRunner',
+	classifiers = [
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 3',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+		'Intended Audience :: Developers',
+		'Development Status :: 5 - Production/Stable',
+		'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+		'Operating System :: OS Independent',
+	],
+)
+
