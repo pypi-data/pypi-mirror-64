ece52886dde6c0acd00032c0165cd9512106b090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:03 +0000

    Add package puma-iar/0.1/puma_iar-0.1.tar.gz

---
+++ packages/puma-iar/puma_iar-0.1.tar.gz/puma_iar-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'puma_iar',
+      version = '0.1',
+      description = 'functions for Puma project',
+      packages = ['puma_iar'],
+      author = 'Carolina Negrelli',
+      author_email = 'caro.negrelli@gmail.com',
+      zip_safe = False)
