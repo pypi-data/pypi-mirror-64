30822d09d4c7c636d167b79d939ed0d62a330092
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:36 +0000

    Add package Bibekdistribution/0.1/Bibekdistribution-0.1.tar.gz

---
+++ packages/Bibekdistribution/Bibekdistribution-0.1.tar.gz/Bibekdistribution-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Bibekdistribution',
+      version='0.1',
+      description='Gaussian and Binary distributions',
+      packages=['Bibekdistribution'],
+      author= 'Bibek Shah Shankhar',
+      author_email='shahshankharbibek@gmail.com',
+      zip_safe=False)
