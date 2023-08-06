3e0e277df2572630e6f19dba1118c4411b75e103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:04 +0000

    Add package mlnd-distributions-rv/0.1/mlnd_distributions_rv-0.1.tar.gz

---
+++ packages/mlnd-distributions-rv/mlnd_distributions_rv-0.1.tar.gz/mlnd_distributions_rv-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='mlnd_distributions_rv',
+      version='0.1',
+      description='Distributions containing Gaussian and Binomial Distributions',
+      packages=['mlnd_distributions_rv'],
+      author='Rahul Vaswani',
+      author_email = 'firstname_first3lettersoflast@gmail.com',
+      zip_safe=False)
