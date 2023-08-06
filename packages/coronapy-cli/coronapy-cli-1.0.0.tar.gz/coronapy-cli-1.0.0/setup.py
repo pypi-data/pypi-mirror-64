e87619aecd91ce893289d526f0d723e1871d3886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:28 +0000

    Add package coronapy-cli/1.0.0/coronapy-cli-1.0.0.tar.gz

---
+++ packages/coronapy-cli/coronapy-cli-1.0.0.tar.gz/coronapy-cli-1.0.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="coronapy-cli",
+    version="1.0.0",
+    author="MouadBH",
+    author_email="mouad123b@gmail.com",
+    description="A command line tool to fetch worldwide data about Corona Virus",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords = ['corona', 'covid-19', 'cli', 'corona-cli'], 
+    url="https://github.com/MouadBH/coronapy-cli",
+    packages=find_packages(),
+    package_data={},
+    install_requires=['Click', 'pyfiglet', 'prettytable', 'yaspin', 'termgraph', 'colorama', 'termcolor'],
+    entry_points='''
+        [console_scripts]
+        coronapy=cli:cli
+    ''',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
