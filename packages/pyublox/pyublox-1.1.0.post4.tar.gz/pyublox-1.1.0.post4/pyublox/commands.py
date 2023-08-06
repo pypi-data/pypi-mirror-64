fe7ff103f11a98241f7002e6c9a83237ebc2e63d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:40 +0000

    Add package pyublox/1.1.0.post4/pyublox-1.1.0.post4.tar.gz

---
+++ packages/pyublox/pyublox-1.1.0.post4.tar.gz/pyublox-1.1.0.post4/pyublox/commands.py
@@ -0,0 +1,11 @@
+import sys
+
+def config(**kwargs):
+
+    
+    sys.stderr.write("Sending configuration to ublox ... ")
+
+
+def record(**kwargs):
+
+    sys.stderr.write("Writing ubx data stream to file ... ")
