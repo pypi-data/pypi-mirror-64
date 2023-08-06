c5001d73f6b74558885789ced6dbb2b13ee1cb38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:37 +0000

    Add package desc/2.0.2/desc-2.0.2-py3-none-any.whl

---
+++ packages/desc/desc-2.0.2-py3-none-any.whl/desc/__init__.py
@@ -0,0 +1,19 @@
+#from . import original as og
+from . import tools
+from . import models
+from . import datasets
+from . import utilities
+
+from anndata import read_h5ad
+from scanpy.api.pp import normalize_per_cell, highly_variable_genes, log1p, scale
+
+from .models.desc import train
+from .tools.test import run_desc_test
+from .tools.read import read_10X
+from .tools.write import write_desc_result
+from .tools.preprocessing import scale_bygroup
+#from .tools.downstream import run_tsne
+__version__ = '2.0.2'
+
+
+
