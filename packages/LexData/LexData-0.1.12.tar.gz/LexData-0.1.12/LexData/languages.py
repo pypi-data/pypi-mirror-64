b584276916ee9a0c81d2d577e76c72d4d45bf96b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:30 +0000

    Add package LexData/0.1.12/LexData-0.1.12.tar.gz

---
+++ packages/LexData/LexData-0.1.12.tar.gz/LexData-0.1.12/LexData/languages.py
@@ -0,0 +1,10 @@
+"""
+This module simply contains a few common Languages with their language-codes
+and QIDs for easier use.
+"""
+from LexData import Language
+
+# feel free to add more languages
+en = Language("en", "Q1860")
+de = Language("de", "Q188")
+fr = Language("fr", "Q150")
