7e07ab1bbf907e2b0da6eac731fb38a8abb3b9a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:31 +0000

    Add package moose-frank/5.0/moose-frank-5.0.tar.gz

---
+++ packages/moose-frank/moose-frank-5.0.tar.gz/moose-frank-5.0/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+
+version = "5.0"
+
+setup(
+    name="moose-frank",
+    packages=find_packages(),
+    version=version,
+    description="A Python package packed with tools that are commonly used in "
+    "Moose projects.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Sven Groot (Mediamoose)",
+    author_email="sven@mediamoose.nl",
+    url="https://gitlab.com/mediamoose/moose-frank/tree/v{}".format(version),
+    download_url="https://gitlab.com/mediamoose/moose-frank/repository/v{}/archive.tar.gz".format(
+        version
+    ),
+    include_package_data=True,
+    install_requires=[],
+    license="MIT",
+    zip_safe=False,
+    keywords=["moose", "frank", "frankenstein"],
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Framework :: Django",
+        "Framework :: Django :: 1.11",
+        "Framework :: Django :: 2.0",
+        "Framework :: Django :: 2.1",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
