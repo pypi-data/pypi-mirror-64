227629a58bde7cc0d9639ca8457ab99bffbede14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:25 +0000

    Add package JsonFlatten_xh/0.0.0/JsonFlatten_xh-0.0.0.tar.gz

---
+++ packages/JsonFlatten_xh/JsonFlatten_xh-0.0.0.tar.gz/JsonFlatten_xh-0.0.0/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+#=============================================================================
+# FileName: setup.py
+# Desc:
+# Author: Alfons
+# Email: xiaohuihui100@gmail.com
+# Version: 0.0.1
+# LastChange:  2020/3/30 10:35
+# History:
+#=============================================================================
+"""
+from setuptools import setup
+
+setup(
+    name='JsonFlatten_xh',
+    version='0.0.0',
+    description='This is tool to flatten json data.',
+    author='Alfons',
+    author_email='xiaohuihui100@gmail.com',
+    url='https://github.com/Alfonsxh/JsonFlatten',
+    packages=["JsonFlatten"],
+)
