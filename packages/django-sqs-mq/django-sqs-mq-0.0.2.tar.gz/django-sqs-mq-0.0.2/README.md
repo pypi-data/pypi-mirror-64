d41ebc52d5361da374a77a7d8a7df524db702734
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:25 +0000

    Add package django-sqs-mq/0.0.2/django-sqs-mq-0.0.2.tar.gz

---
+++ packages/django-sqs-mq/django-sqs-mq-0.0.2.tar.gz/django-sqs-mq-0.0.2/README.md
@@ -0,0 +1,33 @@
+# django-sqs-mq
+
+# 概括
+
+## Documentation
+本包名字为*packer*,使用方法包括...
+
+
+#### Supported Django and Python versions
+
+Django \ Python | 3.4 | 3.5 | 3.6 | 3.7 | 3.8 |
+--------------- | --- | --- | --- | --- | --- |
+1.11 |  *  |  *  |  *  |  *  |  *  |
+2.0  |  *  |  *  |  *  |  *  |  *  |
+
+
+### Installation
+To install django-sqs-mq:
+```shell
+$ pip install django-sqs-mq
+```
+
+## Contribute
+
+
+
+## Code of Conduct
+
+
+
+## License
+
+Copyright (c) 2018-2020   [MIT license](https://opensource.org/licenses/MIT).
---
+++ packages/django-sqs-mq/django-sqs-mq-0.0.2.tar.gz/django-sqs-mq-0.0.2/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author: sun
+@license: (C) Copyright 2016-2019, Light2Cloud (Beijing) Web Service Co., LTD
+@contact: wenhaijie@light2cloud.com
+@software: L2CloudCMP
+@file: setup.py.py
+@ide: PyCharm
+@time: 2020/4/1 13:22
+@desc:
+"""
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="django-sqs-mq",
+    version="0.0.2",
+    author="whj",
+    author_email="whj@linuxbs.com",
+    description="A simple wrapper for boto3 for receive, and sending, to an AWS SQS queue.",
+    long_description=long_description,
+    license="MIT",
+    url="https://github.com/firestarry/django-sqs.git",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'django>=1.11.29',
+        'boto3>=1.12.33'
+    ],
+    zip_safe=True,
+    python_requires='>=3.6',
+)
