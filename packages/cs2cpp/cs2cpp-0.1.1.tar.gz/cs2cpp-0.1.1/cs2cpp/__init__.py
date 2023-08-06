8420e1e00cb3b2fba1277e8e1c1b7558fe955ed1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:50 +0000

    Add package cs2cpp/0.1.1/cs2cpp-0.1.1.tar.gz

---
+++ packages/cs2cpp/cs2cpp-0.1.1.tar.gz/cs2cpp-0.1.1/cs2cpp/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# authors: Konard, Ethosa
+
+from .CSharpToCpp import CSharpToCpp
+
+__version__ = "0.1.1"
+__copyright__ = "2019"
+__authors__ = ["Ethosa", "Konard"]
+
+if __name__ == '__main__':
+    print(CSharpToCpp)
