29af710122128b62bb7bcb2fb7c1f5c37fc911ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:57 +0000

    Add package aws-solutions-konstruk.aws-lambda-dynamodb/0.8.0/aws-solutions-konstruk.aws-lambda-dynamodb-0.8.0.tar.gz

---
+++ packages/aws-solutions-konstruk.aws-lambda-dynamodb/aws-solutions-konstruk.aws-lambda-dynamodb-0.8.0.tar.gz/aws-solutions-konstruk.aws-lambda-dynamodb-0.8.0/src/aws_solutions_konstruk/aws_lambda_dynamodb/_jsii/__init__.py
@@ -0,0 +1,17 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.aws_dynamodb
+import aws_cdk.aws_lambda
+import aws_cdk.core
+import aws_solutions_konstruk.core
+__all__ = []
+
+publication.publish()
