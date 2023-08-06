567b9bbab872918ebc59a83534ef1034b377d024
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:19 +0000

    Add package apitester/0.0/apitester-0.0-py3-none-any.whl

---
+++ packages/apitester/apitester-0.0-py3-none-any.whl/apitester-0.0.data/scripts/apitester.py
@@ -0,0 +1,23 @@
+'''
+Created on 31-Mar-2020
+
+@author: nandal
+'''
+
+
+_version = "0.0"
+
+
+
+
+def version():
+    return _version
+
+def generate_api_tests():
+    raise NotImplementedError("To be implemented")
+
+
+if __name__ == '__main__':
+    print(version())
+
+
