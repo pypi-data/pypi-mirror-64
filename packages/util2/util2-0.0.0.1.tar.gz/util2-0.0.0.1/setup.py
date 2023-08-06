94095832e0474b36471f2d0a49670011d0f52f25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:57 +0000

    Add package util2/0.0.0.1/util2-0.0.0.1.tar.gz

---
+++ packages/util2/util2-0.0.0.1.tar.gz/util2-0.0.0.1/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+import sys
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as readme_file:
+    readme = readme_file.read()
+
+
+info = sys.version_info
+
+setup(
+    name='util2',
+    version='0.0.0.1',
+    install_requires=[],
+    description='This is utils library for Python.',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='Nishizumi',
+    author_email='daiman003@yahoo.co.jp',
+    url='https://github.com/tanaka0079/libs/python/util2/',
+    packages=find_packages(),
+    include_package_data=True,
+    keywords='util',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Natural Language :: English',
+        "License :: OSI Approved :: MIT License",
+        'Programming Language :: Python :: 3.8',
+        "Operating System :: OS Independent",
+    ],
+    entry_points = {
+        'console_scripts': ['elec=util2.util2:main'],
+    },
+    test_suite="util2-test",
+)
---
+++ packages/util2/util2-0.0.0.1.tar.gz/util2-0.0.0.1/util2/__main__.py
@@ -0,0 +1,5 @@
+from util2.util2 import main
+
+
+if __name__ == "__main__":
+    main()
