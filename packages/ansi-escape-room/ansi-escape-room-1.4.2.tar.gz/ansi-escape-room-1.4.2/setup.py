9201fbe0c8555e540dde2c33e94cec6c1a2f5372
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:04 +0000

    Add package ansi-escape-room/1.4.2/ansi-escape-room-1.4.2.tar.gz

---
+++ packages/ansi-escape-room/ansi-escape-room-1.4.2.tar.gz/ansi-escape-room-1.4.2/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+from ansi_escape_room import __version__
+
+
+setup(
+    name="ansi-escape-room",
+    packages=["ansi_escape_room"],
+    version=__version__,
+    description="Simple library for color and formatting to terminal",
+    long_description=open("README.rst").read(),
+    keywords=["color", "colour", "paint", "ansi", "terminal", "linux",
+              "python"],
+    author='HotOffThe Hamster',
+    author_email='hotoffthehamster+ansiescaperoom@gmail.com',
+    url="https://github.com/hotoffthehamster/ansi-escape-room",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Operating System :: POSIX :: Other",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.0",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Unix Shell",
+        "Topic :: Terminals"]
+)
---
+++ packages/ansi-escape-room/ansi-escape-room-1.4.2.tar.gz/ansi-escape-room-1.4.2/tests/test_1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""test foreground and background colors"""
+
+import time
+from ansi_escape_room import fg, bg, attr
+
+
+def main():
+
+    for color in range(0, 256):
+        print("%s This text is colored %s" % (fg(color), attr("reset")))
+        print("%s This background is colored %s" % (bg(color), attr("reset")))
+        time.sleep(0.1)
+
+if __name__ == "__main__":
+    main()
