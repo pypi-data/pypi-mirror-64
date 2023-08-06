ff2ed1295271949073fc3565f8b86631f8abb7a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:31 +0000

    Add package ruia-pyppeteer/0.0.6/ruia_pyppeteer-0.0.6.tar.gz

---
+++ packages/ruia-pyppeteer/ruia_pyppeteer-0.0.6.tar.gz/ruia_pyppeteer-0.0.6/ruia_pyppeteer/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+"""
+ Created by howie.hu at 2018/11/22.
+"""
+
+from .request import PyppeteerRequest
+from .spider import PyppeteerSpider
---
+++ packages/ruia-pyppeteer/ruia_pyppeteer-0.0.6.tar.gz/ruia_pyppeteer-0.0.6/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+"""
+ Created by howie.hu at 2018/11/22.
+"""
+
+import os
+
+from setuptools import find_packages, setup
+
+
+def read(file_name):
+    with open(os.path.join(os.path.dirname(__file__), file_name)) as f:
+        return f.read()
+
+
+setup(
+    name="ruia_pyppeteer",
+    version="0.0.6",
+    author="Howie Hu",
+    description="ruia_pyppeteer - A Ruia plugin for loading javascript - pyppeteer.",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    author_email="xiaozizayang@gmail.com",
+    install_requires=["ruia>=0.6.1", "pyppeteer"],
+    url="https://github.com/ruia-plugins/ruia-pyppeteer",
+    packages=find_packages(),
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    project_urls={
+        "Documentation": "https://github.com/python-ruia/ruia-pyppeteer",
+        "Source": "https://github.com/python-ruia/ruia-pyppeteer",
+    },
+)
