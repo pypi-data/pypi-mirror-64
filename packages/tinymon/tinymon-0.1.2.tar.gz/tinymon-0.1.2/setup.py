88bb83d789d2c72c4d8a1ee61e5c4706ba108565
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:37 +0000

    Add package tinymon/0.1.2/tinymon-0.1.2.tar.gz

---
+++ packages/tinymon/tinymon-0.1.2.tar.gz/tinymon-0.1.2/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, Extension, find_packages
+import tinymon
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+    
+
+setup(
+    name="tinymon",
+    version=tinymon.__version__,
+
+    author="mrzjo",
+    author_email="mrzjo05@gmail.com",
+    url="https://gitlab.com/telelian/peripheral-library/pyfront-vs301/-/tree/master/tinymon",
+
+    description="tinymon",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    
+    packages=find_packages(),
+    package_data = {
+        'font': ['*'],
+        'image': ['videonot.png']
+    },
+
+    install_requires=[
+        'numpy>=1.18.1'
+        ,'Pillow>=7.0.0'
+        ,'ssd1362-py>=0.1.1'
+        ,'loguru>=0.4.1'
+    ],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: System :: Hardware'
+    ],
+)
---
+++ packages/tinymon/tinymon-0.1.2.tar.gz/tinymon-0.1.2/tinymon/textctrl.py
@@ -0,0 +1,19 @@
+from PIL import ImageFont, ImageDraw
+
+class TextCtrl():
+    def __init__(self, pos=(10,10) , fontsize=14, font='font/NanumBarunGothicLight.ttf', text=None):
+        self.font = ImageFont.truetype(font, fontsize)
+        self.pos = pos
+        if text:
+            self.setText(text)
+        
+    def setText(self, text):
+        if isinstance(text, str):
+            self.text = text
+        else:
+            self.text = str(text)
+            
+    def draw(self, im=None, fill=255):
+        draw = ImageDraw.Draw(im)
+        draw.text(self.pos, self.text, font=self.font, fill=fill)
+        return im
