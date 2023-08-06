3ed74b46369ee0daf5f88bc2dd6fb60328056561
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:52 +0000

    Add package wexpect/3.3.1/wexpect-3.3.1.tar.gz

---
+++ packages/wexpect/wexpect-3.3.1.tar.gz/wexpect-3.3.1/tests/test_readline.py
@@ -0,0 +1,55 @@
+import wexpect
+import time
+import sys
+import os
+import unittest
+from tests import PexpectTestCase
+
+here = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, here)
+
+print(wexpect.__version__)
+
+# With quotes (C:\Program Files\Python37\python.exe needs quotes)
+python_executable = '"' + sys.executable + '" '
+child_script = here + '\\lines_printer.py'
+
+class ReadLineTestCase(PexpectTestCase.PexpectTestCase):
+    def testReadline(self):
+        fooPath = python_executable + ' '  + child_script
+        prompt = ': '
+        num = 5
+        
+        # Start the child process
+        p = wexpect.spawn(fooPath)
+        # Wait for prompt
+        p.expect(prompt)
+        p.sendline(str(num))
+        p.expect('Bye!\r\n')
+        expected_lines = p.before.splitlines(True) # Keep the line end
+        expected_lines += [p.match.group()]
+        
+        # Start the child process
+        p = wexpect.spawn(fooPath)
+        # Wait for prompt
+        p.expect(prompt)
+        
+        p.sendline(str(num))
+        for i in range(num +2): # +1 the line of sendline +1: Bye
+            line = p.readline()
+            self.assertEqual(expected_lines[i], line)
+            
+        # Start the child process
+        p = wexpect.spawn(fooPath)
+        # Wait for prompt
+        p.expect(prompt)
+        
+        p.sendline(str(num))
+        readlines_lines = p.readlines()
+        self.assertEqual(expected_lines, readlines_lines)
+        
+            
+if __name__ == '__main__':
+    unittest.main()
+
+suite = unittest.makeSuite(ReadLineTestCase,'test')
