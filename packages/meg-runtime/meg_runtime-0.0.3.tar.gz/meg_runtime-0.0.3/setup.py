5512cfb67b803d54111a35e6c963a4e559c7553b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:12 +0000

    Add package meg-runtime/0.0.3/meg_runtime-0.0.3.tar.gz

---
+++ packages/meg-runtime/meg_runtime-0.0.3.tar.gz/meg_runtime-0.0.3/setup.py
@@ -0,0 +1,39 @@
+""" Multimedia Extensible Git package setup script """
+
+import os
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+runtime_pkg_version = '0.0.0' if 'MEG_RUNTIME_PKG_VERSION' not in os.environ else os.environ['MEG_RUNTIME_PKG_VERSION']
+
+required_packages = [
+    'kivy',
+    'kivy-deps.glew',
+    'kivy-deps.sdl2',
+    'pillow',
+    'pygit2',
+    'python-dateutil',
+    'requests'
+]
+
+setuptools.setup(
+    name="meg_runtime",
+    version=runtime_pkg_version,
+    author="Multimedia Extensible Git",
+    author_email="kyletpugh@users.noreply.github.com",
+    description="Multimedia Extensible Git Runtime Library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MultimediaExtensibleGit/Runtime",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=required_packages,
+    license='MIT'
+)
