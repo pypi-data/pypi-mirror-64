720e339844adcaeab0f7a58e2cdf6ccb7d1590d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:45 +0000

    Add package pySRA/0.4.11/pySRA-0.4.11.tar.gz

---
+++ packages/pySRA/pySRA-0.4.11.tar.gz/pySRA-0.4.11/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as fp:
+    readme = fp.read()
+
+with open('HISTORY.rst') as fp:
+    history = fp.read()
+
+setup(
+    name='pySRA',
+    version='0.4.11',
+    description='Site Response Analysis with Python',
+    long_description=readme + '\n\n' + history,
+    author='Albert Kottke',
+    author_email='albert.kottke@gmail.com',
+    url='http://github.com/arkottke/pysra',
+    packages=find_packages(),
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'pyrvt',
+        'scipy',
+        'setuptools',
+        'typing',
+    ],
+    extras_require={
+        'dataframe': ['pandas'],
+    },
+    keywords='site response',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering',
+        'Intended Audience :: Science/Research',
+    ],
+    test_suite='tests', )
