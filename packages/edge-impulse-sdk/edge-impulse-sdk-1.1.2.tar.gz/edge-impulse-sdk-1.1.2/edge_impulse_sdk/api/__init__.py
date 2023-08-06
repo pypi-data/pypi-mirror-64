c19f93bb0b9f44290a9e132f208ac85989377ce3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:22 +0000

    Add package edge-impulse-sdk/1.1.2/edge-impulse-sdk-1.1.2.tar.gz

---
+++ packages/edge-impulse-sdk/edge-impulse-sdk-1.1.2.tar.gz/edge-impulse-sdk-1.1.2/edge_impulse_sdk/api/__init__.py
@@ -0,0 +1,24 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from edge_impulse_sdk.api.auth_api import AuthApi
+from edge_impulse_sdk.api.cdn_api import CDNApi
+from edge_impulse_sdk.api.classify_api import ClassifyApi
+from edge_impulse_sdk.api.dsp_api import DSPApi
+from edge_impulse_sdk.api.deployment_api import DeploymentApi
+from edge_impulse_sdk.api.devices_api import DevicesApi
+from edge_impulse_sdk.api.export_api import ExportApi
+from edge_impulse_sdk.api.health_api import HealthApi
+from edge_impulse_sdk.api.impulse_api import ImpulseApi
+from edge_impulse_sdk.api.jobs_api import JobsApi
+from edge_impulse_sdk.api.learn_api import LearnApi
+from edge_impulse_sdk.api.login_api import LoginApi
+from edge_impulse_sdk.api.organization_jobs_api import OrganizationJobsApi
+from edge_impulse_sdk.api.organization_requires_admin_api import OrganizationRequiresAdminApi
+from edge_impulse_sdk.api.organizations_api import OrganizationsApi
+from edge_impulse_sdk.api.projects_api import ProjectsApi
+from edge_impulse_sdk.api.raw_data_api import RawDataApi
+from edge_impulse_sdk.api.requires_sudo_api import RequiresSudoApi
+from edge_impulse_sdk.api.user_api import UserApi
