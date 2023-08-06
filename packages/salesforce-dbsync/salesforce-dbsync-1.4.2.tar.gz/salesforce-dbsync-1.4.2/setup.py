05034b25f603e3b607f3ad3ece8488756f21cf7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:43 +0000

    Add package salesforce-dbsync/1.4.2/salesforce-dbsync-1.4.2.tar.gz

---
+++ packages/salesforce-dbsync/salesforce-dbsync-1.4.2.tar.gz/salesforce-dbsync-1.4.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name='salesforce-dbsync',
+	version='1.4.2',
+	author="S Satapathy",
+	author_email="shubhakant.satapathy@gmail.com",
+	description="Python library to download data from Salesforce and synchronize with a relational database",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/satapathy/pypi-salesforce-dbsync",
+	packages=setuptools.find_packages(),
+	install_requires=[
+		'simple_salesforce',
+		'mysql.connector',
+		'screenwriter',
+	],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+ )
