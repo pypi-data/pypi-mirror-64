c3b8ad36f4dd512c2d9181bf13c76c7c0ffdf147
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:25 +0000

    Add package GEDIpy/0.0/GEDIpy-0.0.tar.gz

---
+++ packages/GEDIpy/GEDIpy-0.0.tar.gz/GEDIpy-0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+ 
+setup(
+    name='GEDIpy',
+    packages=['GEDIpy'],
+    version='0.0',
+    license='LGPL v3',
+    description="GEDIpy is a Python Package for NASA's Global Ecosystem Dynamics Investigation (GEDI) mission data extraction, analysis,processing and visualization.",
+    author='Eduin',
+    author_email='eduin@sigma.blue',
+    url='https://github.com/EduinHSERNA/pygedi',
+    keywords='GEDI',
+    classifiers=['Programming Language :: Python'],
+)
