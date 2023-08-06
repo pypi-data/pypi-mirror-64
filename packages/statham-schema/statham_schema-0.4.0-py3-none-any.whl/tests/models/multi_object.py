10c4dc6dc7b91d989f21cd33e1bffcefd20e3072
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:51 +0000

    Add package statham-schema/0.4.0/statham_schema-0.4.0-py3-none-any.whl

---
+++ packages/statham-schema/statham_schema-0.4.0-py3-none-any.whl/tests/models/multi_object.py
@@ -0,0 +1,35 @@
+from typing import List, Union
+
+from statham.dsl.constants import Maybe
+from statham.dsl.elements import (
+    AnyOf,
+    Array,
+    Boolean,
+    Integer,
+    Null,
+    Number,
+    OneOf,
+    Object,
+    ObjectOptions,
+    String,
+)
+from statham.dsl.property import Property
+
+
+class Category(Object):
+
+    required_name: str = Property(String(), required=True)
+
+
+class Child(Object):
+
+    name: Maybe[str] = Property(String())
+
+    category: Maybe[Category] = Property(Category)
+
+
+class Model(Object):
+
+    children: Maybe[List[Child]] = Property(Array(Child))
+
+    category: Maybe[Category] = Property(Category)
