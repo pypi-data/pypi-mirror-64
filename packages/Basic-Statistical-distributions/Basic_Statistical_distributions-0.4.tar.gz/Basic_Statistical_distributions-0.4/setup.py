ec4d0065eaad7b09b8eaab857cbe4682967a7d6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:51 +0000

    Add package Basic-Statistical-distributions/0.4/Basic_Statistical_distributions-0.4.tar.gz

---
+++ packages/Basic-Statistical-distributions/Basic_Statistical_distributions-0.4.tar.gz/Basic_Statistical_distributions-0.4/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Basic_Statistical_distributions',
+      version='0.4',
+      description='Gaussian and Binomial distributions',
+      packages=['Basic_Statistical_distributions'],
+      zip_safe=False)
