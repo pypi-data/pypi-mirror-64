4b6099b225975b1e3f86560c2fea307e3b0e3a69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:13 +0000

    Add package datasmoothie/0.1/datasmoothie-0.1.tar.gz

---
+++ packages/datasmoothie/datasmoothie-0.1.tar.gz/datasmoothie-0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+
+setuptools.setup(
+    name="datasmoothie",
+    packages=['datasmoothie'],
+    version="0.1",
+    licence='MIT',
+    url="https://github.com/datasmoothie/datasmoothie-client-python",
+    download_url="https://github.com/datasmoothie/datasmoothie-client-python/archive/v0.1.tar.gz",
+    author="Geir Freysson",
+    author_email="geir@datasmoothie.com",
+    description="Python wrapper for the v2  of the Datasmoothie API.",
+    keywords=['surveys', 'market research', 'weighting', 'significance tests'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
