3dc25b0213f6caa9a267070f47961267ee671cbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:52 +0000

    Add package rofi-menu/0.1.0/rofi-menu-0.1.0.tar.gz

---
+++ packages/rofi-menu/rofi-menu-0.1.0.tar.gz/rofi-menu-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['rofi_menu', 'rofi_menu.contrib']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'rofi-menu',
+    'version': '0.1.0',
+    'description': 'Create rofi menus via python',
+    'long_description': None,
+    'author': 'miphreal',
+    'author_email': 'miphreal@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
