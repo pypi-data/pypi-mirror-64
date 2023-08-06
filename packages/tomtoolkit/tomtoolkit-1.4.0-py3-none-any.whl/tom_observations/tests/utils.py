9e3219179f990b7303b68b584c9187d758f8f146
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:20 +0000

    Add package tomtoolkit/1.4.0/tomtoolkit-1.4.0-py3-none-any.whl

---
+++ packages/tomtoolkit/tomtoolkit-1.4.0-py3-none-any.whl/tom_observations/tests/utils.py
@@ -0,0 +1,72 @@
+from django import forms
+from datetime import timedelta
+from django.utils import timezone
+from astropy import units
+
+from tom_observations.facility import GenericObservationFacility, GenericObservationForm
+from tom_observations.observing_strategy import GenericStrategyForm
+
+# Site data matches built-in pyephem observer data for Los Angeles
+SITES = {
+    'Los Angeles': {
+        'latitude': 34.052222,
+        'longitude': -117.756306,
+        'elevation': 86.847092
+    },
+    'Siding Spring': {
+        'sitecode': 'coj',
+        'latitude': -31.272,
+        'longitude': 149.07,
+        'elevation': 1116
+    },
+}
+
+
+class FakeFacilityForm(GenericObservationForm):
+    test_input = forms.CharField(help_text='fake form input')
+
+
+class FakeFacilityStrategyForm(GenericStrategyForm):
+    pass
+
+
+class FakeFacility(GenericObservationFacility):
+    name = 'FakeFacility'
+    observation_types = [('FakeFacility Observation', 'OBSERVATION')]
+
+    def get_form(self, observation_type):
+        return FakeFacilityForm
+
+    def get_strategy_form(self, observation_type):
+        return FakeFacilityStrategyForm
+
+    def get_observing_sites(self):
+        return SITES
+
+    def get_observation_url(self, observation_id):
+        return ''
+
+    def data_products(self, observation_record):
+        return [{'id': 'testdpid'}]
+
+    def get_observation_status(self, observation_id):
+        return {
+            'state': 'COMPLETED',
+            'scheduled_start': timezone.now() + timedelta(hours=1),
+            'scheduled_end': timezone.now() + timedelta(hours=2)
+        }
+
+    def get_terminal_observing_states(self):
+        return ['COMPLETED', 'FAILED', 'CANCELED', 'WINDOW_EXPIRED']
+
+    def submit_observation(self, payload):
+        return ['fakeid']
+
+    def get_flux_constant(self):
+        return units.erg / units.angstrom
+
+    def get_wavelength_units(self):
+        return units.angstrom
+
+    def validate_observation(self, observation_payload):
+        return True
