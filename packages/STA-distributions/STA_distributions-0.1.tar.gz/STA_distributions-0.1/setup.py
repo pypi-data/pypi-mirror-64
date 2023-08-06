eec828e71cc91bb3d2cee0f0baaaa0b38e6fc1af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:05 +0000

    Add package STA-distributions/0.1/STA_distributions-0.1.tar.gz

---
+++ packages/STA-distributions/STA_distributions-0.1.tar.gz/STA_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='STA_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['STA_distributions'],
+      author = 'Sloan Austermann',
+      email = 'sloantaustermann@gmail.com',
+      zip_safe=False)
