0bda35286525c1a3d55ed93730dc187ef4901a7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:50 +0000

    Add package test-numpy-setuptools/1.2/test_numpy_setuptools-1.2.tar.gz

---
+++ packages/test-numpy-setuptools/test_numpy_setuptools-1.2.tar.gz/test_numpy_setuptools-1.2/setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+
+# coding: utf-8
+
+from distutils.core import setup
+from setuptools import setup
+
+setup(name='test_numpy_setuptools',  # 打包后的包文件名
+      version='1.2',
+      description='test_numpy_setuptools backpack',  # 说明
+      author='smilencezq',
+      author_email='smilencezq@163.com',
+      url='',
+      py_modules=['test_numpy_setuptools.test_numpy_setuptools_main',
+                  'test_numpy_setuptools.test_numpy_setuptools_multi'],
+      requires=['numpy']  # 你要打包的文件
+      )
