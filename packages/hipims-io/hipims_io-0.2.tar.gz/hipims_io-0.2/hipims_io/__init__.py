1df70e91dc45a738805a4bfa0b8e6b2d61c682d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:50 +0000

    Add package hipims-io/0.2/hipims_io-0.2.tar.gz

---
+++ packages/hipims-io/hipims_io-0.2.tar.gz/hipims_io-0.2/hipims_io/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+init
+To do:
+    initialize a package
+Created on Wed Apr  1 14:56:15 2020
+
+@author: Xiaodong Ming
+"""
+
+def joke():
+    return (u'Helloe, welcome to use hipims_io to setup hipims ... '
+            u'Hope you enjoy this package.')
