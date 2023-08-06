d283039e9ccfaa9f7a91e539a11bc210ae51138f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:19 +0000

    Add package yamkix/0.5.2/yamkix-0.5.2.tar.gz

---
+++ packages/yamkix/yamkix-0.5.2.tar.gz/yamkix-0.5.2/setup.py
@@ -0,0 +1,27 @@
+"""Setup."""
+
+from setuptools import setup
+
+long_description = open("README.rst").read()
+
+install_requires = ["ruamel.yaml>0.15"]
+
+setup(
+    name="yamkix",
+    description="An opinionated yaml formatter based on ruamel.yaml",
+    long_description=long_description,
+    url="https://github.com/looztra/yamkix",
+    author="Christophe Furmaniak",
+    author_email="christophe.furmaniak@gmail.com",
+    version="0.5.2",
+    scripts=["yamkix"],
+    license="[Apache License 2.0](http: // www.apache.org / licenses /)",
+    install_requires=install_requires,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Utilities",
+    ],
+    keywords="yaml format",
+)
