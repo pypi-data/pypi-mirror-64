456da08733c996b0f943585680289cb69ce69abb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:44 +0000

    Add package moncli/0.1.5/moncli-0.1.5.tar.gz

---
+++ packages/moncli/moncli-0.1.5.tar.gz/moncli-0.1.5/setup.py
@@ -0,0 +1,27 @@
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+config = {
+    'description': 'Moncli, a pythonic/DDD client for Monday.com',
+    'author': 'Andrew Shatz, Trix Solutions',
+    'url': r'https://github.com/trix-solutions/moncli',
+    'download_url': r'https://github.com/trix-solutions/moncli',
+    'author_email': 'andrew.shatz@trix.solutions',
+    'version': '0.1.5',
+    'install_requires': [
+        'nose>=1.3.7',
+        'requests>=2.22.0'
+    ],
+    'packages': [
+        'moncli',
+        'moncli.api_v1',
+        'moncli.api_v2',
+        'moncli.entities'
+    ],    
+    'scripts': [],
+    'name': 'moncli'
+}
+
+setup(**config)
