dba4c5cf42d495c32def80e40545809bae738214
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:56 +0000

    Add package autolatex/0.1.3/autolatex-0.1.3-py3-none-any.whl

---
+++ packages/autolatex/autolatex-0.1.3-py3-none-any.whl/autolatex/__init__.py
@@ -0,0 +1,17 @@
+from . import table
+import argparse
+
+
+def excel2table():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--f", help="the excel file path", dest="excel")
+    parser.add_argument(
+        "--type", help="the output type(`tex` or `md`)", dest="type")
+
+    args = parser.parse_args()
+    if args.type == None:
+        args.type = "tex"
+
+    df = table.read_excel(args.excel)
+    print(table.convert(df, typ=args.type))
