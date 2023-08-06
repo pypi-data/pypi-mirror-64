0532b887d68beb30d8036230b99c9a21b28c3a98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:17 +0000

    Add package ml-distributions/1.0/ml_distributions-1.0.tar.gz

---
+++ packages/ml-distributions/ml_distributions-1.0.tar.gz/ml_distributions-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='ml_distributions',
+      version='1.0',
+      description='Gaussian & Binomial distributions',
+      packages=['ml_distributions'],
+      author='Shivam Agarwal',
+      zip_safe=False)
