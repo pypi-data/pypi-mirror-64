724455ddff5a0773c5b1da7221ed6b0043c2356f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:45 +0000

    Add package check-glue-catalog/0.1.8/check_glue_catalog-0.1.8.tar.gz

---
+++ packages/check-glue-catalog/check_glue_catalog-0.1.8.tar.gz/check_glue_catalog-0.1.8/setup.py
@@ -0,0 +1,30 @@
+import io
+from os import path
+from setuptools import setup, find_packages
+
+MYDIR = path.abspath(path.dirname(__file__))
+
+cmdclass = {}
+ext_modules = []
+
+setup(
+    name='check_glue_catalog',  
+    version='0.1.8',
+    author="Marcelo Santino",
+    author_email="eu@marcelosantino.com.br",
+    description="Check for details in Glue Catalog and tell if its outdated",
+    url='https://github.com/msantino/check-glue-catalog',
+    long_description=io.open('README.md', 'r', encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=["boto3"],
+    cmdclass=cmdclass,
+    ext_modules=ext_modules,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+ )
