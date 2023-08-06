48b9a206f0704c6deba66f484b401e135b317db8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:16 +0000

    Add package lmdb-dataset/0.0.1/lmdb_dataset-0.0.1-py3-none-any.whl

---
+++ packages/lmdb-dataset/lmdb_dataset-0.0.1-py3-none-any.whl/lmdb_dataset/dataset.py
@@ -0,0 +1,78 @@
+from torch.utils.data import Dataset
+
+import os
+import lmdb
+import pyarrow
+import random
+
+from .utils import encode_key
+
+
+class LMDBDataset(Dataset):
+    def __init__(self, db_path, readahead=True, restart_every=-1, limit=0, offset=0, transform=lambda item: item):
+        super().__init__()
+        self.db_path = db_path
+        self.use_count = 0
+        assert restart_every != 0
+        self.restart_every = restart_every
+        self.readahead = readahead
+        self.db = None
+        self.transform = transform
+
+        with self.get_db().begin(write=False) as txn:
+            self.length = int(pyarrow.deserialize(txn.get(b'__len__')))
+            try:
+                self.keys = pyarrow.deserialize(txn.get(b'__keys__'))
+            except:
+                self.keys = None  # [txt_utils.encode_key(i) for i in range(self.length)]
+        if limit < 0:
+            self.limit = self.length + limit
+        else:
+            self.limit = limit
+        if offset < 0:
+            self.offset = self.length + offset
+        else:
+            self.offset = offset
+
+    def get_db(self):
+        if self.use_count == self.restart_every:
+            self.close()
+            return self.get_db()
+
+        if not self.db:
+            self.db = lmdb.open(self.db_path, subdir=os.path.isdir(self.db_path),
+                                readonly=True, lock=False,
+                                readahead=self.readahead, meminit=False)
+        self.use_count += 1
+        return self.db
+
+    def close(self):
+        if self.db:
+            self.db.close()
+        self.db = None
+        self.use_count = 0
+
+    def __getitem__(self, index):
+        with self.get_db().begin(write=False) as txn:
+            if self.keys:
+                k = self.keys[index + self.offset]
+            else:
+                k = encode_key(index + self.offset)
+            byteflow = txn.get(k)
+        item = pyarrow.deserialize(byteflow)
+        return self.transform(item)
+
+    def __len__(self):
+        l = self.length - self.offset
+        if self.limit > 0:
+            l = min(self.limit, l)
+        return l
+
+    def __repr__(self):
+        return self.__class__.__name__ + ' (' + self.db_path + ')'
+
+    def shuffle(self):
+        indices = list(range(len(self)))
+        random.shuffle(indices)
+        for idx in indices:
+            yield self[idx]
---
+++ packages/lmdb-dataset/lmdb_dataset-0.0.1-py3-none-any.whl/lmdb_dataset/writer.py
@@ -0,0 +1,69 @@
+import os
+import pyarrow
+import lmdb
+
+from lmdb_dataset.utils import encode_key
+from lmdb_dataset.dataset import LMDBDataset
+
+
+class LMDBDatasetWriter:
+    def __init__(self, dataset_path, map_size=1099511627776 * 10, restart_every=100):
+        try:
+            os.makedirs(dataset_path)
+            self.count = 0
+        except FileExistsError:
+            old_dset = LMDBDataset(dataset_path)
+            self.count = len(old_dset)
+            del old_dset
+
+        self.dataset_path = dataset_path
+        self.map_size = map_size
+        self.use_count = 0
+        self.db = None
+        assert restart_every != 0
+        self.restart_every = restart_every
+
+    def get_next_key(self):
+        next_key = encode_key(self.count)
+        self.count += 1
+        return next_key
+
+    def write_len(self, txn):
+        txn.put(b'__len__', pyarrow.serialize(self.count).to_buffer())
+
+    def write_data(self, elements, *, commit_every=100):
+        it = iter(elements)
+        processing = True
+        written = 0
+        while processing:
+            with self.get_db().begin(write=True) as txn:
+                for _ in range(commit_every):
+                    try:
+                        payload = pyarrow.serialize(next(it)).to_buffer()
+                        txn.put(self.get_next_key(), payload)
+                        written += 1
+                        del payload
+                    except StopIteration:
+                        processing = False
+                self.write_len(txn)
+        self.close()
+        return written
+
+    def get_db(self):
+        if self.use_count == self.restart_every:
+            self.close()
+            return self.get_db()
+
+        if not self.db:
+            self.db = lmdb.open(self.dataset_path, subdir=True,
+                                map_size=self.map_size, readonly=False,
+                                meminit=False, map_async=True, lock=True)
+        self.use_count += 1
+        return self.db
+
+    def close(self):
+        if self.db:
+            self.db.sync()
+            self.db.close()
+        self.db = None
+        self.use_count = 0
