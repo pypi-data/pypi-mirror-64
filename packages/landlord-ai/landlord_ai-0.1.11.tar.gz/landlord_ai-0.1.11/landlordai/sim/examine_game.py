e0155b21d8130dbd444afabdb8ae7c965be9293a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:59 +0000

    Add package landlord-ai/0.1.11/landlord_ai-0.1.11.tar.gz

---
+++ packages/landlord-ai/landlord_ai-0.1.11.tar.gz/landlord_ai-0.1.11/landlordai/sim/examine_game.py
@@ -0,0 +1,31 @@
+from landlordai.game.landlord import LandlordGame
+from landlordai.game.player import LearningPlayer_v1, RandomPlayer
+from landlordai.sim.game_stats import GameStats
+from landlordai.sim.simulate import Simulator
+
+import numpy as np
+from tqdm import tqdm
+
+def load_net(net):
+    return LearningPlayer_v1(name=net, net_dir='../models/' + net)
+
+if __name__ == "__main__":
+    #players = [load_net('3_30_sim5_model8')] + [load_net('3_30_sim5_model14')] + [LearningPlayer_v1(name='random') for _ in range(1)]
+    players = [load_net('3_30_sim5_model15'), load_net('3_30_sim5_model3'), load_net('3_30_sim5_model1')]
+
+    while True:
+        game = LandlordGame(players=players)
+        game.play_round()
+        if game.has_winners():
+            break
+
+    print('\n')
+    for i in range(3):
+        print(players[i].get_future_q())
+        print(players[i].get_record_hand_vectors())
+        print('\n')
+
+    print('')
+
+
+
