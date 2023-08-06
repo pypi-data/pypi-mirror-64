e22dc7b71170157e7a3469147742ad2d0cd7d77c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:49 +0000

    Add package gw-md-lib/0.10.2/gw-md-lib-0.10.2.tar.gz

---
+++ packages/gw-md-lib/gw-md-lib-0.10.2.tar.gz/gw-md-lib-0.10.2/gwlib/base/errors.py
@@ -0,0 +1,50 @@
+class ConstErrors:
+    FieldRequired = "0001"
+    FieldError = "0002"
+
+
+"""
+Authrentication and permission Exceptions
+"""
+
+
+class UserNotAllowed(Exception):
+    def __init__(self, field, errors):
+        # Call the base class constructor with the parameters it needs
+        message = "Wrong User or password"
+        super().__init__(message)
+        # Now for your custom code...
+        self.errors = errors
+
+
+"""
+Fields Exceptions
+
+"""
+
+
+class FieldRequired(Exception):
+    def __init__(self, field, errors):
+        # Call the base class constructor with the parameters it needs
+        message = "The field {} is required".format(field)
+        super().__init__(message)
+        # Now for your custom code...
+        self.errors = errors
+
+
+class FieldError(Exception):
+    def __init__(self, field, errors):
+        # Call the base class constructor with the parameters it needs
+        message = "The field {} is wrong".format(field)
+        super().__init__(message)
+        # Now for your custom code...
+        self.errors = errors
+
+
+class FieldNotInModel(Exception):
+    def __init__(self, field, errors):
+        # Call the base class constructor with the parameters it needs
+        message = "Field Not In Model"
+        super().__init__(message)
+        # Now for your custom code...
+        self.errors = errors
