7412a57b1153f67fa3f761de871b08a2069c3bd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:10 +0000

    Add package oxyba/0.32.0/oxyba-0.32.0.tar.gz

---
+++ packages/oxyba/oxyba-0.32.0.tar.gz/oxyba-0.32.0/oxyba/__init__.py
@@ -0,0 +1,6 @@
+import warnings
+warnings.warn((
+    "The 'oxyba' module is deprecated. "
+    "Specify an old version, e.g. oxyba==0.27.*"),
+    DeprecationWarning,
+    stacklevel=2)
---
+++ packages/oxyba/oxyba-0.32.0.tar.gz/oxyba-0.32.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(name='oxyba',
+      version='0.32.0',
+      description='deprecated',
+      url='http://github.com/kmedian',
+      author='Ulf Hamster',
+      author_email='554c46@gmail.com',
+      license='MIT',
+      packages=['oxyba'],
+      install_requires=['setuptools>=40.0.0'],
+      python_requires='>=3.6',
+      zip_safe=False)
