34a399fb55ffee5f745d4ed332611d97adb234d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:59 +0000

    Add package alfalfa-client/0.1.dev3/alfalfa_client-0.1.dev3.tar.gz

---
+++ packages/alfalfa-client/alfalfa_client-0.1.dev3.tar.gz/alfalfa_client-0.1.dev3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(name='alfalfa_client',
+      version='0.1.dev3',
+      description='A standalone client for the NREL Alfalfa application',
+      url='https://github.com/nrel/alfalfa-client',
+      author=open('AUTHORS.md'),
+      license=open('LICENSE.txt'),
+      packages=find_packages(),
+      long_description=open('README.MD').read(),
+      install_requires=[
+          'numpy',
+          'pandas',
+          'requests',
+          'requests-toolbelt'
+      ])
