421d53de0df7921380e12fdba3116fb95fa7728b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:04 +0000

    Add package setup-python-package/1.1.5/setup_python_package-1.1.5.tar.gz

---
+++ packages/setup-python-package/setup_python_package-1.1.5.tar.gz/setup_python_package-1.1.5/setup_python_package/queries/get_codacy_code.py
@@ -0,0 +1,36 @@
+from ..utils import load_repository_name, load_repository_author_name
+import webbrowser
+from userinput import userinput
+import subprocess
+from ..enablers import enable_codacy
+import os
+
+
+def validate_codacy_code(code: str):
+    return len(code) == 32
+
+
+def get_codacy_code(automatically_open_browser: bool):
+    enable_codacy(automatically_open_browser)
+    if automatically_open_browser:
+        input("Press any key to go to the codacy project settings now to get the project token.")
+        webbrowser.open(
+            "https://app.codacy.com/app/{account}/{repository}/settings/integrations".format(
+                account=load_repository_author_name(),
+                repository=load_repository_name()
+            ),
+            new=2,
+            autoraise=True
+        )
+    test_reported_id = userinput(
+        "CODACY PROJECT TOKEN",
+        validator=validate_codacy_code,
+        cache=False
+    )
+
+    subprocess.run([
+        "travis",
+        "encrypt",
+        "CODACY_PROJECT_TOKEN={}".format(test_reported_id),
+        "--add"
+    ], shell=True, input="\n", stdout=open(os.devnull, 'w'), encoding='ascii')
