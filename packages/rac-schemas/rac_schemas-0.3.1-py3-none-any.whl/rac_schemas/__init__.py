1b13410930764fe59b798dc499938c56d942ac71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:47 +0000

    Add package rac-schemas/0.3.1/rac_schemas-0.3.1-py3-none-any.whl

---
+++ packages/rac-schemas/rac_schemas-0.3.1-py3-none-any.whl/rac_schemas/__init__.py
@@ -0,0 +1,31 @@
+import json
+from os.path import join
+
+from jsonschema import Draft7Validator, RefResolver
+
+
+def handle_schema_filename(filename):
+    extension = filename.split(".")[-1]
+    if extension != "json":
+        if len(filename.split(".")) > 1:
+            filename = filename[:-len(extension)] + ".json"
+        else:
+            filename = filename + ".json"
+    return filename
+
+
+def is_valid(data, schema_name):
+    if not isinstance(data, dict):
+        raise TypeError(
+            "Data to be validated must be a dict, got {} instead".format(
+                type(data)))
+    filename = handle_schema_filename(schema_name)
+    with open(join("rac_schemas", "schemas", "base.json"), "r") as bf:
+        base_schema = json.load(bf)
+        with open(join("rac_schemas", "schemas", filename), "r") as sf:
+            object_schema = json.load(sf)
+            resolver = RefResolver.from_schema(base_schema)
+            validator = Draft7Validator(
+                object_schema, resolver=resolver)
+            validator.validate(data)
+            return True
