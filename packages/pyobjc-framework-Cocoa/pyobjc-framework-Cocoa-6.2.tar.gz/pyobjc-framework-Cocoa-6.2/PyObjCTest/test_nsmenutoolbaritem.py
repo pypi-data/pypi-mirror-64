5ffba574cb97582818dbf7331a320889be3ffd37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:03 +0000

    Add package pyobjc-framework-Cocoa/6.2/pyobjc-framework-Cocoa-6.2.tar.gz

---
+++ packages/pyobjc-framework-Cocoa/pyobjc-framework-Cocoa-6.2.tar.gz/pyobjc-framework-Cocoa-6.2/PyObjCTest/test_nsmenutoolbaritem.py
@@ -0,0 +1,9 @@
+import AppKit
+from PyObjCTools.TestSupport import TestCase, min_sdk_level
+
+
+class TestNSMenuToolbarItem(TestCase):
+    @min_sdk_level("10.15")
+    def test_nethods10_15(self):
+        self.assertResultIsBOOL(AppKit.NSMenuToolbarItem.showsIndicator)
+        self.assertArgIsBOOL(AppKit.NSMenuToolbarItem.setShowsIndicator_, 0)
