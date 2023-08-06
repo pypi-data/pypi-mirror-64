c4e7dd00ba8dacf60be86321dfc62b483134b14d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:36 +0000

    Add package outsystems-pipeline/0.3.1/outsystems-pipeline-0.3.1.tar.gz

---
+++ packages/outsystems-pipeline/outsystems-pipeline-0.3.1.tar.gz/outsystems-pipeline-0.3.1/setup.py
@@ -0,0 +1,97 @@
+from distutils.core import setup
+import os
+
+NAME = 'outsystems-pipeline'
+DESCRIPTION = 'Python package to accelerate the integration of OutSystems with third-party CI/CD tools'
+LONG_DESCRIPTION = '''The outsystems-pipeline Python package provides functions to support the creation of OutSystems CI/CD pipelines using your DevOps automation tool of choice.
+
+Visit the `project repository <https://github.com/OutSystems/outsystems-pipeline>`_ on GitHub for instructions on how to build example OutSystems CI/CD pipelines with common DevOps automation tools, as well as documentation that will help you adapt the examples to your particular scenarios.
+
+
+What's new
+==========
+
+**Deployment Manifest**
+ * Used to promote the same application versions throughout the pipeline execution.
+
+**Air Gap Support**
+ * To deploy OutSystems Applications to a target environment without using the Deployment API.
+
+
+Installing and upgrading
+========================
+
+Install or upgrade outsystems-pipeline to the latest available version as follows:
+::
+
+    pip install -U outsystems-pipeline
+
+'''
+AUTHOR = u'OutSystems'
+EMAIL = u'cicd.integrations@outsystems.com'
+URL = 'https://github.com/OutSystems/outsystems-pipeline'
+LICENSE = 'Apache License 2.0'
+PYTHON_REQUIRES = '>=3.0'
+KEYWORDS = [
+    '',
+]
+
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Intended Audience :: System Administrators',
+    'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Build Tools',
+    'Topic :: Software Development :: Quality Assurance',
+    'Topic :: Software Development :: Testing',
+    'Topic :: Software Development :: Testing :: Acceptance',
+    'Topic :: Software Development :: Testing :: BDD',
+    'Topic :: Software Development :: Testing :: Unit',
+    'Topic :: System :: Software Distribution'
+]
+
+REQUIREMENTS = [
+    'python-dateutil==2.7.5',
+    'requests==2.20.1',
+    'unittest-xml-reporting==2.2.1',
+    'xunitparser==1.3.3',
+    'pytest==4.3.0',
+    'toposort==1.5'
+]
+
+PACKAGES = [
+    'outsystems',
+    'outsystems.bdd_framework',
+    'outsystems.cicd_probe',
+    'outsystems.exceptions',
+    'outsystems.file_helpers',
+    'outsystems.lifetime',
+    'outsystems.osp_tool',
+    'outsystems.pipeline',
+    'outsystems.vars'
+]
+
+if __name__ == '__main__':  # Do not run setup() when we import this module.
+    if os.path.isfile("VERSION"):
+        with open("VERSION", 'r') as version_file:
+            version = version_file.read().replace('\n', '')
+    else:
+        # dummy version
+        version = '1.0.0'
+
+    setup(
+        name=NAME,
+        version='0.3.1',
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        keywords=' '.join(KEYWORDS),
+        author=AUTHOR,
+        author_email=EMAIL,
+        url=URL,
+        license=LICENSE,
+        python_requires=PYTHON_REQUIRES,
+        classifiers=CLASSIFIERS,
+        packages=PACKAGES,
+        install_requires=REQUIREMENTS
+    )
