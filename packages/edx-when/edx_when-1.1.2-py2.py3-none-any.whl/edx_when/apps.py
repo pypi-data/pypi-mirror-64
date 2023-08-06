0b9ccf5cbbae23388e3f343702632a81f2380490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:22 +0000

    Add package edx-when/1.1.2/edx_when-1.1.2-py2.py3-none-any.whl

---
+++ packages/edx-when/edx_when-1.1.2-py2.py3-none-any.whl/edx_when/apps.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+edx_when Django application initialization.
+"""
+
+from __future__ import absolute_import, unicode_literals
+
+from django.apps import AppConfig
+
+
+class EdxWhenConfig(AppConfig):
+    """
+    Configuration for the edx_when Django application.
+    """
+
+    name = 'edx_when'
+    plugin_app = {
+        'url_config': {
+            'lms.djangoapp': {
+                'namespace': u'edx_when',
+                'regex': u'^api/',
+                'relative_path': u'urls',
+            },
+        }
+    }
