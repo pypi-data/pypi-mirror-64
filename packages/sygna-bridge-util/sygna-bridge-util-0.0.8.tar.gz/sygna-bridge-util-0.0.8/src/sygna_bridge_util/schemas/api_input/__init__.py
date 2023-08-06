f75e2594f8d51bd5909f0d6b9c873d60fdbac10f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:54 +0000

    Add package sygna-bridge-util/0.0.8/sygna-bridge-util-0.0.8.tar.gz

---
+++ packages/sygna-bridge-util/sygna-bridge-util-0.0.8.tar.gz/sygna-bridge-util-0.0.8/src/sygna_bridge_util/schemas/api_input/__init__.py
@@ -0,0 +1,9 @@
+from .post_permission import get_post_permission_schema
+from .post_permission_request import get_post_permission_request_schema
+from .post_txid_schema import get_post_txid_schema
+
+__all__ = [
+    'get_post_permission_schema',
+    'get_post_permission_request_schema',
+    'get_post_txid_schema'
+]
