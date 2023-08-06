45127ebd955e7279262703c86f648b21ac5d37a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:45 +0000

    Add package play-in-bots-vk/0.1.7/play_in_bots_vk-0.1.7.tar.gz

---
+++ packages/play-in-bots-vk/play_in_bots_vk-0.1.7.tar.gz/play_in_bots_vk-0.1.7/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='play_in_bots_vk',
+      version='0.1.7',
+      description='Бот, который играет за вас',
+      packages=['play_in_bots_vk'],
+      author_email='ruslan@rym9n.ru',
+      zip_safe=False)
