ebef61dd4057e7da6c7a6244ef8112fb54f7a63f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:13 +0000

    Add package sedkit/1.0.0/sedkit-1.0.0.tar.gz

---
+++ packages/sedkit/sedkit-1.0.0.tar.gz/sedkit-1.0.0/setup.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup, find_packages
+    setup
+except ImportError:
+    from distutils.core import setup
+    setup
+
+from codecs import open
+from os import path
+
+setup(
+    name='sedkit',
+    version='1.0.0',
+    description='Spectral energy distribution construction and analysis tools',
+    url='https://github.com/hover2pi/sedkit',
+    author='Joe Filippazzo',
+    author_email='jfilippazzo@stsci.edu',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='astrophysics',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    install_requires=['numpy','astropy','bokeh','pysynphot','scipy','astroquery','dustmaps', 'pandas', 'svo_filters'],
+    include_package_data=True,
+
+)
