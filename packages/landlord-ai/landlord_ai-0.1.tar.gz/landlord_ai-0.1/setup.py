dee2dc422db193dadc20f9bc7c04f5e30c6ca874
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:31 +0000

    Add package landlord-ai/0.1/landlord_ai-0.1.tar.gz

---
+++ packages/landlord-ai/landlord_ai-0.1.tar.gz/landlord_ai-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='landlord_ai',
+      version='0.1',
+      description='Landlord AI Core',
+      license='GNU',
+      packages=['landlord'],
+      author_email='seokhohong02@gmail.com',
+      install_requires=[],
+      zip_safe=False)
