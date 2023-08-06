f8aaa92856c734d4b7fb10a143b632e9d57f4317
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:27 +0000

    Add package atomic-write-path/1.1.0/atomic_write_path-1.1.0.tar.gz

---
+++ packages/atomic-write-path/atomic_write_path-1.1.0.tar.gz/atomic_write_path-1.1.0/atomic_write_path/__init__.py
@@ -0,0 +1,6 @@
+"""Context manager for atomically writing data to a file path."""
+from atomic_write_path.atomic_write_path import atomic_write_path
+
+
+__all__ = ["atomic_write_path"]
+__version__ = "1.1.0"
