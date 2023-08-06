f6a3450c8b1144cbe9d2021d76764195e456111d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:18 +0000

    Add package Bitcoin-notifications-karishma-agarwal-21/0.0.2/Bitcoin_notifications-karishma-agarwal-21-0.0.2.tar.gz

---
+++ packages/Bitcoin-notifications-karishma-agarwal-21/Bitcoin_notifications-karishma-agarwal-21-0.0.2.tar.gz/Bitcoin_notifications-karishma-agarwal-21-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Bitcoin_notifications-karishma-agarwal-21", # Replace with your own username
+    version="0.0.2",
+    author="Karishma Agarwal",
+    author_email="karishmaag21@gmail.com",
+    description="A small project for bitcoin price notifications",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/karishma-garg-au7/bitcoin_notification",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
