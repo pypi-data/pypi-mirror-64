bdf6051db6290c9d9ac6886c9eda658ba97f1fd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:58 +0000

    Add package hmf/3.0.8/hmf-3.0.8.tar.gz

---
+++ packages/hmf/hmf-3.0.8.tar.gz/hmf-3.0.8/hmf/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "3.0.8"
+from .hmf import MassFunction
+from . import fitting_functions as fits
+from .cosmo import Cosmology
+from .transfer import Transfer
+from .sample import sample_mf
---
+++ packages/hmf/hmf-3.0.8.tar.gz/hmf-3.0.8/setup.py
@@ -0,0 +1,54 @@
+from setuptools import setup, find_packages
+
+import os
+import sys
+import re
+import io
+
+test_suite = "nose.collector"
+
+def read(*names, **kwargs):
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *names),
+        encoding=kwargs.get("encoding", "utf8")
+    ) as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+if sys.argv[-1] == "publish":
+    os.system("rm dist/*")
+    os.system("python setup.py sdist")
+    os.system("python setup.py bdist_wheel")
+    os.system("twine upload dist/*")
+    sys.exit()
+
+
+setup(
+    name="hmf",
+    version=find_version("hmf", "__init__.py"),
+    packages=find_packages(),
+    install_requires=["numpy>=1.6.2",
+                      "scipy>=0.12.0",
+                      "astropy>=1.1",
+                      "camb>=0.1.6"],
+    scripts=["scripts/hmf", "scripts/hmf-fit"],
+    author="Steven Murray",
+    author_email="steven.murray@curtin.edu.au",
+    description="A halo mass function calculator",
+    long_description=read('README.rst'),
+    license="MIT",
+    keywords="halo mass function",
+    url="https://github.com/steven-murray/hmf",
+    classifiers=["Programming Language :: Python :: 2.7",
+                 "Programming Language :: Python :: 3.6",
+                 ]
+    # could also include long_description, download_url, classifiers, etc.
+)
