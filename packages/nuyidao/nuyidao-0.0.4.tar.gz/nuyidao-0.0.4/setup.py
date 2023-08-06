122c7a8eb8a7aa2a60211699fae6e91fab39ed18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:12 +0000

    Add package nuyidao/0.0.4/nuyidao-0.0.4.tar.gz

---
+++ packages/nuyidao/nuyidao-0.0.4.tar.gz/nuyidao-0.0.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nuyidao',
+    version='v0.0.4',
+    description='Astro support',
+    long_description="NU YI DAO utils support + SVG",
+    url='https://nuyidao.com',
+    license='MIT',
+    packages=find_packages(),
+    data_files=[('font', ['nuyidao/font/Roboto-Medium.ttf'])],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+    ],
+    install_requires="ephem==3.7.6.0",
+    python_requires='>3.7',
+)
