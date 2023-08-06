289cb4b964bc45f30f89e91a3647603aeddf63c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:52 +0000

    Add package word-processing/13.20/word_processing-13.20.tar.gz

---
+++ packages/word-processing/word_processing-13.20.tar.gz/word_processing-13.20/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='word_processing',
+	version='13.20',
+	description='anayzing and preparing document for analysis',
+	packages=['word_processing'],
+	zip_safe=False)
---
+++ packages/word-processing/word_processing-13.20.tar.gz/word_processing-13.20/word_processing/process.py
@@ -0,0 +1,60 @@
+class Process:
+    def __init__(self):
+        """
+        Word processing class for processing documents ML for training data
+        """
+    
+    def readFiles(self, file_name):
+        """
+        Args:
+            file_name (string): name of a file to read from
+
+        Returns:
+            str
+        """
+        with open(file_name, "r", encoding="utf8") as file:
+            line = file.read()
+        return line
+    
+    def removePunctuations(self, string):
+        """
+        Args:
+            string (str): string to work on
+
+        Returns:
+            str
+        """
+	import re
+        return re.sub(r"[^a-zA-Z0-9]", " ", string)
+    
+    def convert2Lower(self, string):
+        """
+        Args:
+            file_name (string): name of a file to read from
+
+        Returns:
+            str
+        """
+        return string.lower()
+    
+    def word2List(self, string):
+        """
+        Args:
+            string (str): string to convert
+
+        Returns:
+            list
+        """
+        return string.split()
+    
+    def removeEnglishStopwords(self, listfile):
+        """
+        Args:
+            string (list): list to operate on
+            stopwords (str): stopwords txt file name
+
+        Returns:
+            list
+        """
+        stopwords = self.readFiles('stopwords.txt').split()
+        return [w for w in listfile if w not in stopwords]
