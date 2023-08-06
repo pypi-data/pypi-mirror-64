640fae4c16b6163fbb5814d910ee7a59c9ce9c34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:38 +0000

    Add package ds4mllib/0.0.3/ds4mllib-0.0.3-py3-none-any.whl

---
+++ packages/ds4mllib/ds4mllib-0.0.3-py3-none-any.whl/ds4mllib/__init__.py
@@ -0,0 +1,12 @@
+__version__ = '0.0.1'
+
+from ds4mllib.dataset import DataSet
+from ds4mllib.attribute import Attribute
+from ds4mllib.evaluator import BiFrame
+
+# Default environment variables for data processing and analysis
+# It's not recommend to modify these variables' values.
+params = {
+    'attribute.bins': 20
+}
+
