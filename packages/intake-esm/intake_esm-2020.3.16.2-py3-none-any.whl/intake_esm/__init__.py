71ed45b6e1edfb589279ace9008aa58dc4eef0d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:06 +0000

    Add package intake-esm/2020.3.16.2/intake_esm-2020.3.16.2-py3-none-any.whl

---
+++ packages/intake-esm/intake_esm-2020.3.16.2-py3-none-any.whl/intake_esm/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+""" Top-level module for intake_esm. """
+# Import intake first to avoid circular imports during discovery.
+import intake  # noqa: F401
+from pkg_resources import DistributionNotFound, get_distribution
+
+from .core import esm_datastore  # noqa: F401
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
