c6acfc7a1f033bfbbefd189a3670e18139cb62a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:38 +0000

    Add package blurhash-python/1.0.2/blurhash-python-1.0.2.tar.gz

---
+++ packages/blurhash-python/blurhash-python-1.0.2.tar.gz/blurhash-python-1.0.2/src/build_blurhash.py
@@ -0,0 +1,27 @@
+from __future__ import absolute_import
+
+import cffi
+import sys
+
+
+ffibuilder = cffi.FFI()
+ffibuilder.set_source('blurhash._encode', '''
+    const char* blurHashForPixels(int xComponents, int yComponents, int width,
+                                  int height, uint8_t *rgb,
+                                  size_t bytesPerRow);
+
+    const char* create_hash_from_pixels(int x_components, int y_components,
+                                       int width, int height, uint8_t* rgb,
+                                       size_t bytes_per_row) {
+        return blurHashForPixels(x_components, y_components, width, height,
+                                 rgb, bytes_per_row);
+    }
+''', sources=['src/encode.c'], extra_compile_args=['-std=gnu99'] if sys.platform != 'win32' else [])
+ffibuilder.cdef('''
+    const char* create_hash_from_pixels(int x_components, int y_components,
+                                       int width, int height, uint8_t* rgb,
+                                       size_t bytes_per_row);
+''')
+
+if __name__ == '__main__':
+    ffibuilder.compile()
