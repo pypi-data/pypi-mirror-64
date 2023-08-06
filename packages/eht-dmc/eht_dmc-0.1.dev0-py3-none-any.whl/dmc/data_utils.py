d5247bca38cd9b855867d1b317eba567960e6da8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:34 +0000

    Add package eht-dmc/0.1.dev0/eht_dmc-0.1.dev0-py3-none-any.whl

---
+++ packages/eht-dmc/eht_dmc-0.1.dev0-py3-none-any.whl/dmc/data_utils.py
@@ -0,0 +1,223 @@
+#######################################################
+# imports
+#######################################################
+
+from __future__ import division
+from __future__ import print_function
+from builtins import list
+from builtins import len
+from builtins import range
+from builtins import enumerate
+from builtins import Exception
+
+import numpy as np
+import ehtim as eh
+
+#######################################################
+# functions
+#######################################################
+
+def gain_account(obs):
+    """ Determine the total number of gains that need to be solved for
+
+       Args:
+           obs (obsdata): eht-imaging obsdata object containing VLBI data
+           
+       Returns:
+           T_gains: list of gain timestamps
+           A_gains: list of gain stations
+    
+    """
+
+    # get arrays of station names
+    ant1 = obs.data['t1']
+    ant2 = obs.data['t2']
+
+    # get array of timestamps
+    time = obs.data['time']
+    timestamps = np.unique(time)
+
+    # Determine the total number of gains that need to be solved for
+    N_gains = 0
+    T_gains = list()
+    A_gains = list()
+    for it, t in enumerate(timestamps):
+        ind_here = (time == t)
+        N_gains += len(np.unique(np.concatenate((ant1[ind_here],ant2[ind_here]))))
+        stations_here = np.unique(np.concatenate((ant1[ind_here],ant2[ind_here])))
+        for ii in range(len(stations_here)):
+            A_gains.append(stations_here[ii])
+            T_gains.append(t)
+    T_gains = np.array(T_gains)
+    A_gains = np.array(A_gains)
+
+    return T_gains, A_gains
+
+def gain_design_mats(obs):
+    """ Construct design matrices for gain amplitudes and phases, assuming
+        that there is a single complex gain associated with each timestamp
+        and that all gains are independent of each other
+
+       Args:
+           obs (obsdata): eht-imaging obsdata object containing VLBI data
+           
+       Returns:
+           gain_design_mat_1: gain design matrix for the first station
+           gain_design_mat_2: gain design matrix for the second station
+
+    """
+
+    # get arrays of station names
+    ant1 = obs.data['t1']
+    ant2 = obs.data['t2']
+
+    # get array of times
+    time = obs.data['time']
+
+    # Determine the total number of gains that need to be solved for
+    T_gains, A_gains = gain_account(obs)
+    N_gains = len(T_gains)
+
+    # initialize design matrices
+    gain_design_mat_1 = np.zeros((len(time),N_gains))
+    gain_design_mat_2 = np.zeros((len(time),N_gains))
+
+    # fill in design matrices
+    for i in range(len(time)):
+        ind1 = ((T_gains == time[i]) & (A_gains == ant1[i]))
+        ind2 = ((T_gains == time[i]) & (A_gains == ant2[i]))
+
+        gain_design_mat_1[i,ind1] = 1.0
+        gain_design_mat_2[i,ind2] = 1.0
+
+    return gain_design_mat_1, gain_design_mat_2
+
+def dterm_design_mats(obs):
+    """ Construct design matrices for leakage (D-term) amplitudes and phases,
+        assuming that there is only a single (i.e., time-independent) complex
+        D-term per hand per station over the entire observation
+
+       Args:
+           obs (obsdata): eht-imaging obsdata object containing VLBI data
+           
+       Returns:
+           dterm_design_mat_1: D-term design matrix for the first station
+           dterm_design_mat_2: D-term design matrix for the second station
+
+    """
+
+    # get array of stations
+    ant1 = obs.data['t1']
+    ant2 = obs.data['t2']
+    stations = np.unique(np.concatenate((ant1,ant2)))
+
+    # get array of timestamps
+    time = obs.data['time']
+    timestamps = np.unique(time)
+
+    # determine the total number of D-terms that need to be solved for
+    N_Dterms = len(stations)
+
+    # initialize design matrices
+    dterm_design_mat_1 = np.zeros((len(time),N_Dterms))
+    dterm_design_mat_2 = np.zeros((len(time),N_Dterms))
+
+    # fill in design matrices
+    for istat,station in enumerate(stations):
+        ind1 = (ant1 == station)
+        ind2 = (ant2 == station)
+
+        dterm_design_mat_1[ind1,istat] = 1.0
+        dterm_design_mat_2[ind2,istat] = 1.0
+
+    return stations, dterm_design_mat_1, dterm_design_mat_2
+
+def FRvec(obs,ehtim_convention=True):
+    """ Construct vectors of field rotation corrections for each station and hand
+
+       Args:
+           obs (obsdata): eht-imaging obsdata object containing VLBI data
+           ehtim_convention (bool): if true, assume the field rotation uses the
+                                    eht-imaging pre-rotation convention
+           
+       Returns:
+           FR1: field rotation corrections for the first station
+           FR2: field rotation corrections for the second station
+
+    """
+
+    # read the elevation angles for each station
+    el1 = obs.unpack(['el1'],ang_unit='rad')['el1']
+    el2 = obs.unpack(['el2'],ang_unit='rad')['el2']
+
+    # read the parallactic angles for each station
+    par1 = obs.unpack(['par_ang1'],ang_unit='rad')['par_ang1']
+    par2 = obs.unpack(['par_ang2'],ang_unit='rad')['par_ang2']
+
+    # get the observation array info
+    tarr = obs.tarr
+
+    # get arrays of station names
+    ant1 = obs.data['t1']
+    ant2 = obs.data['t2']
+
+    # get multiplicative prefactors for station 1
+    f_el1 = np.zeros_like(el1)
+    f_par1 = np.zeros_like(par1)
+    f_off1 = np.zeros_like(el1)
+    for ia, a1 in enumerate(ant1):
+        ind1 = (tarr['site'] == a1)
+        f_el1[ia] = tarr[ind1]['fr_elev']
+        f_par1[ia] = tarr[ind1]['fr_par']
+        f_off1[ia] = tarr[ind1]['fr_off']*eh.DEGREE
+
+    # get multiplicative prefactors for station 2
+    f_el2 = np.zeros_like(el2)
+    f_par2 = np.zeros_like(par2)
+    f_off2 = np.zeros_like(el2)
+    for ia, a2 in enumerate(ant2):
+        ind2 = (tarr['site'] == a2)
+        f_el2[ia] = tarr[ind2]['fr_elev']
+        f_par2[ia] = tarr[ind2]['fr_par']
+        f_off2[ia] = tarr[ind2]['fr_off']*eh.DEGREE
+
+    # combine to get field rotations for each station
+    FR1 = (f_el1*el1) + (f_par1*par1) + f_off1
+    FR2 = (f_el2*el2) + (f_par2*par2) + f_off2
+
+    # negate the second station to account for conjugation
+    FR2 *= -1.0
+
+    # if pre-rotation has been applied, multiply by 2.0
+    if ehtim_convention:
+        FR1 *= 2.0
+        FR2 *= 2.0
+
+    return FR1, FR2
+
+def estimate_total_flux(obs):
+    """ Estimate the total flux of an object
+
+       Args:
+           obs (obsdata): eht-imaging obsdata object containing VLBI data
+           
+       Returns:
+           total_flux_estimate: estimate of the total source flux (Jy)
+
+    """
+
+    # (u,v) coordinate info
+    u = obs.data['u']
+    v = obs.data['v']
+    rho = np.sqrt((u**2.0) + (v**2.0))
+
+    # determine the smallest (u,v) point
+    ind = np.argmin(rho)
+
+    # set the polrep to be stokes
+    obs = obs.switch_polrep('stokes')
+
+    # get flux at that point
+    total_flux_estimate = np.abs(obs.data['vis'][ind])
+
+    return total_flux_estimate
---
+++ packages/eht-dmc/eht_dmc-0.1.dev0-py3-none-any.whl/dmc/model_utils.py
@@ -0,0 +1,191 @@
+#######################################################
+# imports
+#######################################################
+
+from __future__ import division
+from __future__ import print_function
+from builtins import list
+from builtins import len
+from builtins import range
+from builtins import enumerate
+from builtins import Exception
+
+import numpy as np
+import ehtim as eh
+import pymc3 as pm
+
+#######################################################
+# constants
+#######################################################
+
+SEFD_error_budget = {'AA':0.10,
+                     'AP':0.11,
+                     'AZ':0.07,
+                     'LM':0.22,
+                     'PV':0.10,
+                     'SM':0.15,
+                     'JC':0.14,
+                     'SP':0.07}
+
+#######################################################
+# functions
+#######################################################
+
+def gain_logamp_prior(obs,SEFD_error_budget=SEFD_error_budget):
+    """ Construct vector of prior means and standard deviations
+        for log gain amplitudes
+
+       Args:
+           obs (obsdata): eht-imaging obsdata object containing VLBI data
+           SEFD_error_budget (dict): dictionary of a priori error budget
+                                     for station SEFDs (fractional)
+           
+       Returns:
+           loggainamp_mean: prior means for log gain amplitudes
+           loggainamp_std: prior standard deviations for log gain amplitudes
+
+    """
+
+    # get arrays of station names
+    ant1 = obs.data['t1']
+    ant2 = obs.data['t2']
+
+    # get array of timestamps
+    time = obs.data['time']
+    timestamps = np.unique(time)
+
+    # Determine the total number of gains that need to be solved for
+    N_gains = 0
+    T_gains = list()
+    A_gains = list()
+    for it, t in enumerate(timestamps):
+        ind_here = (time == t)
+        N_gains += len(np.unique(np.concatenate((ant1[ind_here],ant2[ind_here]))))
+        stations_here = np.unique(np.concatenate((ant1[ind_here],ant2[ind_here])))
+        for ii in range(len(stations_here)):
+            A_gains.append(stations_here[ii])
+            T_gains.append(t)
+    T_gains = np.array(T_gains)
+    A_gains = np.array(A_gains)
+
+    # initialize vectors of gain prior means and standard deviations
+    gainamp_mean = np.ones(N_gains)
+    gainamp_std = np.ones(N_gains)
+
+    # loop over stations
+    for key in SEFD_error_budget.keys():
+        index = (A_gains == key)
+        gainamp_mean[index] = 1.0
+        gainamp_std[index] = SEFD_error_budget[key]
+
+    # take log
+    loggainamp_mean = np.log(gainamp_mean)
+    loggainamp_std = gainamp_std/gainamp_mean
+
+    return loggainamp_mean, loggainamp_std
+
+def gain_phase_prior(obs,ref_station='AA'):
+    """ Construct vector of prior means and inverse standard deviations
+        for gain phases
+
+       Args:
+           obs (obsdata): eht-imaging obsdata object containing VLBI data
+           ref_station (str): name of reference station
+           
+       Returns:
+           gainphase_mu: prior means for gain phases
+           gainphase_kappa: prior inverse standard deviations for gain phases
+
+    """
+
+    # get arrays of station names
+    ant1 = obs.data['t1']
+    ant2 = obs.data['t2']
+
+    # get array of timestamps
+    time = obs.data['time']
+    timestamps = np.unique(time)
+
+    # Determine the total number of gains that need to be solved for
+    N_gains = 0
+    T_gains = list()
+    A_gains = list()
+    for it, t in enumerate(timestamps):
+        ind_here = (time == t)
+        N_gains += len(np.unique(np.concatenate((ant1[ind_here],ant2[ind_here]))))
+        stations_here = np.unique(np.concatenate((ant1[ind_here],ant2[ind_here])))
+        for ii in range(len(stations_here)):
+            A_gains.append(stations_here[ii])
+            T_gains.append(t)
+    T_gains = np.array(T_gains)
+    A_gains = np.array(A_gains)
+
+    # initialize vectors of gain phase means and inverse standard deviations
+    gainphase_mu = np.zeros(N_gains)
+    gainphase_kappa = 0.0001*np.ones(N_gains)
+
+    # set reference station standard devation to be tiny
+    for it, t in enumerate(timestamps):
+        index = (T_gains == t)
+        ants_here = A_gains[index]
+        for ant in ants_here:
+            if ant == ref_station:
+                ind = ((T_gains == t) & (A_gains == ant))
+                gainphase_kappa[ind] = 10000.0
+                break
+
+    return gainphase_mu, gainphase_kappa
+
+def get_step_for_trace(trace=None, model=None, regularize=True, regular_window=5, regular_variance=1e-3, **kwargs):
+    """ Define a tuning procedure that adapts off-diagonal mass matrix terms
+        adapted from a blog post by Dan Foreman-Mackey here:
+        https://dfm.io/posts/pymc3-mass-matrix/
+
+       Args:
+           trace (trace): pymc3 trace object
+           model (model): pymc3 model object
+           
+           regularize (bool): flag to turn on covariance matrix regularization
+           regular_window (int): size of parameter space at which regularization becomes important
+           regular_variance (float): magnitude of covariance floor
+           
+       Returns:
+           pymc3 step_methods object
+
+    """
+
+    model = pm.modelcontext(model)
+    
+    # If not given, use the trivial metric
+    if trace is None:
+        potential = pm.step_methods.hmc.quadpotential.QuadPotentialFull(np.eye(model.ndim))
+        return pm.NUTS(potential=potential, **kwargs)
+    
+    # Loop over samples and convert to the relevant parameter space
+    # while removing divergent samples
+    div_mask = np.invert(np.copy(trace.diverging))
+    samples = np.empty((div_mask.sum() * trace.nchains, model.ndim))
+    i = 0
+    imask = 0
+    for chain in trace._straces.values():
+        for p in chain:
+            if div_mask[imask]:
+                samples[i] = model.bijection.map(p)
+                i += 1
+            imask += 1
+    
+    # Compute the sample covariance
+    cov = np.cov(samples, rowvar=0)
+    
+    # Stan uses a regularized estimator for the covariance matrix to
+    # be less sensitive to numerical issues for large parameter spaces.
+    if regularize:
+        N = len(samples)
+        cov = cov * N / (N + regular_window)
+        cov[np.diag_indices_from(cov)] += regular_variance * regular_window / (N + regular_window)
+    
+    # Use the sample covariance as the inverse metric
+    potential = pm.step_methods.hmc.quadpotential.QuadPotentialFull(cov)
+
+    return pm.NUTS(potential=potential, **kwargs)
+
