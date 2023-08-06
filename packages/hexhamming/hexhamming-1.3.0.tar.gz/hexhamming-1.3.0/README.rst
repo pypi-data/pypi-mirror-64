45d5ed80ba63fa363a4cc1432ea14d7d85e802b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:52 +0000

    Add package hexhamming/1.3.0/hexhamming-1.3.0.tar.gz

---
+++ packages/hexhamming/hexhamming-1.3.0.tar.gz/hexhamming-1.3.0/README.rst
@@ -0,0 +1,130 @@
+Hexadecimal Hamming
+====================
+
+|Pip|_ |Prs|_ |Github|_
+
+.. |Pip| image:: https://badge.fury.io/py/hexhamming.svg
+.. _Pip: https://badge.fury.io/py/hexhamming
+
+.. |Prs| image:: https://img.shields.io/badge/PRs-welcome-brightgreen.svg
+.. _Prs: .github/CONTRIBUTING.md#pull-requests
+
+.. |Github| image:: https://github.com/mrecachinas/hexhamming/workflows/build/badge.svg
+.. _Github: https://github.com/mrecachinas/hexhamming/actions
+
+What does it do?
+----------------
+
+This module performs a fast bitwise hamming distance of two hexadecimal strings.
+
+This looks like::
+
+    DEADBEEF = 11011110101011011011111011101111
+    00000000 = 00000000000000000000000000000000
+    XOR      = 11011110101011011011111011101111
+    Hamming  = number of ones in DEADBEEF ^ 00000000 = 24
+
+This essentially amounts to
+
+::
+
+    >>> import gmpy
+    >>> gmpy.popcount(0xdeadbeef ^ 0x00000000)
+    24
+
+except with Python strings, so
+
+::
+
+    >>> import gmpy
+    >>> gmpy.popcount(int("deadbeef", 16) ^ int("00000000", 16))
+    24
+
+A few assumptions are made and enforced:
+
+* this is a valid hexadecimal string (i.e., ``[a-fA-F0-9]+``)
+* the strings are the same length
+* the strings do not begin with ``"0x"``
+
+Why yet another Hamming distance library?
+-----------------------------------------
+
+There are a lot of fantastic (python) libraries that offer methods to calculate
+various edit distances, including Hamming distances: Distance, textdistance,
+scipy, jellyfish, etc.
+
+In this case, I needed a hamming distance library that worked on hexadecimal
+strings (i.e., a Python ``str``) and performed blazingly fast.
+Furthermore, I often did not care about hex strings greater than 256 bits.
+That length constraint is different vs all the other libraries and enabled me
+to explore vectorization techniques via ``numba``, ``numpy``, and even
+``SSE/AVX``.
+
+Lastly, I wanted to minimize dependencies, meaning you do not need to install
+``numpy``, ``gmpy``, ``cython``, ``pypy``, ``pythran``, etc.
+
+Eventually, after playing around with ``gmpy.popcount``, ``numba.jit``,
+``pythran.run``, ``numpy``, and ``AVX2``, I decided to write what I wanted
+in a raw C header. At this point, I'm using raw ``char*`` and
+``int*``, so exploring re-writing this in Fortran makes little sense. Vectorization
+techniques also ended up adding more overhead due to converting the hex strings to
+vector-register-ingestible floats from ``char*``.
+
+Installation
+-------------
+
+To install, ensure you have Python 2.7 or 3.4+. Run
+
+::
+
+    pip install hexhamming
+
+or to install from source
+
+::
+
+    git clone https://github.com/mrecachinas/hexhamming
+    cd hexhamming
+    python setup.py install # or pip install .
+
+If you want to contribute to hexhamming, you should install the dev
+dependencies
+
+::
+
+    pip install -r requirements-dev.txt
+
+and make sure the tests pass with
+
+::
+
+    pytest -vls
+
+Example
+-------
+
+To use the base C++ extension, you can simply run
+
+::
+
+    >>> from hexhamming import hamming_distance
+    >>> hamming_distance("deadbeef", "00000000")
+    24
+
+Benchmark
+---------
+
+Note: For the below image, to show how optimized this is, I included
+the benchmark of a function that looks like
+
+::
+
+    from gmpy import popcount
+    def ham(a, b):
+        return popcount(int(a, 16) ^ int(b, 16))
+
+Below is a benchmark using ``pytest-benchmark`` with hexhamming==v1.3.0
+my early 2016 1.2 GHz Intel m5 8 GB 1867 MHz LPDDR3 macOS Mojave (10.14.3)
+with Python 3.7.3 and Apple clang version 11.0.0 (clang-1100.0.33.17).
+
+.. image:: https://github.com/mrecachinas/hexhamming/blob/master/benchmark.png?raw=true
---
+++ packages/hexhamming/hexhamming-1.3.0.tar.gz/hexhamming-1.3.0/hexhamming/python_hexhamming.c
@@ -0,0 +1,344 @@
+#include <string.h>
+#if __AVX__
+    #include <emmintrin.h>
+    #include <x86intrin.h>
+#endif
+#include <Python.h>
+
+///////////////////////////////////////////////////////////////
+// C API
+///////////////////////////////////////////////////////////////
+
+/**
+ * An array of size 16 containing the XOR result of
+ * two numbers between 0 and 15 (i.e., '0' - 'F').
+ */
+const unsigned char LOOKUP[16] = {0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4};
+
+/**
+ * Returns the hamming distance of the binary between two hexadecimal strings
+ * of the same length.
+ *
+ * @param a    hexadecimal char array
+ * @param b    hexadecimal char array
+ * @param string_length length of `a` and `b`
+ * @return      the number of bits different between the hexadecimal strings
+ */
+inline int hamming_distance(
+    const char* a,
+    const char* b,
+    size_t string_length
+) {
+    // if both strings are the same, short circuit
+    // and return 0
+    if (strcmp(a, b) == 0) {
+      return 0;
+    }
+
+    int result = 0;
+    int val1, val2;
+    size_t i = 0;
+
+#if __AVX__
+    for (; i + 16 < string_length; ++i) {
+        // load 16 chars from `a` and `b`
+        __m128i a16 = _mm_loadu_si128((__m128i *)&a[i]); 
+        __m128i b16 = _mm_loadu_si128((__m128i *)&b[i]);
+
+        // Set up our masks for both branches of (x > '9') ? (x & ~0x20) - 55: x - '0'
+        __m128i subtract0vec = _mm_set1_ps('0');  // ['0', '0', ...]
+        __m128i subtract55vec = _mm_set1_ps(55);  // [55, 55, ...]
+        __m128i andvec = _mm_set1_ps(~0x20);
+
+        // Compute for both branches
+
+        // Do our compare
+
+        // Do the XOR
+
+        // Store the results
+    }
+#else
+    for (; i < string_length; ++i) {
+        // Convert the hex ascii char to its actual hexadecimal value
+        // e.g., '0' = 0, 'A' = 10, etc.
+        // Note: this is case INSENSITIVE
+        // Also note:
+        //   * 65 = 'A'
+        //   * -55 = -65 + 10
+        val1 = (a[i] > '9') ? (a[i] &~ 0x20) - 55: (a[i] - '0');
+        val2 = (b[i] > '9') ? (b[i] &~ 0x20) - 55: (b[i] - '0');
+
+        // check to make sure all characters are valid
+        // hexadecimal in both strings
+        if (val1 > 15 || val1 < 0 || val2 > 15 || val2 < 0) {
+            return -1;
+        }
+
+        result += LOOKUP[val1 ^ val2];
+    }
+#endif
+
+    return result;
+}
+
+/**
+ * Returns true if hexstrings are within a Hamming distance;
+ * otherwise, returns false (stops early)
+ *
+ * @param a    hexadecimal char array
+ * @param b    hexadecimal char array
+ * @param string_length length of `a` and `b`
+ * @param max_dist maximum allowable hamming distance
+ * @return      whether or not the strings are within some
+ *              predefined hamming distance; -1 means an error has occurred
+ */
+inline int check_hexstrings_within_dist(
+    const char* a,
+    const char* b,
+    size_t string_length,
+    size_t max_dist
+  ) {
+    // if both strings are the same, short circuit
+    // and return 0
+    if (strcmp(a, b) == 0) {
+      return 1;
+    }
+
+    size_t result = 0;
+    int val1, val2;
+    size_t i;
+    for (i = 0; i < string_length; ++i) {
+        // Convert the hex ascii char to its actual hexadecimal value
+        // e.g., '0' = 0, 'A' = 10, etc.
+        // Note: this is case INSENSITIVE
+        // Also note:
+        //   * 65 = 'A'
+        //   * -55 = -65 + 10
+        val1 = (a[i] > '9') ? (a[i] &~ 0x20) - 55: (a[i] - '0');
+        val2 = (b[i] > '9') ? (b[i] &~ 0x20) - 55: (b[i] - '0');
+
+        // check to make sure all characters are valid
+        // hexadecimal in both strings
+        if (val1 > 15 || val1 < 0 || val2 > 15 || val2 < 0) {
+            return -1;
+        }
+
+        result += LOOKUP[val1 ^ val2];
+        if (result > max_dist) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+/**
+ * Python interface for `hamming_distance`
+ *
+ * @param self      Python `self` object
+ * @param args      Python arguments for `hamming_distance` interface
+ *                  - `string1` -- hex string
+ *                  - `string2` -- hex string
+ * @returns         the integer hamming distance between the binary
+ */
+static PyObject * hamming_distance_wrapper(PyObject *self, PyObject *args) {
+    char *input_s1;
+    char *input_s2;
+
+    // get the two strings from `args`
+    // if they are incorrect types (i.e., not 's'), this will raise
+    // a ValueError
+    if (!PyArg_ParseTuple(args, "ss", &input_s1, &input_s2)) {
+        PyErr_SetString(
+            PyExc_ValueError,
+            "error occurred while parsing arguments"
+        );
+        return NULL;
+    }
+
+    // if either c-string is NULL, can't move on, so raise
+    if (input_s1 == NULL || input_s2 == NULL) {
+        PyErr_SetString(PyExc_ValueError, "one or no strings provided!");
+        return NULL;
+    }
+
+    size_t input_s1_len = strlen(input_s1);
+    size_t input_s2_len = strlen(input_s2);
+
+    if (input_s1_len != input_s2_len) {
+        PyErr_SetString(PyExc_ValueError, "strings are NOT the same length");
+        return NULL;
+    }
+
+    // at this point, we can safely proceed with
+    // our `hamming_distance` computation
+    int dist = hamming_distance(input_s1, input_s2, input_s1_len);
+    if (dist == -1) {
+      // this should only happen if the strings contain
+      // invalid hexadecimal characters
+      PyErr_SetString(PyExc_ValueError, "hex string contains invalid char");
+      return NULL;
+    } else {
+      // put the unsigned int into a Python Int object
+      // and return back to the caller!
+      return Py_BuildValue("I", dist);
+    }
+}
+
+/**
+ * Python interface for `check_hexstrings_within_dist`
+ *
+ * @param self      Python `self` object
+ * @param args      Python arguments for `check_hexstrings_within_dist` interface
+ *                  - `string1` -- hex string
+ *                  - `string2` -- hex string
+ * @returns         
+ */
+static PyObject * check_hexstrings_within_dist_wrapper(PyObject *self, PyObject *args) {
+    char *input_s1;
+    char *input_s2;
+    unsigned int max_dist;
+
+    // get the two strings from `args`
+    // if they are incorrect types (i.e., not 's'), this will raise
+    // a ValueError
+    if (!PyArg_ParseTuple(args, "ssI", &input_s1, &input_s2, &max_dist)) {
+        PyErr_SetString(
+            PyExc_ValueError,
+            "error occurred while parsing arguments"
+        );
+        return NULL;
+    }
+
+    // if either c-string is NULL, can't move on, so raise
+    if (input_s1 == NULL || input_s2 == NULL) {
+        PyErr_SetString(PyExc_ValueError, "one or no strings provided!");
+        return NULL;
+    }
+
+    size_t input_s1_len = strlen(input_s1);
+    size_t input_s2_len = strlen(input_s2);
+
+    if (input_s1_len != input_s2_len) {
+        PyErr_SetString(PyExc_ValueError, "strings are NOT the same length");
+        return NULL;
+    }
+
+    if ((int)max_dist < 0) {
+        PyErr_SetString(PyExc_ValueError, "`max_dist` must be >0");
+        return NULL;
+    }
+
+    if (max_dist > input_s1_len) {
+        return Py_BuildValue("O", Py_True);
+    }
+
+    // at this point, we can safely proceed with
+    // our `hamming_distance` computation
+    int result = check_hexstrings_within_dist(
+        input_s1,
+        input_s2,
+        input_s1_len,
+        max_dist
+    );
+    if (result == -1) {
+      // this should only happen if the strings contain
+      // invalid hexadecimal characters
+      PyErr_SetString(PyExc_ValueError, "hex string contains invalid char");
+      return NULL;
+    } else {
+      // put the unsigned int into a Python Int object
+      // and return back to the caller!
+      return Py_BuildValue("O", result == 1 ? Py_True : Py_False);
+    }
+}
+
+///////////////////////////////////////////////////////////////
+// Docstrings
+///////////////////////////////////////////////////////////////
+static char hamming_docstring[] =
+    "Calculate the hamming distance of two strings\n\n"
+    "This is equivalent to\n\n"
+    "    bin(int(a, 16) ^ int(b, 16)).count('1')\n\n"
+    "with the only difference being it was written in C and optimized\n"
+    "using a lookup table of pre-calculated hexadecimal hamming distances.\n"
+    ":param a: hexadecimal string\n"
+    ":type a: str\n"
+    ":param b: hexadecimal string\n"
+    ":type b: str\n"
+    ":returns: the hamming distance between the bits of two hexadecimal strings\n"
+    ":rtype: int\n"
+    ":raises ValueError: if either string doesn't exist, "
+    "if the strings are different lengths, or if the strings aren't valid hex";
+
+static char check_hexstrings_within_dist_docstring[] =
+    "Check if the hex strings are within a specified Hamming Distance\n\n"
+    "This is equivalent to\n\n"
+    "    bin(int(a, 16) ^ int(b, 16)).count('1') < max_dist \n\n"
+    "with the only difference being it was written in C and optimized\n"
+    "using a lookup table of pre-calculated hexadecimal hamming distances.\n"
+    ":param a: hexadecimal string\n"
+    ":type a: str\n"
+    ":param b: hexadecimal string\n"
+    ":type b: str\n"
+    ":param max_dist: maximum allowable Hamming Distance\n"
+    ":type max_dist: int\n"
+    ":returns: whether or not the two hex strings are within `max_dist`\n"
+    ":rtype: int\n"
+    ":raises ValueError: if either string doesn't exist, "
+    "if the strings are different lengths, or if the strings aren't valid hex";
+
+static char CompareDocstring[] =
+    "Module for calculating hamming distance of two hexadecimal strings";
+
+///////////////////////////////////////////////////////////////
+// Python C-extension Initialization
+///////////////////////////////////////////////////////////////
+static PyMethodDef CompareMethods[] = {
+    {"hamming_distance", hamming_distance_wrapper, METH_VARARGS, hamming_docstring},
+    {"check_hexstrings_within_dist", check_hexstrings_within_dist_wrapper, METH_VARARGS, check_hexstrings_within_dist_docstring},
+    {NULL, NULL, 0, NULL}
+};
+
+#if PY_MAJOR_VERSION >= 3
+
+static struct PyModuleDef hexhammingdef = {
+        PyModuleDef_HEAD_INIT,
+        "hexhamming",
+        CompareDocstring,
+        -1,
+        CompareMethods,
+        NULL
+};
+
+#define INITERROR return NULL
+
+PyMODINIT_FUNC
+PyInit_hexhamming(void)
+
+#else
+#define INITERROR return
+
+PyMODINIT_FUNC
+inithexhamming(void)
+#endif
+
+{
+#if PY_MAJOR_VERSION >= 3
+    PyObject *module = PyModule_Create(&hexhammingdef);
+#else
+    PyObject *module = Py_InitModule3("hexhamming", CompareMethods, CompareDocstring);
+#endif
+    if (PyModule_AddStringConstant(module, "__version__", "1.3.0")) {
+        Py_XDECREF(module);
+        INITERROR;
+    }
+    if (module == NULL) {
+        INITERROR;
+    }
+
+#if PY_MAJOR_VERSION >= 3
+    return module;
+#endif
+}
