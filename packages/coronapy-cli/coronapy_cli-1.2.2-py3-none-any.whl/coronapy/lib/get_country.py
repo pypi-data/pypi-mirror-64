6633061d466a3ec5a144bb298203a4b9f6c0769c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:10 +0000

    Add package coronapy-cli/1.2.2/coronapy_cli-1.2.2-py3-none-any.whl

---
+++ packages/coronapy-cli/coronapy_cli-1.2.2-py3-none-any.whl/coronapy/lib/get_country.py
@@ -0,0 +1,29 @@
+import requests
+from datetime import datetime
+
+def get_country(country):
+    url = 'https://corona.lmao.ninja/countries/' + country
+    response = requests.get(url)
+    data = [
+        response.json()['country'], 
+        response.json()['cases'], 
+        response.json()['todayCases'], 
+        response.json()['deaths'], 
+        response.json()['todayDeaths'], 
+        response.json()['recovered'], 
+        response.json()['active'], 
+        response.json()['critical']
+    ]
+    return data
+
+def get_country_hist(country, type):
+    url = 'https://corona.lmao.ninja/v2/historical/' + country
+    response = requests.get(url)
+    data = response.json()['timeline']
+    dicts = data[type]
+    labels = []
+    hist_data = []
+    for key, value in list(dicts.items())[-20:]:
+        labels.append(datetime.strptime(key, '%m/%d/%y').strftime('%d %b'))
+        hist_data.append([value])
+    return labels, hist_data
