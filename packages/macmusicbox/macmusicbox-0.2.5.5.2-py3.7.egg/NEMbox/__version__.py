d67c0204a79ae496b1b3fc9620a7f6c7ba364b3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:52 +0000

    Add package macmusicbox/0.2.5.5.2/macmusicbox-0.2.5.5.2-py3.7.egg

---
+++ packages/macmusicbox/macmusicbox-0.2.5.5.2-py3.7.egg/NEMbox/__version__.py
@@ -0,0 +1,14 @@
+'''
+                                            _      _
+ _ __ ___   __ _  ___   _ __ ___  _   _ ___(_) ___| |__   _____  __
+| '_ ` _ \ / _` |/ __| | '_ ` _ \| | | / __| |/ __| '_ \ / _ \ \/ /
+| | | | | | (_| | (__  | | | | | | |_| \__ \ | (__| |_) | (_) >  <
+|_| |_| |_|\__,_|\___| |_| |_| |_|\__,_|___/_|\___|_.__/ \___/_/\_\
+'''
+__title__ = 'macmusicbox'
+__version__ = '0.2.5.5.2'
+__description__ = 'A sexy command line interface musicbox for mac and linux'
+__url__ = 'https://github.com/wangjianyuan10/musicbox'
+__author__ = 'walker'
+__author_email__ = 'wangjianyuan10@163.com'
+__license__ = 'MIT'
