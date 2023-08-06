7ac06f0f83e3030f825876cb8b4400b6b9937a87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:04 +0000

    Add package deepevent/0.3.1/deepevent-0.3.1.tar.gz

---
+++ packages/deepevent/deepevent-0.3.1.tar.gz/deepevent-0.3.1/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+from setuptools import find_packages,setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="deepevent",
+    version="0.3.1",
+    author="Lempereur Mathieu",
+    author_email="mathieu.lempereur@univ-brest.fr",
+    description="Deep Learning to identify gait events",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    packages=find_packages(),
+    scripts=['deepevent/deepevent_script.py'],
+    install_requires=['tensorflow>=2.0.0',
+                      'keras>=2.3.1',
+                      'numpy>=1.18.1',
+                      'scipy>=1.4.1',
+                      'pyBTK>=0.1.1'],
+    classifiers=['Programming Language :: Python',
+                 'Programming Language :: Python :: 3.7',
+                 'Operating System :: Microsoft :: Windows',
+                 'Natural Language :: English'],
+
+)
