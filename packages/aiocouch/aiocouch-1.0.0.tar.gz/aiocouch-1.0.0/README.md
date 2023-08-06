f35adeaa1b3c7e794f72a04908b14a2c7e88f652
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:17 +0000

    Add package aiocouch/1.0.0/aiocouch-1.0.0.tar.gz

---
+++ packages/aiocouch/aiocouch-1.0.0.tar.gz/aiocouch-1.0.0/README.md
@@ -0,0 +1,21 @@
+![Python package](https://github.com/metricq/aiocouch/workflows/Python%20package/badge.svg)
+
+# aiocouch
+An asynchronous client library for CouchDB 2.0 based on asyncio using aiohttp
+
+## Installation
+
+Run `pip install .` within the root directory.
+
+## Run tests
+
+- Setup the CouchDB URL and credentials using the environment variables
+- (Optional) install using `pip install --editable '.[tests]'`
+- run `pytest --cov=aiocouch`
+
+
+Or shorter for CI:
+
+```
+COUCHDB_HOST=http://localhost:5984 COUCHDB_USER=admin COUCHDB_PASS=admin python setup.py test
+```
---
+++ packages/aiocouch/aiocouch-1.0.0.tar.gz/aiocouch-1.0.0/aiocouch/exception.py
@@ -0,0 +1,89 @@
+import aiohttp
+
+from contextlib import suppress
+import functools
+
+
+class BadRequestError(ValueError):
+    pass
+
+
+class UnauthorizedError(PermissionError):
+    pass
+
+
+class ForbiddenError(PermissionError):
+    pass
+
+
+class NotFoundError(KeyError):
+    pass
+
+
+class ConflictError(KeyError):
+    pass
+
+
+class ExpectationFailedError(ValueError):
+    pass
+
+
+class PreconditionFailedError(RuntimeError):
+    pass
+
+
+class UnsupportedMediaTypeError(ValueError):
+    pass
+
+
+def raise_for_endpoint(endpoint, message, exception, exception_type=None):
+    if exception_type is None:
+        if exception.status == 400:
+            exception_type = BadRequestError
+        elif exception.status == 401:
+            exception_type = UnauthorizedError
+        elif exception.status == 403:
+            exception_type = ForbiddenError
+        elif exception.status == 404:
+            exception_type = NotFoundError
+        elif exception.status == 409:
+            exception_type = ConflictError
+        elif exception.status == 412:
+            exception_type = PreconditionFailedError
+        elif exception.status == 415:
+            exception_type = UnsupportedMediaTypeError
+        elif exception.status == 417:
+            exception_type = ExpectationFailedError
+        else:
+            raise ValueError(
+                "Something went wrong, but I couldn't deduce the type of exception nor "
+                "format a nice exception for you. I'm sorry."
+            ) from exception
+
+    message_input = {}
+
+    with suppress(KeyError, AttributeError):
+        message_input["id"] = endpoint.id
+        message_input["endpoint"] = endpoint.endpoint
+        message_input["rev"] = endpoint._data["_rev"]
+    with suppress(KeyError, AttributeError):
+        message_input["document_id"] = endpoint._document.id
+        message_input["document_rev"] = endpoint._document._data["_rev"]
+
+    raise exception_type(message.format(**message_input)) from exception
+
+
+def raises(status, message, exception_type=None):
+    def decorator_raises(func):
+        @functools.wraps(func)
+        async def wrapper(endpoint, *args, **kwargs):
+            try:
+                return await func(endpoint, *args, **kwargs)
+            except aiohttp.ClientResponseError as exception:
+                if status == exception.status:
+                    raise_for_endpoint(endpoint, message, exception, exception_type)
+                raise exception
+
+        return wrapper
+
+    return decorator_raises
