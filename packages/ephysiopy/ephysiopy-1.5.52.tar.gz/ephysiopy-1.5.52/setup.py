459b09aa255fadd1b6374afbfb1302baf9f1b5d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:15 +0000

    Add package ephysiopy/1.5.52/ephysiopy-1.5.52.tar.gz

---
+++ packages/ephysiopy/ephysiopy-1.5.52.tar.gz/ephysiopy-1.5.52/setup.py
@@ -0,0 +1,35 @@
+from ephysiopy import __version__
+import setuptools
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'ephysiopy', 'README.md'), newline='', encoding='utf-8') as f:
+	long_description = f.read()
+	long_description_content_type = 'text/markdown'
+
+setuptools.setup(name='ephysiopy',
+	  version=__version__,
+	  description='Analysis of electrophysiological data recorded with the Axona or OpenEphys recording systems',
+	  long_description_content_type=long_description_content_type,
+	  long_description=long_description,
+	  url='https://github.com/rhayman/ephysiopy',
+	  author='Robin Hayman',
+	  author_email='r.hayman@ucl.ac.uk',
+	  license='MIT',
+	  packages=setuptools.find_packages(),
+	  scripts=[path.join(this_directory, "ephysiopy/scripts/RENAME_ME_oe2axona_debugging.py"),
+	  			path.join(this_directory, "ephysiopy/scripts/RENAME_ME_oe2axona_multiplefiles.py"),
+	  			path.join(this_directory, "ephysiopy/scripts/RENAME_ME_oe2numpy_script.py")],
+	  package_data={'ephysiopy' : ['dacq2py/*.pkl', '*.md']},
+	  python_requires='>=3.5',
+	  install_requires=[
+	  	'numpy',
+	  	'scipy',
+	  	'matplotlib',
+	  	'astropy',
+	  	'scikit-image',
+	  	'mahotas',
+	  	'h5py',
+	  	'scikit-learn<0.21'
+	  ],
+	  zip_safe=False)
