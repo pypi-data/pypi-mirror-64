3897c797efb5903fff15645a7de697c481f2d9c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:16 +0000

    Add package creme/0.5.1/creme-0.5.1-cp37-cp37m-manylinux1_i686.whl

---
+++ packages/creme/creme-0.5.1-cp37-cp37m-manylinux1_i686.whl/creme/utils/__init__.py
@@ -0,0 +1,21 @@
+"""Utility classes and functions."""
+from . import estimator_checks
+from . import math
+from . import pretty
+from .histogram import Histogram
+from .sdft import SDFT
+from .skyline import Skyline
+from .window import Window
+from .window import SortedWindow
+
+
+__all__ = [
+    'estimator_checks',
+    'Histogram',
+    'math',
+    'pretty',
+    'SDFT',
+    'Skyline',
+    'SortedWindow',
+    'Window'
+]
