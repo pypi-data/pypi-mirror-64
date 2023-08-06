1c86f93317e18acc8265a7e3c9c0188a0141f445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:42 +0000

    Add package thebe/0.0.2/thebe-0.0.2-py3-none-any.whl

---
+++ packages/thebe/thebe-0.0.2-py3-none-any.whl/thebe/core/ledger.py
@@ -0,0 +1,103 @@
+from itertools import zip_longest
+import time, sys, datetime, glob, re, sys, time, os, copy
+from hashlib import md5
+from io import StringIO
+from subprocess import Popen, PIPE
+from flask import url_for
+from pygments import highlight
+from pygments.lexers import BashLexer, PythonLexer
+from pygments.formatters import HtmlFormatter
+from flask_socketio import emit, SocketIO
+import thebe.core.constants as Constants
+import copy
+
+#def updateCells(oldCellDict, fileContent):
+def update(oldCellList, fileContent):
+    cellList=[]
+    hashList=getHashList(oldCellList)
+    for cellCount, code in enumerate(list(filter(None, fileContent.split(Constants.CellDelimiter)))):
+        #Hash to be used for identifying priviously run code
+        cellHash=hashCode(code)
+        #Set outputs
+        cell=setOutputs(oldCellList, cellList, cellHash, hashList)
+        #Set Code
+        cell['code']=code
+        #That cell count(it's order in the cell list)
+        cell['cellCount']=str(cellCount)
+        cellList.append(cell)
+    return cellList
+
+#Form the hashes from the cell list into a list
+def getHashList(cellList):
+    return [cell['hash'] for cell in cellList]
+
+#Set outputs of cell depending on whether it has been run before 
+def setOutputs(oldCellList, cellList, cellHash, hashList, dl='list'):
+    #for one cell
+    if dl=='list':
+        return assembleCell(oldCellList, hashList, cellHash)
+    else:
+        cellDict['hash'].append(cellHash)
+        try:
+            x=oldCellDict['hash'].index(cellHash)
+            cellDict['changed'].append(False)
+            cellDict['stdout'].append(oldCellDict['stdout'][x])
+            cellDict['stderr'].append(oldCellDict['stderr'][x])
+            cellDict['image/png'].append(oldCellDict['image/png'][x])
+        except IndexError:
+            cellDict['changed'].append(True)
+            cellDict['stdout'].append('')
+            cellDict['stderr'].append('')
+            cellDict['image/png'].append('')
+
+#If the hash exists set new cell to old cell
+def assembleCell(oldCellList, hashList, cellHash):
+        cell=copy.deepcopy(Constants.Cell)
+        '''
+        Check if the cell existed before
+        If it has set to old cell
+        '''
+        try:
+            x=hashList.index(cellHash)
+            cell=oldCellList[x]
+        #If not, change hash, set changed, and new changed
+        except ValueError:
+            cell['changed']=True
+            cell['time']=time.strftime("%x %X", time.gmtime())
+            cell['hash']=cellHash
+        return cell
+
+#Hash the string of code
+def hashCode(code):
+    return md5(code.encode()).hexdigest()
+
+#Old func for pre=try/catch
+def isChanged(cellHash, hashList):
+        try:
+            hashList.index(cellHash)
+            return True
+        except ValueError:
+            return False
+
+#Old func 
+def getNewCellsToRun(ledger, allCellsList):
+    newCellsToRun=[]
+    for currentCell, ledgerCell in zip_longest(allCellsList, ledger, fillvalue=None):
+        if currentCell['hash']!=ledgerCell:
+            currentCell['changed']=True
+#            print(oldCell['stdout'])
+#            print(oldCell['hash'])
+        newCellsToRun.append(currentCell)
+    return newCellsToRun
+
+#Old func for when using dict of lists
+def dictToList(cellDict):
+    cellList=[]
+    keys=cellDict.keys()
+    values=cellDict.values()
+    for cell in np.array(values).t:
+        cellList.append(dict(zip(keys, cell)))
+    return cellList        
+
+def updateChanged(changedList):
+    changedList=[False for x in changedList]
---
+++ packages/thebe/thebe-0.0.2-py3-none-any.whl/thebe/thebe.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+from pygments.formatters import HtmlFormatter
+from flask import Flask, render_template, Markup, Response, g
+from flask_socketio import SocketIO, emit
+from multiprocessing import Process
+import satyrn.core.run as Run
+import satyrn.core.html as Html
+import satyrn.core.ledger as Ledger
+import satyrn.core.database as Database
+import satyrn.core.update as Update
+import time, os, sys, webbrowser, argparse, logging, sqlite3, logging
+
+#Parse commandline argument
+parser = argparse.ArgumentParser(description='Display python information live in browser.')
+parser.add_argument('file', metavar='F', help='python file to run')
+args = parser.parse_args()
+fileLocation=args.file# file to execute
+
+#Initialize port and url
+port=5000
+url = 'localhost:%s' % port
+
+#Initialize flask
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['SECRET_KEY'] = 'secret!'
+socketio= SocketIO(app)
+
+#Configure logging
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
+#Initialize some globals
+myDir=os.path.dirname(os.path.abspath(__file__))#Absolute directory of package
+
+'''
+Set some headers and get and send css for all of the HtmlFormatter components.
+'''
+@app.route('/')
+def home():
+    css=HtmlFormatter().get_style_defs('.highlight')
+    response=Response(render_template('main.html', css=css))
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate" # HTTP 1.1.
+    response.headers["Pragma"] = "no-cache" # HTTP 1.0.
+    response.headers["Expires"] = "0" # Proxies
+    return response
+
+'''
+Connect and disconnect events.
+'''
+@socketio.on('connect')
+def connect():
+    print('Connected to client')
+    #Show
+    Update.checkUpdate(socketio, fileLocation, connected=True)
+    #Start pinging
+    socketio.emit('ping client')
+@socketio.on('disconnect')
+def disconnect():
+    print('Client disconnected')
+
+'''
+Ping back and forth from client to server.
+Checks whether or not the file has been saved and running it when changed.
+'''
+@socketio.on('check if saved')
+def check():
+    print('Check if target updated...')
+    Update.checkUpdate(socketio, fileLocation)
+    socketio.emit('ping client')
+
+'''
+Run flask and socketio.
+'''
+def main():
+    ledger=[]
+    def startFlask():
+        socketio.run(app, port=5000)#, debug=True)
+#        webbrowser.open_new_tab(url)
+    try:
+        print('Starting flask process...')
+        flask=Process(target=startFlask)
+        flask.start()
+    except KeyboardInterrupt:
+        print("Terminating flask server.")
+        flask.terminate()
+        flask.join()
+        print("Terminated flask server.")
