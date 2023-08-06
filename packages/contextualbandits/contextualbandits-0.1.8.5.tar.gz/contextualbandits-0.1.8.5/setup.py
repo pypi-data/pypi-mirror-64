0aeb6317a4ebdd8781b9ff8a796fbe4d10235b0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:18 +0000

    Add package contextualbandits/0.1.8.5/contextualbandits-0.1.8.5.tar.gz

---
+++ packages/contextualbandits/contextualbandits-0.1.8.5.tar.gz/contextualbandits-0.1.8.5/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+  name = 'contextualbandits',
+  packages = ['contextualbandits'],
+  install_requires=[
+   'pandas',
+   'numpy',
+   'scipy',
+   'pandas',
+   'joblib>=0.13'
+],
+  version = '0.1.8.5',
+  description = 'Python Implementations of Algorithms for Contextual Bandits',
+  author = 'David Cortes',
+  author_email = 'david.cortes.rivera@gmail.com',
+  url = 'https://github.com/david-cortes/contextualbandits',
+  keywords = 'contextual bandits offset tree doubly robust policy linucb thompson sampling',
+  classifiers = [],
+)
