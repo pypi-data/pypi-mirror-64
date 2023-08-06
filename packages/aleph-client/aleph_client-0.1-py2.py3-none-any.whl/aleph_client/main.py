87b68f853640848152d2559e2fb5468efd00329f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:42 +0000

    Add package aleph-client/0.1/aleph_client-0.1-py2.py3-none-any.whl

---
+++ packages/aleph-client/aleph_client-0.1-py2.py3-none-any.whl/aleph_client/main.py
@@ -0,0 +1,6 @@
+from .asynchronous import (sync_ipfs_push as ipfs_push,
+                           sync_broadcast as broadcast,
+                           sync_create_post as create_post,
+                           sync_create_aggregate as create_aggregate,
+                           sync_submit as submit,
+                           sync_fetch_aggregate as fetch_aggregate)
