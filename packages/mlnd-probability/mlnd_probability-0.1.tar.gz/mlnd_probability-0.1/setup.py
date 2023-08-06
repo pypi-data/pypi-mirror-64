1b16660054956cac69011301171b3231202e93e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:40 +0000

    Add package mlnd-probability/0.1/mlnd_probability-0.1.tar.gz

---
+++ packages/mlnd-probability/mlnd_probability-0.1.tar.gz/mlnd_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='mlnd_probability',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['mlnd_probability'],
+      author = 'Ahmed Shoukr',
+      author_email = 'ahmedshoukr10@gmail.com',
+      zip_safe=False)
