430f230300f59e0707b1ec6d9ec4125bd3b200db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:45 +0000

    Add package ahrli-huobi-client/1.0.8/ahrli_huobi_client-1.0.8-py3-none-any.whl

---
+++ packages/ahrli-huobi-client/ahrli_huobi_client-1.0.8-py3-none-any.whl/huobi/constant/system.py
@@ -0,0 +1,12 @@
+
+class WebSocketDefine:
+    Uri = "wss://api.huobi.io/"   # "wss://api.huobi.so/"  # "wss://api.huobi.vn/"  # "wss://api-aws.huobi.pro"
+class RestApiDefine:
+    Url = "https://api.huobi.io"  # "https://api.huobi.so"  # "https://api.huobi.vn"
+
+class ApiVersion:
+    VERSION_V1 = "v1"
+    VERSION_V2 = "v2"
+
+
+
