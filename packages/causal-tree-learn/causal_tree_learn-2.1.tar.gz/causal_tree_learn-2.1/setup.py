55eac9b2eb2442f5984824f2ed375c7bb2e3ea04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:27 +0000

    Add package causal-tree-learn/2.1/causal_tree_learn-2.1.tar.gz

---
+++ packages/causal-tree-learn/causal_tree_learn-2.1.tar.gz/causal_tree_learn-2.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+from setuptools import find_packages
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="causal_tree_learn",
+    version="2.1",
+    author="Christopher Tran",
+    author_email="ctran29@uic.edu",
+    description="Python implementation of causal trees with validation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/edgeslab/CTL",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['numpy',
+                      'scikit-learn',
+                      'scipy'
+                      ],
+    python_requires='>=3.6',
+)
