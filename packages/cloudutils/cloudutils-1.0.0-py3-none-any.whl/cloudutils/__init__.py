5afd9570c66e29c6697ded69a258f72fb242a27e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:11 +0000

    Add package cloudutils/1.0.0/cloudutils-1.0.0-py3-none-any.whl

---
+++ packages/cloudutils/cloudutils-1.0.0-py3-none-any.whl/cloudutils/__init__.py
@@ -0,0 +1,6 @@
+#Copyright © 2020 Noel Kaczmarek
+__version__ = '1.0.0'
+__author__ = 'Noel Kaczmarek'
+__maintainer__ = 'Noel Kaczmarek'
+__email__ = 'noel.Kaczmarek@gmail.com'
+__copyright__ = 'Copyright © 2020 Noel Kaczmarek'
