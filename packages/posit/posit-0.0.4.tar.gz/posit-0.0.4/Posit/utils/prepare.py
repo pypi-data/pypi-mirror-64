26b76626b79558c5b3b82fda9176863f07e6baf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:09 +0000

    Add package posit/0.0.4/posit-0.0.4.tar.gz

---
+++ packages/posit/posit-0.0.4.tar.gz/posit-0.0.4/Posit/utils/prepare.py
@@ -0,0 +1,44 @@
+import random
+import os
+from rdkit import Chem
+from Posit.utils.RMSD import calcRMSD
+import zipfile
+
+
+def download_data(number):
+    pass
+
+
+def unzip(filename, output_path='./'):
+    zFile = zipfile.ZipFile(filename, 'r')
+    for fileM in zFile.namelist():
+        zFile.extract(fileM, output_path)
+    zFile.close()
+
+
+def prepare_data(play_dir, pdb_dir):
+    ligands = os.path.join(play_dir, pdb_dir, 'ligands.sdf')
+    mols = [i for i in Chem.SDMolSupplier(ligands)]
+    mols_h = [i for i in Chem.SDMolSupplier(ligands, removeHs=False)]
+    ref = mols[:][0]
+    atoms = [i for i in range(ref.GetNumAtoms())]
+    random.shuffle(mols)
+
+    # calc rmsd
+    best_mol = None
+    rmsd_value = {}
+    w = Chem.SDWriter(ligands)
+    for idx, mol in enumerate(mols):
+        name = str(idx + 1)
+        # rename mols
+        mols_h[idx].SetProp('_Name', name)
+        atomMaps = list(zip(atoms, atoms))
+        rmsd = calcRMSD(mol, ref, atomMaps)
+        rmsd_value[name] = rmsd
+        # print(idx + 1, rmsd)
+        if rmsd == 0:
+            best_mol = name
+        w.write(mols_h[idx])
+    w.close()
+    # rmsd_value - {"name": rmsd}
+    return [rmsd_value, best_mol]
