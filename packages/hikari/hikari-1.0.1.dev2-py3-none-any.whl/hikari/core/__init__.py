748a411249940c0bc415432523c8a6a579a9058f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:21 +0000

    Add package hikari/1.0.1.dev2/hikari-1.0.1.dev2-py3-none-any.whl

---
+++ packages/hikari/hikari-1.0.1.dev2-py3-none-any.whl/hikari/core/__init__.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright © Nekokatt 2019-2020
+#
+# This file is part of Hikari.
+#
+# Hikari is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hikari is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Hikari. If not, see <https://www.gnu.org/licenses/>.
+"""The core API for interacting with Discord directly."""
+from hikari.core.configs import *
+from hikari.core.clients import *
+from hikari.core.channels import *
+from hikari.core.entities import *
+from hikari.core.events import *
+from hikari.core.gateway_bot import *
+from hikari.core.guilds import *
+from hikari.core.invites import *
+from hikari.core.messages import *
+from hikari.core.oauth2 import *
+from hikari.core.permissions import *
+from hikari.core.snowflakes import *
+from hikari.core.users import *
