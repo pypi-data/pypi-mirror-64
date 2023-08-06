7b5c3e7ea0623a8f482c7968a2baba5fe5256350
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:22 +0000

    Add package pynovice/0.0.2/pynovice-0.0.2.tar.gz

---
+++ packages/pynovice/pynovice-0.0.2.tar.gz/pynovice-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+	name='pynovice',
+	version='0.0.2',
+	description='data mining for novice and expert',
+	url='https://github.com/wqwangchn/novice',
+	author='wqwangchn',
+	author_email='wqwangchn@163.com',
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	packages=setuptools.find_packages(),
+	install_requires = ['wordninja>=2.0.0','bert4keras>=0.6.5'],
+	classifiers=[
+	"Programming Language :: Python :: 3",
+	"License :: OSI Approved :: MIT License",
+	"Operating System :: OS Independent",
+	],
+	zip_safe=False
+)
