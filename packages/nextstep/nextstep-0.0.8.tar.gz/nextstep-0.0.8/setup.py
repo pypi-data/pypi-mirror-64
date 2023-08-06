80bda5a638a0b4916f95c6899f33eb5187b8337d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:53 +0000

    Add package nextstep/0.0.8/nextstep-0.0.8.tar.gz

---
+++ packages/nextstep/nextstep-0.0.8.tar.gz/nextstep-0.0.8/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nextstep",
+    version="0.0.8",
+    author="Yang Yuesong",
+    author_email="yangyuesongyys@gmail.com",
+    description="USEP price prediction",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/YangYuesong0323/nextstep",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    install_requires = [
+        'wwo-hist == 0.0.4',
+        'tenserflow == 2.1.0',
+        'statsmodels == 0.11.0',
+        'dataflows == 0.0.71']
+)
