9fceda707fb85798c45ed8f4da00ff4ba69bf08a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:11 +0000

    Add package pyunifi/2.20.1/pyunifi-2.20.1.tar.gz

---
+++ packages/pyunifi/pyunifi-2.20.1.tar.gz/pyunifi-2.20.1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='pyunifi',
+      version='2.20.1',
+      description='API for Ubiquity Networks UniFi controller',
+      author='Caleb Dunn',
+      author_email='finish.06@gmail.com',
+      url='https://github.com/finish06/unifi-api',
+      packages=['pyunifi'],
+      scripts=['unifi-create-voucher', 'unifi-ls-clients'],
+      classifiers=[],
+      install_requires=['requests'],
+      )
