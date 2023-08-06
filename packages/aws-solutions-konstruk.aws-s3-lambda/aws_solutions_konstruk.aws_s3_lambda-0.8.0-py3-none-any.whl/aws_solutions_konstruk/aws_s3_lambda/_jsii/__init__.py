9e6c341cce4dc3f9bb092a2bc6f67dfcb21de4a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:55 +0000

    Add package aws-solutions-konstruk.aws-s3-lambda/0.8.0/aws_solutions_konstruk.aws_s3_lambda-0.8.0-py3-none-any.whl

---
+++ packages/aws-solutions-konstruk.aws-s3-lambda/aws_solutions_konstruk.aws_s3_lambda-0.8.0-py3-none-any.whl/aws_solutions_konstruk/aws_s3_lambda/_jsii/__init__.py
@@ -0,0 +1,20 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.aws_iam
+import aws_cdk.aws_lambda
+import aws_cdk.aws_lambda_event_sources
+import aws_cdk.aws_s3
+import aws_cdk.aws_s3_notifications
+import aws_cdk.core
+import aws_solutions_konstruk.core
+__all__ = []
+
+publication.publish()
