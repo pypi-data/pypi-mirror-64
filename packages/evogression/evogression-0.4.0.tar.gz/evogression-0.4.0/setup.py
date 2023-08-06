3a94f5b757f74ff3824cbb96e37e9c08a028e760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:47 +0000

    Add package evogression/0.4.0/evogression-0.4.0.tar.gz

---
+++ packages/evogression/evogression-0.4.0.tar.gz/evogression-0.4.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+from setuptools.extension import Extension
+from Cython.Build import cythonize
+
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+extensions = [
+    Extension('evogression.calc_target_cython', ['evogression/calc_target_cython.pyx']),
+    Extension('evogression.generate_parameter_coefficients_calc', ['evogression/generate_parameter_coefficients_calc.pyx']),
+]
+
+
+setup(name='evogression',
+      version='0.4.0',
+      packages=find_packages(),
+      license='MIT',
+      author='Zach Bateman',
+      description='Evolutionary Regression',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/zachbateman/evogression.git',
+      download_url='https://github.com/zachbateman/evogression/archive/v_0.4.0.tar.gz',
+      keywords=['REGRESSION', 'MACHINE', 'LEARNING', 'EVOLUTION'],
+      install_requires=['tqdm', 'easy_multip'],
+      classifiers=['Development Status :: 3 - Alpha',
+                    'License :: OSI Approved :: MIT License',
+                    'Programming Language :: Python :: 3',
+                    'Programming Language :: Python :: 3.6',
+                    'Programming Language :: Python :: 3.7',
+                    'Programming Language :: Python :: 3.8',
+                    ],
+    ext_modules=cythonize(extensions)
+)
---
+++ packages/evogression/evogression-0.4.0.tar.gz/evogression-0.4.0/tests/parameter_pruning_evolution_test.py
@@ -0,0 +1,30 @@
+import unittest
+import cProfile
+import sys
+sys.path.insert(1, '..')
+import evogression
+import easy_multip
+from test_data_many_dimensions import data as many_d_data
+from pprint import pprint as pp
+import matplotlib
+import matplotlib.pyplot as plt
+
+
+class TestManyDimensionParameterPruningRegression(unittest.TestCase):
+
+    def test_best_creature_parabola_regression_evolution(self):
+        pruned_evolution_group = evogression.evolution_group.parameter_pruned_evolution_group(many_d_data, target_param='Target', max_parameters=15, num_creatures=100, num_cycles=3, num_groups=3)
+
+        pp(evogression.evolution_group.group_parameter_usage(pruned_evolution_group))
+        evogression.evolution_group.output_group_regression_funcs(pruned_evolution_group)
+
+        self.assertTrue(True)
+
+
+
+if __name__ == '__main__':
+    # have to do the below magic to make cProfile work with unittest
+    suite = unittest.TestLoader().discover('.')
+    def run_tests():
+        unittest.TextTestRunner().run(suite)
+    cProfile.run('unittest.main()', 'parameter_pruning_test.profile')
