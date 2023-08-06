1b124a59af9ad9419acb511ec258bd62a5ad6f84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:53 +0000

    Add package arg-ranker/2.3/arg_ranker-2.3.tar.gz

---
+++ packages/arg-ranker/arg_ranker-2.3.tar.gz/arg_ranker-2.3/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+setup(
+    name="arg_ranker",
+    packages=['arg_ranker'],
+    version="2.3",
+    description="Ranking the risk of antibiotic resistance for metagenomes",
+    author='An-Ni Zhang',
+    author_email='anniz44@mit.edu',
+    url='https://github.com/caozhichongchong/ARG_Ranker',
+    keywords=['antibiotic resistance', 'risk', 'one health', 'clinical AMR', 'mobile AMR'],
+    license='MIT',
+    #install_requires=['python>=3.0'],
+    include_package_data=True,
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    package_dir={'arg_ranker': 'arg_ranker'},
+    package_data={'arg_ranker': ['data/*','bin/*']},
+    entry_points={'console_scripts': ['arg_ranker = arg_ranker.__main__:main']},
+    #zip_safe=False,
+    #setup_requires=['pytest-runner'],
+    #tests_require=['pytest'],
+    classifiers=[
+        #'Development Status :: 1 - Alpha',
+        #'Intended Audience :: Bioinformatics and Researchers',
+        #"License :: MIT",
+        #'Operating System :: MacOS',
+        #'Operating System :: Microsoft :: Windows',
+        #"Operating System :: LINUX",
+        #'Programming Language :: Python :: 2.7',
+        "Programming Language :: Python :: 3",
+        #"Topic :: Antibiotic resistance :: risk ranking",
+        #"Topic :: Metagenomes :: Antibiotic resistance",
+    ]
+)
