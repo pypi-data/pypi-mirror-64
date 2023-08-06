35697fb33b2410b1e9d148b6494225630db63fb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:38 +0000

    Add package csvquery/1.1.0/csvquery-1.1.0.tar.gz

---
+++ packages/csvquery/csvquery-1.1.0.tar.gz/csvquery-1.1.0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="csvquery",
+    version="1.1.0",
+    author="Houston Youth Computer Science Group",
+    author_email="houstoncsgroup@gmail.com",
+    description="A versatile python package that allows you to execute MongoDB-style queries on CSV files.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hycsg/csvquery",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'requests',
+    ],
+    python_requires='>=3.4',
+)
