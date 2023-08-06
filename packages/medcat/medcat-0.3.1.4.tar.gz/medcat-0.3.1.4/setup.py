23a782aa1fe2d7adf7f94f58eb9f5ca26223015d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:05 +0000

    Add package medcat/0.3.1.4/medcat-0.3.1.4.tar.gz

---
+++ packages/medcat/medcat-0.3.1.4.tar.gz/medcat-0.3.1.4/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+from setuptools.command.install import install
+from setuptools.command.develop import develop
+from setuptools.command.egg_info import egg_info
+
+with open("./README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="medcat",
+    version="0.3.1.4",
+    author="w-is-h",
+    author_email="w.kraljevic@gmail.com",
+    description="Concept annotation tool for Electronic Health Records",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CogStack/MedCAT",
+    packages=['medcat', 'medcat.utils', 'medcat.preprocessing'],
+    install_requires=[
+        'numpy~=1.15',
+        'pandas~=0.23',
+        'gensim~=3.7',
+        'spacy==2.2.4',
+        'scipy~=1.1',],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
