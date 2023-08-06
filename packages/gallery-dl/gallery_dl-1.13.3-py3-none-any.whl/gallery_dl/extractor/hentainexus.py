51eec687a6e6fd17fc99e600173ec1c376e15fec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:25 +0000

    Add package gallery-dl/1.13.3/gallery_dl-1.13.3-py3-none-any.whl

---
+++ packages/gallery-dl/gallery_dl-1.13.3-py3-none-any.whl/gallery_dl/extractor/hentainexus.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2019 Mike Fährmann
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+
+"""Extractors for https://hentainexus.com/"""
+
+from .common import GalleryExtractor, Extractor, Message
+from .. import text, util
+import json
+
+
+class HentainexusGalleryExtractor(GalleryExtractor):
+    """Extractor for image galleries on hentainexus.com"""
+    category = "hentainexus"
+    root = "https://hentainexus.com"
+    pattern = (r"(?i)(?:https?://)?(?:www\.)?hentainexus\.com"
+               r"/(?:view|read)/(\d+)")
+    test = (
+        ("https://hentainexus.com/view/5688", {
+            "url": "746d0043e20030f1171aae5ea113176607302517",
+            "keyword": "77702b42f8f76ecfe5d8a14cfbbcbd855eb14d7f",
+        }),
+        ("https://hentainexus.com/read/5688"),
+    )
+
+    def __init__(self, match):
+        self.gallery_id = match.group(1)
+        url = "{}/view/{}".format(self.root, self.gallery_id)
+        GalleryExtractor.__init__(self, match, url)
+
+    def metadata(self, page):
+        rmve = text.remove_html
+        extr = text.extract_from(page)
+        data = {
+            "gallery_id" : text.parse_int(self.gallery_id),
+            "tags"       : extr('"og:description" content="', '"').split(", "),
+            "thumbnail"  : extr('"og:image" content="', '"'),
+            "title"      : extr('<h1 class="title">', '</h1>'),
+            "artist"     : rmve(extr('viewcolumn">Artist</td>'     , '</td>')),
+            "book"       : rmve(extr('viewcolumn">Book</td>'       , '</td>')),
+            "language"   : rmve(extr('viewcolumn">Language</td>'   , '</td>')),
+            "magazine"   : rmve(extr('viewcolumn">Magazine</td>'   , '</td>')),
+            "parody"     : rmve(extr('viewcolumn">Parody</td>'     , '</td>')),
+            "publisher"  : rmve(extr('viewcolumn">Publisher</td>'  , '</td>')),
+            "description": rmve(extr('viewcolumn">Description</td>', '</td>')),
+        }
+        data["lang"] = util.language_to_code(data["language"])
+        return data
+
+    def images(self, page):
+        url = "{}/read/{}".format(self.root, self.gallery_id)
+        extr = text.extract_from(self.request(url).text)
+        urls = extr("initReader(", "]") + "]"
+        return [(url, None) for url in json.loads(urls)]
+
+
+class HentainexusSearchExtractor(Extractor):
+    """Extractor for search results on hentainexus.com"""
+    category = "hentainexus"
+    subcategory = "search"
+    root = "https://hentainexus.com"
+    pattern = (r"(?i)(?:https?://)?(?:www\.)?hentainexus\.com"
+               r"(?:/page/\d+)?/?(?:\?(q=[^/?#]+))?$")
+    test = (
+        ("https://hentainexus.com/?q=tag:%22heart+pupils%22%20tag:group", {
+            "pattern": HentainexusGalleryExtractor.pattern,
+            "count": ">= 50",
+        }),
+        ("https://hentainexus.com/page/3?q=tag:%22heart+pupils%22"),
+    )
+
+    def __init__(self, match):
+        Extractor.__init__(self, match)
+        self.params = text.parse_query(match.group(1))
+
+    def items(self):
+        yield Message.Version, 1
+        params = self.params
+        path = "/"
+
+        while path:
+            page = self.request(self.root + path, params=params).text
+            extr = text.extract_from(page)
+            data = {"_extractor": HentainexusGalleryExtractor}
+
+            while True:
+                gallery_id = extr('<a href="/view/', '"')
+                if not gallery_id:
+                    break
+                yield Message.Queue, self.root + "/view/" + gallery_id, data
+
+            path = extr('class="pagination-next" href="', '"')
---
+++ packages/gallery-dl/gallery_dl-1.13.3-py3-none-any.whl/gallery_dl/extractor/mastodon.py
@@ -0,0 +1,204 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2019-2020 Mike Fährmann
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+
+"""Extractors for mastodon instances"""
+
+from .common import Extractor, Message
+from .. import text, config, exception
+import re
+
+
+class MastodonExtractor(Extractor):
+    """Base class for mastodon extractors"""
+    basecategory = "mastodon"
+    directory_fmt = ("mastodon", "{instance}", "{account[username]}")
+    filename_fmt = "{category}_{id}_{media[id]}.{extension}"
+    archive_fmt = "{media[id]}"
+    cookiedomain = None
+    instance = None
+    root = None
+
+    def __init__(self, match):
+        Extractor.__init__(self, match)
+        self.api = MastodonAPI(self)
+
+    def config(self, key, default=None, *, sentinel=object()):
+        value = Extractor.config(self, key, sentinel)
+        if value is not sentinel:
+            return value
+        return config.interpolate(
+            ("extractor", "mastodon", self.instance, self.subcategory),
+            key, default,
+        )
+
+    def items(self):
+        yield Message.Version, 1
+        for status in self.statuses():
+            attachments = self.prepare(status)
+            yield Message.Directory, status
+            for media in attachments:
+                status["media"] = media
+                url = media["url"]
+                yield Message.Url, url, text.nameext_from_url(url, status)
+
+    def statuses(self):
+        """Return an iterable containing all relevant Status-objects"""
+        return ()
+
+    def prepare(self, status):
+        """Prepare a status object"""
+        status["instance"] = self.instance
+        status["tags"] = [tag["name"] for tag in status["tags"]]
+        attachments = status["media_attachments"]
+        del status["media_attachments"]
+        return attachments
+
+
+class MastodonUserExtractor(MastodonExtractor):
+    """Extractor for all images of an account/user"""
+    subcategory = "user"
+
+    def __init__(self, match):
+        MastodonExtractor.__init__(self, match)
+        self.account_name = match.group(1)
+
+    def statuses(self):
+        results = self.api.account_search("@" + self.account_name, 1)
+        for account in results:
+            if account["username"] == self.account_name:
+                break
+        else:
+            raise exception.NotFoundError("account")
+        return self.api.account_statuses(account["id"])
+
+
+class MastodonStatusExtractor(MastodonExtractor):
+    """Extractor for images from a status"""
+    subcategory = "status"
+
+    def __init__(self, match):
+        MastodonExtractor.__init__(self, match)
+        self.status_id = match.group(1)
+
+    def statuses(self):
+        return (self.api.status(self.status_id),)
+
+
+class MastodonAPI():
+    """Minimal interface for the Mastodon API
+
+    https://github.com/tootsuite/mastodon
+    https://github.com/tootsuite/documentation/blob/master/Using-the-API/API.md
+    """
+
+    def __init__(self, extractor, access_token=None):
+        self.root = extractor.root
+        self.extractor = extractor
+
+        if not access_token:
+            access_token = extractor.config(
+                "access-token", extractor.access_token)
+        self.headers = {"Authorization": "Bearer {}".format(access_token)}
+
+    def account_search(self, query, limit=40):
+        """Search for content"""
+        params = {"q": query, "limit": limit}
+        return self._call("accounts/search", params)
+
+    def account_statuses(self, account_id):
+        """Get an account's statuses"""
+        endpoint = "accounts/{}/statuses".format(account_id)
+        params = {"only_media": "1"}
+        return self._pagination(endpoint, params)
+
+    def status(self, status_id):
+        """Fetch a Status"""
+        return self._call("statuses/" + status_id)
+
+    def _call(self, endpoint, params=None):
+        url = "{}/api/v1/{}".format(self.root, endpoint)
+        response = self.extractor.request(
+            url, params=params, headers=self.headers)
+        return self._parse(response)
+
+    def _pagination(self, endpoint, params):
+        url = "{}/api/v1/{}".format(self.root, endpoint)
+        while url:
+            response = self.extractor.request(
+                url, params=params, headers=self.headers)
+            yield from self._parse(response)
+            url = response.links.get("next", {}).get("url")
+
+    @staticmethod
+    def _parse(response):
+        """Parse an API response"""
+        if response.status_code == 404:
+            raise exception.NotFoundError()
+        return response.json()
+
+
+def generate_extractors():
+    """Dynamically generate Extractor classes for Mastodon instances"""
+
+    symtable = globals()
+    extractors = config.get(("extractor",), "mastodon")
+    if extractors:
+        EXTRACTORS.update(extractors)
+    config.set(("extractor",), "mastodon", EXTRACTORS)
+
+    for instance, info in EXTRACTORS.items():
+
+        if not isinstance(info, dict):
+            continue
+
+        category = info.get("category") or instance.replace(".", "")
+        root = info.get("root") or "https://" + instance
+        name = (info.get("name") or category).capitalize()
+        token = info.get("access-token")
+        pattern = info.get("pattern") or re.escape(instance)
+
+        class Extr(MastodonUserExtractor):
+            pass
+
+        Extr.__name__ = Extr.__qualname__ = name + "UserExtractor"
+        Extr.__doc__ = "Extractor for all images of a user on " + instance
+        Extr.category = category
+        Extr.instance = instance
+        Extr.pattern = (r"(?:https?://)?" + pattern +
+                        r"/@([^/?&#]+)(?:/media)?/?$")
+        Extr.root = root
+        Extr.access_token = token
+        symtable[Extr.__name__] = Extr
+
+        class Extr(MastodonStatusExtractor):
+            pass
+
+        Extr.__name__ = Extr.__qualname__ = name + "StatusExtractor"
+        Extr.__doc__ = "Extractor for images from a status on " + instance
+        Extr.category = category
+        Extr.instance = instance
+        Extr.pattern = r"(?:https?://)?" + pattern + r"/@[^/?&#]+/(\d+)"
+        Extr.root = root
+        Extr.access_token = token
+        symtable[Extr.__name__] = Extr
+
+
+EXTRACTORS = {
+    "pawoo.net": {
+        "category"     : "pawoo",
+        "access-token" : "286462927198d0cf3e24683e91c8259a"
+                         "ac4367233064e0570ca18df2ac65b226",
+        "client-id"    : "97b142b6904abf97a1068d51a7bc2f2f"
+                         "cf9323cef81f13cb505415716dba7dac",
+        "client-secret": "e45bef4bad45b38abf7d9ef88a646b73"
+                         "75e7fb2532c31a026327a93549236481",
+    },
+}
+
+
+generate_extractors()
