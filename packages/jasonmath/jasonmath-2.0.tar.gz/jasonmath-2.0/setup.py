ecc1ca1f3b06968c466b22490b40506275118009
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:56 +0000

    Add package jasonmath/2.0/jasonmath-2.0.tar.gz

---
+++ packages/jasonmath/jasonmath-2.0.tar.gz/jasonmath-2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+name='jasonmath', # 对外我们模块的名字
+version='2.0', # 版本号
+description='这是第一个对外发布的模块，测试哦', #描述
+author='jason', # 作者
+author_email='jason@gmail.com',
+py_modules=['jasonmath.demo1'] # 要发布的模块
+)
