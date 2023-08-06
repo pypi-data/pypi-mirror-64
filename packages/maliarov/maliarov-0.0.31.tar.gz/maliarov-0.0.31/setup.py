6961b6e8b347155b2137e3a462b3f14cbfc1ca4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:57 +0000

    Add package maliarov/0.0.31/maliarov-0.0.31.tar.gz

---
+++ packages/maliarov/maliarov-0.0.31.tar.gz/maliarov-0.0.31/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="maliarov",
+    version="0.0.31",
+    description="Routine automation",
+    py_modules=['maliarov'],
+    package_dir={'':'src'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires = [
+        "selenium ~= 3.1",
+        "datetime ~= 4.3",
+    ],
+    author="Anatolii Maliarov",
+    author_email="tly.mov@gmail.com",
+    url="https://github.com/tlmy/webscraping",
+)
