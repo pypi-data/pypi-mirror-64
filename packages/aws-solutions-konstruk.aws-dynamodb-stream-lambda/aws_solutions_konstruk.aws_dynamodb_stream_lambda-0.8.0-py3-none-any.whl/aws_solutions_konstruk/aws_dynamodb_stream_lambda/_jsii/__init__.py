4c1863cc10df0967bccf2f49f6a5232774dbbe49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:54 +0000

    Add package aws-solutions-konstruk.aws-dynamodb-stream-lambda/0.8.0/aws_solutions_konstruk.aws_dynamodb_stream_lambda-0.8.0-py3-none-any.whl

---
+++ packages/aws-solutions-konstruk.aws-dynamodb-stream-lambda/aws_solutions_konstruk.aws_dynamodb_stream_lambda-0.8.0-py3-none-any.whl/aws_solutions_konstruk/aws_dynamodb_stream_lambda/_jsii/__init__.py
@@ -0,0 +1,18 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.aws_dynamodb
+import aws_cdk.aws_lambda
+import aws_cdk.aws_lambda_event_sources
+import aws_cdk.core
+import aws_solutions_konstruk.core
+__all__ = []
+
+publication.publish()
