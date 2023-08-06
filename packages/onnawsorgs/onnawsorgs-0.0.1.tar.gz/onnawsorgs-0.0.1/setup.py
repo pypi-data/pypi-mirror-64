9ba1d0d478f9a7dac8036c1e69505599f9f4b8c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:00 +0000

    Add package onnawsorgs/0.0.1/onnawsorgs-0.0.1.tar.gz

---
+++ packages/onnawsorgs/onnawsorgs-0.0.1.tar.gz/onnawsorgs-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='onnawsorgs',
+    version='0.0.1',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    url='https://oznetnerd.com',
+    install_requires=[
+        'onnlogger==0.0.5'
+    ],
+    license='',
+    author='Will Robinson',
+    author_email='will@oznetnerd.com',
+    description='Convenience Python module for AWS Organisations & STS'
+)
