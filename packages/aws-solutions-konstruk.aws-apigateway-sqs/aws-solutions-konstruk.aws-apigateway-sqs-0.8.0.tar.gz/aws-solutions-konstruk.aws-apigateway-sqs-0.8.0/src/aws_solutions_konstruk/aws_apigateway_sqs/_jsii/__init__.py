3c5f2a83a1bfe7b0238fb203bb09eda813a08fa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:55 +0000

    Add package aws-solutions-konstruk.aws-apigateway-sqs/0.8.0/aws-solutions-konstruk.aws-apigateway-sqs-0.8.0.tar.gz

---
+++ packages/aws-solutions-konstruk.aws-apigateway-sqs/aws-solutions-konstruk.aws-apigateway-sqs-0.8.0.tar.gz/aws-solutions-konstruk.aws-apigateway-sqs-0.8.0/src/aws_solutions_konstruk/aws_apigateway_sqs/_jsii/__init__.py
@@ -0,0 +1,19 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.aws_apigateway
+import aws_cdk.aws_iam
+import aws_cdk.aws_kms
+import aws_cdk.aws_sqs
+import aws_cdk.core
+import aws_solutions_konstruk.core
+__all__ = []
+
+publication.publish()
