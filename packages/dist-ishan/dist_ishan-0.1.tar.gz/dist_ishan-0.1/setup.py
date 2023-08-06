859a5d37e636d386e5f2844acca3edf8bcd79cad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:53 +0000

    Add package dist-ishan/0.1/dist_ishan-0.1.tar.gz

---
+++ packages/dist-ishan/dist_ishan-0.1.tar.gz/dist_ishan-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dist_ishan',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dist_ishan'],
+      author = 'Ishan Dubey',
+      author_email = 'ishandubey@ishandubey.com',
+      zip_safe=False)
