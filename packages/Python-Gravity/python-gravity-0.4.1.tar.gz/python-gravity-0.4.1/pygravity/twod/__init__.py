14d61987b20ad4913a9ccadb29ac4d4b2cbd4052
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:20 +0000

    Add package Python-Gravity/0.4.1/python-gravity-0.4.1.tar.gz

---
+++ packages/Python-Gravity/python-gravity-0.4.1.tar.gz/python-gravity-0.4.1/pygravity/twod/__init__.py
@@ -0,0 +1,5 @@
+from .vector import Vector2
+from .gravity import GravityContainer, GravityCaster, GravityAcceptor
+
+__all__ = ['Vector2',
+           'GravityContainer', 'GravityCaster', 'GravityAcceptor']
