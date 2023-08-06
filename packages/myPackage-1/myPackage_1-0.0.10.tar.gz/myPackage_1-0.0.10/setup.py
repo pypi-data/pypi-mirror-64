b3429b02c63bfbce49d6d59966d82a2c9c496da7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:29 +0000

    Add package myPackage-1/0.0.10/myPackage_1-0.0.10.tar.gz

---
+++ packages/myPackage-1/myPackage_1-0.0.10.tar.gz/myPackage_1-0.0.10/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='myPackage_1',
+    version='0.0.10',
+    description='my package 01',
+    py_modules=["mypackage"],
+    package_dir={'':'src'},
+)
