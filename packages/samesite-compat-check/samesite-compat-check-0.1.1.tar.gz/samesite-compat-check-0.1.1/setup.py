c71a910f876eef15bca1bdb08c0e441af2f6e184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:12 +0000

    Add package samesite-compat-check/0.1.1/samesite-compat-check-0.1.1.tar.gz

---
+++ packages/samesite-compat-check/samesite-compat-check-0.1.1.tar.gz/samesite-compat-check-0.1.1/setup.py
@@ -0,0 +1,46 @@
+import io
+import os
+import re
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read(filename):
+    filename = os.path.join(os.path.dirname(__file__), filename)
+    text_type = type(u"")
+    with io.open(filename, mode="r", encoding='utf-8') as fd:
+        return re.sub(text_type(r':[a-z]+:`~?(.*?)`'), text_type(r'``\1``'), fd.read())
+
+
+# fmt: off
+setup(
+    name="samesite-compat-check",
+    version="0.1.1",
+    url="https://github.com/peergradeio/samesite-compat-check",
+    license='Apache License 2.0',
+
+    author="Malthe Jørgensen",
+    author_email="malthe.jorgensen@gmail.com",
+
+    description="A port of Chrome's browser compatibility check for SameSite=None cookies",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+
+    packages=find_packages(exclude=('tests',)),
+
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
