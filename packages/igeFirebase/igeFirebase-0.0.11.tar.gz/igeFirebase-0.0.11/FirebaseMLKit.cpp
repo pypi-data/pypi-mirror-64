44686f647d38bf4b954c166e9df3f86cd3dab158
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:57 +0000

    Add package igeFirebase/0.0.11/igeFirebase-0.0.11.tar.gz

---
+++ packages/igeFirebase/igeFirebase-0.0.11.tar.gz/igeFirebase-0.0.11/FirebaseMLKit.cpp
@@ -0,0 +1,50 @@
+#include "FirebaseMLKit.h"
+#include "FirebaseMLKitImpl.h"
+
+FirebaseMLKit* FirebaseMLKit::instance = nullptr;
+using namespace firebase;
+
+FirebaseMLKit::FirebaseMLKit()
+	: m_firebaseMLKitImpl(new FirebaseMLKitImpl())
+{
+	LOG("FirebaseMLKit()");
+}
+FirebaseMLKit::~FirebaseMLKit()
+{
+	LOG("~FirebaseMLKit()");
+}
+
+bool FirebaseMLKit::isSupported()
+{
+	return m_firebaseMLKitImpl->IsSupported();
+}
+
+void FirebaseMLKit::init(int mode)
+{
+	m_firebaseMLKitImpl->Init(mode);
+}
+
+void FirebaseMLKit::release()
+{
+	m_firebaseMLKitImpl->Release();
+}
+
+void FirebaseMLKit::preview()
+{
+	m_firebaseMLKitImpl->Preview();
+}
+
+float* FirebaseMLKit::getContours(int& size)
+{
+	return m_firebaseMLKitImpl->GetContours(size);
+}
+
+float FirebaseMLKit::getHeadEulerAngleY()
+{
+	return m_firebaseMLKitImpl->GetHeadEulerAngleY();
+}
+
+float FirebaseMLKit::getHeadEulerAngleZ()
+{
+	return m_firebaseMLKitImpl->GetHeadEulerAngleZ();
+}
