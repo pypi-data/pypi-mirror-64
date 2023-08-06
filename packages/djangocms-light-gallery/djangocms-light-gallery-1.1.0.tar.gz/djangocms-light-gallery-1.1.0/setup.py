7c88896e267c8a6832f87aeeeae90ca648c175c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:24 +0000

    Add package djangocms-light-gallery/1.1.0/djangocms-light-gallery-1.1.0.tar.gz

---
+++ packages/djangocms-light-gallery/djangocms-light-gallery-1.1.0.tar.gz/djangocms-light-gallery-1.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(name='djangocms-light-gallery',
+      version='1.1.0',
+      description='Light Gallery plugin for django CMS',
+      url='https://github.com/andyklimczak/djangocms-light-gallery',
+      author='Andy Klimczak',
+      author_email='andyklimczak@fastmail.com',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+      long_description=open('README.rst').read(),
+      install_requires=[
+          'django>=1.8',
+          'django-cms>=3.0',
+          'django-filer',
+          'django-sekizai',
+          'easy-thumbnails'
+      ],
+      zip_safe=False)
