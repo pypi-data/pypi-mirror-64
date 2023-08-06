128fdc5850df09510353e77047edeb4d0922bac7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:22 +0000

    Add package django-geoposition-2/0.3.11/django-geoposition-2-0.3.11.tar.gz

---
+++ packages/django-geoposition-2/django-geoposition-2-0.3.11.tar.gz/django-geoposition-2-0.3.11/setup.py
@@ -0,0 +1,37 @@
+# coding: utf-8
+from setuptools import setup, find_packages
+
+setup(
+    name='django-geoposition-2',
+    version=__import__('geoposition').__version__,
+    description=
+    'Django model field that can hold a geoposition, and corresponding admin widget.',
+    author='Dario Castañé',
+    author_email='i@dario.im',
+    url='http://github.com/imdario/django-geoposition',
+    packages=find_packages(),
+    zip_safe=False,
+    package_data={
+        'geoposition': [
+            'locale/*/LC_MESSAGES/*',
+            'templates/geoposition/widgets/*.html',
+            'static/geoposition/*',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Framework :: Django',
+    ])
