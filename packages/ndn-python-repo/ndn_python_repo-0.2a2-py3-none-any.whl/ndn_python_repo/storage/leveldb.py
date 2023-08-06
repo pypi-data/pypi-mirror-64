562bb43e37d8299c2169cf7009ded8cb309b8c20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:09 +0000

    Add package ndn-python-repo/0.2a2/ndn_python_repo-0.2a2-py3-none-any.whl

---
+++ packages/ndn-python-repo/ndn_python_repo-0.2a2-py3-none-any.whl/ndn_python_repo/storage/leveldb.py
@@ -0,0 +1,64 @@
+import os
+import pickle
+import plyvel
+from .storage_base import Storage
+import time
+
+
+class LevelDBStorage(Storage):
+    def __init__(self, dir: str):
+        db_dir = os.path.expanduser(dir)
+        if not os.path.exists(db_dir):
+            try:
+                os.makedirs(db_dir)
+            except PermissionError:
+                raise PermissionError(f'Could not create database directory: {db_path}') from None
+        self.db = plyvel.DB(db_dir, create_if_missing=True)
+    
+    def __del__(self):
+        self.db.close()
+
+    def _put(self, key: bytes, value: bytes, expire_time_ms: int=None):
+        """
+        Insert value and its expiration time into levelDB, overwrite if already exists.
+        :param key: bytes.
+        :param value: bytes.
+        :param expire_time_ms: Optional[int]. Value is not fresh if expire_time_ms is not specified.
+        """
+        self.db.put(key, pickle.dumps((value, expire_time_ms)))
+
+    def _get(self, key: bytes, can_be_prefix=False, must_be_fresh=False) -> bytes:
+        """
+        Get value from levelDB.
+        :param key: bytes.
+        :param can_be_prefix: bool.
+        :param must_be_fresh: bool.
+        :return: bytes.
+        """
+        if not can_be_prefix:
+            record = self.db.get(key)
+            if record == None:
+                return None
+            value, expire_time_ms = pickle.loads(record)
+            if not must_be_fresh or expire_time_ms != None and expire_time_ms > int(time.time() * 1000):
+                return value
+            else:
+                return None
+        else:
+            for _, v_e in self.db.iterator(prefix=key):
+                value, expire_time_ms = pickle.loads(v_e)
+                if not must_be_fresh or expire_time_ms != None and expire_time_ms > int(time.time() * 1000):
+                    return value
+            return None
+
+    def _remove(self, key: bytes) -> bool:
+        """
+        Remove value from levelDB. Return whether removal is successful.
+        :param key: bytes.
+        :return: bool.
+        """
+        if self._get(key) != None:
+            self.db.delete(key)
+            return True
+        else:
+            return False
---
+++ packages/ndn-python-repo/ndn_python_repo-0.2a2-py3-none-any.whl/ndn_python_repo/storage/storage_base.py
@@ -0,0 +1,43 @@
+from ndn.encoding.tlv_var import parse_tl_num
+from ndn.encoding import Name, parse_data, NonStrictName
+import time
+
+
+class Storage:
+    """
+    Interface for storage functionalities
+    """
+    def _put(self, key: bytes, data: bytes, expire_time_ms: int=None):
+        raise NotImplementedError
+
+    def _get(self, key: bytes, can_be_prefix=False, must_be_fresh=False) -> bytes:
+        raise NotImplementedError
+
+    def _remove(self, key: bytes) -> bool:
+        raise NotImplementedError
+
+    @staticmethod
+    def _get_name_bytes_wo_tl(name: NonStrictName) -> bytes:
+        # remove name's TL as key to support efficient prefix search
+        name = Name.to_bytes(name)
+        offset = 0
+        offset += parse_tl_num(name, offset)[1]
+        offset += parse_tl_num(name, offset)[1]
+        return name[offset:]
+
+
+    ###### wrappers around key-value store
+
+    def put_data_packet(self, name: NonStrictName, data: bytes):
+        # compute expire_time_ms by adding freshnessPeriod to current time
+        _, meta_info, _, _ = parse_data(data)
+        expire_time_ms = int(time.time() * 1000)
+        if meta_info.freshness_period:
+            expire_time_ms += meta_info.freshness_period
+        self._put(self._get_name_bytes_wo_tl(name), data, expire_time_ms)
+
+    def get_data_packet(self, name: NonStrictName, can_be_prefix=False, must_be_fresh=False):
+        return self._get(self._get_name_bytes_wo_tl(name), can_be_prefix, must_be_fresh)
+
+    def remove_data_packet(self, name: NonStrictName):
+        return self._remove(self._get_name_bytes_wo_tl(name))
