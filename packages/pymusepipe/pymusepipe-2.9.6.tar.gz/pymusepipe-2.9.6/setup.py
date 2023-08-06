dc0e9e956550e1f5fc6c987b924fe5d036d2371e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:20 +0000

    Add package pymusepipe/2.9.6/pymusepipe-2.9.6.tar.gz

---
+++ packages/pymusepipe/pymusepipe-2.9.6.tar.gz/pymusepipe-2.9.6/setup.py
@@ -0,0 +1,36 @@
+# Licensed under a MIT style license - see LICENSE.txt
+
+from __future__ import absolute_import, division, print_function
+
+from setuptools import setup, find_packages
+
+version = {}
+with open("pymusepipe/version.py") as fp:
+    exec(fp.read(), version)
+
+with open('README.md', 'r') as f:
+    readme = f.read()
+
+with open('LICENSE.txt') as f:
+    license = f.read()
+
+setup(name='pymusepipe',
+      version = version['__version__'],
+      description='python module to reduce MUSE Raw data and combine them',
+      long_description=readme,
+      long_description_content_type="text/markdown",
+      keywords='MUSE, PHANGS',
+      url="https://github.com/emsellem/pipemusepipe",
+      author='Eric Emsellem',
+      author_email='eric.emsellem@eso.org',
+      license="MIT License",
+      packages=find_packages(exclude=('tests', 'docs')),
+      install_requires=['mpdaf', 'astropy', 'numpy'],
+      include_package_data=True,
+      zip_safe=False,
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+       ],
+      )
