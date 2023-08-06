b93ad9b38077648b11db31578c07a8e79febba7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:51 +0000

    Add package plib.dbtools/1.0.5/plib.dbtools-1.0.5.tar.gz

---
+++ packages/plib.dbtools/plib.dbtools-1.0.5.tar.gz/plib.dbtools-1.0.5/plib/test/dbtools/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""
+Sub-Package TEST.DBTOOLS of Package PLIB
+Copyright (C) 2008-2020 by Peter A. Donis
+
+Released under the GNU General Public License, Version 2
+See the LICENSE and README files for more information
+
+This sub-package contains the PLIB3.DBTOOLS test suite.
+"""
