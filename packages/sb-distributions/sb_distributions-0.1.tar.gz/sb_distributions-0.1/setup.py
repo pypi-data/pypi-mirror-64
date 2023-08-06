3931936ebce1fb4a6468325a366950bf0ad35ae1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:41 +0000

    Add package sb-distributions/0.1/sb_distributions-0.1.tar.gz

---
+++ packages/sb-distributions/sb_distributions-0.1.tar.gz/sb_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='sb_distributions',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['sb_distributions'],
+      author = 'Sergio Beltran',
+      author_email = 'engsergiobeltran@gmail.com',
+      zip_safe=False)
