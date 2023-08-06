a29371618fa71ab59ad4eca4b3ef45135ae94443
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:27 +0000

    Add package causal-tree-learn/2.1/causal_tree_learn-2.1-py3-none-any.whl

---
+++ packages/causal-tree-learn/causal_tree_learn-2.1-py3-none-any.whl/CTL/causal_tree.py
@@ -0,0 +1,17 @@
+from CTL.ct_util import *
+from abc import ABC, abstractmethod
+
+
+class CausalTree(ABC):
+
+    @abstractmethod
+    def fit(self, x, y, t):
+        pass
+
+    @abstractmethod
+    def fit_r(self, train_x, train_y, train_t, val_x, val_y, val_t):
+        pass
+
+    @abstractmethod
+    def predict(self):
+        pass
---
+++ packages/causal-tree-learn/causal_tree_learn-2.1-py3-none-any.whl/CTL/ctl_base.py
@@ -0,0 +1,7 @@
+from CTL.causal_tree.ctl.binary_ctl import *
+
+
+class CausalTreeBase(CausalTreeLearn):
+
+    def __init__(self):
+        pass
