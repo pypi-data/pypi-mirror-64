4735a354e6cb6878389c8473c8923e0d80c7de09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:37 +0000

    Add package eclyssia/1.2.0/eclyssia-1.2.0.tar.gz

---
+++ packages/eclyssia/eclyssia-1.2.0.tar.gz/eclyssia-1.2.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='eclyssia',
+    packages=['eclyssia'],
+    version='1.2.0',
+    description='An eclyssia-api wrapper built for python3+',
+    author='Zenrac',
+    author_email='zenrac@outlook.fr',
+    url='https://github.com/Zenrac/eclyssia.py',
+    download_url='https://github.com/Zenrac/eclyssia.py/archive/1.2.0.tar.gz',
+    keywords=['eclyssia'],
+    include_package_data=True,
+    install_requires=['aiohttp']
+)
