6009c72603c7a1c55d151f88e69be835cfd80ead
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:06 +0000

    Add package dosk/0.0.5/dosk-0.0.5-py3-none-any.whl

---
+++ packages/dosk/dosk-0.0.5-py3-none-any.whl/dosk-0.0.5.data/scripts/dosk_cli.py
@@ -0,0 +1,25 @@
+#!python
+
+import sys, os
+from dosk.yaml_reader import yaml_reader
+import argparse 
+  
+def main(): 
+  
+    parser = argparse.ArgumentParser(prog ='dock', 
+                                     description ='Do Task - The Simple DevOps Task Runner') 
+  
+    parser.add_argument('task', metavar ='T', nargs ='+', 
+                        help ='an integer for the accumulator') 
+
+    parser.add_argument('-local', action ='store_const', const = True, 
+                        default = False, dest ='local', 
+                        help ="Load local variables") 
+  
+    args = parser.parse_args()
+
+    yaml_reader.test()
+  
+    
+if __name__ == "__main__":
+    main()
