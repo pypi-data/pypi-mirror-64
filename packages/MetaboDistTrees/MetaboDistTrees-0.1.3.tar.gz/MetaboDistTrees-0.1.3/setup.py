59503e0393e28d5b6c62456335eedbcb5814ae5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:47 +0000

    Add package MetaboDistTrees/0.1.3/MetaboDistTrees-0.1.3.tar.gz

---
+++ packages/MetaboDistTrees/MetaboDistTrees-0.1.3.tar.gz/MetaboDistTrees-0.1.3/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="MetaboDistTrees",
+    version="0.1.3",
+    author="Madeleine Ernst",
+    author_email="mernst@ucsd.edu",
+    description="A python implementation for building chemically informed distance trees",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/madeleineernst/MetaboDistTrees",
+    packages=setuptools.find_packages(),
+    install_requires = [
+    	'scipy',
+    	'numpy',
+    	'pandas',
+    	'obonet',
+    	'networkx'
+    ],
+    package_data={
+        # If any package contains *.txt files, include them:
+        '': ['*.txt'],
+        # And include any *.dat files found in the 'data' subdirectory
+        # of the 'mypkg' package, also:
+        'MetaboDistTrees': ['data/*.obo','test/*.py']
+    },
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+)
