2df823f99011c318184357b21bb3505eab1599ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:10 +0000

    Add package pycov/1.5/pycov-1.5.tar.gz

---
+++ packages/pycov/pycov-1.5.tar.gz/pycov-1.5/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(name='pycov',
+      version='1.5',
+      description='A library getting how many people are now ill with coronavirus',
+      packages=['pycov'],
+      author_email='kewldanil1@gmail.com',
+	author="kewldan",
+	url="https://pypi.org/project/pycov/",
+      zip_safe=False,
+	python_requires='>=3',
+	python_version=">=3",
+	classifiers=[
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+		  'Programming Language :: Python :: 3.5',
+		  'Programming Language :: Python :: 3.6',
+		  'Programming Language :: Python :: 3.7',
+		  'Programming Language :: Python :: 3.8'
+      ],
+	install_requires=[
+	  "requsts",
+	  "lxml",
+	  "bs4",
+	"python_version<'3.4'"
+	  ]
+)
