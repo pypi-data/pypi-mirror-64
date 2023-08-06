25668641d51320d162d94ee89d7dc9f9f51ecf52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:29 +0000

    Add package DiscordDB/0.0.5/DiscordDB-0.0.5.tar.gz

---
+++ packages/DiscordDB/DiscordDB-0.0.5.tar.gz/DiscordDB-0.0.5/discordDB/models/types.py
@@ -0,0 +1,21 @@
+class Data(dict):
+    """Actually a superset class of python dictionaries, which also supports accessing of its keys using . syntax.
+
+    Extra Attributes
+    ----------------
+
+    created_at : datetime.datetime
+        The time this data was created in UTC.
+
+    """
+
+    def __getattribute__(self, item):
+        try:
+            data = self[str(item)]
+        except KeyError:
+            raise AttributeError
+        else:
+            try:
+                return int(data)
+            except (ValueError, TypeError):
+                return data
