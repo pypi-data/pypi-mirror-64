3309d9bfd62824eb1266062cab5fd361de2237e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:58 +0000

    Add package CEP-por-endereco-completo/0.1.2/CEP-por-endereco-completo-0.1.2.tar.gz

---
+++ packages/CEP-por-endereco-completo/CEP-por-endereco-completo-0.1.2.tar.gz/CEP-por-endereco-completo-0.1.2/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='CEP-por-endereco-completo',
+    version='0.1.2',
+    url='https://github.com/rodrigobercini/CEP-por-endereco-completo',
+    license='MIT License',
+    author='Rodrigo Bercini Martins',
+    author_email='rodrigobercinimartins@gmail.com',
+    keywords='cep endereço completo zip code brazil address correios',
+    description=u'Encontrando CEP a partir de endereços completos',
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # This is important!
+    packages=['CEP-por-endereco-completo'],
+    install_requires=["beautifulsoup4"],
+)
