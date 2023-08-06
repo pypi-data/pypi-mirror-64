70454f85c7fb9be2a6cef565f430614f4db8d232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:24 +0000

    Add package vyper-config/0.6.2/vyper-config-0.6.2.tar.gz

---
+++ packages/vyper-config/vyper-config-0.6.2.tar.gz/vyper-config-0.6.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(
+    name="vyper-config",
+    version="0.6.2",
+    description="Python configuration with (more) fangs",
+    url="http://github.com/admiralobvious/vyper",
+    author="Alexandre Ferland",
+    author_email="aferlandqc@gmail.com",
+    license="MIT",
+    packages=["vyper"],
+    zip_safe=False,
+    install_requires=[
+        "distconfig>=0.1.0",
+        'pathlib>=1.0.1;python_version<"3.4"',
+        "toml>=0.10.0",
+        "PyYAML>=5.1",
+        "watchdog>=0.9.0",
+    ],
+    setup_requires=["pytest-runner>=5.1"],
+    tests_require=["pytest>=5.0.1"],
+    platforms="any",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
