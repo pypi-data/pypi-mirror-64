51b35ba470f486b24255ad3745195863dcfd3439
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:35 +0000

    Add package pyre-gevent/0.2.4.dev0/pyre_gevent-0.2.4.dev0-py2.py3-none-any.whl

---
+++ packages/pyre-gevent/pyre_gevent-0.2.4.dev0-py2.py3-none-any.whl/pyre_gevent/__init__.py
@@ -0,0 +1,8 @@
+import re
+
+__all__ = ['pyre', 'zbeacon', 'zhelper']
+__version__ = '0.2.4.dev0'
+__version_info__ = tuple(int(v) for v in re.sub('[a-zA-Z]', '', __version__).split('.'))
+
+from .pyre import Pyre
+from .pyre_event import PyreEvent
