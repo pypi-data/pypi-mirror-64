96f42d622d7aeee5784a5de8b919eeb36894f368
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:26 +0000

    Add package sqlfmt/0.0.0/sqlfmt-0.0.0.tar.gz

---
+++ packages/sqlfmt/sqlfmt-0.0.0.tar.gz/sqlfmt-0.0.0/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+setup(
+    name="sqlfmt",
+    version="0.0.0",
+    author="HH-MWB",
+    author_email="h.hong@mail.com",
+    description="The uncompromising SQL query formatter.",
+    url="https://github.com/HH-MWB/sqlfmt",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Quality Assurance",
+    ],
+    python_requires=">=3.6",
+)
