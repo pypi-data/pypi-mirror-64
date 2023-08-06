37f9b5e9d03c9707e486a42e489fa69cd89319fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:42 +0000

    Add package my-statistical-distributions/1.0/my_statistical_distributions-1.0.tar.gz

---
+++ packages/my-statistical-distributions/my_statistical_distributions-1.0.tar.gz/my_statistical_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='my_statistical_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['my_statistical_distributions'],
+      author='Timo Hartmann',
+      author_email='timo.hartmann@posteo.de',
+      zip_safe=False)
