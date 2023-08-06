7e978d40a2b35c0afa0963b5b4e9da5a35c84252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:45 +0000

    Add package pykcode/0.0.2/pykcode-0.0.2-py3-none-any.whl

---
+++ packages/pykcode/pykcode-0.0.2-py3-none-any.whl/kcode/__init__.py
@@ -0,0 +1,13 @@
+import turtle
+
+turtle.tracer(0)
+
+
+def square(t, size, flag=True):
+    for x in range(4):
+        t.fd(size)
+        if flag:
+            t.right(90)
+        else:
+            t.left(90)
+    turtle.update()
