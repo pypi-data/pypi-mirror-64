c8c78eacd5ebfec72d85141a079ef5466552030d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:53 +0000

    Add package jsonize-db/0.1.2/jsonize_db-0.1.2.tar.gz

---
+++ packages/jsonize-db/jsonize_db-0.1.2.tar.gz/jsonize_db-0.1.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='jsonize_db',
+      version='0.1.2',
+      description='Loading and unloading DB to JSONs',
+      packages=['jsonize_db'],
+      author_email='c-mao@mail.ru',
+      zip_safe=False)
