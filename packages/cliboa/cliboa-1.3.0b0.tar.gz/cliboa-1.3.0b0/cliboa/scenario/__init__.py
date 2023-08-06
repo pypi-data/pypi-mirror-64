59de71a127af632f29d1de0ffb4236fe1da7809e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:57 +0000

    Add package cliboa/1.3.0b0/cliboa-1.3.0b0.tar.gz

---
+++ packages/cliboa/cliboa-1.3.0b0.tar.gz/cliboa-1.3.0b0/cliboa/scenario/__init__.py
@@ -0,0 +1,56 @@
+#
+# Copyright 2019 BrainPad Inc. All Rights Reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+from .base import Stdout, SqliteQueryExecute
+
+from .extract.aws import S3Download
+from .extract.file import CsvRead
+from .extract.ftp import FtpDownload, FtpDownloadFileDelete
+from .extract.sftp import (
+    SftpDownload,
+    SftpDelete,
+    SftpDownloadFileDelete,
+)
+from .extract.http import HttpDownload
+from .extract.gcp import (
+    BigQueryReadCache,
+    BigQueryFileDownload,
+    GcsDownload,
+    GcsDownloadFileDelete,
+    FirestoreDownloadDocument,
+)
+from .extract.sqlite import SqliteRead, SqliteReadRow
+
+from .transform.file import (
+    FileDecompress,
+    FileCompress,
+    CsvColsExtract,
+    ColumnLengthAdjust,
+    DateFormatConvert,
+    ExcelConvert,
+    CsvMerge,
+    CsvHeaderConvert,
+    FileDivide,
+    FileRename,
+)
+
+from .load.aws import S3Upload
+from .load.file import CsvWrite
+from .load.sftp import SftpFileLoad, SftpUpload
+from .load.sqlite import SqliteCreation, CsvReadSqliteCreate
+from .load.gcp import (
+    BigQueryCreate,
+    GcsFileUpload,
+    CsvReadBigQueryCreate,
+    FirestoreDocumentCreate,
+)
---
+++ packages/cliboa/cliboa-1.3.0b0.tar.gz/cliboa-1.3.0b0/cliboa/scenario/load/aws.py
@@ -0,0 +1,89 @@
+#
+# Copyright 2019 BrainPad Inc. All Rights Reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+import os
+
+from cliboa.scenario.aws import BaseS3
+from cliboa.util.constant import StepStatus
+from cliboa.scenario.validator import EssentialParameters
+
+
+class S3Upload(BaseS3):
+    """
+    Upload to S3
+    """
+
+    def __init__(self):
+        super().__init__()
+        self._key = None
+        self._src_dir = None
+        self._src_pattern = None
+        self._quit = False
+
+    @property
+    def key(self):
+        return self._key
+
+    @key.setter
+    def key(self, key):
+        self._key = key
+
+    @property
+    def src_dir(self):
+        return self._src_dir
+
+    @src_dir.setter
+    def src_dir(self, src_dir):
+        self._src_dir = src_dir
+
+    @property
+    def src_pattern(self):
+        return self._src_pattern
+
+    @src_pattern.setter
+    def src_pattern(self, src_pattern):
+        self._src_pattern = src_pattern
+
+    @property
+    def quit(self):
+        return self._quit
+
+    @quit.setter
+    def quit(self, quit):
+        self._quit = quit
+
+    def execute(self, *args):
+        super().execute()
+
+        valid = EssentialParameters(
+            self.__class__.__name__, [self._src_dir, self._src_pattern]
+        )
+        valid()
+
+        resource = self._s3_resource()
+        bucket = resource.Bucket(self._bucket)
+        files = super().get_target_files(self._src_dir, self._src_pattern)
+
+        if len(files) > 0:
+            for f in files:
+                bucket.upload_file(
+                    Key=os.path.join(self._key, os.path.basename(f)), Filename=f
+                )
+        else:
+            self._logger.info(
+                "Files to upload do not exist. File pattern: {}".format(
+                    os.path.join(self._src_dir, self._src_pattern)
+                )
+            )
+            if self._quit is True:
+                return StepStatus.SUCCESSFUL_TERMINATION
