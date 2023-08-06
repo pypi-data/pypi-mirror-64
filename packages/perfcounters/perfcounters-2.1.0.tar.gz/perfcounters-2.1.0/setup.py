81620138f26c2ab7a61e879ca77a2dc9c5956544
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:08 +0000

    Add package perfcounters/2.1.0/perfcounters-2.1.0.tar.gz

---
+++ packages/perfcounters/perfcounters-2.1.0.tar.gz/perfcounters-2.1.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+from setuptools import find_packages
+
+long_description = open("README.md").read()
+version = '2.1.0'
+setup(name='perfcounters',
+      version=version,
+      description='Easily add performance counters to your code',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author='Elie Bursztein',
+      author_email='code@elie.net',
+      url='https://github.com/ebursztein/perfcounters',
+      license='Apache 2',
+      install_requires=[
+          'tabulate',
+          'numpy'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Education',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: Software Development :: Debuggers',
+          'Topic :: Software Development :: Testing'
+      ],
+      packages=find_packages())
