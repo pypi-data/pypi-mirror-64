01a0e0abd3b27f468f745e81c39107fffa0160d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:14 +0000

    Add package distributions-wjn/0.1/distributions_wjn-0.1.tar.gz

---
+++ packages/distributions-wjn/distributions_wjn-0.1.tar.gz/distributions_wjn-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_wjn',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_wjn'],
+      zip_safe=False)
