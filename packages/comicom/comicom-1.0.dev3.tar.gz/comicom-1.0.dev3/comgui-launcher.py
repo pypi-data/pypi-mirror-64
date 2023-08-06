a0d281791c0af24ce8646a1d23c49104d36b8386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:56 +0000

    Add package comicom/1.0.dev3/comicom-1.0.dev3.tar.gz

---
+++ packages/comicom/comicom-1.0.dev3.tar.gz/comicom-1.0.dev3/comgui-launcher.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import tkinter
+
+from comicom import comgui
+
+window = tkinter.Tk()
+comgui = comgui.MainWindow(window)
+window.mainloop()
---
+++ packages/comicom/comicom-1.0.dev3.tar.gz/comicom-1.0.dev3/comicom.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import tkinter
+
+from comicom import compiler
+from comicom import comgui
+from comicom import arguments
+
+args = arguments.parse()
+
+# Trigger the actual program....
+if args.gui:
+    window = tkinter.Tk()
+    comgui = comgui.MainWindow(window)
+    window.mainloop()
+else:
+    compiler.run(args)
