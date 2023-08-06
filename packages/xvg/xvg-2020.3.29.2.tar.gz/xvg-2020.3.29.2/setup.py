5013549bd9d08afdd1569735f65d6ddef9f02d11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:06 +0000

    Add package xvg/2020.3.29.2/xvg-2020.3.29.2.tar.gz

---
+++ packages/xvg/xvg-2020.3.29.2.tar.gz/xvg-2020.3.29.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="xvg",
+    version="2020.3.29.2",
+    author="Hayden McDonald",
+    author_email="lrgstu@gmail.com",
+    description="A scriptable vector graphics library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lrgstu/xvgpy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
---
+++ packages/xvg/xvg-2020.3.29.2.tar.gz/xvg-2020.3.29.2/xvg/core/__init__.py
@@ -0,0 +1,5 @@
+from .context import Context
+from .engine import Engine
+from .error import Error, NotSupportedError, NotImplementedError
+from .interpreter import Interpreter
+from .renderer import Renderer
