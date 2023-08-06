4ac10e1827e9688f4607675c48c49ee605f7d5e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:41 +0000

    Add package brymck-securities/0.0.6.dev5/brymck-securities-0.0.6.dev5.tar.gz

---
+++ packages/brymck-securities/brymck-securities-0.0.6.dev5.tar.gz/brymck-securities-0.0.6.dev5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='brymck-securities',
+    version='0.0.6.dev5',
+    author='Bryan Lee McKelvey',
+    author_email='bryan.mckelvey@gmail.com',
+    url='https://github.com/brymck/proto',
+    description='',
+    long_description='',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'grpcio>=1.0,<2.0',
+        'grpcio-tools>=1.0,<2.0',
+        'protobuf>=3.0,<4.0',
+        'brymck-dates',
+    ],
+)
