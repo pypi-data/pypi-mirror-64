8942d55438bbc0875de36849ccbfde729006ddd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:53 +0000

    Add package TaiwanTrainVerificationCode2text/1.0.17/TaiwanTrainVerificationCode2text-1.0.17.tar.gz

---
+++ packages/TaiwanTrainVerificationCode2text/TaiwanTrainVerificationCode2text-1.0.17.tar.gz/TaiwanTrainVerificationCode2text-1.0.17/TaiwanTrainVerificationCode2text/demo.py
@@ -0,0 +1,39 @@
+
+
+import os
+import platform
+from TaiwanTrainVerificationCode2text import verification_code2text
+from TaiwanTrainVerificationCode2text import work_vcode 
+from TaiwanTrainVerificationCode2text import download 
+import TaiwanTrainVerificationCode2text
+PATH = TaiwanTrainVerificationCode2text.__path__[0]
+import cv2
+import matplotlib.pyplot as plt
+import random
+from datetime import datetime
+
+download.weight()
+download.ttf()
+
+
+work_vcode.work_vcode_fun(10,'test_data',5)
+work_vcode.work_vcode_fun(10,'test_data',6)
+
+if 'Windows' in platform.platform():
+    file_path = '{}/{}/'.format(PATH,'test_data')
+    train_image_path = [file_path + i for i in os.listdir(file_path+'/')]
+else:
+    file_path = '{}\\{}\\'.format(PATH,'test_data')
+    train_image_path = [file_path + i for i in os.listdir(file_path+'\\')]
+
+image_name = train_image_path[random.sample( range(len(train_image_path)) ,1)[0]]
+
+s = datetime.now()
+image = cv2.imread(image_name)
+plt.imshow(image)
+
+text = verification_code2text.main(image)
+print(text)
+
+t = datetime.now() - s
+print(t)
---
+++ packages/TaiwanTrainVerificationCode2text/TaiwanTrainVerificationCode2text-1.0.17.tar.gz/TaiwanTrainVerificationCode2text-1.0.17/TaiwanTrainVerificationCode2text/download.py
@@ -0,0 +1,43 @@
+
+
+import urllib
+import os,sys
+
+import platform
+if 'Windows' in platform.platform():
+    PATH = "\\".join( os.path.abspath(__file__).split('\\')[:-1])
+else:
+    PATH = "/".join( os.path.abspath(__file__).split('/')[:-1])
+sys.path.append(PATH)
+
+def weight():
+
+    url = 'https://raw.githubusercontent.com/linsamtw/TaiwanTrainVerificationCode2text/master/cnn_weight/verificatioin_code.h5'  
+    try:
+        if 'Windows' in platform.platform():
+            os.makedirs('{}\\cnn_weight'.format(PATH))
+        else:
+            os.makedirs('{}/cnn_weight'.format(PATH))
+    except:
+        pass
+    if 'Windows' in platform.platform():
+        urllib.request.urlretrieve(url, '{}\\{}\\verificatioin_code.h5'.format(PATH,'cnn_weight')) 
+    else:
+        urllib.request.urlretrieve(url, '/{}/{}/verificatioin_code.h5'.format(PATH,'cnn_weight')) 
+
+
+def ttf():
+    
+    url = 'https://raw.githubusercontent.com/linsamtw/TaiwanTrainVerificationCode2text/master/Courier-BoldRegular.ttf'  
+    if 'Windows' in platform.platform():
+        urllib.request.urlretrieve(url, '{}\\Courier-BoldRegular.ttf'.format(PATH)) 
+    else:
+        urllib.request.urlretrieve(url, '/{}/Courier-BoldRegular.ttf'.format(PATH)) 
+
+    url = 'https://raw.githubusercontent.com/linsamtw/TaiwanTrainVerificationCode2text/master/Times%20Bold.ttf'  
+    if 'Windows' in platform.platform():
+        urllib.request.urlretrieve(url, '{}\\Times Bold.ttf'.format(PATH)) 
+    else:    
+        urllib.request.urlretrieve(url, '/{}/Times Bold.ttf'.format(PATH)) 
+
+
