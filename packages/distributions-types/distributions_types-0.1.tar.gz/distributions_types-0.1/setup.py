da9e50cd061fcadb001d124efd2071b12b3e9043
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:40 +0000

    Add package distributions-types/0.1/distributions_types-0.1.tar.gz

---
+++ packages/distributions-types/distributions_types-0.1.tar.gz/distributions_types-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_types',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['distributions_types'],
+      author='Shubhangi Agrawal',
+      zip_safe=False)
