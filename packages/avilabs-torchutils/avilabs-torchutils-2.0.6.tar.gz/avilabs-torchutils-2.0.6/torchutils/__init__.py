c5992e25129ccdab271485ae32b0081354403ef8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:04 +0000

    Add package avilabs-torchutils/2.0.6/avilabs-torchutils-2.0.6.tar.gz

---
+++ packages/avilabs-torchutils/avilabs-torchutils-2.0.6.tar.gz/avilabs-torchutils-2.0.6/torchutils/__init__.py
@@ -0,0 +1,5 @@
+from .core.trainer import Trainer, TrainerArgs
+from .core.hyperparams import Hyperparams, HyperparamsSpec
+from .core.evaluator import evaluate
+from .core.auto_tuner import AutoTuner
+from .helper_funcs import train, autotune
