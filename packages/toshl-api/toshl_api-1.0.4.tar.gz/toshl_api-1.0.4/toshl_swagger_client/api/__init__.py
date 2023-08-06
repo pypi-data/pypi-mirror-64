86003b08ccddbcd27b10d22cd8a64791a8417ea2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:25 +0000

    Add package toshl-api/1.0.4/toshl_api-1.0.4.tar.gz

---
+++ packages/toshl-api/toshl_api-1.0.4.tar.gz/toshl_api-1.0.4/toshl_swagger_client/api/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from toshl_swagger_client.api.accounts_api import AccountsApi
+from toshl_swagger_client.api.bank_api import BankApi
+from toshl_swagger_client.api.categories_api import CategoriesApi
+from toshl_swagger_client.api.entries_api import EntriesApi
+from toshl_swagger_client.api.misc_api import MiscApi
