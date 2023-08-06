54cc126677acd9ef0bd49da67a0e92ceb08ce80d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:40 +0000

    Add package pyobo/0.1.0/pyobo-0.1.0.tar.gz

---
+++ packages/pyobo/pyobo-0.1.0.tar.gz/pyobo-0.1.0/src/pyobo/version.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""Version information for PyOBO."""
+
+__all__ = [
+    'VERSION',
+    'get_version',
+]
+
+VERSION = '0.1.0'
+
+
+def get_version() -> str:
+    """Get the software version of PyOBO."""
+    return VERSION
