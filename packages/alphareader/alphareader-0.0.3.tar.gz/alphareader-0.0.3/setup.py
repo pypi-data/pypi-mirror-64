5f202277410271adea6d8c7b18026a01c1633d45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:59 +0000

    Add package alphareader/0.0.3/alphareader-0.0.3.tar.gz

---
+++ packages/alphareader/alphareader-0.0.3.tar.gz/alphareader-0.0.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="alphareader", # Replace with your own username
+    version="0.0.3",
+    author="Herminio Vazquez",
+    author_email="canimus@gmail.com",
+    description="A reader for large files with custom delimiters and encodings",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/canimus/alphareader",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Topic :: Text Processing",
+        "Topic :: Utilities"
+    ],
+    python_requires='>=3.6',
+)
