927709bcb1ba2eac0bc005064dd7ed5fce068b3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:56 +0000

    Add package hzf-test/1.0.0/hzf_test-1.0.0.tar.gz

---
+++ packages/hzf-test/hzf_test-1.0.0.tar.gz/hzf_test-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name = 'hzf_test',
+	version = '1.0.0',
+	py_mondules = ['test_0'],
+	autor = 'hzf',
+	autor_mail = '15184455137@163.com',
+	url = 'http://www.hzfdechuangzi.club',
+	description = 'A test',
+	)
