52063cb43838ed2b3c3b0d5f9593a130948ed1e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:52 +0000

    Add package suanpan/0.12.8/suanpan-0.12.8.tar.gz

---
+++ packages/suanpan/suanpan-0.12.8.tar.gz/suanpan-0.12.8/suanpan/model/arguments.py
@@ -0,0 +1,46 @@
+# coding=utf-8
+from __future__ import absolute_import, print_function
+
+from suanpan.components import Result
+from suanpan.storage.arguments import Folder
+
+
+class CommonModel(Folder):
+    def __init__(self, key, type, version="latest", **kwargs):
+        super(CommonModel, self).__init__(key, **kwargs)
+        self.version = version
+        self.modelType = type
+        self.model = self.modelType()
+
+    def load(self, args):
+        super(CommonModel, self).load(args)
+        if self.folderPath:
+            self.value = self.model
+        return self
+
+    def transform(self, value):
+        if not self.folderPath:
+            return None
+        return self.model.load(self.folderPath)
+
+    def clean(self):
+        super(CommonModel, self).clean()
+        if self.folderPath:
+            self.value = self.model
+        return self
+
+    def save(self, result):
+        model = result.value
+        model.save(self.folderPath)
+        return super(CommonModel, self).save(Result.froms(value=self.folderPath))
+
+
+class HotReloadModel(CommonModel):
+    def transform(self, value):
+        if not self.folderName:
+            return None
+        self.model.loadFrom(self.folderName, version=self.version)
+        return self.model
+
+    def save(self, result):
+        raise NotImplementedError("Not support save!")
