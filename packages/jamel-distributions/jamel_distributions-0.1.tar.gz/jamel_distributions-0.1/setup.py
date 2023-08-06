0a8c0e14e09b571cb4e006a482f3c868dde48ccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:32 +0000

    Add package jamel-distributions/0.1/jamel_distributions-0.1.tar.gz

---
+++ packages/jamel-distributions/jamel_distributions-0.1.tar.gz/jamel_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='jamel_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['jamel_distributions'],
+      zip_safe=False)
