14ea5887f16cf2b48fdae2a44404a036b0a1e55f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:50 +0000

    Add package decapt/0.0.3/decapt-0.0.3.tar.gz

---
+++ packages/decapt/decapt-0.0.3.tar.gz/decapt-0.0.3/decapt/sudobg.py
@@ -0,0 +1,16 @@
+import sys
+import json
+import subprocess
+
+
+for line in sys.stdin:
+    if not line:
+        continue
+    pargs, kwargs = json.loads(line)
+    ret = subprocess.run(
+        *pargs, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, **kwargs
+    )
+    sys.stdout.write(
+        json.dumps(dict(code=ret.returncode, out=ret.stdout, err=ret.stderr,)) + "\n"
+    )
+    sys.stdout.flush()
