488c9d9611138811e481a96eaf3d6dfa3c0b8334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:18 +0000

    Add package distribut-mlnd/0.1/distribut_mlnd-0.1.tar.gz

---
+++ packages/distribut-mlnd/distribut_mlnd-0.1.tar.gz/distribut_mlnd-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distribut_mlnd',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distribut_mlnd'],
+      author='Rasmus Toivanen',
+      author_email='rasmus.toivanen@hotmail.com',
+      zip_safe=False)
