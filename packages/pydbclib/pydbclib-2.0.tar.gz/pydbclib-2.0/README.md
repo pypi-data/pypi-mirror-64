e90ee2de70395e90233dc13aefa8f2c42171f79b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:01 +0000

    Add package pydbclib/2.0/pydbclib-2.0.tar.gz

---
+++ packages/pydbclib/pydbclib-2.0.tar.gz/pydbclib-2.0/README.md
@@ -0,0 +1,66 @@
+## pydbclib: Python Database Connectivity Lib
+pydbclib is a database utils for python
+
+### Installation:
+    pip install pydbclib
+
+#### Usage:
+
+    import pydbclib
+    # 使用with上下文，可以自动提交，自动关闭连接
+    with pydbclib.connect("sqlite:///:memory:") as db:
+        db.execute('CREATE TABLE foo(a integer, b varchar(20))')
+        
+        # # 数据库基础操作 # #
+        # 单个插入和批量插入，结果返回影响行数
+        record = {"a": 1, "b": "one"}
+        db.write("INSERT INTO foo(a,b) values(:a,:b)", record)
+        db.write_many("INSERT INTO foo(a,b) values(:a,:b)", [record]*10)
+        # 查询操作，返回生成器字典集合
+        db.read("select * from foo")
+        # 参数to_dict=False，返回生成器元祖集合
+        db.read("select * from foo", to_dict=False)
+        # 查询结果只返回一条记录：{"a": 1, "b": "one"}
+        db.read_one("select * from foo")
+        
+        # # 应对表的快捷操作 # #
+        # 插入单条和插入多条，输入参数字典的键值必须和表中字段同名
+        db.get_table("foo").insert_one({"a": 1, "b": "one"})
+        db.get_table("foo").insert_many([{"a": 1, "b": "one"}])
+        # 查询字段a=1所有记录
+        db.get_table("foo").find({"a": 1})
+        # 也可以直接写成sql条件表达式，下面几个函数的条件部分都可以写成sql条件表达式
+        db.get_table("foo").find("a=1")
+        # 查询字段a=1第一条记录
+        db.get_table("foo").find_one({"a": 1})
+        # 将a=1那条记录的b字段值更新为"first"
+        db.get_table("foo").update({"a": 1}, {"b": "first"})
+        # 将a=1那条记录删除
+        db.get_table("foo").delete({"a": 1})
+
+
+#### 常用数据库连接  
+Common Driver  
+
+    # 使用普通数据库驱动连接，driver参数必须指定驱动包名称，如pymysql包参数driver='pymysql',其他参数和driver参数指定的包的连接参数一致
+    # 连接mysql
+    db = pydbclib.connect(driver="pymysql", user="user", password="password", database="test")
+    # 连接oracle
+    db = pydbclib.connect('user/password@local:1521/xe', driver="cx_Oracle")
+    # 通过odbc方式连接
+    db = pydbclib.connect('DSN=mysqldb;UID=user;PWD=password', driver="pyodbc")  
+    # 通过已有驱动连接方式连接
+    import pymysql
+    con = pymysql.connect(user="user", password="password", database="test")
+    db = pydbclib.connect(driver=con)
+
+Sqlalchemy Driver
+
+    # 连接oracle
+    db = pydbclib.connection("oracle://user:password@local:1521/xe")
+    # 连接mysql
+    db = pydbclib.connection("mysql+pyodbc://:@mysqldb")
+    # 通过已有engine连接
+    from sqlalchemy import create_engine
+    engine = create_engine("mysql+pymysql://user:password@localhost:3306/test")
+    db = pydbclib.connection(driver=engine)
---
+++ packages/pydbclib/pydbclib-2.0.tar.gz/pydbclib-2.0/pydbclib/database.py
@@ -0,0 +1,242 @@
+# -*- coding: utf-8 -*-
+"""
+@time: 2020/3/26 11:30 上午
+@desc:
+"""
+from abc import ABC, abstractmethod
+
+from pydbclib.exceptions import ParameterError
+
+
+class BaseDatabase(ABC):
+    def __init__(self, driver):
+        self.driver = driver
+
+    def get_table(self, name):
+        return Table(name, self)
+
+    @abstractmethod
+    def execute(self, sql, args=None):
+        pass
+
+    @abstractmethod
+    def read(self, sql, args=None, to_dict=True):
+        pass
+
+    @abstractmethod
+    def read_one(self, sql, args=None, to_dict=True):
+        pass
+
+    @abstractmethod
+    def write(self, sql, args=None):
+        pass
+
+    @abstractmethod
+    def write_many(self, sql, args=None):
+        pass
+
+    def commit(self):
+        self.driver.commit()
+
+    def rollback(self):
+        self.driver.rollback()
+
+    def close(self):
+        self.driver.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        try:
+            if exc_type is None:
+                self.commit()
+            else:
+                self.rollback()
+        finally:
+            self.close()
+
+
+class Database(BaseDatabase):
+    """
+    数据库操作封装
+    """
+
+    def execute(self, sql, args=None):
+        """
+        原生数据库驱动操作方法
+        :param sql: sql语句
+        :param args: sql语句参数
+        """
+        self.driver.execute(sql, args)
+
+    def _readall(self, to_dict, batch_size):
+        records = self.driver.fetchmany(batch_size)
+        while records:
+            if to_dict:
+                columns = [i[0].lower() for i in self.driver.description()]
+                records = [dict(zip(columns, i)) for i in records]
+            for record in records:
+                yield record
+            records = self.driver.fetchmany(batch_size)
+
+    def read(self, sql, args=None, to_dict=True, batch_size=5000):
+        """
+        查询返回所有表记录
+        :param sql: sql语句
+        :param args: sql语句参数
+        :param to_dict: 返回记录是否转换成字典形式（True: [{"a": 1, "b": "one"}]， False: [(1, "one)]），默认为True
+        :param batch_size: 每次查询返回的缓存的数量，大数据量可以适当提高大小
+        :return: 生成器对象
+        """
+        self.driver.execute(sql, args)
+        # records = self.driver.fetchall()
+        # if to_dict:
+        #     columns = [i[0].lower() for i in self.driver.description()]
+        #     records = [dict(zip(columns, i)) for i in records]
+        # return records
+        return self._readall(to_dict, batch_size)
+
+    def read_one(self, sql, args=None, to_dict=True):
+        """
+        查询返回一条表记录
+        :param sql: sql语句
+        :param args: sql语句参数
+        :param to_dict: 返回记录是否转换成字典形式（True: [{"a": 1, "b": "one"}]， False: [(1, "one)]），默认为True
+        :return: to_dict=True {"a": 1, "b": "one"}, to_dict=False (1, "one")
+        """
+        self.driver.execute(sql, args)
+        record = self.driver.fetchone()
+        if to_dict:
+            if record is None:
+                return {}
+            else:
+                columns = [i[0].lower() for i in self.driver.description()]
+                return dict(zip(columns, record))
+        else:
+            return record
+
+    def write(self, sql, args=None):
+        """
+        插入数据库记录
+        :param sql: sql语句
+        :param args: sql语句参数
+        :return: 影响行数
+        """
+        self.driver.execute(sql, args)
+        rowcount = self.driver.rowcount()
+        return rowcount
+
+    def write_many(self, sql, args=None):
+        """
+        批量插入数据库记录
+        :param sql: sql语句
+        :param args: sql语句参数
+        :return: 影响行数
+        """
+        self.driver.execute_many(sql, args)
+        rowcount = self.driver.rowcount()
+        return rowcount
+
+
+def format_condition(condition):
+    param = {}
+    if isinstance(condition, dict):
+        expressions = []
+        for i, k in enumerate(condition):
+            param[f"c{i}"] = condition[k]
+            expressions.append(f"{k}=:c{i}")
+        condition = " and ".join(expressions)
+    condition = f" where {condition}" if condition else ""
+    return condition, param
+
+
+def format_update(update):
+    param = {}
+    if isinstance(update, dict):
+        expressions = []
+        for i, k in enumerate(update):
+            param[f"u{i}"] = update[k]
+            expressions.append(f"{k}=:u{i}")
+        update = ",".join(expressions)
+    if not update:
+        raise ParameterError("'update' 参数不能为空值")
+    return update, param
+
+
+class Table(object):
+
+    def __init__(self, name, db):
+        self.name = name
+        self.db = db
+
+    def update(self, condition, update):
+        """
+        表更新操作
+        :param condition: 更新条件，字典类型或者sql条件表达式
+        :param update: 要更新的字段，字典类型
+        :return: 返回影响行数
+        """
+        condition, p1 = format_condition(condition)
+        update, p2 = format_update(update)
+        p1.update(p2)
+        return self.db.write(f"update {self.name} set {update}{condition}", p1)
+
+    def delete(self, condition):
+        """
+        删除表中记录
+        :param condition: 删除条件，字典类型或者sql条件表达式
+        :return: 返回影响行数
+        """
+        condition, param = format_condition(condition)
+        return self.db.write(f"delete from {self.name}{condition}", param)
+
+    def find_one(self, condition=None):
+        """
+        按条件查询一条表记录
+        :param condition: 查询条件，字典类型或者sql条件表达式
+        :return: 字典类型，如 {"a": 1, "b": "one"}
+        """
+        condition, param = format_condition(condition)
+        return self.db.read_one(f"select * from {self.name}{condition}", param)
+
+    def find(self, condition=None):
+        """
+        按条件查询所有符合条件的表记录
+        :param condition: 查询条件，字典类型或者sql条件表达式
+        :return: 生成器类型
+        """
+        condition, param = format_condition(condition)
+        return self.db.read(f"select * from {self.name}{condition}", param)
+
+    def _get_insert_sql(self, columns):
+        return f"insert into {self.name} ({','.join(columns)})" \
+               f" values ({','.join([':%s' % i for i in columns])})"
+
+    def insert_one(self, record):
+        """
+        表中插入一条记录
+        :param record: 要插入的记录数据，字典类型
+        """
+        if isinstance(record, dict):
+            columns = record.keys()
+            return self.db.write(self._get_insert_sql(columns), record)
+        else:
+            raise ParameterError("无效的参数")
+
+    def insert_many(self, records):
+        """
+        表中插入多条记录
+        :param records: 要插入的记录数据，字典集合
+        """
+        if not isinstance(records, (tuple, list)):
+            raise ParameterError("records param must iterable")
+        sample = records[0]
+        if isinstance(sample, dict):
+            columns = sample.keys()
+            return self.db.write_many(self._get_insert_sql(columns), records)
+        else:
+            raise ParameterError("无效的参数")
+
+    # def merge(self):
+    #     pass
