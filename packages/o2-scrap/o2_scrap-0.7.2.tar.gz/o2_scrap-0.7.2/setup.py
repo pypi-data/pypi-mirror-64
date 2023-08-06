a907f167f9ec894b4d2eb80b50a43d95fc2e2512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:43 +0000

    Add package o2-scrap/0.7.2/o2_scrap-0.7.2.tar.gz

---
+++ packages/o2-scrap/o2_scrap-0.7.2.tar.gz/o2_scrap-0.7.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(name='o2_scrap',
+    version='0.7.2',
+    description='library to get data-usage and plan details from O2 germany mobile contracts',
+    url='http://github.com/grindsa/o2_scrap',
+    author='grindsa',
+    author_email='grindelsack@gmail.com',
+    license='GPL',
+    packages=['o2_scrap'],
+    install_requires=[
+        'selenium',
+        'bs4',
+        'six',
+        'lxml'
+    ],    
+    classifiers = [
+        'Programming Language :: Python',
+        'Development Status :: 4 - Beta',
+        'Natural Language :: German',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],        
+    zip_safe=False)
