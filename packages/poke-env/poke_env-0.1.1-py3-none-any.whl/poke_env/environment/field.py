2f560386ae4c95331028372212c8f7317d2d51b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:54 +0000

    Add package poke-env/0.1.1/poke_env-0.1.1-py3-none-any.whl

---
+++ packages/poke-env/poke_env-0.1.1-py3-none-any.whl/poke_env/environment/field.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""This module defines the Field class, which represents a battle field.
+"""
+# pyre-ignore-all-errors[45]
+from enum import Enum, unique, auto
+
+
+@unique
+class Field(Enum):
+    """Enumeration, represent a non null field in a battle."""
+
+    ELECTRIC_TERRAIN = auto()
+    GRASSY_TERRAIN = auto()
+    MISTY_TERRAIN = auto()
+    PSYCHIC_TERRAIN = auto()
+    TRICK_ROOM = auto()
+
+    def __str__(self) -> str:
+        return f"{self.name} (field) object"
+
+    @staticmethod
+    def from_showdown_message(message: str) -> "Field":
+        """Returns the Field object corresponding to the message.
+
+        :param message: The message to convert.
+        :type message: str
+        :return: The corresponding Field object.
+        :rtype: Field
+        """
+        message = message.replace("move: ", "")
+        message = message.replace(" ", "_")
+        return Field[message.upper()]
