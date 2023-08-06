6204f5b40595f8928a12c48604fcb4ad9cd2de73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:56 +0000

    Add package aws-solutions-konstruk.aws-iot-kinesisfirehose-s3/0.8.0/aws-solutions-konstruk.aws-iot-kinesisfirehose-s3-0.8.0.tar.gz

---
+++ packages/aws-solutions-konstruk.aws-iot-kinesisfirehose-s3/aws-solutions-konstruk.aws-iot-kinesisfirehose-s3-0.8.0.tar.gz/aws-solutions-konstruk.aws-iot-kinesisfirehose-s3-0.8.0/src/aws_solutions_konstruk/aws_iot_kinesisfirehose_s3/_jsii/__init__.py
@@ -0,0 +1,20 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.aws_iam
+import aws_cdk.aws_iot
+import aws_cdk.aws_kinesisfirehose
+import aws_cdk.aws_s3
+import aws_cdk.core
+import aws_solutions_konstruk.aws_kinesis_firehose_s3
+import aws_solutions_konstruk.core
+__all__ = []
+
+publication.publish()
