469fddbfadeb44663cc76fc0acf332098cad2fc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:55 +0000

    Add package yengde/1.0.2/yengde-1.0.2.tar.gz

---
+++ packages/yengde/yengde-1.0.2.tar.gz/yengde-1.0.2/README.md
@@ -0,0 +1,11 @@
+# yengde
+
+A Python package to get number table.
+
+## Usage
+
+Following query on terminal will provide you the table of number 9
+
+```
+yengde no 9
+```
---
+++ packages/yengde/yengde-1.0.2.tar.gz/yengde-1.0.2/yengde/functions.py
@@ -0,0 +1,13 @@
+def sum(x,y):
+    return x+y
+
+def average(x,y):
+    return (x+y)/2
+
+def power(x,y):
+    return x**y
+
+
+def table(number):
+    for i in range(0,11,1):
+        print(number,"X",i,"=",number * i)                 
