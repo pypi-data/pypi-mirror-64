7d9d35dcb5059ec9e540e534e56241a2f58f796d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:57 +0000

    Add package bookleter/0.4.5/bookleter-0.4.5.tar.gz

---
+++ packages/bookleter/bookleter-0.4.5.tar.gz/bookleter-0.4.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name = 'bookleter',
+    version = '0.4.5',
+    license='MIT',
+    description = 'Turns pdfs into a6 sized foldable booklets',
+    author = 'Mohsen Barzegar',
+    author_email = 'mohsennbarzegar@gmail.com',
+    url = 'https://github.com/reinenichts/bookleter',
+    download_url = 'https://github.com/reinenichts/bookleter/archive/gui.tar.gz',
+    keywords = ['book', 'booklet', 'pdf'],
+    packages = ['bookleter'],
+    install_requires=[
+            'PyPDF2',
+            'Pillow',
+            'pdfCropMargins',
+            'pygubu',
+            # 'PyQt5'
+        ],
+    entry_points = {
+        'console_scripts': [
+            'bookleter = bookleter.__main__:main'
+        ]
+    })
