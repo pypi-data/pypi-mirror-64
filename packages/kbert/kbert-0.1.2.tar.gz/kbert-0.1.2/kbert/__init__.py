2ad8a2f722bd6ccc721d6b74c6fed5ee03de921d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:52 +0000

    Add package kbert/0.1.2/kbert-0.1.2.tar.gz

---
+++ packages/kbert/kbert-0.1.2.tar.gz/kbert-0.1.2/kbert/__init__.py
@@ -0,0 +1,9 @@
+"""
+@author: SunYanCN
+@contact: sunyanhust@163.com
+@blog: https://sunyancn.github.io
+@version: 1.0
+@license: MIT Licence
+@file: __init__.py.py
+@time: 2020-03-31 00:26:48
+"""
---
+++ packages/kbert/kbert-0.1.2.tar.gz/kbert-0.1.2/kbert/version.py
@@ -0,0 +1,6 @@
+# encoding: utf-8
+# author: SunYan
+# contact: sunyanhust@163.com
+# file: __version__.py.py
+
+__version__ = '0.1.2'
