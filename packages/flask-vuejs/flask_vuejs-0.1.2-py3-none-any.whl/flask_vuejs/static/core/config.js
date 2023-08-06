9fd939ab0d6e828735d4d20c067b1e7ebff61899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:28 +0000

    Add package flask-vuejs/0.1.2/flask_vuejs-0.1.2-py3-none-any.whl

---
+++ packages/flask-vuejs/flask_vuejs-0.1.2-py3-none-any.whl/flask_vuejs/static/core/config.js
@@ -0,0 +1,9 @@
+/**
+ * Organization: Pacotei
+ * Github: https://github.com/orgs/pacotei
+ * Author: Marcus Pereira
+ * E-mail: oi@pacotei.xyz
+ */
+
+//  Set Config for Vue
+Vue.options.delimiters = ["#{", "}"];
