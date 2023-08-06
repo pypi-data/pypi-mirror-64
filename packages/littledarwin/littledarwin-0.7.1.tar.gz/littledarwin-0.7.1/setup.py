9557c077498bfd2f99b97137ee03fee6a91ad13c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:30 +0000

    Add package littledarwin/0.7.1/littledarwin-0.7.1.tar.gz

---
+++ packages/littledarwin/littledarwin-0.7.1.tar.gz/littledarwin-0.7.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+import littledarwin
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='littledarwin',
+    version=littledarwin.__version__,
+    url=littledarwin.__url__,
+    author=littledarwin.__author__,
+    author_email="ali.parsai@live.com",
+    description="LittleDarwin Mutation Analysis Framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='GNU GPLv3',
+    packages=setuptools.find_packages(),
+    install_requires=['graphviz'],
+    entry_points={'console_scripts': ['littledarwin=littledarwin.__main__:entryPoint', ], },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
+
