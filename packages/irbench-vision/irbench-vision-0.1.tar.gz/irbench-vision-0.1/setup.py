8aa1e4a19bbecca67172b56900d70c9cfd6ebef6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:07 +0000

    Add package irbench-vision/0.1/irbench-vision-0.1.tar.gz

---
+++ packages/irbench-vision/irbench-vision-0.1.tar.gz/irbench-vision-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from setuptools import find_packages
+
+setup(name='irbench-vision',
+      version='0.1',
+      description='Image Retrieval Performance Benchmark on Large-scale Dataset',
+      url='',
+      author='Minchul Shin',
+      author_email='min.nashory@navercorp.com',
+      license='INTERNER_USE_ONLY',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      zip_safe=False)
