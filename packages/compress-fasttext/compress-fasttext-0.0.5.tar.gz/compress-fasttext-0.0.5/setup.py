0d3bbb35e4b963230ad78762544e8465fc60ff56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:55 +0000

    Add package compress-fasttext/0.0.5/compress-fasttext-0.0.5.tar.gz

---
+++ packages/compress-fasttext/compress-fasttext-0.0.5.tar.gz/compress-fasttext-0.0.5/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="compress-fasttext",
+    version="0.0.5",
+    author="David Dale",
+    author_email="dale.david@mail.ru",
+    description="A set of tools to compress gensim fasttext models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/avidale/compress-fasttext",
+    packages=['compress_fasttext'],
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'gensim>=3.8.1',
+        'numpy',
+        'pqkmeans',
+    ],
+    extras_require={
+        'full': ['scikit-learn'],
+    }
+)
