f645148ab03f12c9c8e5cfb636c8eb7421adc0ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:07 +0000

    Add package netflow/0.9.1/netflow-0.9.1.tar.gz

---
+++ packages/netflow/netflow-0.9.1.tar.gz/netflow-0.9.1/netflow/utils.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+"""
+This file belongs to https://github.com/bitkeks/python-netflow-v9-softflowd.
+
+Copyright 2017-2020 Dominik Pataky <dev@bitkeks.eu>
+Licensed under MIT License. See LICENSE.
+"""
+
+import struct
+
+from .v1 import V1ExportPacket
+from .v5 import V5ExportPacket
+from .v9 import V9ExportPacket
+
+
+class UnknownNetFlowVersion(Exception):
+    def __init__(self, data, version):
+        self.data = data
+        self.version = version
+        r = repr(data)
+        data_str = ("{:.25}..." if len(r) >= 28 else "{}").format(r)
+        super().__init__(
+            "Unknown NetFlow version {} for data {}".format(version, data_str)
+        )
+
+
+def get_netflow_version(data):
+    return struct.unpack('!H', data[:2])[0]
+
+
+def parse_packet(data, templates=None):
+    if templates is None:  # compatibility for v1 and v5
+        templates = {}
+
+    if type(data) == str:
+        # hex dump as string
+        data = bytes.fromhex(data)
+    elif type(data) == bytes:
+        # check representation based on utf-8 decoding result
+        try:
+            # hex dump as bytes, but not hex
+            dec = data.decode()
+            data = bytes.fromhex(dec)
+        except UnicodeDecodeError:
+            # use data as given, assuming hex-formatted bytes
+            pass
+
+    version = get_netflow_version(data)
+    if version == 1:
+        return V1ExportPacket(data)
+    elif version == 5:
+        return V5ExportPacket(data)
+    elif version == 9:
+        return V9ExportPacket(data, templates)
+    raise UnknownNetFlowVersion(data, version)
---
+++ packages/netflow/netflow-0.9.1.tar.gz/netflow-0.9.1/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='netflow',
+    version='0.9.1',
+    description='NetFlow v1, v5, and v9 tool suite implemented in Python 3.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Dominik Pataky',
+    author_email='dev@bitkeks.eu',
+    url='https://github.com/bitkeks/python-netflow-v9-softflowd',
+    packages=["netflow"],
+    license='MIT',
+    python_requires='>=3.5',
+    keywords='netflow collector parser',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators"
+    ],
+)
