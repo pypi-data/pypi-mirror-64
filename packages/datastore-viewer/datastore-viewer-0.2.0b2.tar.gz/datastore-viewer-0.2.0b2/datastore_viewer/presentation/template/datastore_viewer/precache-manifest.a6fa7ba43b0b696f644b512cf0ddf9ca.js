c88fca1c1f078e682ec3f6cc0a8af17aede7af01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:53 +0000

    Add package datastore-viewer/0.2.0b2/datastore-viewer-0.2.0b2.tar.gz

---
+++ packages/datastore-viewer/datastore-viewer-0.2.0b2.tar.gz/datastore-viewer-0.2.0b2/datastore_viewer/presentation/template/datastore_viewer/precache-manifest.a6fa7ba43b0b696f644b512cf0ddf9ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a63be262042aff7fa76f0dd5253a07c1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "61c779cbe5d025a15ada",
+    "url": "./static/css/main.b927196a.chunk.css"
+  },
+  {
+    "revision": "26be428e9cf54ae38863",
+    "url": "./static/js/2.b4c0cfcc.chunk.js"
+  },
+  {
+    "revision": "5455a644ad04f3fe306d0df919e9daa8",
+    "url": "./static/js/2.b4c0cfcc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "61c779cbe5d025a15ada",
+    "url": "./static/js/main.478ca7ad.chunk.js"
+  },
+  {
+    "revision": "62ac6b2afe4799580ba2",
+    "url": "./static/js/runtime-main.4f0c640e.js"
+  }
+]);
