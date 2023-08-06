8978676a9ac25baa9b8646db85753b9be6bbceda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:16 +0000

    Add package creme/0.5.1/creme-0.5.1-cp37-cp37m-manylinux1_x86_64.whl

---
+++ packages/creme/creme-0.5.1-cp37-cp37m-manylinux1_x86_64.whl/creme/feature_selection/__init__.py
@@ -0,0 +1,11 @@
+"""Online feature selection."""
+from .k_best import SelectKBest
+from .random import PoissonInclusion
+from .variance import VarianceThreshold
+
+
+__all__ = [
+    'PoissonInclusion',
+    'SelectKBest',
+    'VarianceThreshold'
+]
