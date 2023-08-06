9b774a3f79586e8b721191a3e0778c1e1df23ea0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:38 +0000

    Add package fn-graph-studio/0.2.11/fn_graph_studio-0.2.11.tar.gz

---
+++ packages/fn-graph-studio/fn_graph_studio-0.2.11.tar.gz/fn_graph_studio-0.2.11/setup.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['fn_graph_studio']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['dash-treebeard>=0.0.1,<0.0.2',
+ 'dash>=1.7,<2.0',
+ 'dash_core_components>=1.6,<2.0',
+ 'dash_interactive_graphviz>=0.1.0,<0.2.0',
+ 'dash_split_pane>=1.0,<2.0',
+ 'fn_graph>=0.5.0',
+ 'pandas>=0.25.3',
+ 'plotly>=4.4,<5.0',
+ 'sh>=1.12,<2.0']
+
+entry_points = \
+{'console_scripts': ['run_graph_studio = '
+                     'fn_graph_studio.cli:run_studio_command']}
+
+setup_kwargs = {
+    'name': 'fn-graph-studio',
+    'version': '0.2.11',
+    'description': 'A web based explorer for fn_graph function composers',
+    'long_description': None,
+    'author': 'James Saunders',
+    'author_email': 'james@businessoptics.biz',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
