6715d60368f4c93cc42bb795528f7594d519f461
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:43 +0000

    Add package gw-md-lib/0.10.1/gw_md_lib-0.10.1-py3-none-any.whl

---
+++ packages/gw-md-lib/gw_md_lib-0.10.1-py3-none-any.whl/gwlib/base/base_controller.py
@@ -0,0 +1,25 @@
+import sqlalchemy
+from sqlalchemy.exc import IntegrityError
+
+from gwlib.base.base_service import BaseService
+from gwlib.http.responses import HTTP_BAD_REQUEST, HTTP_CONFLICT
+
+
+class BaseController:
+
+    def build_response(self, method=None, **kwargs):
+        """
+        Method to call a Service function and return a Http Response
+        :type kwargs: dict
+        :type method: function
+        """
+        try:
+            response = method(**kwargs)
+        except KeyError as e:
+            return HTTP_BAD_REQUEST(e)
+        except IntegrityError as e:
+            return HTTP_CONFLICT(e)
+        except sqlalchemy.exc.InvalidRequestError as e:
+            return HTTP_CONFLICT(e)
+        return response
+
