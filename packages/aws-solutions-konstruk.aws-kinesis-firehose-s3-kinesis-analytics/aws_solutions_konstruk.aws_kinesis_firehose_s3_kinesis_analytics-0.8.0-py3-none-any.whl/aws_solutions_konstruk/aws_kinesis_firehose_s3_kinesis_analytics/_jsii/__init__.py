aa13688f6fdc6c7989713a0fe69feaffc7a7e794
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:54 +0000

    Add package aws-solutions-konstruk.aws-kinesis-firehose-s3-kinesis-analytics/0.8.0/aws_solutions_konstruk.aws_kinesis_firehose_s3_kinesis_analytics-0.8.0-py3-none-any.whl

---
+++ packages/aws-solutions-konstruk.aws-kinesis-firehose-s3-kinesis-analytics/aws_solutions_konstruk.aws_kinesis_firehose_s3_kinesis_analytics-0.8.0-py3-none-any.whl/aws_solutions_konstruk/aws_kinesis_firehose_s3_kinesis_analytics/_jsii/__init__.py
@@ -0,0 +1,21 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.aws_iam
+import aws_cdk.aws_kinesis
+import aws_cdk.aws_kinesisanalytics
+import aws_cdk.aws_kinesisfirehose
+import aws_cdk.aws_s3
+import aws_cdk.core
+import aws_solutions_konstruk.aws_kinesis_firehose_s3
+import aws_solutions_konstruk.core
+__all__ = []
+
+publication.publish()
