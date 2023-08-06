817096db7479ff244c32854c745810d61f4b217b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:26 +0000

    Add package oep-client/0.0.0/oep-client-0.0.0.tar.gz

---
+++ packages/oep-client/oep-client-0.0.0.tar.gz/oep-client-0.0.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="oep-client",
+    version="0.0.0",
+    description="client side tool for openenergy platform",
+    long_description=long_description,
+    packages=["oep-client"],
+    author="Christian Winger",
+    author_email="c.winger@oeko.de",
+    url="https://github.com/wingechr/oep-client",
+    install_requires=["omi"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        "console_scripts": [
+            # 'CMD = package.module:function'
+        ]
+    },
+    package_data={
+        # 'package.module: [file_patterns]'
+    },
+)
