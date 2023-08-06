06226bc79012b4954f31815ef8d86cdfb32e361d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:18 +0000

    Add package pymug/1.4.0/pymug-1.4.0.tar.gz

---
+++ packages/pymug/pymug-1.4.0.tar.gz/pymug-1.4.0/README.md
@@ -0,0 +1,1 @@
+A Pymug joke
---
+++ packages/pymug/pymug-1.4.0.tar.gz/pymug-1.4.0/pymug/__main__.py
@@ -0,0 +1,8 @@
+import pymug
+
+def main():
+    print('Greetings From The Pymug (Python Mauritius User Group')
+    print('github.com/pymug')
+
+if __name__ == '__main__':
+    main()
