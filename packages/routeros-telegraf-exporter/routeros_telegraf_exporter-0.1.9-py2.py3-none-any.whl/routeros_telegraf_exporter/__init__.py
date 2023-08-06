39db3a6b07e47d356e84667e7743e560b446a0f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:49 +0000

    Add package routeros-telegraf-exporter/0.1.9/routeros_telegraf_exporter-0.1.9-py2.py3-none-any.whl

---
+++ packages/routeros-telegraf-exporter/routeros_telegraf_exporter-0.1.9-py2.py3-none-any.whl/routeros_telegraf_exporter/__init__.py
@@ -0,0 +1,50 @@
+"""Main entry point
+"""
+
+__author__ = """Andres Kepler"""
+__email__ = 'andres@kepler.ee'
+__version__ = '0.1.9'
+
+import os
+import re
+
+from pyramid.config import Configurator
+
+DEFAULT_VALUES_FIELDS = "state"
+DEFAULT_TAGS_FIELDS = "name"
+DEFAULT_MEASUREMNT = "routerstat"
+CONNECTIONS = {}
+DEFAULT_CONF = """
+- default:
+      resources:
+          - path: /system/resource
+            interval: 30
+            tags:
+                  - board-name
+                  - disabled
+            values:
+                  - cpu-load
+- rte_default_gw:
+      resources:
+          - path: /interface/ethernet
+            interval: 15
+            tags:
+                - name
+                - speed
+                - disabled
+            values:
+                - rx-bytes
+                - tx-bytes
+"""
+
+
+def main(global_config, **settings):
+    for key, value in settings.items():
+        if re.match("^routeros_exporter.*", key):
+            settings[key] = value
+            os.environ[key.upper()] = value
+
+    config = Configurator(settings=settings)
+    config.include("cornice")
+    config.scan("routeros_telegraf_exporter.views")
+    return config.make_wsgi_app()
