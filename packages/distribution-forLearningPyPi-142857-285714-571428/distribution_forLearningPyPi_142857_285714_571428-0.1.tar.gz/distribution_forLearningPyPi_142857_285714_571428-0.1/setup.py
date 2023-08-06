cc49b48e7c8822c67219bbff31ec6d56f7fec6f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:55 +0000

    Add package distribution-forLearningPyPi-142857-285714-571428/0.1/distribution_forLearningPyPi_142857_285714_571428-0.1.tar.gz

---
+++ packages/distribution-forLearningPyPi-142857-285714-571428/distribution_forLearningPyPi_142857_285714_571428-0.1.tar.gz/distribution_forLearningPyPi_142857_285714_571428-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distribution_forLearningPyPi_142857_285714_571428',
+      version='0.1',
+      description='Gaussian & Binomial distribution distributions',
+      packages=['distribution_forLearningPyPi_142857_285714_571428'],
+      author='',
+      author_email = '',
+      zip_safe=False)
