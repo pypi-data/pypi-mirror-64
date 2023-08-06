cff42ed715d24fe56ddc579830a3c06579629099
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:33 +0000

    Add package PrediProt/1.0.10/PrediProt-1.0.10.tar.gz

---
+++ packages/PrediProt/PrediProt-1.0.10.tar.gz/PrediProt-1.0.10/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name = 'PrediProt',
+    version = '1.0.10',
+    description = 'Protein complex structure predictor',
+    long_description = readme(),
+    long_description_content_type = 'text/markdown',
+    author = 'The Dream Team',
+    license = 'GPL',
+    packages = ['prediprot','prediprot.imports'],
+    install_requires = ['argparse'],
+    python_requires='>=3',
+    scripts=['prediprot/PrediProt', 'prediprot/PrediProt_getfasta', 'prediprot/PrediProt_GUI'],
+    include_package_data=True,
+    zip_safe=False)
