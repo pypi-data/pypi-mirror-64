fa7c307bc03ed21426caa2a7d8ea224c8fb79c38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:08 +0000

    Add package MKLpy/0.4.2b0/MKLpy-0.4.2b0.tar.gz

---
+++ packages/MKLpy/MKLpy-0.4.2b0.tar.gz/MKLpy-0.4.2b0/MKLpy/__init__.py
@@ -0,0 +1,16 @@
+"""
+==========================================================
+MKLpy: A framework for Multiple Kernel Learning for Python
+==========================================================
+
+MKLpy is a framework for Multiple Kernel Learning and Kernel Learning for python
+based on numpy, scikit-learn and cvxopt.
+
+authos: Lauriola Ivano ivano.lauriola @phd.unipd.it
+"""
+
+
+__all__ = ['algorithms',
+           'utils',
+           'preprocessing',
+           'metrics']
