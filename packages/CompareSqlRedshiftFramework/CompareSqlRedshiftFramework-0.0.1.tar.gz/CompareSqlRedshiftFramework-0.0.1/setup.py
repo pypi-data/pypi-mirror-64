11b6e9632f341132347502a0f9dc0fd53a55ea66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:17 +0000

    Add package CompareSqlRedshiftFramework/0.0.1/CompareSqlRedshiftFramework-0.0.1.tar.gz

---
+++ packages/CompareSqlRedshiftFramework/CompareSqlRedshiftFramework-0.0.1.tar.gz/CompareSqlRedshiftFramework-0.0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='CompareSqlRedshiftFramework',
+    version='0.0.1',
+    description='CompareSqlRedshiftFramework Test Upload',
+    py_modules=["Compare_SQL_RedShift", "DaskUtil", "MasterClass"],
+    package_dir={'': 'src'},
+    author="Rajnish Kumar",
+    author_email="rajnish.kumar@acacceptance.com",
+    include_package_data=True,
+    url="https://acapgit.acacceptance.com/projects/AWA/repos/libraries/browse",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Operating System :: OS Independent",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=3.7",
+        ],
+    },
+)
