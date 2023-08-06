960d5259d525d2dea84a6b016fdca0827fb2b6af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:51 +0000

    Add package spaceone-repository/0.1.2.post141/spaceone_repository-0.1.2.post141-py3-none-any.whl

---
+++ packages/spaceone-repository/spaceone_repository-0.1.2.post141-py3-none-any.whl/spaceone/repository/manager/repository_manager/local_repository_manager.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from spaceone.repository.manager.repository_manager import RepositoryManager
+from spaceone.repository.model.repository_model import Repository
+
+
+class LocalRepositoryManager(RepositoryManager):
+
+    def register_repository(self, params):
+        """
+        Args:
+            params:
+                - name
+                - repository_type: local
+        """
+        # Assume there is only one local repository
+
+        return self.repo_model.create(params)
