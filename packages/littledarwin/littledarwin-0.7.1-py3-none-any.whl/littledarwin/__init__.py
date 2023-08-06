db29f7a5ca7f9c8f2f699790c3150f14ad1d39c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:30 +0000

    Add package littledarwin/0.7.1/littledarwin-0.7.1-py3-none-any.whl

---
+++ packages/littledarwin/littledarwin-0.7.1-py3-none-any.whl/littledarwin/__init__.py
@@ -0,0 +1,11 @@
+"""
+__init__ script for littledarwin package
+"""
+
+from littledarwin import LittleDarwin
+
+__version__ = LittleDarwin.littleDarwinVersion
+__author__ = "Ali Parsai"
+__package__ = 'littledarwin'
+__license__ = 'GPLv3'
+__url__ = 'https://littledarwin.parsai.net/'
