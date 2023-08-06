93e89b12506e66f889758e6b0e62c9bd8060803a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:02 +0000

    Add package medcat/0.3.0.2/medcat-0.3.0.2.tar.gz

---
+++ packages/medcat/medcat-0.3.0.2.tar.gz/medcat-0.3.0.2/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("./README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="medcat",
+    version="0.3.0.2",
+    author="w-is-h",
+    author_email="w.kraljevic@gmail.com",
+    description="Concept annotation tool for Electronic Health Records",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CogStack/MedCAT",
+    packages=['medcat', 'medcat.utils', 'medcat.preprocessing'],
+    install_requires=[
+        'numpy~=1.15',
+        'pandas~=0.23',
+        'gensim~=3.7',
+        'spacy==2.2.4',
+        'scipy~=1.1',
+        'scimdls',],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    dependency_links=['https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.2.4/en_core_sci_md-0.2.4.tar.gz#scimdls'],
+)
