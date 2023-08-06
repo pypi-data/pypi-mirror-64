f49033936a28d43507ec12371c94578eb25f13f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:16 +0000

    Add package distributionsilkexercise/0.1/distributionsilkexercise-0.1.tar.gz

---
+++ packages/distributionsilkexercise/distributionsilkexercise-0.1.tar.gz/distributionsilkexercise-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributionsilkexercise',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributionsilkexercise'],
+      zip_safe=False)
