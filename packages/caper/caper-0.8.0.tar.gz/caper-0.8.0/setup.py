57e07225c8fe481d9587857bf08d6e670877b676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:13 +0000

    Add package caper/0.8.0/caper-0.8.0.tar.gz

---
+++ packages/caper/caper-0.8.0.tar.gz/caper-0.8.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+import caper
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='caper',
+    version=caper.__version__,
+    python_requires='>3.4.1',
+    scripts=['bin/caper', 'bin/run_mysql_server_docker.sh',
+             'bin/run_mysql_server_singularity.sh'],
+    author='Jin Lee',
+    author_email='leepc12@gmail.com',
+    description='Cromwell Assisted Pipeline ExecutoR',
+    long_description='https://github.com/ENCODE-DCC/caper',  #long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/ENCODE-DCC/caper',
+    packages=setuptools.find_packages(exclude=['mysql*', 'docs']),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+    ],
+    install_requires=['pyhocon', 'requests', 'pyopenssl', 'autouri>=0.1.1']
+)
