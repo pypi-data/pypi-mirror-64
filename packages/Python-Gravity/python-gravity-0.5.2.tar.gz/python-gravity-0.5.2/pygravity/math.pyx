2e9fc0dcb5a02549133c9b3b769ca9d0e535c237
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:07 +0000

    Add package Python-Gravity/0.5.2/python-gravity-0.5.2.tar.gz

---
+++ packages/Python-Gravity/python-gravity-0.5.2.tar.gz/python-gravity-0.5.2/pygravity/math.pyx
@@ -0,0 +1,9 @@
+gravitational_constant = 6.67430e-11
+
+def acceleration_due_to_gravity(double mass, double distance):
+    return (gravitational_constant * mass) / distance ** 2
+
+def invert_angle(double angle):
+    return (angle + 180) % 360
+
+__all__ = ['gravitational_constant', 'acceleration_due_to_gravity']
