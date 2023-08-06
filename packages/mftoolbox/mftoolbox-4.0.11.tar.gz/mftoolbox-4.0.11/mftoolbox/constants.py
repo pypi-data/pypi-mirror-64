1b41f26dcc4c9c04ca706b8e06f6e2737560595f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:25 +0000

    Add package mftoolbox/4.0.11/mftoolbox-4.0.11.tar.gz

---
+++ packages/mftoolbox/mftoolbox-4.0.11.tar.gz/mftoolbox-4.0.11/mftoolbox/constants.py
@@ -0,0 +1,18 @@
+class Color:
+   purple = '\033[95m'
+   cyan = '\033[96m'
+   darkcyan = '\033[36m'
+   blur = '\033[94m'
+   green = '\033[92m'
+   yellow = '\033[93m'
+   red = '\033[91m'
+   bold = '\033[1m'
+   underline = '\033[4m'
+   end = '\033[0m'
+
+class Header:
+
+    header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) '
+                           'AppleWebKit/537.36 (KHTML, like Gecko) '
+                           'Chrome/39.0.2171.95 Safari/537.36',
+                           'Cache-Control': 'no-cache'}
