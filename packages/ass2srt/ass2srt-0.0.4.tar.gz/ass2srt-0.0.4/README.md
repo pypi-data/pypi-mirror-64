41caf5299d316558603c1e9227c61459496e03ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:25 +0000

    Add package ass2srt/0.0.4/ass2srt-0.0.4.tar.gz

---
+++ packages/ass2srt/ass2srt-0.0.4.tar.gz/ass2srt-0.0.4/README.md
@@ -0,0 +1,36 @@
+## ass2srt
+
+A tool that convert .ass subtitles to .srt
+
+### Install
+```
+pip install ass2srt
+```
+
+### Usage
+```
+$ ass2srt -h
+
+usage: ass2srt.py [-h] [-s {zh,en,fr,de}] [-l {0,1,2}] [-i] file
+
+positional arguments:
+  file                  .ass file to convert
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -s {zh,en,fr,de}, --suffix {zh,en,fr,de}
+                        add suffix to subtitles name
+  -l {0,1,2}, --line {0,1,2}
+                        keep double subtitles
+  -i, --info            display subtitles infomation
+  -o OUT, --out OUT     output file name
+
+```
+
+### Example :
+```
+ass2srt Movies.S01.E01.Name.ass
+```
+
+
+
---
+++ packages/ass2srt/ass2srt-0.0.4.tar.gz/ass2srt-0.0.4/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ass2srt",  # Replace with your own username
+    version="0.0.4",
+    author="zcq100",
+    author_email="zcq100@gmail.com",
+    description="A tool that convert .ass subtitles to .srt",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zcq100/ass2srt",
+    packages=setuptools.find_packages(),
+    entry_points={
+      'console_scripts':[
+          'ass2srt=ass2srt:main',
+      ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Topic :: Text Processing',
+    ],
+    python_requires='>=3.6',
+)
