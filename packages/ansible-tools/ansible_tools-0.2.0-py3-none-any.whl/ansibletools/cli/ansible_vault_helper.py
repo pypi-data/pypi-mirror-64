aca1d805eba2cbb89816c652523d576ce86de81b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:48 +0000

    Add package ansible-tools/0.2.0/ansible_tools-0.2.0-py3-none-any.whl

---
+++ packages/ansible-tools/ansible_tools-0.2.0-py3-none-any.whl/ansibletools/cli/ansible_vault_helper.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python3
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2020 Lorenzo Villani
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+import argparse
+import configparser
+import getpass
+import inspect
+import os
+import os.path
+import sys
+import typing
+
+import keyring
+
+
+CFG_OPTION = "name"
+CFG_SECTION = "vault"
+KEYRING_SERVICE = "ansible-vault"
+
+
+def main():
+    args = parse_args()
+
+    if args.update:
+        save()
+        return
+
+    secret_name, err = get_secret_name()
+    if err:
+        fatal(err)
+
+    secret, err = get_secret(secret_name)
+    if err:
+        fatal(err)
+
+    print(secret)
+
+
+def parse_args():
+    args = argparse.ArgumentParser()
+    args.add_argument(
+        "-u",
+        "--update",
+        action="store_true",
+        help="Stores/Updates a vault unlock password in the keyring, saving the association in ansible.cfg",
+    )
+
+    return args.parse_args()
+
+
+def save():
+    secret_name, err = get_secret_name()
+    if err:
+        name = input("Vault name: ")
+    else:
+        print("WARNING: Changing password for %s" % secret_name)
+        name = secret_name
+
+    password = getpass.getpass("Password: ")
+    set_secret(name, password)
+
+
+def set_secret(name, secret):
+    """Stores a secret into the keyring, creating or updating the relevant stanza
+    in `ansible.cfg' in the process.
+
+    """
+    c = load_ansible_cfg()
+
+    if not c.has_section("vault"):
+        c.add_section("vault")
+
+    c.set(CFG_SECTION, CFG_OPTION, name)
+
+    with open(get_ansible_cfg_path(), "w") as fp:
+        c.write(fp)
+
+    keyring.set_password(KEYRING_SERVICE, name, secret)
+
+
+def load_ansible_cfg():
+    """Returns a ConfigParser instance with the contents of ansible.cfg. If the
+    file does not exist, it returns an empty ConfigParser object instead.
+
+    """
+    c = configparser.ConfigParser()
+
+    if is_ansible_cfg_there():
+        c.read(get_ansible_cfg_path())
+
+    return c
+
+
+def is_ansible_cfg_there():
+    return os.path.isfile(get_ansible_cfg_path())
+
+
+def get_ansible_cfg_path():
+    return os.path.join(os.getcwd(), "ansible.cfg")
+
+
+def get_secret_name():
+    """Gets the secret name from local ansible.cfg file.
+
+    Returns a tuple (secret_name, error_string) with `secret_name' being the
+    empty string in case of errors, in which case `error_string' is set to a
+    human-readable, non-localized, error message.
+
+    """
+    if not is_ansible_cfg_there():
+        return ("", "Unable to find ansible.cfg in the current directory.")
+
+    c = load_ansible_cfg()
+
+    if not c.has_section(CFG_SECTION):
+        return ("", "Unable to find 'vault' section within ansible.cfg")
+
+    if not c.has_option(CFG_SECTION, CFG_OPTION):
+        return (
+            "",
+            "Unable to find option '%s' in section '%s' within ansible.cfg"
+            % (CFG_OPTION, CFG_SECTION),
+        )
+
+    return (c.get(CFG_SECTION, CFG_OPTION), "")
+
+
+def fatal(*args):
+    print(" ".join(args), file=sys.stderr)
+    sys.exit(1)
+
+
+def get_secret(name) -> typing.Tuple[str, typing.Optional[str]]:
+    """Obtains secret from the keyring."""
+    p = keyring.get_password(KEYRING_SERVICE, name)
+
+    if not p:
+        return (
+            "",
+            inspect.cleandoc(
+                """
+                Unable to obtain password for vault "{}" from the keyring.
+
+                Call "ansible-vault-helper --update" to add or update the vault's password
+                to the keyring.
+                """.format(
+                    name
+                )
+            ) + "\n",
+        )
+
+    return (p, None)
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/ansible-tools/ansible_tools-0.2.0-py3-none-any.whl/ansibletools/cli/vaultify.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2020 Lorenzo Villani
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+import os
+import subprocess
+import sys
+import typing
+
+
+def main():
+    helper_path = vault_helper_path()
+    if helper_reports_error(helper_path):
+        die("Unable to run ansible-vault-helper. Cannot continue.")
+
+    # ansible-vault command expects the --vault-password-file argument to be after the action
+    # argument
+    if sys.argv[1] == "ansible-vault":
+        command = (
+            [sys.argv[1], sys.argv[2]] + ["--vault-password-file=%s" % helper_path] + sys.argv[3:]
+        )
+    else:
+        command = [sys.argv[1]] + ["--vault-password-file=%s" % helper_path] + sys.argv[2:]
+
+    sys.exit(subprocess.call(command))
+
+
+def vault_helper_path() -> str:
+    p = which("ansible-vault-helper")
+    if not p:
+        die("Cannot find ansible-vault-helper in your $PATH or it isn't executable. Aborting")
+
+    return p
+
+
+def which(exe: str) -> typing.Optional[str]:
+    for p in os.environ["PATH"].split(os.path.pathsep):
+        full_path = os.path.join(p, exe)
+
+        if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
+            return full_path
+
+    return None
+
+
+def helper_reports_error(helper_path: str) -> bool:
+    """Ansible doesn't check the return code of the helper script we give to the
+    ``--vault-password-file`` command line switch and will happily use whatever we print to standard
+    output when we exit with a non-zero status code, including an empty string.
+
+    This method should be called before invoking ``ansible``, ``ansible-playbook`` or
+    ``ansible-vault`` to make sure that the helper script exits cleanly (i.e.: without errors), so
+    that we don't do stuff with a bogus passphrase.
+
+    """
+    try:
+        # Use check_output, this way we don't print the unlock password when
+        # there are no errors.
+        subprocess.check_output(helper_path)
+
+        return False
+    except:
+        return True
+
+
+def die(*args) -> typing.NoReturn:
+    print(" ".join(args), file=sys.stderr)
+    sys.exit(1)
