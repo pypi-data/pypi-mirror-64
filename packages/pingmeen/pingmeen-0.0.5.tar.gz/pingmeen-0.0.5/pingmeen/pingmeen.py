4aa4fb3aa3dadfb33a1518da6393d6fea9ec29f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:14 +0000

    Add package pingmeen/0.0.5/pingmeen-0.0.5.tar.gz

---
+++ packages/pingmeen/pingmeen-0.0.5.tar.gz/pingmeen-0.0.5/pingmeen/pingmeen.py
@@ -0,0 +1,15 @@
+import requests
+
+class Pingmeen:
+    def __init__(self, token):
+        self.token = token
+        response = self.__server_query('validate_token')
+        if response != 'true':
+            raise Exception
+        
+    def finish(self):
+        self.__server_query('send_message')
+        
+    def __server_query(self, method):
+        url = 'http://2kolobka.pro/nikita/pingmeen/'
+        return requests.get(url + method + '.php?token' + self.token).text
