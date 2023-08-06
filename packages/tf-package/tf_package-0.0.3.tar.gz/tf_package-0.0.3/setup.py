59a597e662d891708e087d5e3a60a26920803e53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:37 +0000

    Add package tf-package/0.0.3/tf_package-0.0.3.tar.gz

---
+++ packages/tf-package/tf_package-0.0.3.tar.gz/tf_package-0.0.3/setup.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='tf_package',  # 项目的名称,pip3 install get-time
+    version='0.0.3',  # 项目版本
+    author='YuHe',  # 项目作者
+    author_email='yuying11130108@gmail.com',  # 作者email
+    url='https://github.com/yuhe-tf/tf_package',  # 项目代码仓库
+    description='增加TensorFlow没有的功能函数',  # 项目描述
+    packages=['tf_package'],  # 包名
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'attention_layer=tf_package.attention:attention_layer',
+        ]
+    },
+    python_requires='>=3.6',
+)
