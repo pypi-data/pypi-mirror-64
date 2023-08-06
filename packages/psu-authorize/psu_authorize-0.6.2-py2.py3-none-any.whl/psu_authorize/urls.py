b0c56f74c32d5677d4226925594391a340ff8e94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:29 +0000

    Add package psu-authorize/0.6.2/psu_authorize-0.6.2-py2.py3-none-any.whl

---
+++ packages/psu-authorize/psu_authorize-0.6.2-py2.py3-none-any.whl/psu_authorize/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # A simple test page
+    path('', views.index, name='authorize_index'),
+    path('app/<str:app_code>/', views.app, name='authorize_app'),
+    path('grant', views.grant, name='grant_permission'),
+    path('revoke', views.revoke, name='revoke_permission_tbd'),
+    path('revoke/<int:permission_id>/', views.revoke, name='revoke_permission')
+]
