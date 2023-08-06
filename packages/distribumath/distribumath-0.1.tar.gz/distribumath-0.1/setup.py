948b373d811a597680f122d80c6a6b5486845434
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:18 +0000

    Add package distribumath/0.1/distribumath-0.1.tar.gz

---
+++ packages/distribumath/distribumath-0.1.tar.gz/distribumath-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distribumath',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distribumath'],
+      zip_safe=False)
