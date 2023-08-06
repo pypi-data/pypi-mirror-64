7fd6177fb1cebf077a9f378ff60048364381aae7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:42 +0000

    Add package encyclopedia/0.41/encyclopedia-0.41-py3-none-any.whl

---
+++ packages/encyclopedia/encyclopedia-0.41-py3-none-any.whl/bits/__init__.py
@@ -0,0 +1,6 @@
+'''
+Utility functions to support Encyclopedia
+'''
+import bits.file
+import bits.time
+import bits.cast
---
+++ packages/encyclopedia/encyclopedia-0.41-py3-none-any.whl/encyclopedia/jsonopedia.py
@@ -0,0 +1,33 @@
+# standard
+import unittest
+import json
+# external
+# internal
+from encyclopedia import Forest
+'''
+
+'''
+class JSON(Forest):
+
+    def __init__():
+
+        pass
+
+    def __str__(self):
+        pass
+
+
+
+class Test_(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_basic(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
