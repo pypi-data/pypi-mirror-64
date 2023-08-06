e58bbfb463f1713aa01af9b622ca80b9a84586e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:37 +0000

    Add package shotwell-mover/0.1.0/shotwell-mover-0.1.0.tar.gz

---
+++ packages/shotwell-mover/shotwell-mover-0.1.0.tar.gz/shotwell-mover-0.1.0/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['shotwell_mover']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=7.1.1,<8.0.0']
+
+entry_points = \
+{'console_scripts': ['shotwell-mover = shotwell_mover.cli:main']}
+
+setup_kwargs = {
+    'name': 'shotwell-mover',
+    'version': '0.1.0',
+    'description': "Tool for changing paths of media stored in Shotwell's database",
+    'long_description': None,
+    'author': 'Tadej Janež',
+    'author_email': 'tadej.j@nez.si',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
