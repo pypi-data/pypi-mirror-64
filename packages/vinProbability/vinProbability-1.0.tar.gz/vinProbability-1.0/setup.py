d07e2ba3a3cbf9f9dcdf8437375fd243d0151fac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:13 +0000

    Add package vinProbability/1.0/vinProbability-1.0.tar.gz

---
+++ packages/vinProbability/vinProbability-1.0.tar.gz/vinProbability-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='vinProbability',
+      version='1.0',
+      description='Gaussian & Binomial distributions created by VinJ',
+      packages=['vinProbability'],
+      zip_safe=False,
+      author = 'Vineet J'
+      )
