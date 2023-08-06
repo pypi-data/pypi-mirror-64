7eec81f082d28563bddc17a480d9ee2b97f8a9c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:06 +0000

    Add package scrape-it/0.3.8/scrape_it-0.3.8.tar.gz

---
+++ packages/scrape-it/scrape_it-0.3.8.tar.gz/scrape_it-0.3.8/scrape_it/run.py
@@ -0,0 +1,5 @@
+from scrape_it import Scrape_it
+
+scrape_it = Scrape_it(url='https://www.lyvly.uk', country='us', method='webdriver')
+
+scrape_it.scrape()
---
+++ packages/scrape-it/scrape_it-0.3.8.tar.gz/scrape_it-0.3.8/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+import os
+import sys
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="scrape_it",
+    DATA_DIR = os.path.join(sys.prefix, 
+		"local/lib/python3.6/dist-packages", "scrape_it"),
+    version="0.3.8",
+    py_modules=["scrape_it"],
+    author="Valentyna Fihurska",
+    author_email="valentineburn@gmail.com",
+    description="Systemitized tool for scraping",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/erelin6613/Scrape_it",
+    license='Apache License 2.0',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: Apache Software License"
+    ],
+    install_requires=["requests", "tqdm", "numpy", "nltk",
+			"address-parser", "beautifulsoup4",
+			"bs4", "lxml", "nltk", 
+			"phonenumbers", "pyap", "regex",
+			"selenium", "tldextract"],
+    python_requires='>=3.5',
+)
