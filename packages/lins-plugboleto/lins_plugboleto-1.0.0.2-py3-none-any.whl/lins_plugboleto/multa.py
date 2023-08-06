2dcafb8cdc5b3395876b30b99cf7ed85ece54a2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:24 +0000

    Add package lins-plugboleto/1.0.0.2/lins_plugboleto-1.0.0.2-py3-none-any.whl

---
+++ packages/lins-plugboleto/lins_plugboleto-1.0.0.2-py3-none-any.whl/lins_plugboleto/multa.py
@@ -0,0 +1,6 @@
+class Multa(object):
+
+    def __init__(self, cod_multa,data_multa,valor):
+        self.TituloCodigoMulta = None
+        self.TituloDataMulta = None
+        self.TituloValorMultaTaxa = None
---
+++ packages/lins-plugboleto/lins_plugboleto-1.0.0.2-py3-none-any.whl/lins_plugboleto/protesto.py
@@ -0,0 +1,5 @@
+class Protesto(object):
+
+    def __init__(self, name):
+        self.TituloCodProtesto = None
+        self.TituloPrazoProtesto = None
