2100c30bb5c5a7a6a794f9c694068621cbe6d808
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:47 +0000

    Add package rfhub2/0.14/rfhub2-0.14-py3-none-any.whl

---
+++ packages/rfhub2/rfhub2-0.14-py3-none-any.whl/rfhub2/db/model/collection.py
@@ -0,0 +1,39 @@
+from sqlalchemy import Column, Integer, Sequence, Text
+from sqlalchemy.orm import relationship
+from typing import List
+
+from rfhub2.db.model.base_class import Base
+from rfhub2.db.model.mixins import DocMixin
+from rfhub2.db.repository.ordering import OrderingItem
+
+
+class Collection(Base, DocMixin):
+    id = Column(Integer, Sequence("collection_id_seq"), primary_key=True)
+    name = Column(Text, index=True)
+    type = Column(Text)
+    version = Column(Text)
+    scope = Column(Text)
+    named_args = Column(Text)
+    path = Column(Text)
+    doc = Column(Text)
+    doc_format = Column(Text)
+    keywords = relationship(
+        "Keyword",
+        backref="collection",
+        cascade="all, delete-orphan",
+        passive_deletes=True,
+        order_by="Keyword.name",
+    )
+
+    def __str__(self):  # pragma: no cover
+        return (
+            f"Collection({self.id},{self.name},{self.type},{self.version},"
+            + f"{self.scope},{self.named_args},{self.path},{self.doc},{self.doc_format})"
+        )
+
+    def __repr__(self):  # pragma: no cover
+        return str(self)
+
+    @classmethod
+    def default_ordering(cls) -> List[OrderingItem]:
+        return [OrderingItem("name")]
