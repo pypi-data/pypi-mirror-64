9ca8307a36104b1d77ddc75396903c1d2f3a2f09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:20 +0000

    Add package sharpify/0.0.8/sharpify-0.0.8.tar.gz

---
+++ packages/sharpify/sharpify-0.0.8.tar.gz/sharpify-0.0.8/sharpify/sharpify.js
@@ -0,0 +1,28 @@
+/* SECTION - [data-controller] & [data-action] */
+elements = document.querySelectorAll("[data-controller]");
+elements.forEach(element => {
+    
+    actionString = "/";
+    
+    if(element.dataset.controller != null && element.dataset.action != null) {
+        actionString += element.dataset.controller + "/" + element.dataset.action + "/";
+        element.removeAttribute("data-controller");
+        element.removeAttribute("data-action");
+    }
+    else{
+        actionString = "Missing [data-controller] OR [data-action]";
+    }
+
+    // Check what element type we are dealing with, to set the correct attribute
+    switch(element.nodeName) {
+        case "A":
+            console.log("A");
+            element.href = actionString;
+            break;
+
+        case "FORM":
+            console.log("FORM");
+            element.action = actionString;
+            break;
+    }    
+});
