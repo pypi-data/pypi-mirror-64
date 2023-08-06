0b7402ff83ffea3c98ba862ff43be3c3d9373ee7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:15 +0000

    Add package gabi-distributions/0.0.1/gabi-distributions-0.0.1.tar.gz

---
+++ packages/gabi-distributions/gabi-distributions-0.0.1.tar.gz/gabi-distributions-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gabi-distributions",
+    version="0.0.1",
+    author="Ahmed Gamaleldin",
+    author_email="ahmedgamal1496@gmail.com",
+    description="Package for Gaussian and Bionomial Distributions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
