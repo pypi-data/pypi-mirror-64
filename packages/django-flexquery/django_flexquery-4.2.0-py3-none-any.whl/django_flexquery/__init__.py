aaa759254629425fd251f9f3ecc965b1f6f632bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:57 +0000

    Add package django-flexquery/4.2.0/django_flexquery-4.2.0-py3-none-any.whl

---
+++ packages/django-flexquery/django_flexquery-4.2.0-py3-none-any.whl/django_flexquery/__init__.py
@@ -0,0 +1,15 @@
+"""
+Reusable QuerySet filtering logic for Django.
+"""
+
+try:
+    from .flexquery import *
+    from .q import *
+except ImportError:  # pragma: no cover
+    # Django is missing, just provide version
+    __all__ = []
+else:
+    __all__ = ["FlexQuery", "Manager", "Q", "QuerySet"]
+
+
+__version__ = "4.2.0"
