c18295d22882a4d278e79076bf3c45c798879cf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:34 +0000

    Add package wpy/0.4.2/wpy-0.4.2.tar.gz

---
+++ packages/wpy/wpy-0.4.2.tar.gz/wpy-0.4.2/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: wxnacy(wxnacy@gmail.com)
+# Description:
+
+from wpy import __version__
+from setuptools import setup, find_packages
+
+setup(
+    name = 'wpy',
+    version = __version__,
+    keywords='python3',
+    description = 'a library for python Developer',
+    license = 'MIT License',
+    url = 'https://github.com/wxnacy/wpy',
+    author = 'wxnacy',
+    author_email = 'wxnacy@gmail.com',
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = 'any',
+    install_requires = [
+        'requests>=2.19.1',
+        'pycrypto>=2.6.1',
+        'Pillow>=5.2.0',
+        'click>=6.7',
+    ],
+    entry_points={
+        'console_scripts': ['wpy=wpy.app:run']
+        },
+    #  scripts=['bin/wpytool']
+)
+
