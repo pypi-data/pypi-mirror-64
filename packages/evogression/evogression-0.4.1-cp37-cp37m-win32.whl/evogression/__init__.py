701826dad3ee1ed7954f549c24c78b3803c69f5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:14 +0000

    Add package evogression/0.4.1/evogression-0.4.1-cp37-cp37m-win32.whl

---
+++ packages/evogression/evogression-0.4.1-cp37-cp37m-win32.whl/evogression/__init__.py
@@ -0,0 +1,6 @@
+from . import evolution
+from .evolution import Evolution
+from .creatures import EvogressionCreature
+from . import standardize
+from . import groups
+from .groups import evolution_group
