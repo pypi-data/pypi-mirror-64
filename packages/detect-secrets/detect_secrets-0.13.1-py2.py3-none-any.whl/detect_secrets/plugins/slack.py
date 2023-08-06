db4ff877fa06b41b3c034c84973bf02f3bc8d0fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:23 +0000

    Add package detect-secrets/0.13.1/detect_secrets-0.13.1-py2.py3-none-any.whl

---
+++ packages/detect-secrets/detect_secrets-0.13.1-py2.py3-none-any.whl/detect_secrets/plugins/slack.py
@@ -0,0 +1,52 @@
+"""
+This plugin searches for Slack tokens
+"""
+from __future__ import absolute_import
+
+import re
+
+import requests
+
+from .base import RegexBasedDetector
+from detect_secrets.core.constants import VerifiedResult
+
+
+class SlackDetector(RegexBasedDetector):
+    """Scans for Slack tokens."""
+    secret_type = 'Slack Token'
+
+    denylist = (
+        # Slack Token
+        re.compile(r'xox(?:a|b|p|o|s|r)-(?:\d+-)+[a-z0-9]+', flags=re.IGNORECASE),
+        # Slack Webhooks
+        re.compile(
+            r"""
+            https://hooks.slack.com/services/T[a-zA-Z0-9_]{8}/B[a-zA-Z0-9_]{8}/[a-zA-Z0-9_]{24}
+            """,
+            flags=re.IGNORECASE | re.VERBOSE,
+        ),
+    )
+
+    def verify(self, token, **kwargs):  # pragma: no cover
+        if token.startswith('https://hooks.slack.com/services/T'):
+            response = requests.post(
+                token,
+                json={
+                    'text': '',
+                },
+            )
+            valid = response.text == 'missing_text_or_fallback_or_attachments'
+        else:
+            response = requests.post(
+                'https://slack.com/api/auth.test',
+                data={
+                    'token': token,
+                },
+            ).json()
+            valid = response['ok']
+
+        return (
+            VerifiedResult.VERIFIED_TRUE
+            if valid
+            else VerifiedResult.VERIFIED_FALSE
+        )
