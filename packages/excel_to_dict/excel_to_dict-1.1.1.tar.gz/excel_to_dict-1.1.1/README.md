294f98a42d82021f3d4a8d65781c6be3dc32a1b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:09 +0000

    Add package excel_to_dict/1.1.1/excel_to_dict-1.1.1.tar.gz

---
+++ packages/excel_to_dict/excel_to_dict-1.1.1.tar.gz/excel_to_dict-1.1.1/README.md
@@ -0,0 +1,2 @@
+A small package to convert record in excel into dict.
+
---
+++ packages/excel_to_dict/excel_to_dict-1.1.1.tar.gz/excel_to_dict-1.1.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+requires = [
+    "pandas == 0.23.4",
+    "xlrd == 0.9.0",
+]
+
+setuptools.setup(
+    name="excel_to_dict",
+    version="1.1.1",
+    author="nixinxin",
+    author_email="1025464043@qq.com",
+    description="A small package to convert records in excel into dict",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nixinxin/excel_to_dict",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    install_requires=requires,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
