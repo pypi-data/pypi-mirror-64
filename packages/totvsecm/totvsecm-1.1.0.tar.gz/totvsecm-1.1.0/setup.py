0cc5081890d98e4dfc660e7b36c69dcba2a0ae58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:30 +0000

    Add package totvsecm/1.1.0/totvsecm-1.1.0.tar.gz

---
+++ packages/totvsecm/totvsecm-1.1.0.tar.gz/totvsecm-1.1.0/setup.py
@@ -0,0 +1,15 @@
+﻿
+from setuptools import setup
+
+setup(name='totvsecm',
+      version='1.1.0',
+      description='API para webservices do TOTVS ECM.',
+      url='http://github.com/soslaio/totvsecm',
+      author='soslaio',
+      author_email='rogeriorp@gmail.com',
+      license='MIT',
+      packages=['totvsecm'],
+      install_requires=[
+          'zeep',
+      ],
+      zip_safe=False)
