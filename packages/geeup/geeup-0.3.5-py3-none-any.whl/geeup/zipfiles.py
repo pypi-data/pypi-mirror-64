bbde087a75f1412bd6bdcb4e048e38319036ad8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:24 +0000

    Add package geeup/0.3.5/geeup-0.3.5-py3-none-any.whl

---
+++ packages/geeup/geeup-0.3.5-py3-none-any.whl/geeup/zipfiles.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os
+from zipfile import ZipFile
+
+ex = ['.shp', '.prj', '.dbf', '.shx']
+
+file_paths = []
+i=1
+def zipshape(directory,export):
+    for (root, directories, files) in os.walk(directory):
+        for filename in files:
+            if filename.endswith('.shp'):
+                file_paths=[]
+                filebase = filename.split('.')[0]
+                try:
+                    for things in ex:
+                        if os.path.exists(os.path.join(root, filebase+things)):
+                            filepath = os.path.join(root, filebase+things)
+                            #print(filepath)
+                            file_paths.append(filepath)
+                    os.chdir(export)
+                    if not os.path.exists(filebase+'.zip') and int(len(file_paths))==int(4):
+                        with ZipFile(filebase+'.zip','w') as zip:
+                            print("Creating zipped folder "+str(filebase)+'.zip'+' at '+str(export))
+                            # writing each file one by one
+                            for file in file_paths:
+                                fname=os.path.basename(file)
+                                zip.write(file,fname)
+                    else:
+                        print('File already exists: '+str(filebase+'.zip')+' SKIPPING')
+                except:
+                    pass
+#get_all_file_paths(directory=r"C:\Users\samapriya\Downloads\nexgengrid",export=r'D:\Library')
