598dcba05f9ddb4c85f1d88dbe6a3ae06506d8ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:11 +0000

    Add package ebookshelf/0.0.3/ebookshelf-0.0.3-py3-none-any.whl

---
+++ packages/ebookshelf/ebookshelf-0.0.3-py3-none-any.whl/ebookshelf/ebookshelf.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+import xml.etree.ElementTree as ET
+from .book import Book
+from .library import Library
+
+def main():
+    books=[]
+
+    tree = ET.parse('MyBooks.xml')
+    for record in tree.getroot():
+       books.append(Book(record))
+
+    library=Library()
+    for book in books:
+        library.add(book)
+
+    library.finalize()
+    library.draw()
+    
+if __name__ == '__main__':
+    main()
