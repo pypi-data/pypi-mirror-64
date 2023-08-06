8c18ef197d8a3e0364c651e5d5e42e20ccd5e56b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:57 +0000

    Add package unplate/0.2.7/unplate-0.2.7-py3-none-any.whl

---
+++ packages/unplate/unplate-0.2.7-py3-none-any.whl/unplate/__init__.py
@@ -0,0 +1,67 @@
+import unplate.compile as unplate_compile
+import unplate.tokenize_util as tku
+import unplate.util
+import unplate.options
+
+# export UnplateSyntaxError
+UnplateSyntaxError = unplate_compile.UnplateSyntaxError
+
+def template(*args, **kwargs):
+  raise Exception("Something's gone wrong. You should never actually invoke unplate.template().")
+
+
+"""
+
+Top-level Unplate API.
+
+>>> import unplate
+>>> if unplate.true:
+>>>   exec(unplate.compile(__file__), globals(), locals())
+>>> else:
+>>>   template = unplate.template(
+>>>     # Here's my template
+>>>   )
+
+unplate.true always evaluates to True.
+
+This enters the first block and means that the
+second block (the else: block) is never run.
+The call to unplate.compile reads the file source
+code, compiles the unplate templates, and
+returns native Python source. This is then
+executed by the call to exec().
+Additionally, the return value of unplate.compile
+has all instances of "unplate.true" replaced
+with False in order to prevent infinite recursion.
+
+"""
+
+
+true = True
+
+
+def compile(file_loc, options=options.defaults):
+
+  with open(file_loc, 'r') as f:
+    code = f.read()
+
+  return compile_code(code, options, file_loc=file_loc)
+
+
+def compile_anon(code, options=options.defaults):
+  return compile_code(code, options, file_loc='<anonymous>')
+
+
+def compile_code(code, options=options.defaults, *, file_loc):
+
+  tokens = tku.tokenize_string(code)
+  compiled_tokens = unplate_compile.compile_top(tokens, options, file_loc=file_loc)
+
+  # Remove the wrapper
+  unplate_wrapper = tku.tokenize_expr('unplate.true')
+  true_token = tku.tokenize_one('False')
+  compiled_tokens = util.replace_sublist(compiled_tokens, unplate_wrapper, [true_token])
+
+  compiled_code = tku.untokenize(compiled_tokens)
+  return compiled_code
+
