07f8a5923a7bc0d976e358cceccf5eb755ee4c4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:22 +0000

    Add package sharpify/0.0.1/sharpify-0.0.1.tar.gz

---
+++ packages/sharpify/sharpify-0.0.1.tar.gz/sharpify-0.0.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools;
+
+with open("README.md", "r") as fh:
+    long_description = fh.read();
+
+setuptools.setup(
+    name="sharpify",
+    version="0.0.1",
+    author="Ricki",
+    author_email="",
+    description="Turn the flask routing / architecture into Asp.Net Core like",
+    long_description = long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    install_requires = ["Flask"],
+    license = "MIT",
+    include_package_data = True
+);
