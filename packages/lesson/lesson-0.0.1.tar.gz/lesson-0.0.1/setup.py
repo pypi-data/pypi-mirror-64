c668f2cd6ca93ac865abc1cdad865310a3936fe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:16 +0000

    Add package lesson/0.0.1/lesson-0.0.1.tar.gz

---
+++ packages/lesson/lesson-0.0.1.tar.gz/lesson-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+
+setup(
+    name = 'lesson',
+    version = '0.0.1',
+    license = 'MIT',
+    description = 'A tool for conducting coding lessons',
+    author = 'Artyom Danilov',
+    author_email = 'defytheflow@gmail.com',
+    url = 'https://github.com/defytheflow/lesson.git',
+    download_url = 'https://github.com/defytheflow/lesson/archive/v_0.0.1.tar.gz',
+    install_requires = [],
+)
