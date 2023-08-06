4649d6b29f1b9553410134c1871efd1f0f2f4b1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:08 +0000

    Add package watchlogs/0.1.3.21/watchlogs-0.1.3.21.tar.gz

---
+++ packages/watchlogs/watchlogs-0.1.3.21.tar.gz/watchlogs-0.1.3.21/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+
+setuptools.setup(
+    name="watchlogs",
+    version="0.1.3.21",
+    entry_points={
+        "console_scripts": [
+            "watchlogs=watchlogs.watchlogs:main",
+        ],
+    },
+    author="Samuel Albanie",
+    description="A simple, colourful logfile watcher",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/albanie/watchlogs",
+    packages=["watchlogs"],
+    python_requires=">=3.6",
+    install_requires=[
+        "colored",
+        "seaborn",
+        "tailf",
+        "psutil",
+        "humanize",
+        "typeguard",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        'Operating System :: POSIX :: Linux',
+    ],
+)
