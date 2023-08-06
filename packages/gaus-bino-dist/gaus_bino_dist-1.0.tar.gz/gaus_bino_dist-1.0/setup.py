7f4e37c32491aabacd173002066501f32d11db23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:39 +0000

    Add package gaus-bino-dist/1.0/gaus_bino_dist-1.0.tar.gz

---
+++ packages/gaus-bino-dist/gaus_bino_dist-1.0.tar.gz/gaus_bino_dist-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='gaus_bino_dist',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['gaus_bino_dist'],
+      author='Maitrey Talware',
+      zip_safe=False)
