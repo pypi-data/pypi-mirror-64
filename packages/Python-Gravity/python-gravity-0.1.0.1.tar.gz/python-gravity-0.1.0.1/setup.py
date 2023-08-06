07edf57f0decfc6a8adc413bc7e21b875b4eef21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:27 +0000

    Add package Python-Gravity/0.1.0.1/python-gravity-0.1.0.1.tar.gz

---
+++ packages/Python-Gravity/python-gravity-0.1.0.1.tar.gz/python-gravity-0.1.0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+from Cython.Build import cythonize
+
+setuptools.setup(
+    name = 'python-gravity',
+    version = '0.1.0.1',
+    url = 'https://github.com/gaming32/pygravity',
+    author = 'Gaming32',
+    author_email = 'gaming32i64@gmail.com',
+    license = 'License :: OSI Approved :: MIT License',
+    description = 'Library for calculating stuff having to do with gravity',
+    long_description = '',
+    long_description_content_type = 'text/markdown',
+    packages = [
+        'pygravity',
+        'pygravity.2d',
+    ],
+    ext_modules = cythonize('pygravity/math.pyx'),
+    zip_safe = False,
+)
