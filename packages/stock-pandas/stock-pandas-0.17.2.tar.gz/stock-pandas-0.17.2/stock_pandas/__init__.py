7398f6ba6907aec33a54081dccaf7bdff5abf314
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:52 +0000

    Add package stock-pandas/0.17.2/stock-pandas-0.17.2.tar.gz

---
+++ packages/stock-pandas/stock-pandas-0.17.2.tar.gz/stock-pandas-0.17.2/stock_pandas/__init__.py
@@ -0,0 +1,10 @@
+# beta version
+__version__ = '0.17.2'
+
+from .dataframe import StockDataFrame  # noqa
+from .common import DirectiveCache     # noqa
+
+from .exceptions import (  # noqa
+    DirectiveSyntaxError,
+    DirectiveValueError
+)
