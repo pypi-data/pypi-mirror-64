85394f27f4e3a80a9634f39b8678e12aaa68ea1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:13 +0000

    Add package GBI-distributions/0.2/GBI_distributions-0.2.tar.gz

---
+++ packages/GBI-distributions/GBI_distributions-0.2.tar.gz/GBI_distributions-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='GBI_distributions',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['GBI_distributions'],
+      author = 'Parinaz Farajiparvar',
+      zip_safe=False)
