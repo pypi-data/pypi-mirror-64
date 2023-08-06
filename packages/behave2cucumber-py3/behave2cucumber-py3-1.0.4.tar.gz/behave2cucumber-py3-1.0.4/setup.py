931156f165eeffcb782a7effa205cab33d3fb111
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:58 +0000

    Add package behave2cucumber-py3/1.0.4/behave2cucumber-py3-1.0.4.tar.gz

---
+++ packages/behave2cucumber-py3/behave2cucumber-py3-1.0.4.tar.gz/behave2cucumber-py3-1.0.4/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='behave2cucumber-py3',
+
+    version='1.0.4',
+
+    description='Behave to Cucumber json converter',
+    long_description='This project helps solving the incompatibilty of Behave\'s genereated json reports '
+                     'to tools using Cucumber json reports. '
+                     'Its done by reformatting the Behave json to Cucumber json.',
+
+    url='https://github.com/asc3ns10n/behave2cucumber',
+
+    author='Andrey Goldgamer, Zvika Messing',
+    author_email='andrey.goldgamer@behalf.com, zvika@behalf.com ',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+    ],
+
+    keywords='behave2cucumber setuptools development cucumber behave automation json',
+
+    packages=find_packages(),
+
+    install_requires=[],
+
+    extras_require={},
+
+    data_files=[],
+
+    entry_points={
+        'console_scripts': [
+            'behave2cucumber = behave2cucumber.behave2cucumber.__main__:main'
+        ],
+    },
+)
