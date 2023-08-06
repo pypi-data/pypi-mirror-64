721c0b6752f4898d6f05efff8ac3ac545a80f4b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:02 +0000

    Add package sarcastic-color-scheme/0.1.0/sarcastic-color-scheme-0.1.0.tar.gz

---
+++ packages/sarcastic-color-scheme/sarcastic-color-scheme-0.1.0.tar.gz/sarcastic-color-scheme-0.1.0/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sarcastic-color-scheme",
+    version="0.1.0",
+    author="Kavi Gupta",
+    author_email="sarcastic@kavigupta.org",
+    description="A sarcastic color scheme for matplotlib.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kavigupta/sarcastic-color-scheme",
+    download_url="https://github.com/kavigupta/sarcastic-color-scheme/archive/0.1.0.zip",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+    install_requires=[
+        'matplotlib'
+    ]
+)
