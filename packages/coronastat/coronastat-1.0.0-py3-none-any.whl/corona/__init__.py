caa023446daaa75ce33cf1414b057e9d8d3777ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:20 +0000

    Add package coronastat/1.0.0/coronastat-1.0.0-py3-none-any.whl

---
+++ packages/coronastat/coronastat-1.0.0-py3-none-any.whl/corona/__init__.py
@@ -0,0 +1,24 @@
+########### IMPORTING THE REQURIED LIBRARIES ###########
+
+from corona import Corona
+from utils import *
+import sys
+
+######### DRIVER METHOD ##########
+
+def main():
+	corona = Corona();
+	args = sys.argv
+
+	displayLoadingAnim()
+	displayASCIIArt()
+
+	if( len( args ) == 1 or args[ 1 ] == "-h" or args[ 1 ] == "--help" ):
+		displayHelp()
+	elif( args[ 1 ] == "-w" or args[ 1 ] == "--world" ):
+		displayWorldInfo( corona )
+	elif( args[ 1 ] == "-c" or args[ 1 ] == "--country" ):
+		displayCountryInfo( corona )
+
+if __name__ == "__main__":
+	main()
