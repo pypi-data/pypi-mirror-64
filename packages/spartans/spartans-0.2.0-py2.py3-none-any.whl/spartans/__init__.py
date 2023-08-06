0dc67d191f445d669b3927d85c4b8406986942a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:45 +0000

    Add package spartans/0.2.0/spartans-0.2.0-py2.py3-none-any.whl

---
+++ packages/spartans/spartans-0.2.0-py2.py3-none-any.whl/spartans/__init__.py
@@ -0,0 +1,11 @@
+"""Top-level package for Spartans."""
+from .core import *
+from .feature_matrix import FeatureMatrix
+
+__author__ = """Dean Langsam"""
+__email__ = 'deanla@gmail.com'
+__version__ = '0.2.0'
+
+"""SPARTANS - SPARse Tools for ANalysiS"""
+
+__all__ = ['core', 'FeatureMatrix']
