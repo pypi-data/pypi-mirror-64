18f9be4d06ba126ffa85fc86f4b505024462dff8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:24 +0000

    Add package smartbytes/1.0/smartbytes-1.0.tar.gz

---
+++ packages/smartbytes/smartbytes-1.0.tar.gz/smartbytes-1.0/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import setuptools
+from distutils.core import setup
+
+setup(name = 'smartbytes',
+      version = '1.0',
+      description = 'makes bytes less painful',
+      author = 'Aaron Esau',
+      author_email = 'python@aaronesau.com',
+      url = 'https://github.com/Arinerron/smartbytes',
+      packages = ['smartbytes'])
