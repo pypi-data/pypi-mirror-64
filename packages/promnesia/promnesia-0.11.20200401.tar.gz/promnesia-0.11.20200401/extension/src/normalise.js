30e72f2cf5fa17470f3e1691a7a1e1cfe097d990
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:24 +0000

    Add package promnesia/0.11.20200401/promnesia-0.11.20200401.tar.gz

---
+++ packages/promnesia/promnesia-0.11.20200401.tar.gz/promnesia-0.11.20200401/extension/src/normalise.js
@@ -0,0 +1,95 @@
+/* @flow */
+import {unwrap, asList} from './common';
+import type {Url} from './common';
+
+// TODO should probably be merged with common or something...
+
+//
+var R = RegExp;
+
+export const
+STRIP_RULES = [
+    [[R('.*')                     , R('^\\w+://'         )]],
+    [[R('.*')                     , R('(www|ww|amp)\\.'  )]],
+    [[R('.*')                     , R('[&#].*$'          )]],
+    [
+        // TODO get rid of these...
+        [R('^(youtube|urbandictionary|tesco|scottaaronson|answers.yahoo.com|code.google.com)') , null],
+        [R('.*'), R('[\\?].*$')],
+    ],
+    [[R('.*')                     , R('/$'               )]],
+]
+; // TODO perhaps that should be semi-configurable
+
+// TODO maybe use that normalisation library and then adjust query params etc
+
+/*
+  I think, most common usecases are:
+  - blacklisting whole domain (e.g. for privacy reasons, like bank/etc or if something is broken)
+  - blacklisting specific pages (e.g. reddit/twitter/fb main page so it doesn't result it too many child contexts)
+  For that current approach is fine.
+*/
+
+// TODO careful about dots etc?
+
+export function normalise_url(url: string): string {
+    let cur = url;
+    STRIP_RULES.forEach((rules: Array<Array<?RegExp>>) => { // meh impure foreach..
+        for (const rule of rules) {
+            const target: RegExp = unwrap(rule[0]);
+            const reg: ?RegExp = rule[1];
+            if (cur.search(target) >= 0) {
+                console.log("[normalise] %s: matched %s, applying %s", cur, target, reg);
+                if (reg != null) {
+                    cur = cur.replace(reg, '');
+                }
+                break;
+            }
+        }
+    });
+    return cur;
+}
+
+const _re = R('^(www|ww|amp)\\.'  );
+export function normaliseHostname(url: string): string {
+    return url.replace(_re, '');
+}
+
+
+export function normalisedURLHostname(url: Url): string {
+    const _hostname = new URL(url).hostname;
+    const hostname = normaliseHostname(_hostname);
+    return hostname;
+}
+
+
+export function isBlacklistedHelper(url: Url, blacklist: string): ?string {
+    // https://github.com/gorhill/uBlock/wiki/How-to-whitelist-a-web-site kind of following this logic and syntax
+
+    // TODO need to be careful about normalising domains here; e.g. cutting off amp/www could be bit unexpected...
+    const bl = asList(blacklist);
+    if (bl.includes(url)) {
+        return "User-defined blacklist (exact page)";
+    }
+
+    const hostname = normalisedURLHostname(url);
+    if (bl.includes(hostname)) {
+        return "User-defined blacklist (domain)"; // TODO maybe supply item number?
+    }
+
+    // TODO eh, it's a bit annoying; it tries to handle path segments which we don't really want...
+    // const mm = require('micromatch');
+    // // console.log(pm.isMatch('http://github.com/issues', ['*github.com/issues*']));
+    // if (mm.isMatch(url, bl, {contains: true})) {
+    //     return "User-defined blacklist (wildcard)";
+    // }
+
+    const regexes = bl.filter(s => s[0] == '/' && s.slice(-1) == '/');
+    for (const regex of regexes) {
+        if (url.search(RegExp(regex)) >= 0) {
+            return `User-defined blacklist (regex: ${regex})`;
+        }
+    }
+
+    return null;
+}
---
+++ packages/promnesia/promnesia-0.11.20200401.tar.gz/promnesia-0.11.20200401/src/promnesia/__main__.py
@@ -0,0 +1,179 @@
+import argparse
+import os
+import logging
+import inspect
+import sys
+from typing import List, Tuple, Optional, Dict, Sequence
+from pathlib import Path
+from datetime import datetime
+from tempfile import TemporaryDirectory
+
+
+from . import config
+from . import server
+from .misc import install_server
+from .common import PathIsh, History, make_filter, get_logger, get_tmpdir
+from .common import previsits_to_history, Indexer
+from .dump import dump_histories
+
+
+def _do_index():
+    cfg = config.get()
+
+    logger = get_logger()
+
+    indexers = cfg.INDEXERS
+
+    output_dir = Path(cfg.OUTPUT_DIR)
+    if not output_dir.exists():
+        raise ValueError(f"Expected OUTPUT_DIR({output_dir}) to exist")
+
+    filters = [make_filter(f) for f in cfg.FILTERS]
+    for f in filters:
+        History.add_filter(f) # meh..
+
+
+    all_histories = []
+    all_errors = []
+
+    for extractor in indexers:
+        ex = extractor
+        if callable(ex):
+            # lazy indexers
+            ex = ex()
+        assert isinstance(ex, Indexer)
+
+        einfo = f'{ex.ff.__module__}:{ex.ff.__name__} {ex.args} {ex.kwargs}'
+
+        hist, errors = previsits_to_history(ex, src=ex.src)
+        all_errors.extend(errors)
+        all_histories.append((einfo, hist))
+
+    # TODO perhaps it's better to open connection and dump as we collect so it consumes less memory?
+    dump_histories(all_histories)
+
+    if len(all_errors) > 0:
+        sys.exit(1)
+
+
+def do_index(config_file: Path):
+    try:
+        config.load_from(config_file)
+        _do_index()
+    finally:
+        config.reset()
+
+
+def demo_indexers():
+    def lazy(name: str):
+        # helper to avoid failed imports etc, since people might be lacking necessary dependencies
+        def inner(*args, **kwargs):
+            from . import indexers
+            import importlib
+            module = importlib.import_module('promnesia.indexers' + '.' + name)
+            return getattr(module, 'index')
+        return inner
+
+    return {
+        # TODO ugh, this runs against merged db
+        # 'chrome' : browser.chrome,
+        # 'firefox': browser.firefox,
+        'auto'    : lazy('auto'),
+        # TODO org mode
+
+        'takeout' : lazy('takeout'),
+        'telegram': lazy('telegram'),
+        'guess'   : lazy('guess')
+    }
+
+
+
+def do_demo(index_as: str, params: Sequence[str], port: Optional[str]):
+    logger = get_logger()
+    from pprint import pprint
+    # TODO logging?
+    idx = demo_indexers()[index_as]()
+
+    idxr = Indexer(idx, *params, src='demo')
+    hist, errors = previsits_to_history(idxr, src=idxr.src)
+
+    for e in errors:
+        logger.error(e)
+    logger.info("Finished indexing {} {}, {} total".format(index_as, params, len(hist)))
+    # TODO color?
+    with TemporaryDirectory() as tdir:
+        outdir = Path(tdir)
+        cfg = config.Config(
+            OUTPUT_DIR=outdir,
+            INDEXERS=[],
+        )
+        config.instance = cfg
+        dump_histories([('demo', hist)])
+
+        if port is None:
+            logger.warning("Port isn't specified, not serving!")
+        else:
+            server._run(port=port, db=outdir / 'promnesia.sqlite', timezone=server.get_system_tz(), quiet=False)
+
+        if sys.stdin.isatty():
+            input("Press any key when ready")
+
+
+def main():
+    # TODO longer, literate description?
+    from .common import setup_logger
+    setup_logger(get_logger(), level=logging.DEBUG)
+
+    F = argparse.ArgumentDefaultsHelpFormatter
+    p = argparse.ArgumentParser(formatter_class=F)
+    # p.add_argument('--hello', required=False, default='YES', help='alala')
+    subp = p.add_subparsers(dest='mode', )
+    ep = subp.add_parser('index', help='Create/update the link database', formatter_class=F)
+    ep.add_argument('--config', type=Path, default=Path('config.py'))
+    # TODO use some way to override or provide config only via cmdline?
+    ep.add_argument('--intermediate', required=False)
+
+    sp = subp.add_parser('serve', help='Serve a link database', formatter_class=F)
+    server.setup_parser(sp)
+
+    # TODO not sure what would be a good name? rename to 'demo'?
+    ap = subp.add_parser('demo', help='Demo mode: index and serve a directory in single command', formatter_class=F)
+    # TODO use docstring or something?
+    ap.add_argument('--port', type=str, help='Port to serve. If omitted, will only create the index without serving.', required=False)
+    ap.add_argument(
+        '--as',
+        choices=list(sorted(demo_indexers().keys())),
+        default='guess',
+        help='Index the path as',
+    )
+    ap.add_argument('params', nargs='*', help='Optional extra params for the indexer')
+
+    isp = subp.add_parser('install-server', help='Install server as a systemd service (for autostart)', formatter_class=F)
+    install_server.setup_parser(isp)
+
+    args = p.parse_args()
+
+    # TODO is there a way to print full help? i.e. for all subparsers
+    if args.mode is None:
+        print('ERROR: Please specify a mode', file=sys.stderr)
+        p.print_help(sys.stderr)
+        sys.exit(1)
+
+    # TODO maybe, it's better for server to compute intermediate represetnation?
+    # the only downside is storage. dunno.
+    # worst case -- could use database?
+
+    with get_tmpdir() as tdir: # TODO??
+        if args.mode == 'index':
+             do_index(config_file=args.config)
+        elif args.mode == 'serve':
+            server.run(args)
+        elif args.mode == 'demo':
+            do_demo(index_as=getattr(args, 'as'), params=args.params, port=args.port)
+        elif args.mode == 'install-server':
+            install_server.install(args)
+        else:
+            raise AssertionError(f'unexpected mode {args.mode}')
+
+if __name__ == '__main__':
+    main()
