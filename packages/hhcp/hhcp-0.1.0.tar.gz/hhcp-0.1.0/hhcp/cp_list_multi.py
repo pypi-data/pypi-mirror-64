7585ecd6032b0ace3c58c85727cd3d7c834ee6e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:26 +0000

    Add package hhcp/0.1.0/hhcp-0.1.0.tar.gz

---
+++ packages/hhcp/hhcp-0.1.0.tar.gz/hhcp-0.1.0/hhcp/cp_list_multi.py
@@ -0,0 +1,68 @@
+"""
+指定 filelist，将其中的所有文件复制到指定 path
+"""
+import multiprocessing
+import os
+import shutil
+import threading
+
+from hhcp.utils.chunks import get_chunks
+
+
+def cp_list(filelist, destination, prefix, overwrite):
+
+    class myThread(threading.Thread):
+
+        def __init__(self, threadID, filelist, destination, overwrite):
+            threading.Thread.__init__(self)
+            self.threadID = threadID
+            self.filelist = filelist
+            self.destination = destination
+            self.overwrite = overwrite
+
+        def run(self):
+            print("开启线程： " + self.name)
+            # 获取锁，用于线程同步
+            # threadLock.acquire()
+
+            for i, f in enumerate(self.filelist):
+                if self.overwrite:
+                    shutil.copy(f, self.destination)
+                    print("thread ID {}, {}/{}, copy [{}] to [{}]".format(
+                        self.threadID, i, len(self.filelist), f,
+                        self.destination))
+                else:
+                    target_file = os.path.join(self.destination,
+                                               f.split("/")[-1])
+                    if os.path.exists(target_file):
+                        print("thread ID {}, {}/{}, File {} already exists.".
+                              format(self.threadID, i, len(self.filelist),
+                                     target_file))
+                    else:
+                        shutil.copy(f, self.destination)
+                        print("thread ID {}, {}/{}, copy [{}] to [{}]".format(
+                            self.threadID, i, len(self.filelist), f,
+                            self.destination))
+
+    thread_num = multiprocessing.cpu_count()
+
+    files = []
+    for l in open(filelist):
+        files.append(os.path.join(prefix, l.strip("\n")))
+
+    filechunks = get_chunks(files, thread_num)
+    thread_num = len(filechunks)
+
+    # 线程锁
+    # threadLock = threading.Lock()
+    threads = []
+
+    for i in range(thread_num):
+        thread = myThread(i, filechunks[i], destination, overwrite)
+        threads.append(thread)
+        thread.start()
+
+    # 等待所有线程完成
+    for t in threads:
+        t.join()
+    print("cp 结束，退出主线程")
---
+++ packages/hhcp/hhcp-0.1.0.tar.gz/hhcp-0.1.0/hhcp/version.py
@@ -0,0 +1,2 @@
+__version__ = '0.1.0'
+short_version = '0.1.0'
