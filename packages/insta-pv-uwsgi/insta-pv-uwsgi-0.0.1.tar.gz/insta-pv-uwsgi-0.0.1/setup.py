195004764778789fa6606086d7fb984709f1241d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:35 +0000

    Add package insta-pv-uwsgi/0.0.1/insta-pv-uwsgi-0.0.1.tar.gz

---
+++ packages/insta-pv-uwsgi/insta-pv-uwsgi-0.0.1.tar.gz/insta-pv-uwsgi-0.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="insta-pv-uwsgi", # Replace with your own username
+    version="0.0.1",
+    author="UWSGI",
+    author_email="author@example.com",
+    description="A small example package",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
