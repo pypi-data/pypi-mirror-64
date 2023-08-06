427a97ba3ed8de4e919ee4d451e6d97d4d9bdc19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:33 +0000

    Add package lins-plugboleto/1.0.0.7/lins_plugboleto-1.0.0.7-py3-none-any.whl

---
+++ packages/lins-plugboleto/lins_plugboleto-1.0.0.7-py3-none-any.whl/lins_plugboleto/cedente.py
@@ -0,0 +1,7 @@
+class Cedente(object):
+
+    def __init__(self, codigo_banco,conta_numero,dv,convenio):
+        self.CedenteContaCodigoBanco = str(codigo_banco)
+        self.CedenteContaNumero = str(conta_numero)
+        self.CedenteContaNumeroDV = str(dv)
+        self.CedenteConvenioNumero = str(convenio)
