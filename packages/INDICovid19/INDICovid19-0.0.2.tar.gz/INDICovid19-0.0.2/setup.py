55e93119527015bf928c98f86a71b64f17b182ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:45 +0000

    Add package INDICovid19/0.0.2/INDICovid19-0.0.2.tar.gz

---
+++ packages/INDICovid19/INDICovid19-0.0.2.tar.gz/INDICovid19-0.0.2/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="INDICovid19",
+    version="0.0.2",
+    author="Arghadeep Chaudhury",
+    author_email="arghadeep.chaudhury@gmail.com",
+    description="Python Lib for api call on Indian Covid 19 dataset near real time",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/deepstartup/INDICovid19",
+    packages=find_packages(),
+    install_requires=['pandas','json2xml','requests'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
