77799851092e0a0e85f637b3a555c581076639dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:57 +0000

    Add package aws-solutions-konstruk.core/0.8.0/aws-solutions-konstruk.core-0.8.0.tar.gz

---
+++ packages/aws-solutions-konstruk.core/aws-solutions-konstruk.core-0.8.0.tar.gz/aws-solutions-konstruk.core-0.8.0/src/aws_solutions_konstruk/core/_jsii/__init__.py
@@ -0,0 +1,33 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.aws_apigateway
+import aws_cdk.aws_cloudfront
+import aws_cdk.aws_cloudwatch
+import aws_cdk.aws_cognito
+import aws_cdk.aws_dynamodb
+import aws_cdk.aws_elasticsearch
+import aws_cdk.aws_events
+import aws_cdk.aws_iam
+import aws_cdk.aws_iot
+import aws_cdk.aws_kinesis
+import aws_cdk.aws_kinesisanalytics
+import aws_cdk.aws_kinesisfirehose
+import aws_cdk.aws_kms
+import aws_cdk.aws_lambda
+import aws_cdk.aws_lambda_event_sources
+import aws_cdk.aws_logs
+import aws_cdk.aws_s3
+import aws_cdk.aws_sns
+import aws_cdk.aws_sqs
+import aws_cdk.core
+__all__ = []
+
+publication.publish()
