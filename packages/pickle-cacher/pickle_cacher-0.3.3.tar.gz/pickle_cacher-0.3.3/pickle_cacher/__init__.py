f76fde224c8636b901a1c9b37620cfb7ac37897a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:51 +0000

    Add package pickle-cacher/0.3.3/pickle_cacher-0.3.3.tar.gz

---
+++ packages/pickle-cacher/pickle_cacher-0.3.3.tar.gz/pickle_cacher-0.3.3/pickle_cacher/__init__.py
@@ -0,0 +1,7 @@
+"""Pickle-based caching."""
+from pickle_cacher.pickle_cacher import cached  # noqa: S403
+from pickle_cacher.pickle_cacher import PickleCacher  # noqa: S403
+
+
+__all__ = ["cached", "PickleCacher"]
+__version__ = "0.3.3"
