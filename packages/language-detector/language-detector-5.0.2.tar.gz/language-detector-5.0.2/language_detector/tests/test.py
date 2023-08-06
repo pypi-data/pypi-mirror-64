2a62e3219ecdf7d8afe211d0d68049e7245ca5cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:33 +0000

    Add package language-detector/5.0.2/language-detector-5.0.2.tar.gz

---
+++ packages/language-detector/language-detector-5.0.2.tar.gz/language-detector-5.0.2/language_detector/tests/test.py
@@ -0,0 +1,95 @@
+#-*- coding: utf-8 -*-
+import unittest
+from language_detector import detect_language as dl
+from language_detector import *
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_arabic(self):
+        text = """
+        وجاء في بيان صادر عن مكتب رئيس الحكومة حيدر العبادي إن الخطوة التركية "انتهاك خطير للسيادة العراقية".
+
+        """
+        self.assertEqual(dl(text), "Arabic")
+
+    def test_english(self):
+        text = """
+        JEM Raw is recalling nut butters after health officials found a likely link to a salmonella outbreak. 82 Shares. Email. A salmonella outbreak linked to an organic line of nut butters has sickened 11 people according to the Centers for Disease Control 
+        """
+        self.assertEqual(dl(text), "English")
+
+    '''
+    def test_farsi(self):
+        text = """
+         شهردار نیویورک: تجهیزات پزشکی لازم برای کرونا یک هفته دیگر تمام می‌شود
+        """
+        self.assertEqual(dl(text), "Farsi")
+    '''
+
+    def test_french(self):
+        text = """
+        Trois assaillants ont été tués et deux autres arrêtés vendredi à Bujumbura après avoir tendu une embuscade à Christophe Manirambona, chef du bureau des unités spécialisées, un haut responsable de la police qui n'était pas dans son véhicule.
+        """
+        self.assertEqual(dl(text), "French")
+
+    def test_german(self):
+        text = """
+        "Die unendliche Geschichte" fasziniert Leser, Hörer und Zuschauer seit fast vierzig Jahren. Eine Kinderjury zeichnete nun die klangvolle Neuvertonung des Klassikers als bestes Kinderhörspiel aus.
+        """
+        self.assertEqual(dl(text), "German")
+
+    def test_kurmanci(self):
+        text = """
+        Luv xelata xwe pêşkêşî 55 milyon kurd û pêşmergeyan kir Zêdetir
+        """
+        self.assertEqual(dl(text), "Kurmanci")
+
+    def test_mandarin(self):
+        text = """
+        肺炎疫情：美国纽约州长称“不想当武汉，不想被封城”
+        """
+        self.assertEqual(dl(text), "Mandarin")
+
+    def test_russian(self):
+        text = """
+        Коронавирус: в Москве больше тысячи заболевших, в России - 270 новых случаев
+        """
+        self.assertEqual(dl(text), "Russian")
+
+    def test_sorani(self):
+        text = """
+        22:18 | دەمیرتاش: كورد گەیشتووەتە "خاڵێكی مەزن" بۆ راگەیاندنی دەوڵەتی خۆی
+        """
+        self.assertEqual(dl(text), "Sorani")
+
+    '''
+    def test_spanish(self):
+        text = """Coronavirus | "Esto se va a parecer mucho a una economía de guerra": la advertencia de la Cepal de que la pandemia aumentará el desempleo y la pobreza en América Latina"""
+        self.assertEqual(dl(text), "Spanish")
+    '''
+
+    def test_turkish(self):
+        text = """
+        Amerikan Federal Soruşturma Bürosu (FBI), ABD'nin Kaliforniya Eyaleti'nde Çarşamba günü gerçekleştirilen ve 14 kişinin yaşamını yitirdiği saldırının 'terörizm eylemi' olarak inceleneceğini söyledi.
+        """
+        self.assertEqual(dl(text), "Turkish")
+
+    def test_english_iterable(self):
+        iterable = ["Washington", "Adams", "Jefferson"]
+        self.assertEqual(dl(iterable), "English")
+
+    def test_is_english(self):
+        text = "Bla bla bla I'm not sure where to start."
+        self.assertEqual(isEnglish(text), True)
+
+    def test_return_as_code(self):
+        text = "Bla bla bla I'm not sure where to start."
+        self.assertEqual(dl(text, return_as_code=True), "en")
+
+    def test_iterable(self):
+        iterable = [u'01', u'03', u'04', u'07', u'08', u'10', u'12', u'13', u'16', u'15', u'18', u'11', u'17', u'09', u'06', u'02', u'05', u'14']
+        language = detect_language_iterable(iterable)
+        self.assertEqual(language, None)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ packages/language-detector/language-detector-5.0.2.tar.gz/language-detector-5.0.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+from os.path import abspath, dirname, join
+
+root_dir = abspath(dirname(__file__))
+
+with open(join(root_dir, "README.md")) as f:
+    long_description = f.read()
+
+setup(
+    name = 'language-detector',
+    packages = ['language_detector'],
+    package_dir = {'language_detector': 'language_detector'},
+    package_data = {'language_detector': ['prep/char_language.txt','tests/__init__.py','tests/test.py']},
+    version = '5.0.2',
+    description = 'Detect language of text',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author = 'Daniel J. Dufour',
+    author_email = 'daniel.j.dufour@gmail.com',
+    url = 'https://github.com/DanielJDufour/language-detector',
+    download_url = 'https://github.com/DanielJDufour/language-detector/tarball/download',
+    keywords = ['arabic','farsi','french','kurdish','kurmanci','language','nlp','python','sorani','spanish','tagging','turkish'],
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ]
+)
