ecc34d859cfe3e86df383715466ae661549b4f48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:42 +0000

    Add package nester_hzf_2/1.3.0/nester_hzf_2-1.3.0.tar.gz

---
+++ packages/nester_hzf_2/nester_hzf_2-1.3.0.tar.gz/nester_hzf_2-1.3.0/nester.py
@@ -0,0 +1,15 @@
+"""这是"nester.py"模块，提供了一个名为print_lol()的函数
+用来打印列表，其中包含或包含嵌套列表"""
+def print_lol(the_list, indent=False, tab=0):
+        for each in the_list:
+                if isinstance(each, list):
+                        print_lol(each, indent, tab+1)
+                else:
+                        if indent:
+                                for tab_stop in range(tab):
+                                        print("\t", end='')
+                        print(each)
+
+def new(name):
+        print(name)
+"""用来测试是不是可以用"""
---
+++ packages/nester_hzf_2/nester_hzf_2-1.3.0.tar.gz/nester_hzf_2-1.3.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'nester_hzf_2',
+    version = '1.3.0',
+    py_modules = ['nester'],
+    autor = 'hzf_python',
+    autor_email = '15184455137@163.com',
+    url = 'http://www.hzfdechuangzi.club',
+    description = 'A simple printer of nested lists',
+    )
