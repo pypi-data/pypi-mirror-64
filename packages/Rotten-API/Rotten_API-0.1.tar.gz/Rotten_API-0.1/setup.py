bf6d212ab7722543d10d91d24397e70bd962bcf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:24 +0000

    Add package Rotten-API/0.1/Rotten_API-0.1.tar.gz

---
+++ packages/Rotten-API/Rotten_API-0.1.tar.gz/Rotten_API-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Rotten_API',
+    version='0.1',
+    packages=['RottenPy'],
+    url='https://github.com/chyvn/Rotten_API',
+    license='MIT',
+    author='Avinash Tulasi',
+    author_email='avinasht@iiitd.ac.in',
+    description='Rotten Tomatoes API'
+)
