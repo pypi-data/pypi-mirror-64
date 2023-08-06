1ad26adbf471bafbc5fc5f57c0be6cd0e98e386a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:45 +0000

    Add package first-dist-pack-2/0.2/first_dist_pack_2-0.2.tar.gz

---
+++ packages/first-dist-pack-2/first_dist_pack_2-0.2.tar.gz/first_dist_pack_2-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='first_dist_pack_2',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['first_dist_pack_2'],
+      zip_safe=False)
