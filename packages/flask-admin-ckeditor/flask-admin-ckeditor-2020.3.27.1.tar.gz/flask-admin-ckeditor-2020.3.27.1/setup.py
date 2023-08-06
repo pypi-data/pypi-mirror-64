16ef1f9154ae44143fad43cc1aec929825c295e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:25 +0000

    Add package flask-admin-ckeditor/2020.3.27.1/flask-admin-ckeditor-2020.3.27.1.tar.gz

---
+++ packages/flask-admin-ckeditor/flask-admin-ckeditor-2020.3.27.1.tar.gz/flask-admin-ckeditor-2020.3.27.1/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+
+# with open("README.md", "r") as fh:
+# 	long_description = fh.read()
+
+long_description = "ckeditor for flask-admin"
+
+
+setuptools.setup(
+	name = "flask-admin-ckeditor",
+	version="2020.3.27.1",
+	auth="hxh",
+	author_email="13750192465@163.com",
+	description=long_description,
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/suckmybigdick/flask-admin-ckeditor",
+	packages=setuptools.find_packages(),
+	include_package_data=True,
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	install_requires=[
+		"setuptools==41.0.1",
+		"Flask_Admin==1.5.5",
+		"Flask_CKEditor==0.4.3",
+		"WTForms==2.2.1",
+		"Flask==0.10.1",
+		"peewee==3.13.1",
+		"wtf-peewee==3.0.0",
+	],
+)
