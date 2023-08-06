ecf618b5c270df3efa4873e891efdf8b177b6780
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:58 +0000

    Add package zihello/1.2/zihello-1.2.tar.gz

---
+++ packages/zihello/zihello-1.2.tar.gz/zihello-1.2/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(name='zihello',
+                 version='1.2',
+                 packages=setuptools.find_packages(),
+                 zip_safe=False,
+                 include_package_data=True,
+                 )
