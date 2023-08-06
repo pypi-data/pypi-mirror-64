7c4e4fa7630ac3af006b7a2cd59ed71860c059d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:38 +0000

    Add package SysMPy/0.0.7/SysMPy-0.0.7.tar.gz

---
+++ packages/SysMPy/SysMPy-0.0.7.tar.gz/SysMPy-0.0.7/sysmpy/__init__.py
@@ -0,0 +1,19 @@
+""" This package declares meta data for SAI schema.
+"""
+
+# Force __all__ modules to be imported when importing sysmpy package
+# from sysmpy import *
+
+# in __init__.py
+from sysmpy.entity import *
+from sysmpy.relationship import *
+from sysmpy.interfaceanalyzer import *
+from sysmpy.matrix_for_graph import *
+from sysmpy.script_to_model import *
+from sysmpy.script_generator import *
+from sysmpy.sms_searcher import *
+from sysmpy.gui.gui_main import *
+
+# Only these modules will be imported with "import *"
+# __all__ = ["entity", "relationship"]
+
