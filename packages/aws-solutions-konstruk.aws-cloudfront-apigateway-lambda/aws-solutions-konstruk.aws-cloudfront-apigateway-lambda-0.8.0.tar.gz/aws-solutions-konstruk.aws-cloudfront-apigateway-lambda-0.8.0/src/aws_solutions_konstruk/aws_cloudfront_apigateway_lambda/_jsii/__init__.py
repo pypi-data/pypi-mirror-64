9e6d278a8c8f8c5fa5e26e6ef5ec3f53a8087312
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:56 +0000

    Add package aws-solutions-konstruk.aws-cloudfront-apigateway-lambda/0.8.0/aws-solutions-konstruk.aws-cloudfront-apigateway-lambda-0.8.0.tar.gz

---
+++ packages/aws-solutions-konstruk.aws-cloudfront-apigateway-lambda/aws-solutions-konstruk.aws-cloudfront-apigateway-lambda-0.8.0.tar.gz/aws-solutions-konstruk.aws-cloudfront-apigateway-lambda-0.8.0/src/aws_solutions_konstruk/aws_cloudfront_apigateway_lambda/_jsii/__init__.py
@@ -0,0 +1,20 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.aws_apigateway
+import aws_cdk.aws_cloudfront
+import aws_cdk.aws_lambda
+import aws_cdk.aws_logs
+import aws_cdk.core
+import aws_solutions_konstruk.aws_cloudfront_apigateway
+import aws_solutions_konstruk.core
+__all__ = []
+
+publication.publish()
