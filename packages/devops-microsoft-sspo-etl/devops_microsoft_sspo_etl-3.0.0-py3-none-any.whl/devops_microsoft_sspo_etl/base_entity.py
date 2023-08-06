584a6be81362f4730041a7eea89562c9113845e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:58 +0000

    Add package devops-microsoft-sspo-etl/3.0.0/devops_microsoft_sspo_etl-3.0.0-py3-none-any.whl

---
+++ packages/devops-microsoft-sspo-etl/devops_microsoft_sspo_etl-3.0.0-py3-none-any.whl/devops_microsoft_sspo_etl/base_entity.py
@@ -0,0 +1,56 @@
+from sspo_db.application import factories as application_factories
+from sspo_db.model import factories as model_factories
+import re
+from pprint import pprint
+from tfs.tfs import TFS
+import logging
+logging.basicConfig(level=logging.INFO)
+
+class BaseEntity():
+
+    def __init__(self):
+        
+        self.organization = None
+        
+    def config (self, data):
+        
+        self.data = data
+
+        self.organization_id = self.data['organization_id']
+        self.tfs_key = self.data['tfs_key']
+        self.tfs_url = self.data['tfs_url']
+        self.application = self.data['application']
+        
+        self.retrive_tfs()
+        
+        self.application_application_reference = application_factories.ApplicationReferenceFactory()
+        self.application_service = application_factories.ApplicationFactory()
+        self.application_organization = application_factories.OrganizationFactory()
+        
+        self.organization = self.application_organization.get_by_uuid(self.organization_id)
+        self.application = self.application_service.get_by_uuid(self.application)
+        
+    
+    def retrive_tfs(self):
+        pprint ("Retrive TFS: Created")
+        self.tfs =  TFS(self.tfs_key, self.tfs_url) 
+        
+    def check_value (self, work_item, key):
+        return work_item.fields[key] if key in work_item.fields else None
+
+    def create_application_reference(self,external_id, external_url,external_type_entity, internal_uuid, entity_name ):
+        #application reference
+        application_reference = model_factories.ApplicationReferenceFactory(
+                                                    name = None,
+                                                    description = None,
+                                                    application = self.application.id,
+                                                    external_id = external_id,
+                                                    external_url = external_url,
+                                                    external_type_entity = external_type_entity,
+                                                    internal_uuid = internal_uuid,
+                                                    entity_name = entity_name
+                                                )
+
+        self.application_application_reference.create(application_reference)
+    
+    
